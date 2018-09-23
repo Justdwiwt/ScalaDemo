90e53afb17b50aeea3452bd733a9bae379e48952
Justdwiwt <>
2018-09-23 21:26:31 +0800

    test var

--- src/main/scala/basic/FirstDemo.scala
+++ src/main/scala/basic/FirstDemo.scala
@@ -4,4 +4,18 @@ object FirstDemo {
   def main(args: Array[String]): Unit = {
     print("SUCCESS")
   }
+
+  var x = 5
+  var x0 = 6
+  var y = 6
+  var y0 = 5
+
+  private def sqrt(i: Int) = i
+
+  //noinspection ScalaUnusedSymbol
+  private val distance = {
+    val dx = x - x0
+    val dy = y - y0
+    sqrt(dx * dx + dy * dy)
+  }
 }
