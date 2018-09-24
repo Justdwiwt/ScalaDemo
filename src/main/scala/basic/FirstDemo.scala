98cd5fe58adb806bfb4f95a52e53211d26e9cacc
Justdwiwt <>
2018-09-24 10:01:55 +0800

    for推导式

--- src/main/scala/basic/FirstDemo.scala
+++ src/main/scala/basic/FirstDemo.scala
@@ -2,20 +2,20 @@ package basic
 
 object FirstDemo {
   def main(args: Array[String]): Unit = {
-    print("SUCCESS")
-  }
 
-  var x = 5
-  var x0 = 6
-  var y = 6
-  var y0 = 5
-
-  private def sqrt(i: Int) = i
-
-  //noinspection ScalaUnusedSymbol
-  private val distance = {
-    val dx = x - x0
-    val dy = y - y0
-    sqrt(dx * dx + dy * dy)
+    // for推导式
+    // 生成的集合与它的第一个生成器是类型兼容的
+    for (i <- 1 to 3; j <- 1 to 3)
+      print(f"${10 * i + j}%3d")
+    for (i <- 1 to 3; j <- 1 to 3 if i != j)
+      print(f"${10 * i + j}%3d")
+    for (i <- 1 to 3; from = 4 - i; j <- from to 3)
+      print(f"${10 * i + j}%3d")
+    for (i <- 1 to 10)
+      yield i % 3
+    for (c <- "test"; i <- 0 to 1)
+      yield (c + i).toChar
+    for (i <- 0 to 1; c <- "refactor")
+      yield (c + i).toChar
   }
 }
