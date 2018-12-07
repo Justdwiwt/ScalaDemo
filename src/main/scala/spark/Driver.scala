1c604a671029e30cca58d88c8301e301ec01f219
Justdwiwt <len_master@hotmail.com>
2018-12-07 16:35:18 +0800

    Spark Driver

---
+++ src/main/scala/spark/Driver.scala
@@ -0,0 +1,25 @@
+package spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Driver {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+      .setMaster("spark://hadoop01:7077")
+      .setAppName("wordcount")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile("hdfs://hadoop01:9000/word.txt", 3)
+    val result = data.flatMap {
+      _.split(" ")
+    }.map {
+      (_, 1)
+    }.reduceByKey {
+      _ + _
+    }
+    result.saveAsTextFile("hdfs://hadoop01:9000/r2")
+
+  }
+
+}
---
+++ src/main/scala/spark/DriverLocal.scala
@@ -0,0 +1,25 @@
+package spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object DriverLocal {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setMaster("local").setAppName("wordcount")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile("D://data/word.txt", 2)
+    val result = data.flatMap {
+      _.split(" ")
+    }.map {
+      (_, 1)
+    }.reduceByKey {
+      _ + _
+    }
+    result.foreach {
+      println
+    }
+
+  }
+
+}
