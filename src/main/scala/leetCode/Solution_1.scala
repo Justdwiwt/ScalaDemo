746654ed00c56bfe3f19712dfa3bb2baa8976580
Justdwiwt <len_master@hotmail.com>
2019-06-14 10:46:35 +0800

    leetCode 01

---
+++ src/main/scala/leetCode/Solution_1.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_1 {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    var map = Map[Int, Int]()
+    for (i <- nums.indices) {
+      val complement = target - nums(i)
+      if (map.contains(complement)) {
+        return Array(map(complement), i)
+      }
+      else {
+        map += (nums(i) -> i)
+      }
+    }
+    Array(0, 0)
+  }
+}
