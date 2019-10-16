34790b4b12f100bd9153fe9d9d420445d2092cda
Justdwiwt <len_master@hotmail.com>
2019-10-16 11:32:08 +0800

    leetCode 26

---
+++ src/main/scala/leetCode/Solution_26.scala
@@ -0,0 +1,16 @@
+package leetCode
+
+object Solution_26 {
+  def removeDuplicates(nums: Array[Int]): Int = {
+    var ans = 0
+    if (nums.length < 2) return nums.length
+    for (i <- 1 until nums.length) {
+      if (nums(ans) != nums(i)) {
+        ans = ans + 1
+        nums(ans) = nums(i)
+      }
+    }
+    ans += 1
+    ans
+  }
+}
