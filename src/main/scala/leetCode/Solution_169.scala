83a4b0e31c464139480aab5fd6899f68899b4791
Justdwiwt <len_master@hotmail.com>
2019-10-17 09:37:44 +0800

    leetCode 169

---
+++ src/main/scala/leetCode/Solution_169.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_169 {
+  def majorityElement(nums: Array[Int]): Int = {
+    var res = 0
+    var cnt = 0
+    for (num <- nums)
+      if (cnt == 0) {
+        res = num
+        cnt += 1
+      } else if (num == res) cnt += 1
+      else cnt -= 1
+    res
+  }
+}
