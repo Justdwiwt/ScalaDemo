b6dfa2c78d1f106c4ced674b1c2251f2caf9d6d8
Justdwiwt <len_master@hotmail.com>
2019-10-17 14:22:31 +0800

    leetCode 230

---
+++ src/main/scala/leetCode/Solution_230.scala
@@ -0,0 +1,30 @@
+package leetCode
+
+class TreeNode(var _value: Int) {
+  var value: Int = _value
+  var left: TreeNode = _
+  var right: TreeNode = _
+}
+
+object Solution_230 {
+
+  var time = 0
+  var ans = 0
+
+  def kthSmallest(root: TreeNode, k: Int): Int = {
+    dfs(root, k)
+    ans
+  }
+
+  def dfs(root: TreeNode, k: Int): Unit = {
+    if (root == null) return
+    dfs(root.left, k)
+    time += 1
+    if (time == k) {
+      ans = root.value
+      return
+    }
+    dfs(root.right, k)
+  }
+
+}
