d6e776c1e06cc80e9e68c647b026547edde176bd
Justdwiwt <len_master@hotmail.com>
2019-10-17 14:34:13 +0800

    leetCode 234

---
+++ src/main/scala/leetCode/Solution_234.scala
@@ -0,0 +1,34 @@
+package leetCode
+
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = _
+  var x: Int = _x
+}
+
+object Solution_234 {
+  def isPalindrome(head: ListNode): Boolean = {
+    if (head == null || head.next == null) return true
+    var pre: ListNode = null
+    var slow: ListNode = head
+    var fast: ListNode = head
+    var s: ListNode = null
+    while (fast != null && fast.next != null) {
+      pre = slow
+      slow = slow.next
+      fast = fast.next.next
+      pre.next = s
+      s = pre
+    }
+    var tmp: ListNode = slow
+    if (fast != null) tmp = tmp.next
+    slow = pre
+    while (tmp != null) {
+      if (tmp.x != slow.x) return false
+      else {
+        tmp = tmp.next
+        slow = slow.next
+      }
+    }
+    true
+  }
+}
