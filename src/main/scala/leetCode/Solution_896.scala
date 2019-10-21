f7fd11859eff5f1edfef3a4f118a4a32b2887c8b
Justdwiwt <len_master@hotmail.com>
2019-10-21 09:38:12 +0800

    leetCode 896

---
+++ src/main/scala/leetCode/Solution_896.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_896 {
+  def isMonotonic(A: Array[Int]): Boolean = {
+    val res = true
+    var flag = 0
+    for (i <- 0 until A.length - 1) {
+      if (A(i) > A(i + 1)) {
+        if (flag == 0) flag = 2
+        else if (flag == 1) return false
+      } else if (A(i) < A(i + 1))
+        if (flag == 0) flag = 1
+        else if (flag == 2) return false
+    }
+    res
+  }
+}
