5e0363cf3b399d537e6cf17fd44931c787c54bcc
Justdwiwt <len_master@hotmail.com>
2019-11-04 17:12:20 +0800

    leetCode 42

---
+++ src/main/scala/leetCode/Solution_42.scala
@@ -0,0 +1,7 @@
+package leetCode
+
+object Solution_42 {
+  def trap(height: Array[Int]): Int = {
+    height.scan(0)(_ max _).drop(1).zip(height.scanRight(0)(_ max _)).map(Function.tupled(_ min _)).sum - height.sum
+  }
+}
