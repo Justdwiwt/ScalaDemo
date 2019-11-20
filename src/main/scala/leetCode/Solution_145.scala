bab468d7ba05909394018e640f0e7f653208ef22
Justdwiwt <len_master@hotmail.com>
2019-11-20 16:03:11 +0800

    leetCode 49,86,145

---
+++ src/main/scala/leetCode/Solution_145.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_145 {
+  def postorderTraversal(root: TreeNode): List[Int] = root match {
+    case null => Nil
+    case _ => func(root :: Nil, Nil)
+  }
+
+  @annotation.tailrec
+  def func(root: List[TreeNode], res: List[Int]): List[Int] = root match {
+    case h :: t =>
+      val r = h.value :: res
+      if (h.left != null && h.right != null) func(h.right :: h.left :: t, r)
+      else if (h.left != null) func(h.left :: t, r)
+      else if (h.right != null) func(h.right :: t, r)
+      else func(t, r)
+    case _ => res
+  }
+}
---
+++ src/main/scala/leetCode/Solution_49.scala
@@ -0,0 +1,5 @@
+package leetCode
+
+object Solution_49 {
+  def groupAnagrams(strs: Array[String]): List[List[String]] = strs.map(x => (x.sorted, x)).groupBy(_._1).map(y => y._2.map(_._2).toList).toList
+}
