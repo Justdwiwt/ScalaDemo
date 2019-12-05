1001ad85a51e0e0f901781a296ac6b9905534dbe
Justdwiwt <len_master@hotmail.com>
2019-12-05 15:31:28 +0800

    leetCode 1103,1123,1219

---
+++ src/main/scala/leetCode/Solution_1123.scala
@@ -0,0 +1,16 @@
+package leetCode
+
+object Solution_1123 {
+  @scala.annotation.tailrec
+  def lcaDeepestLeaves(root: TreeNode): TreeNode = root match {
+    case null => null
+    case _ =>
+      val left = depth(root.left)
+      val right = depth(root.right)
+      if (left == right) root
+      else if (left > right) lcaDeepestLeaves(root.left)
+      else lcaDeepestLeaves(root.right)
+  }
+
+  def depth(root: TreeNode): Int = if (root == null) 0 else 1 + depth(root.left).max(depth(root.right))
+}
