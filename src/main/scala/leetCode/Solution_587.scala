2bfa32790f872849eb4e6bd14b27c0684c780152
Justdwiwt <len_master@hotmail.com>
2019-12-10 10:33:29 +0800

    leetCode 398,529,841

---
+++ src/main/scala/leetCode/Solution_587.scala
@@ -0,0 +1,55 @@
+package leetCode
+
+import scala.collection.mutable
+import scala.util.control.Breaks._
+
+object Solution_587 {
+  def outerTrees(points: Array[Array[Int]]): Array[Array[Int]] = {
+    if (points.length == 1) return points
+    var curX = Int.MaxValue
+    var curY = 0
+    points.foreach(i => if (i(0) <= curX) {
+      curX = i(0)
+      curY = i(1)
+    })
+    var vecX = 0
+    var vecY = 1
+    var res = Array.empty[Array[Int]]
+    val added = new mutable.HashMap[Int, mutable.HashSet[Int]]()
+    added.put(curX, mutable.HashSet.empty)
+    res :+= Array(curX, curY)
+    added(curX).add(curY)
+    breakable {
+      while (true) {
+        var nextX = 0
+        var nextY = 0
+        var cosMx = -Float.MaxValue
+        var lenMin = Int.MaxValue.toLong
+        points.foreach(i => {
+          breakable {
+            if (i(0) == curX && i(1) == curY) break
+          }
+          val vX = i(0) - curX
+          val vY = i(1) - curY
+          val vLen = vX * vX + vY * vY
+          val cos = (vecX * vX + vecY * vY) / (math.sqrt(vecX * vecX + vecY * vecY) * math.sqrt(vLen)).toFloat
+          if ((cos > cosMx) || (cos == cosMx && lenMin > vLen)) {
+            nextX = i(0)
+            nextY = i(1)
+            lenMin = vLen
+            cosMx = cos
+          }
+        })
+        if (added.contains(nextX) && added(nextX).contains(nextY)) break
+        else {
+          vecX = nextX - curX
+          vecY = nextY - curY
+          curX = nextX
+          curY = nextY
+          res :+= Array(curX, curY)
+        }
+      }
+    }
+    res
+  }
+}
---
+++ src/main/scala/leetCode/Solution_972.scala
@@ -0,0 +1,20 @@
+package leetCode
+
+object Solution_972 {
+  def isRationalEqual(S: String, T: String): Boolean = {
+    val M = 1e-8
+    (func(S) - func(T)).abs <= M
+  }
+
+  def func(S: String): Double = {
+    if (S.contains("(")) {
+      val dot = S.indexOf(".")
+      val l = S.indexOf("(")
+      val r = S.indexOf(")")
+      val p = S.split('(').head.toDouble
+      val p2 = S.substring(l + 1, r).toDouble / Array.fill(r - l - 1)("9").reduce(_ + _).toDouble / Math.pow(10, l - dot - 1)
+      p + p2
+    } else S.toDouble
+  }
+
+}
