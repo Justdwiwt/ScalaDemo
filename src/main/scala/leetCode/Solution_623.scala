6c6df5c5606b60fb8c1695dc2a7a9d7afc97e67d
Justdwiwt <len_master@hotmail.com>
2020-02-07 16:17:58 +0800

    leetCode 542,546,623

---
+++ src/main/scala/leetCode/Solution_623.scala
@@ -0,0 +1,16 @@
+package leetCode
+
+object Solution_623 {
+  def addOneRow(root: TreeNode, v: Int, d: Int): TreeNode = {
+    if (d == 0 || d == 1) {
+      val node = new TreeNode(v)
+      if (d > 0) node.left = root else node.right = root
+      return node
+    }
+    if (root != null && d > 1) {
+      root.left = addOneRow(root.left, v, if (d > 2) d - 1 else 1)
+      root.right = addOneRow(root.right, v, if (d > 2) d - 1 else 0)
+    }
+    root
+  }
+}
