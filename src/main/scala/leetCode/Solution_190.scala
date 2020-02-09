9518e16a98970c06d6fea6eac38be6590cd2c4f4
Justdwiwt <len_master@hotmail.com>
2020-02-09 15:33:29 +0800

    leetCode 190

--- src/main/scala/leetCode/Solution_190.scala
+++ src/main/scala/leetCode/Solution_190.scala
@@ -1,7 +1,7 @@
 package leetCode
 
 object Solution_190 {
-  def reverseBits(n: BigInt): Int = {
+  def reverseBits(n: Int): Int = {
     var res = 0
     (0 until 32).foreach(i => res |= ((n >> i) & 1) << (32 - i))
     res
