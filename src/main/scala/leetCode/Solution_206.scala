bfc4fdc805198855975e9308efaffad7feff1fa4
Justdwiwt <len_master@hotmail.com>
2020-02-14 15:55:10 +0800

    leetCode 206

---
+++ src/main/scala/leetCode/Solution_206.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_206 {
+  def reverseList(head: ListNode): ListNode = {
+    if (head == null || head.next == null) return head
+    val cur = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    cur
+  }
+}
