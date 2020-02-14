10fa098098db8867acb06970af8b0f02c8454a43
Justdwiwt <len_master@hotmail.com>
2020-02-14 15:54:27 +0800

    leetCode 剑指Offer 21,22,24

---
+++ src/main/scala/leetCode/Offer_21.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Offer_21 {
+  def exchange(nums: Array[Int]): Array[Int] = {
+    var odd = Array.empty[Int]
+    var even = Array.empty[Int]
+    nums.foreach(i => if (i % 2 == 0) even :+= i else odd :+= i)
+    odd ++ even
+  }
+}
---
+++ src/main/scala/leetCode/Offer_22.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Offer_22 {
+  def getKthFromEnd(head: ListNode, k: Int): ListNode = {
+    var p = head
+    var q = head
+    var t = k
+    while (p != null) {
+      if (t > 0) {
+        p = p.next
+        t -= 1
+      } else {
+        p = p.next
+        q = q.next
+      }
+    }
+    q
+  }
+}
