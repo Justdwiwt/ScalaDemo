9ceaa7635d0d2307aea8d7ddd5c3e6166aaa43a2
Justdwiwt <len_master@hotmail.com>
2020-02-15 16:12:17 +0800

    leetCode 102,946

---
+++ src/main/scala/leetCode/Solution_102.scala
@@ -0,0 +1,25 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_102 {
+  def levelOrder(root: TreeNode): List[List[Int]] = {
+    var res = List.empty[List[Int]]
+    if (root == null) return res
+    val q = new mutable.Queue[TreeNode]()
+    var p: TreeNode = null
+    q.enqueue(root)
+    while (q.nonEmpty) {
+      var t = List.empty[Int]
+      q.indices.foreach(_ => {
+        p = q.front
+        t :+= p.value
+        q.dequeue()
+        if (p.left != null) q.enqueue(p.left)
+        if (p.right != null) q.enqueue(p.right)
+      })
+      res :+= t
+    }
+    res
+  }
+}
