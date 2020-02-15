d20cbfefaeee850ddbbe720d85dad5fecefba969
Justdwiwt <len_master@hotmail.com>
2020-02-15 16:12:42 +0800

    leetCode 剑指Offer 31,32-1,32-2,32-3

---
+++ src/main/scala/leetCode/Offer_32_1.scala
@@ -0,0 +1,20 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Offer_32_1 {
+  def levelOrder(root: TreeNode): Array[Int] = {
+    var res = Array.empty[Int]
+    if (root == null) return res
+    val q = new mutable.Queue[TreeNode]()
+    q.enqueue(root)
+    while (q.nonEmpty) {
+      val cur = q.front
+      q.dequeue()
+      res :+= cur.value
+      if (cur.left != null) q.enqueue(cur.left)
+      if (cur.right != null) q.enqueue(cur.right)
+    }
+    res
+  }
+}
