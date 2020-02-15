3716ac3a9750c7a6d80f3d2104309791cb3d2fa8
Justdwiwt <len_master@hotmail.com>
2020-02-15 15:33:31 +0800

    leetCode 101,155

---
+++ src/main/scala/leetCode/Solution_101.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_101 {
+  def isSymmetric(root: TreeNode): Boolean = f(root, root)
+
+  def f(left: TreeNode, right: TreeNode): Boolean = {
+    if (left == null && right == null) return true
+    if (left == null || right == null) return false
+    (left.value == right.value) && f(left.left, right.right) && f(left.right, right.left)
+  }
+}
---
+++ src/main/scala/leetCode/Solution_155.scala
@@ -0,0 +1,28 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_155 {
+
+  class MinStack() {
+
+    private val s1 = new mutable.Stack[Int]()
+    private val s2 = new mutable.Stack[Int]()
+
+    def push(x: Int) {
+      s1.push(x)
+      if (s2.isEmpty || x <= s2.top) s2.push(x)
+    }
+
+    def pop() {
+      if (s1.top == s2.top) s2.pop()
+      s1.pop()
+    }
+
+    def top(): Int = s1.top
+
+    def getMin(): Int = s2.top
+
+  }
+
+}
