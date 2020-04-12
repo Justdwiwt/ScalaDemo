23d881e6a336809bfe54c509a8e98676ad324cde
Justdwiwt <len_master@hotmail.com>
2020-04-12 18:16:20 +0800

    leetCode 剑指Offer 65,66,67

---
+++ src/main/scala/leetCode/Offer_66.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Offer_66 {
+  def constructArr(a: Array[Int]): Array[Int] = {
+    val diffL = Array.fill(a.length)(1)
+    val diffR = Array.fill(a.length)(1)
+    (1 until a.length).foreach(i => diffL(i) = diffL(i - 1) * a(i - 1))
+    (a.length - 2 until -1 by (-1)).foreach(i => diffR(i) = diffR(i + 1) * a(i + 1))
+    a.indices.foreach(i => diffL(i) = diffL(i) * diffR(i))
+    diffL
+  }
+}
