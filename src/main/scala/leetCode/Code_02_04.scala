20b6bafb19ab94888a671f37bbc43dc319717f12
Justdwiwt <len_master@hotmail.com>
2020-04-26 18:28:11 +0800

    Cracking the Coding Interview 01.08,01.09,02.01,02.02,02.04

---
+++ src/main/scala/leetCode/Code_02_04.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Code_02_04 {
+  def partition(head: ListNode, x: Int): ListNode = {
+    var node = head
+    if (node == null || node.next == null) return node
+    val p = partition(node.next, x)
+    if (node.x < x) node.next = p
+    else {
+      var q = p
+      while (q.next != null) q = q.next
+      q.next = node
+      node.next = null
+      node = p
+    }
+    node
+  }
+}
