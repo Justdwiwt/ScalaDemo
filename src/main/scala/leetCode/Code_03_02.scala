bc34390f2649a2444f0b90c3f1c1ce425ec34b1a
Justdwiwt <len_master@hotmail.com>
2020-04-27 14:48:42 +0800

    Cracking the Coding Interview 03.01,03.02,03.03

---
+++ src/main/scala/leetCode/Code_03_02.scala
@@ -0,0 +1,30 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Code_03_02 {
+
+  class MinStack() {
+
+    private val m = new mutable.Stack[Int]
+    private val s = new mutable.Stack[Int]
+
+    def push(x: Int): Unit = {
+      s.push(x)
+      if (m.isEmpty || x <= m.top) m.push(x)
+    }
+
+    def pop(): Unit = {
+      if (s.nonEmpty) {
+        if (s.top == m.top) m.pop()
+        s.pop()
+      }
+    }
+
+    def top(): Int = s.top
+
+    def getMin(): Int = m.top
+
+  }
+
+}
