eb1c8903acc4c9dbe292c699e641cec795f4c4f8
Justdwiwt <len_master@hotmail.com>
2020-04-28 16:24:10 +0800

    Cracking the Coding Interview 08.01,08.02,08.03,08.04,08.05

---
+++ src/main/scala/leetCode/Code_08_04.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Code_08_04 {
+  def subsets(nums: Array[Int]): List[List[Int]] = {
+    var res = List.empty[List[Int]]
+    val bmp = math.pow(2, nums.length).toInt
+    (0 until bmp).foreach(i => {
+      var t = List.empty[Int]
+      nums.indices.foreach(j => if ((i >>> j & 1) == 1) t :+= nums(j))
+      res :+= t
+    })
+    res
+  }
+}
---
+++ src/main/scala/leetCode/Code_08_05.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Code_08_05 {
+  def multiply(A: Int, B: Int): Int = B match {
+    case 0 => 0
+    case _ => (multiply(A, B >> 1) << 1) + (if ((B & 1) == 1) A else 0)
+  }
+}
