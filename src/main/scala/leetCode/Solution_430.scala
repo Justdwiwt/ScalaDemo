4e2ee250b0e407cb0b3c2cc4151ea5044d171b06
Justdwiwt <len_master@hotmail.com>
2020-05-23 18:30:14 +0800

    leetCode 430

---
+++ src/main/scala/leetCode/Solution_430.scala
@@ -0,0 +1,31 @@
+package leetCode
+
+object Solution_430 {
+
+  class Node(var _value: Int) {
+    var value: Int = _value
+    var prev: Node = _
+    var next: Node = _
+    var child: Node = _
+  }
+
+  def flatten(head: Node): Node = {
+    if (head == null) return head
+    var cur = head
+    while (cur != null) {
+      if (cur.child != null) {
+        val childNode = cur.child
+        val nextNode = cur.next
+        cur.child = null
+        cur.next = null
+        var retNode = flatten(childNode)
+        retNode.prev = cur
+        cur.next = retNode
+        while (retNode.next != null) retNode = retNode.next
+        retNode.next = nextNode
+        if (nextNode != null) nextNode.prev = retNode
+      } else cur = cur.next
+    }
+    head
+  }
+}
