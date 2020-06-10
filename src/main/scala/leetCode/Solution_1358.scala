93ab0b9dc7596d386202f73bee5324b1aa9a4dfa
Justdwiwt <len_master@hotmail.com>
2020-06-10 21:15:08 +0800

    leetCode 1356,1357,1358,1359,1362

---
+++ src/main/scala/leetCode/Solution_1358.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_1358 {
+  def numberOfSubstrings(s: String): Int = {
+    var res = 0
+    val diff = Array(-1, -1, -1)
+    s.indices.foreach(i => {
+      s(i) match {
+        case 'a' => res += diff(1).min(diff(2)) + 1
+        case 'b' => res += diff(0).min(diff(2)) + 1
+        case _ => res += diff(0).min(diff(1)) + 1
+      }
+      diff(s(i) - 'a') = i
+    })
+    res
+  }
+}
---
+++ src/main/scala/leetCode/Solution_1359.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_1359 {
+  def countOrders(n: Int): Int = {
+    var res = BigDecimal(1)
+    val M = BigDecimal(1e9 + 7)
+    (2 to n).foreach(i => res = res * (2 * i - 1) * i % M)
+    res.toInt
+  }
+}
