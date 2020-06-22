c27ffd66b80999825afd45dd8116ba5b23966138
Justdwiwt <len_master@hotmail.com>
2020-06-22 19:51:10 +0800

    Cracking the Coding Interview 16.16,16.17,16.18

---
+++ src/main/scala/leetCode/Code_16_17.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Code_16_17 {
+  def maxSubArray(nums: Array[Int]): Int = {
+    var res = Int.MinValue
+    var sum = 0
+    nums.foreach(i => {
+      sum += i
+      res = res.max(sum)
+      if (sum < 0) sum = 0
+    })
+    res
+  }
+}
