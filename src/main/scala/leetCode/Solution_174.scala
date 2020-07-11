4a3fcefb8a3df634b370b5899f6e20489e117d85
Justdwiwt <len_master@hotmail.com>
2020-07-12 00:27:35 +0800

    leetCode 174

--- src/main/scala/leetCode/Solution_174.scala
+++ src/main/scala/leetCode/Solution_174.scala
@@ -2,9 +2,13 @@ package leetCode
 
 object Solution_174 {
   def calculateMinimumHP(dungeon: Array[Array[Int]]): Int = {
-    val dp = Array.fill(dungeon(0).length + 1)(Int.MaxValue)
-    dp(dungeon(0).length - 1) = 1
-    (dp.length - 1 to 0 by -1).foreach(i => (dungeon(0).length - 1 to 0 by -1).foreach(j => dp(j) = math.max(1, math.min(dp(j), dp(j + 1) - dungeon(i)(j)))))
-    dp(0)
+    val dp = Array.ofDim[Int](dungeon.length + 1, dungeon(0).length + 1)
+    (0 to dungeon.length).foreach(i => dp(i) = Array.fill(dp(i).length)(Int.MaxValue))
+    dp(dungeon.length)(dungeon(0).length - 1) = 1
+    dp(dungeon.length - 1)(dungeon(0).length) = 1
+    (dungeon.length - 1 to 0 by (-1)).foreach(i => (dungeon(0).length - 1 to 0 by (-1)).foreach(j => {
+      dp(i)(j) = (dp(i + 1)(j).min(dp(i)(j + 1)) - dungeon(i)(j)).max(1)
+    }))
+    dp(0)(0)
   }
 }
