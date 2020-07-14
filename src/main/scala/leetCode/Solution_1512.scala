375ce46254a11c3712eb6fef6f472956e31c1dbf
Justdwiwt <len_master@hotmail.com>
2020-07-14 18:51:44 +0800

    leetCode 1512

--- src/main/scala/leetCode/Solution_1512.scala
+++ src/main/scala/leetCode/Solution_1512.scala
@@ -2,7 +2,7 @@ package leetCode
 
 object Solution_1512 {
   def numIdenticalPairs(nums: Array[Int]): Int = {
-    var cnt = Array.fill(101)(0)
+    val cnt = Array.fill(101)(0)
     var res = 0
     nums.foreach(i => {
       res += cnt(i)
