97be895384cd2e52159b5e00c39686abe0fe7540
Justdwiwt <len_master@hotmail.com>
2020-07-28 16:14:25 +0800

    leetCode 1220,1275,1289

---
+++ src/main/scala/leetCode/Solution_1275.scala
@@ -0,0 +1,23 @@
+package leetCode
+
+object Solution_1275 {
+  def tictactoe(moves: Array[Array[Int]]): String = {
+    var a = 0
+    var b = 0
+    (moves.indices by 2).foreach(i => {
+      a |= 1 << moves(i)(0) * 3 + moves(i)(1)
+      if (i != moves.length - 1) b |= 1 << moves(i + 1)(0) * 3 + moves(i + 1)(1)
+    })
+    if (((~a) & 273) == 0 || ((~a) & 84) == 0) return "A"
+    if (((~b) & 273) == 0 || ((~b) & 84) == 0) return "B"
+    var rol = 7
+    var col = 73
+    (0 until 3).foreach(_ => {
+      if (((~a) & rol) == 0 || ((~a) & col) == 0) return "A"
+      if (((~b) & rol) == 0 || ((~b) & col) == 0) return "B"
+      rol <<= 3
+      col <<= 1
+    })
+    if (moves.length < 9) "Pending" else "Draw"
+  }
+}
---
+++ src/main/scala/leetCode/Solution_1289.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_1289 {
+  def minFallingPathSum(arr: Array[Array[Int]]): Int = {
+    (0 to arr.length - 2).reverse.foreach(i => arr.indices.foreach(j => {
+      var mn = Int.MaxValue
+      arr.indices.foreach(k => if (k != j) mn = mn.min(arr(i + 1)(k)))
+      arr(i)(j) += mn
+    }))
+    var res = Int.MaxValue
+    arr.indices.foreach(i => res = res.min(arr(0)(i)))
+    res
+  }
+}
