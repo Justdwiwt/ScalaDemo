fd8332be61ef3eff1e2352a4a5f192d3fb5c18e5
Justdwiwt <len_master@hotmail.com>
2020-08-02 15:34:11 +0800

    leetCode xh-1.4

---
+++ src/main/scala/leetCode/XH_1_4.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object XH_1_4 {
+  def array_interval_std(nums: Array[Int], queries: Array[Array[Int]]): Array[Double] = {
+    var res = Array.empty[Double]
+    queries.indices.foreach(i => {
+      val p = (queries(i)(0), queries(i)(1))
+      val len = (p._1 to p._2).length
+      var arr = Array.emptyDoubleArray
+      (p._1 to p._2).foreach(v => arr :+= nums(v - 1).toDouble)
+      val sum = arr.sum
+      val dAve = sum / len
+      var dVar = 0.0
+      arr.indices.foreach(v => dVar += ((arr(v) - dAve) * (arr(v) - dAve)))
+      res :+= (dVar / len)
+    })
+    res
+  }
+}
