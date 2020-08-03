80ec8e6a7a958cee790672db247bad8aee19d728
Justdwiwt <len_master@hotmail.com>
2020-08-03 21:05:52 +0800

    leetCode xh-6.1

---
+++ src/main/scala/leetCode/XH_6_1.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object XH_6_1 {
+
+  class LCQueue() {
+
+    private var l = List.empty[Int]
+
+    def push(x: Int): Unit = l :+= x
+
+    def pop(): Unit = l = l.tail
+
+    def size(): Int = l.length
+
+    def front(): Int = l.head
+
+  }
+
+}
