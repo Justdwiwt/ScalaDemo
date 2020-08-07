481968c907f8f5e69b8cc1b2985688f436627aa0
Justdwiwt <len_master@hotmail.com>
2020-08-07 15:56:53 +0800

    leetCode 100

---
+++ src/main/scala/leetCode/Solution_100.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_100 {
+  def isSameTree(p: TreeNode, q: TreeNode): Boolean = {
+    if (p == q) return true
+    if (p == null || q == null) return false
+    if (p.value != q.value) return false
+    isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+  }
+}
