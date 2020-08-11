17c961b8792862833c45757469427860f865e4ee
Justdwiwt <len_master@hotmail.com>
2020-08-11 19:35:56 +0800

    leetCode 1544

---
+++ src/main/scala/leetCode/Solution_1544.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_1544 {
+  def makeGood(s: String): String = {
+    val res = new StringBuilder(s)
+    var i = 0
+    while (i <= res.length - 2) {
+      if ((res(i) - res(i + 1)).abs == 32) {
+        res.delete(i, i + 2)
+        i = 0
+      } else i += 1
+    }
+    res.toString()
+  }
+}
