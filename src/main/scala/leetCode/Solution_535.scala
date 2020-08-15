e907e25668dc06b98a4193ffa956eac01f0ba680
Justdwiwt <len_master@hotmail.com>
2020-08-15 19:21:34 +0800

    leetCode 535

---
+++ src/main/scala/leetCode/Solution_535.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_535 {
+
+  class Codec {
+    // Encodes a URL to a shortened URL.
+    def encode(longURL: String): String = longURL
+
+    // Decodes a shortened URL to its original URL.
+    def decode(shortURL: String): String = shortURL
+  }
+
+}
