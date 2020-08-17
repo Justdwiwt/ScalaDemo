8d59acd71bd6a6452be20e063b8a2320a63e19f8
Justdwiwt <len_master@hotmail.com>
2020-08-17 17:45:07 +0800

    leetCode 593

---
+++ src/main/scala/leetCode/Solution_593.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_593 {
+  def validSquare(p1: Array[Int], p2: Array[Int], p3: Array[Int], p4: Array[Int]): Boolean = {
+    var s = new mutable.HashSet[Int]()
+    s += f(p1, p2)
+    s += f(p1, p3)
+    s += f(p1, p4)
+    s += f(p2, p3)
+    s += f(p2, p4)
+    s += f(p3, p4)
+    s.size == 2 && !s.contains(0)
+  }
+
+  def f(a: Array[Int], b: Array[Int]): Int = (a(0) - b(0)) * (a(0) - b(0)) + (a(1) - b(1)) * (a(1) - b(1))
+}
