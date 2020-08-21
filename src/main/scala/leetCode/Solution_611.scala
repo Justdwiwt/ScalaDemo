0cecfd0a32e6828812695172a7e8e9c2e3319250
Justdwiwt <len_master@hotmail.com>
2020-08-21 14:35:18 +0800

    leetCode 611

---
+++ src/main/scala/leetCode/Solution_611.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_611 {
+  def triangleNumber(nums: Array[Int]): Int = {
+    val arr = nums.sorted
+    var res = 0
+    (2 until arr.length).reverse.foreach(i => {
+      var k = 0
+      var j = i - 1
+      while (k < j) {
+        if (arr(k) + arr(j) > arr(i)) {
+          res += j - k
+          j -= 1
+        } else k += 1
+      }
+    })
+    res
+  }
+}
