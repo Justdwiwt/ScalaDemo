25842cf8752ae9fa2256918267a9094345aa7b3a
Justdwiwt <len_master@hotmail.com>
2020-08-30 22:09:57 +0800

    leetCode 1157

---
+++ src/main/scala/leetCode/Solution_1157.scala
@@ -0,0 +1,34 @@
+package leetCode
+
+object Solution_1157 {
+
+  class MajorityChecker(_arr: Array[Int]) {
+
+    private val arr = _arr
+
+    def count(x: Int): Array[Int] = {
+      val B = Array.fill(20001)(0)
+      (0 to x).foreach(i => B(arr(i)) += 1)
+      B
+    }
+
+    private val m = scala.collection.mutable.HashMap[Int, Array[Int]]()
+
+    def query(left: Int, right: Int, threshold: Int): Int = {
+      if (left == right && threshold == 1) return arr(left)
+      val ls = if (m.contains(left)) m(left) else count(left)
+      val rs = if (m.contains(right)) m(right) else count(right)
+      m += left -> ls
+      m += right -> rs
+      rs(arr(left)) += 1
+      (1 to 20000).withFilter(i => rs(i) - ls(i) >= threshold).foreach(i => {
+        rs(arr(left)) -= 1
+        return i
+      })
+      rs(arr(left)) -= 1
+      -1
+    }
+
+  }
+
+}
