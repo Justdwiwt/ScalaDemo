2c631b952a6b23ecf54e02ceb6bd06e6f55408ce
Justdwiwt <len_master@hotmail.com>
2020-09-10 17:28:30 +0800

    leetCode 40

--- src/main/scala/leetCode/Solution_40.scala
+++ src/main/scala/leetCode/Solution_40.scala
@@ -1,40 +1,14 @@
 package leetCode
 
 object Solution_40 {
-  private var res = List[List[Int]]()
-  private var out = List[Int]()
-
   def combinationSum2(candidates: Array[Int], target: Int): List[List[Int]] = {
-    val t = candidates.sorted
-    func(t, target, 0, 0)
-
-    def func(t: Array[Int], target: Int, sum: Int, level: Int): Int = {
-      if (sum > target) return 1
-      if (sum == target) {
-        if (!res.contains(out)) res ::= out
-        return 1
-      }
-      if (t.length == 1) {
-        if (target == t(0)) {
-          out :::= List(t(0))
-          res ::= out
-        }
-        return 0
-      }
-      var s = sum
-      var i = level
-      while (i < t.length) {
-        s += t(i)
-        out :::= List(t(i))
-        val j = func(t, target, s, i + 1)
-        out = out.drop(out.length - 1)
-        s -= t(i)
-        if (j == 1) i = t.length
-        i += 1
-      }
-      0
+    def f(l: List[Int], t: Int): List[List[Int]] = l match {
+      case _ if t < 0 => Nil
+      case _ if t == 0 => List(Nil)
+      case Nil => Nil
+      case c :: cc => f(cc, t - c).map(c :: _) ++ f(cc, t)
     }
 
-    res
+    f(candidates.toList.sorted, target).distinct
   }
 }
