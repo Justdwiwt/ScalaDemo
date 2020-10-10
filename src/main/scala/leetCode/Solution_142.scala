b9674f386f6c2955bd181c242e9bdd17f4a6c5d8
Justdwiwt <len_master@hotmail.com>
2020-10-10 13:54:13 +0800

    leetCode 142

---
+++ src/main/scala/leetCode/Solution_142.scala
@@ -0,0 +1,21 @@
+package leetCode
+
+object Solution_142 {
+  def detectCycle(head: ListNode): ListNode = {
+    @scala.annotation.tailrec
+    def f(slow: ListNode, fast: ListNode): ListNode = {
+      if (slow == fast) g(head, slow)
+      else if (fast == null || fast.next == null) null
+      else f(slow.next, fast.next.next)
+    }
+
+    @scala.annotation.tailrec
+    def g(cur: ListNode, slow: ListNode): ListNode = {
+      if (cur == slow) slow
+      else g(cur.next, slow.next)
+    }
+
+    if (head == null || head.next == null) null
+    else f(head.next, head.next.next)
+  }
+}
