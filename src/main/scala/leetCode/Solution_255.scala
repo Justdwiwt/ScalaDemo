80bd8957b20a3f86e7f26c1ba7995927dfd57765
Justdwiwt <len_master@hotmail.com>
2020-10-21 17:18:53 +0800

    leetCode 252,253,254,255,256

---
+++ src/main/scala/leetCode/Solution_255.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_255 {
+  def verifyPreorder(preorder: Array[Int]): Boolean = {
+    var top = -1
+    var mn = Int.MinValue
+    val st = Array.fill(preorder.length)(0)
+    preorder.foreach(v => {
+      if (v < mn) return false
+      while (top > -1 && v > st(top)) {
+        mn = st(top)
+        top -= 1
+      }
+      top += 1
+      st(top) = v
+    })
+    true
+  }
+}
