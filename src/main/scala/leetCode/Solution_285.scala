f9e31fee14fe9d5e64166e63199c29211f607ba9
Justdwiwt <len_master@hotmail.com>
2020-10-28 15:37:37 +0800

    leetCode 285

---
+++ src/main/scala/leetCode/Solution_285.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_285 {
+  def inorderSuccessor(root: TreeNode, p: TreeNode): TreeNode = {
+    if (p == null || root == null) return null
+    if (root.value <= p.value) return inorderSuccessor(root.right, p)
+    val res = inorderSuccessor(root.left, p)
+    if (res != null && res.value < root.value) res else root
+  }
+}
