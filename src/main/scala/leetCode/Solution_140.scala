fc44a35211116b1025c9d03ecc6e4d874ea61ccb
Justdwiwt <len_master@hotmail.com>
2020-11-01 00:23:15 +0800

    leetCode 140

--- src/main/scala/leetCode/Solution_140.scala
+++ src/main/scala/leetCode/Solution_140.scala
@@ -1,23 +1,23 @@
 package leetCode
 
-import scala.collection.mutable
-import scala.util.control.Breaks._
-
 object Solution_140 {
-  private val m = new mutable.HashMap[String, List[String]]()
+  def f[T, S](func: (T => S) => T => S, cache: scala.collection.mutable.HashMap[T, S]): T => S = n => {
+    if (cache.contains(n)) cache(n)
+    else {
+      val t = func(f(func, cache))(n)
+      cache(n) = t
+      t
+    }
+  }
 
   def wordBreak(s: String, wordDict: List[String]): List[String] = {
-    if (m.contains(s)) return m(s)
-    if (s.isEmpty) return List.empty
-    var res = List.empty[String]
-    wordDict.foreach(word => {
-      breakable {
-        if (s.substring(0, word.length) != word) break()
+    val word: (String => List[String]) => String => List[String] = f => {
+      case "" => List[String]("")
+      case r => wordDict./:(List[String]()) { (l, w) =>
+        if (r.endsWith(w) && f(r.dropRight(w.length)).nonEmpty) l ::: f(r.dropRight(w.length)).map(str => if (str.nonEmpty) str + " " + w else w) else l
       }
-      val rem = wordBreak(s.substring(word.length), wordDict)
-      rem.foreach(str => res :+= (word + (if (str.isEmpty) "" else " ") + str))
-    })
-    m(s) = res
-    res
+    }
+    val m = f(word, new scala.collection.mutable.HashMap[String, List[String]])
+    m(s)
   }
 }
