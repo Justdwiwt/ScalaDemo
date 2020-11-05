d223c8baffadef92ed242fd71b3aed50d286245f
Justdwiwt <len_master@hotmail.com>
2020-11-05 17:22:19 +0800

    leetCode 127,369,370,379,417

--- src/main/scala/leetCode/Solution_127.scala
+++ src/main/scala/leetCode/Solution_127.scala
@@ -1,32 +1,29 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_127 {
   def ladderLength(beginWord: String, endWord: String, wordList: List[String]): Int = {
-    val s = new mutable.HashSet[String]()
-    wordList.foreach(i => s.add(i))
-    val q = new mutable.Queue[String]()
-    q.enqueue(beginWord)
-    var res = 0
+    var q = scala.collection.immutable.Queue[String](beginWord)
+    var m = Map[String, Int](wordList.toStream.map((_, Integer.MAX_VALUE)): _*) + (beginWord -> 1)
     while (q.nonEmpty) {
-      (q.length until 0 by -1).foreach(_ => {
-        val word = q.front
-        q.dequeue
-        if (word == endWord) return res + 1
-        word.indices.foreach(j => {
-          val newWord = new mutable.StringBuilder
-          newWord.append(word)
-          ('a' to 'z').foreach(ch => {
-            newWord(j) = ch
-            if (s.contains(newWord.toString) && newWord.toString != word) {
-              q.enqueue(newWord.toString)
-              s.remove(newWord.toString)
+      val curWord = q.dequeue._1
+      val curDist = m.getOrElse(curWord, Integer.MAX_VALUE)
+      q = q.dequeue._2
+      if (curWord == endWord) return m(endWord)
+      val ch = curWord.toCharArray
+      ('a' to 'z').foreach(t => {
+        (0 until ch.length).foreach(i => {
+          val old = ch(i)
+          ch(i) = t
+          val nextNode = ch./:("")(_ + _)
+          m.get(nextNode).foreach(d => {
+            if (d > curDist + 1) {
+              m = m + (nextNode -> (curDist + 1))
+              q = q.enqueue(nextNode)
             }
           })
+          ch(i) = old
         })
       })
-      res += 1
     }
     0
   }
---
+++ src/main/scala/leetCode/Solution_369.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_369 {
+  def plusOne(head: ListNode): ListNode = f(head) match {
+    case 1 =>
+      val h = new ListNode(1)
+      h.next = head
+      h
+    case _ => head
+  }
+
+  def f(node: ListNode): Int = node match {
+    case null => 1
+    case _ =>
+      val t = node.x
+      node.x = (node.x + f(node.next)) % 10
+      if (node.x < t) 1 else 0
+  }
+}
