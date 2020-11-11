3da511dad72812ca593daf46e2f7261e1d7db244
Justdwiwt <len_master@hotmail.com>
2020-11-11 14:10:08 +0800

    leetCode 514

--- src/main/scala/leetCode/Solution_514.scala
+++ src/main/scala/leetCode/Solution_514.scala
@@ -2,15 +2,8 @@ package leetCode
 
 object Solution_514 {
   def findRotateSteps(ring: String, key: String): Int = {
-    val dp = Array.fill(key.length + 1, ring.length)(0)
-    (key.length - 1 to 0 by -1).foreach(i => (0 until ring.length).foreach(j => {
-      dp(i)(j) = Int.MaxValue
-      (0 until ring.length).foreach(k => if (ring(k) == key(i)) {
-        val diff = (j - k).abs
-        val step = diff.min(ring.length - diff)
-        dp(i)(j) = dp(i)(j).min(step + dp(i + 1)(k))
-      })
-    }))
-    dp(0)(0) + key.length
+    var dp = Array.fill[(Int, Int)](1)(0, 0)
+    key.foreach(k => dp = ring.zipWithIndex.filter(_._1 == k).map({ case (_, i) => (dp.map(t => t._1 + (i - t._2).abs.min(ring.length - (i - t._2).abs) + 1).min, i) }).toArray)
+    dp.minBy(_._1)._1
   }
 }
