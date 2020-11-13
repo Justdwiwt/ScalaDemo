3cfec5b16f6f466a0e565fdb4c19e923761d1ba2
Justdwiwt <len_master@hotmail.com>
2020-11-13 14:45:10 +0800

    leetCode 328

--- src/main/scala/leetCode/Solution_328.scala
+++ src/main/scala/leetCode/Solution_328.scala
@@ -1,18 +1,27 @@
 package leetCode
 
 object Solution_328 {
-  def oddEvenList(head: ListNode): ListNode = {
-    if (head == null || head.next == null) return head
-    var odd = head
-    var even = head.next
-    val evenHead = even
-    while (even != null && even.next != null) {
-      odd.next = even.next
-      odd = odd.next
-      even.next = odd.next
-      even = even.next
+
+  object Converse {
+
+    implicit def nodeToList(x: ListNode): List[Int] = x match {
+      case a if a == null => Nil
+      case b if b != null => b.x :: nodeToList(b.next)
     }
-    odd.next = evenHead
-    head
+
+    implicit def listToNode(x: List[Int]): ListNode = x match {
+      case Nil => null
+      case h :: t =>
+        val head = new ListNode(h)
+        head.next = listToNode(t)
+        head
+    }
+  }
+
+  def oddEvenList(head: ListNode): ListNode = {
+    import Converse._
+
+    val list = head.zipWithIndex.partition({ case (_, y) => (y + 1) % 2 == 1 })
+    list._1.map(_._1) ++ list._2.map(_._1)
   }
 }
