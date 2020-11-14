f57422a9e917d29a95edc486fb1f2b7e527abe95
Justdwiwt <len_master@hotmail.com>
2020-11-15 00:14:19 +0800

    leetCode 402

--- src/main/scala/leetCode/Solution_402.scala
+++ src/main/scala/leetCode/Solution_402.scala
@@ -21,4 +21,33 @@ object Solution_402 {
 
     g(f("", num, k))
   }
+
+  case class Zipper(left: List[Int], value: Int, right: List[Int]) {
+    def toList: List[Int] = List(left.reverse, List(value), right).flatten
+  }
+
+  @scala.annotation.tailrec
+  def go(zipper: Zipper, k: Int): Zipper = {
+    if (k == 0) zipper match {
+      case Zipper(Nil, 0, Nil) => Zipper(Nil, 0, Nil)
+      case Zipper(Nil, 0, x :: xs) => go(Zipper(Nil, x, xs), 0)
+      case z => z
+    } else zipper match {
+      case Zipper(Nil, _, Nil) => Zipper(Nil, 0, Nil)
+      case Zipper(Nil, 0, x :: xs) => go(Zipper(Nil, x, xs), k)
+      case Zipper(Nil, head, x :: xs) =>
+        if (head > x) go(Zipper(Nil, x, xs), k - 1)
+        else go(Zipper(List(head), x, xs), k)
+      case Zipper(left :: ls, head, x :: xs) =>
+        if (head >= left && head > x) go(Zipper(ls, left, x :: xs), k - 1)
+        else go(Zipper(head :: left :: ls, x, xs), k)
+      case Zipper(left :: ls, _, Nil) => go(Zipper(ls, left, Nil), k - 1)
+    }
+  }
+
+
+  def removeKdigits2(num: String, k: Int): String = num.map(_.asDigit).toList match {
+    case Nil => "0"
+    case x :: xs => go(Zipper(List.empty, x, xs), k).toList.mkString
+  }
 }
