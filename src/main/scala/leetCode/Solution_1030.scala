28641c7a6c749b01ebe496a0cd1192d98f5f4902
Justdwiwt <len_master@hotmail.com>
2020-11-17 17:17:58 +0800

    leetCode 1030

--- src/main/scala/leetCode/Solution_1030.scala
+++ src/main/scala/leetCode/Solution_1030.scala
@@ -2,37 +2,13 @@ package leetCode
 
 object Solution_1030 {
   def allCellsDistOrder(R: Int, C: Int, r0: Int, c0: Int): Array[Array[Int]] = {
-    var max = 0
-    var xy = Array.fill(2)(0)
-    var res = Array.empty[Array[Int]]
-    xy(0) = r0
-    xy(1) = c0
-    res :+= xy
-    if (R - 1 - r0 > r0) max = R - 1 - r0 else max = r0
-    if (C - 1 - c0 > c0) max += (C - 1 - c0) else max += c0
-    (1 to max).foreach(k => (0 to k).foreach(i => {
-      val j = k - i
-      if (r0 - i >= 0 && c0 - j >= 0) {
-        xy(0) = r0 - i
-        xy(1) = c0 - j
-        res :+= xy
-      }
-      if (r0 - i >= 0 && c0 + j < C && j > 0) {
-        xy(0) = r0 - i
-        xy(1) = c0 + j
-        res :+= xy
-      }
-      if (r0 + i < R && c0 - j >= 0 && i > 0) {
-        xy(0) = r0 + i
-        xy(1) = c0 - j
-        res :+= xy
-      }
-      if (r0 + i < R && c0 + j < C && i * j > 0) {
-        xy(0) = r0 + i
-        xy(1) = c0 + j
-        res :+= xy
-      }
+    val res = Array.ofDim[Int](R * C, 2)
+    var idx = 0
+    (0 until R).foreach(i => (0 until C).foreach(j => {
+      res(idx) = Array(i, j)
+      idx += 1
     }))
+    java.util.Arrays.sort(res, (o1: Array[Int], o2: Array[Int]) => (o1.head - r0).abs + (o1(1) - c0).abs - (o2.head - r0).abs - (o2(1) - c0).abs)
     res
   }
 }
