5a34b5b8806175562c79c23c629ff088ba36890c
Justdwiwt <len_master@hotmail.com>
2020-11-18 15:32:54 +0800

    leetCode 134

--- src/main/scala/leetCode/Solution_134.scala
+++ src/main/scala/leetCode/Solution_134.scala
@@ -1,18 +1,20 @@
 package leetCode
 
 object Solution_134 {
+
+  case class Acc(gasInTank: Int, minGas: Int, minGasIdx: Int, prevCost: Int)
+
   def canCompleteCircuit(gas: Array[Int], cost: Array[Int]): Int = {
-    val g = gas.indices.map(x => gas(x) - cost(x))
-    if (g.sum < 0) return -1
-    var t = 0
-    var start = 0
-    g.indices.foreach(i => {
-      t += g(i)
-      if (t < 0) {
-        start = i + 1
-        t = 0
-      }
-    })
-    start
+    val pre = gas.zip(cost).zipWithIndex./:(Acc(0, 0, -1, 0))(step)
+    val res = step(pre, ((0, 0), 0))
+    if (res.gasInTank >= 0) res.minGasIdx else -1
   }
+
+  def step(acc: Acc, next: ((Int, Int), Int)): Acc = next match {
+    case ((nextGas, nextCost), idx) =>
+      val tripGas = acc.gasInTank - acc.prevCost
+      val newGas = tripGas + nextGas
+      Acc(newGas, tripGas.min(acc.minGas), if (tripGas <= acc.minGas) idx else acc.minGasIdx, nextCost)
+  }
+
 }
