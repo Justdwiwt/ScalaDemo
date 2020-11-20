55c7a40d76b38496596a83f684f75d5171233dd3
Justdwiwt <len_master@hotmail.com>
2020-11-20 15:46:21 +0800

    leetCode 147

--- src/main/scala/leetCode/Solution_147.scala
+++ src/main/scala/leetCode/Solution_147.scala
@@ -2,17 +2,49 @@ package leetCode
 
 object Solution_147 {
   def insertionSortList(head: ListNode): ListNode = {
-    val dummy = new ListNode(-1)
-    var cur = dummy
-    var root = head
-    while (root != null) {
-      val t = root.next
-      cur = dummy
-      while (cur.next != null && cur.next.x <= root.x) cur = cur.next
-      root.next = cur.next
-      cur.next = root
-      root = t
+    @scala.annotation.tailrec
+    def sort(sorted: ListNode, unsorted: ListNode): ListNode = unsorted match {
+      case null => sorted
+      case _ =>
+        val (n, ns) = headTail(unsorted)
+        sort(insert(sorted, n), ns)
     }
-    dummy.next
+
+    def headTail(list: ListNode): (ListNode, ListNode) = {
+      val hd = list
+      val tl = list.next
+      hd.next = null
+      (hd, tl)
+    }
+
+    @scala.annotation.tailrec
+    def insert(sorted: ListNode, node: ListNode, prefixHead: ListNode = null, prefixLast: ListNode = null): ListNode = (sorted, prefixHead) match {
+      case (null, null) =>
+        node
+
+      case (null, _) =>
+        prefixLast.next = node
+        prefixHead
+
+      case (_, null) if node.x < sorted.x =>
+        node.next = sorted
+        node
+
+      case _ if node.x < sorted.x =>
+        node.next = sorted
+        prefixLast.next = node
+        prefixHead
+
+      case (_, null) =>
+        val (n, ns) = headTail(sorted)
+        insert(ns, node, n, n)
+
+      case _ =>
+        val (n, ns) = headTail(sorted)
+        prefixLast.next = n
+        insert(ns, node, prefixHead, n)
+    }
+
+    sort(null, head)
   }
 }
