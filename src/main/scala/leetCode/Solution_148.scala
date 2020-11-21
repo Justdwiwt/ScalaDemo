13a3dacad72486c75d6424d172ed46f2f9078651
Justdwiwt <len_master@hotmail.com>
2020-11-21 17:58:37 +0800

    leetCode 148

---
+++ src/main/scala/leetCode/Solution_148.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_148 {
+  def sortList(head: ListNode): ListNode = {
+    @scala.annotation.tailrec
+    def tolist(node: ListNode, acc: List[Int] = List()): List[Int] = if (node == null) acc else tolist(node.next, node.x :: acc)
+
+    val newHead = new ListNode()
+    tolist(head).sorted./:(newHead) { case (tail: ListNode, x: Int) =>
+      tail.next = new ListNode(x)
+      tail.next
+    }
+    newHead.next
+  }
+}
