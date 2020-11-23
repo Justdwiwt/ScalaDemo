dea30485dce1c5a755e849cc4fb821a9b1a3a5a9
Justdwiwt <len_master@hotmail.com>
2020-11-23 17:51:40 +0800

    leetCode 452

--- src/main/scala/leetCode/Solution_452.scala
+++ src/main/scala/leetCode/Solution_452.scala
@@ -2,16 +2,8 @@ package leetCode
 
 object Solution_452 {
   def findMinArrowShots(points: Array[Array[Int]]): Int = {
-    if (points.isEmpty) return 0
-    val p = points.sorted((o1: Array[Int], o2: Array[Int]) => o1(1) - o2(1))
-    var res = 1
-    var t = p(0)(1)
-    (1 until p.length).foreach(i => {
-      if (t < p(i)(0)) {
-        res += 1
-        t = p(i)(1)
-      }
-    })
-    res
+    points.sortBy(_.head)./:((Int.MinValue, 0)) { case ((pre, cnt), p) =>
+      if (p.head > pre || cnt == 0) (p(1), cnt + 1) else (pre.min(p(1)), cnt)
+    }._2
   }
 }
