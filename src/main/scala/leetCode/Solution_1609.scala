d93901f2d990990e15ac727d0a78b76d84c992bd
Justdwiwt <len_master@hotmail.com>
2020-11-26 15:56:56 +0800

    leetCode 1608,1609,1611,1614,1615

---
+++ src/main/scala/leetCode/Solution_1609.scala
@@ -0,0 +1,24 @@
+package leetCode
+
+object Solution_1609 {
+  def evenF(cur: List[TreeNode]): Boolean = {
+    cur.forall(_.value % 2 == 1) && cur.map(_.value).sorted.distinct == cur.map(_.value)
+  }
+
+  def oddF(cur: List[TreeNode]): Boolean = {
+    cur.forall(_.value % 2 == 0) && cur.map(_.value).sorted.distinct.reverse == cur.map(_.value)
+  }
+
+  def isEvenOddTree(root: TreeNode): Boolean = {
+    @scala.annotation.tailrec
+    def f(curr: List[TreeNode], even: Boolean): Boolean =
+      if (curr.isEmpty) true
+      else {
+        val next = curr.flatMap(node => Option(node.left) ++: Option(node.right) ++: List.empty)
+        if (even) evenF(curr) && f(next, !even)
+        else oddF(curr) && f(next, !even)
+      }
+
+    f(List(root), even = true)
+  }
+}
