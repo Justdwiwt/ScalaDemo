54af1b1fa3315add30f5131a719df29615da1116
Justdwiwt <len_master@hotmail.com>
2020-12-01 17:42:54 +0800

    leetCode 34,1577,1578,1582,1583

---
+++ src/main/scala/leetCode/Solution_1577.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Solution_1577 {
+  def numTriplets(nums1: Array[Int], nums2: Array[Int]): Int = {
+    val m1 = nums1.map(x => 1L * x * x).groupBy(identity).mapValues(_.length)
+    val m2 = nums2.map(x => 1L * x * x).groupBy(identity).mapValues(_.length)
+    var res = 0
+    nums1.indices.dropRight(1).foreach(i => (i + 1 until nums1.length).foreach(j => {
+      val v = 1L * nums1(i) * nums1(j)
+      if (m2.contains(v)) res = res + m2(v)
+    }))
+    nums2.indices.dropRight(1).foreach(i => (i + 1 until nums2.length).foreach(j => {
+      val v = 1L * nums2(i) * nums2(j)
+      if (m1.contains(v)) res = res + m1(v)
+    }))
+    res
+  }
+}
---
+++ src/main/scala/leetCode/Solution_34.scala
@@ -0,0 +1,25 @@
+package leetCode
+
+object Solution_34 {
+  def searchRange(nums: Array[Int], target: Int): Array[Int] = {
+    val left = binarySearch(nums, target, 0, nums.length - 1, left = true)
+    val right = binarySearch(nums, target, 0, nums.length - 1, left = false)
+    Array(left, right)
+  }
+
+  @scala.annotation.tailrec
+  def binarySearch(nums: Array[Int], target: Int, start: Int, end: Int, left: Boolean): Int =
+    if (start > end) -1
+    else {
+      val mid = (end - start) / 2 + start
+      if (left) nums(mid) match {
+        case n if n == target && !nums.lift(mid - 1).contains(target) => mid
+        case n if n < target => binarySearch(nums, target, mid + 1, end, left = true)
+        case _ => binarySearch(nums, target, start, mid - 1, left = true)
+      } else nums(mid) match {
+        case n if n == target && !nums.lift(mid + 1).contains(target) => mid
+        case n if n > target => binarySearch(nums, target, start, mid - 1, left = false)
+        case _ => binarySearch(nums, target, mid + 1, end, left = false)
+      }
+    }
+}
