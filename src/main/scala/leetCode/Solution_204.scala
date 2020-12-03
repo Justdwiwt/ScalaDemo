e3b8ee6ef85edf5d62efdc044068215a13294090
Justdwiwt <len_master@hotmail.com>
2020-12-03 14:52:39 +0800

    leetCode 204

---
+++ src/main/scala/leetCode/Solution_204.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_204 {
+  def countPrimes(n: Int): Int = {
+    lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i => ps.takeWhile(j => j * j <= i).forall(k => i % k > 0))
+    ps.takeWhile(_ < n).size
+  }
+}
