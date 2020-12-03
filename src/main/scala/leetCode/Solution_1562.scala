b217517779b4ab3284f0bdd0f7be506c3d0538e4
Justdwiwt <len_master@hotmail.com>
2020-12-03 14:53:05 +0800

    leetCode 1560,1561,1562,1566,1572

---
+++ src/main/scala/leetCode/Solution_1562.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_1562 {
+  def f(arr: Array[Int], idx: Int, m: Int): Boolean = {
+    arr(idx) = 0
+    ((idx - 1 to 0 by -1).takeWhile(i =>
+      arr(i) == 1 && idx - i <= m + 2).size == m) ||
+      ((idx + 1 until arr.length).takeWhile(i =>
+        arr(i) == 1 && i - idx <= m + 2).size == m)
+  }
+
+  def findLatestStep(arr: Array[Int], m: Int): Int = {
+    if (m == arr.length) m
+    else {
+      val A = Array.fill[Int](arr.length)(1)
+      arr.indices.drop(1).reverse.find(idx => f(A, arr(idx) - 1, m)).getOrElse(-1)
+    }
+  }
+}
