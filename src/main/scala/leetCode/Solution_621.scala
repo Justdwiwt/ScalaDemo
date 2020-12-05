fd7a698f2a9e47e483dc0f7606361f19c0dd6739
Justdwiwt <len_master@hotmail.com>
2020-12-05 18:42:18 +0800

    leetCode 621

--- src/main/scala/leetCode/Solution_621.scala
+++ src/main/scala/leetCode/Solution_621.scala
@@ -2,14 +2,7 @@ package leetCode
 
 object Solution_621 {
   def leastInterval(tasks: Array[Char], n: Int): Int = {
-    val cnt = Array.ofDim[Int](26)
-    var mx = 0
-    tasks.foreach(i => {
-      cnt(i - 'A') += 1
-      mx = mx.max(cnt(i - 'A'))
-    })
-    var res = (mx - 1) * (n + 1)
-    (0 until 26).foreach(i => if (cnt(i) == mx) res += 1)
-    res.max(tasks.length)
+    val seq = tasks./:(Seq.fill(26)(0)) { case (acc, ch) => acc.updated(ch - 'A', acc(ch - 'A') + 1) }
+    tasks.length.max((seq.max - 1) * (n + 1) + seq.count(_ == seq.max))
   }
 }
