e08be6c3273ab9c00e6a6212ec90b63d24070449
Justdwiwt <len_master@hotmail.com>
2020-12-08 09:31:33 +0800

    leetCode 842,861

--- src/main/scala/leetCode/Solution_861.scala
+++ src/main/scala/leetCode/Solution_861.scala
@@ -5,7 +5,7 @@ object Solution_861 {
     val flipped = A.map(line => if (line.head == 0) line.map(1 - _) else line)
     A.head.indices.map(col => {
       val toggled = flipped.map(_ (col)).sum
-      (1 << (A.head.length - col - 1)) * (toggled max (A.length - toggled))
+      (1 << (A.head.length - col - 1)) * toggled.max(A.length - toggled)
     }).sum
   }
 }
