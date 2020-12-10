d4e6a28b73f5ab88e6921aaf6c1e4c2a9f229dea
Justdwiwt <len_master@hotmail.com>
2020-12-10 19:54:52 +0800

    leetCode 860

--- src/main/scala/leetCode/Solution_860.scala
+++ src/main/scala/leetCode/Solution_860.scala
@@ -2,25 +2,18 @@ package leetCode
 
 object Solution_860 {
   def lemonadeChange(bills: Array[Int]): Boolean = {
-    var five = 0
-    var ten = 0
-    bills.foreach(i =>
-      if (i == 5) five += 1
-      else if (i == 10) {
-        ten += 1
-        five -= 1
-        if (five < 0) return false
-      } else {
-        if (ten > 0) {
-          ten -= 1
-          five -= 1
-          if (five < 0) return false
-        } else if (five > 0) {
-          five -= 3
-          if (five < 0) return false
-        }
-        else return false
-      })
-    true
+    @scala.annotation.tailrec
+    def f(bills: Array[Int], changes: Map[Int, Int]): Boolean = {
+      if (bills.isEmpty) true
+      else bills.head match {
+        case 5 => f(bills.tail, changes.updated(5, changes(5) + 1))
+        case 10 => if (changes(5) == 0) false else f(bills.tail, changes.updated(5, changes(5) - 1).updated(10, changes(10) + 1))
+        case 20 => if (changes(10) > 0 && changes(5) > 0) f(bills.tail, changes.updated(5, changes(5) - 1).updated(10, changes(10) - 1))
+        else if (changes(5) >= 3) f(bills.tail, changes.updated(5, changes(5) - 3))
+        else false
+      }
+    }
+
+    f(bills, Map(5 -> 0, 10 -> 0))
   }
 }
