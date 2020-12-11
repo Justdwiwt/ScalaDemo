c202b5b943b804a17c9a007eea6815cf416e2ccf
Justdwiwt <len_master@hotmail.com>
2020-12-12 00:20:03 +0800

    leetCode 376

--- src/main/scala/leetCode/Solution_376.scala
+++ src/main/scala/leetCode/Solution_376.scala
@@ -2,12 +2,16 @@ package leetCode
 
 object Solution_376 {
   def wiggleMaxLength(nums: Array[Int]): Int = {
-    var up = 1
-    var down = 1
-    (1 until nums.length).foreach(i => {
-      if (nums(i) > nums(i - 1)) up = down + 1
-      else if (nums(i) < nums(i - 1)) down = up - 1
-    })
-    if (nums.isEmpty) 0 else up.max(down)
+    @scala.annotation.tailrec
+    def f(pos: Int, posSize: Int, negSize: Int, posNum: Int, negNum: Int): Int =
+      if (pos >= nums.length) posSize.max(negSize)
+      else {
+        val num = nums(pos)
+        val (_posSize, _posNum) = if (negNum - num > 0) (negSize + 1, num) else (posSize, posNum)
+        val (_negSize, _negNum) = if (posNum - num < 0) (posSize + 1, num) else (negSize, negNum)
+        f(pos + 1, _posSize, _negSize, _posNum, _negNum)
+      }
+
+    if (nums.length < 2) nums.length else f(1, 1, 1, nums.head, nums.head)
   }
 }
