8ba75b8805b5cb5dc24a19249c614baa71906274
Justdwiwt <len_master@hotmail.com>
2021-01-11 17:02:30 +0800

    leetCode 791

--- src/main/scala/leetCode/Solution_791.scala
+++ src/main/scala/leetCode/Solution_791.scala
@@ -2,21 +2,6 @@ package leetCode
 
 object Solution_791 {
   def customSortString(S: String, T: String): String = {
-    var res = ""
-    val cnt = Array.fill(26)(0)
-    T.foreach(i => cnt(i - 'a') += 1)
-    S.foreach(i => {
-      while (cnt(i - 'a') > 0) {
-        cnt(i - 'a') -= 1
-        res :+= i
-      }
-    })
-    T.foreach(i => {
-      while (cnt(i - 'a') > 0) {
-        cnt(i - 'a') -= 1
-        res :+= i
-      }
-    })
-    res
+    T.sortBy(S.zipWithIndex.toMap.withDefault(_ => Int.MaxValue))
   }
 }
