57b8f54ef922876a3214d25fa7015d78634e8b8d
Justdwiwt <len_master@hotmail.com>
2021-03-01 14:24:00 +0800

    leetCode 1288

--- src/main/scala/leetCode/Solution_1288.scala
+++ src/main/scala/leetCode/Solution_1288.scala
@@ -1,18 +1,9 @@
 package leetCode
 
 object Solution_1288 {
-  def removeCoveredIntervals(intervals: Array[Array[Int]]): Int = {
-    val t = intervals.sortWith((a, b) => if (a(1) == b(1)) a(0) < b(0) else a(1) > b(1))
-    var l = t(0)(0)
-    var r = t(0)(1)
-    var res = 1
-    (1 until t.length).foreach(i => {
-      if (t(i)(0) < l) {
-        res += 1
-        l = t(i)(0)
-        r = t(i)(1)
-      }
-    })
-    res
-  }
+  def removeCoveredIntervals(intervals: Array[Array[Int]]): Int = intervals
+    .sortWith((a, b) => a.head < b.head || (a.head == b.head && a(1) >= b(1)))
+    ./:((0, 0)) { case (res@(pre, cnt), Array(_, end)) =>
+      if (end > pre) (end, cnt + 1) else res
+    }._2
 }
