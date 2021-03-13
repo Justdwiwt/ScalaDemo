03225fe5f0f1eccb5c0e2c8104984f65b31779e7
Justdwiwt <len_master@hotmail.com>
2021-03-13 18:12:25 +0800

    leetCode 187

--- src/main/scala/leetCode/Solution_187.scala
+++ src/main/scala/leetCode/Solution_187.scala
@@ -1,18 +1,12 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_187 {
-  def findRepeatedDnaSequences(s: String): List[String] = {
-    val st = new mutable.HashSet[String]()
-    val res = new mutable.HashSet[String]()
-    (0 until s.length - 9).foreach(i => {
-      val t = s.substring(i, 10)
-      if (st.contains(t)) res.add(t)
-      else st.add(t)
-    })
-    var ans = List[String]()
-    res.foreach(i => ans :+= i)
-    ans
-  }
+  def findRepeatedDnaSequences(s: String): List[String] = s
+    .sliding(10)
+    .toList
+    .groupBy(identity)
+    .mapValues(_.size)
+    .filter(_._2 > 1)
+    .keys
+    .toList
 }
