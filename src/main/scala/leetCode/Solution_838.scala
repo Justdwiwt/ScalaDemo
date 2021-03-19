e84702e27b5e7180497007fdbd2d232dcf495e27
Justdwiwt <len_master@hotmail.com>
2021-03-19 16:09:33 +0800

    leetCode 838

--- src/main/scala/leetCode/Solution_838.scala
+++ src/main/scala/leetCode/Solution_838.scala
@@ -2,25 +2,31 @@ package leetCode
 
 object Solution_838 {
   def pushDominoes(dominoes: String): String = {
-    val s = dominoes.toCharArray
-    val N = s.length
-    val arr = Array.fill(s.length)(0)
-    var t = 0
-    s.indices.foreach(i => {
-      if (s(i) == 'R') t = N
-      else if (s(i) == 'L') t = 0
-      else t = (t - 1).max(0)
-      arr(i) += t
-    })
-    t = 0
-    (s.length - 1 to 0 by (-1)).foreach(i => {
-      if (s(i) == 'L') t = N
-      else if (s(i) == 'R') t = 0
-      else t = (t - 1).max(0)
-      arr(i) -= t
-    })
-    var res = ""
-    arr.foreach(i => res :+= (if (i > 0) 'R' else if (i < '0') 'L' else '.'))
-    res
+    val res = ("L" + dominoes + "R").toCharArray
+    val len = res.length
+    var idx = 1
+    while (idx < len) {
+      if (res(idx) == '.') {
+        var j = idx
+        while (res(j + 1) == '.') j += 1
+        if (res(idx - 1) == 'R' && res(j + 1) == 'R')
+          (idx to j).foreach(k => res(k) = 'R')
+        else if (res(idx - 1) == 'L' && res(j + 1) == 'L')
+          (idx to j).foreach(k => res(k) = 'L')
+        else if (res(idx - 1) == 'R' && res(j + 1) == 'L') {
+          var a = idx
+          var b = j
+          while (a < b) {
+            res(a) = 'R'
+            res(b) = 'L'
+            a += 1
+            b -= 1
+          }
+        }
+        idx = j
+      }
+      idx += 1
+    }
+    res.mkString.substring(1, len - 1)
   }
 }
