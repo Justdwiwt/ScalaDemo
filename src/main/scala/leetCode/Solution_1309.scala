4d722eea52cd7c5b41c0f429b0f743d94b07e2ca
Justdwiwt <len_master@hotmail.com>
2021-03-22 12:44:17 +0800

    leetCode 1309

--- src/main/scala/leetCode/Solution_1309.scala
+++ src/main/scala/leetCode/Solution_1309.scala
@@ -1,19 +1,12 @@
 package leetCode
 
 object Solution_1309 {
-  def freqAlphabets(s: String): String = {
-    var res = ""
-    var idx = 0
-    while (idx < s.length) {
-      if (idx <= s.length - 3 && s.charAt(idx + 2).equals('#')) {
-        res = res + (s.slice(idx, idx + 2).toInt + 96).toChar
-        idx = idx + 3
-      }
-      else {
-        res = res + (s.charAt(idx) + 'a' - '1').toChar
-        idx = idx + 1
-      }
+  def freqAlphabets(s: String): String = ('a' to 'z')
+    .zipWithIndex
+    .view
+    .reverse
+    ./:(s) { case (s, (c, i)) =>
+      val rep = (i + 1) + (if (i < 9) "" else "#")
+      s.replaceAll(rep, "" + c)
     }
-    res
-  }
 }
