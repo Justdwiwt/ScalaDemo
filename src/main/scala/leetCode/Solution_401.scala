efca21787021644b9a79358aae0b75c2c5693006
Justdwiwt <len_master@hotmail.com>
2021-04-01 12:56:50 +0800

    leetCode 401

--- src/main/scala/leetCode/Solution_401.scala
+++ src/main/scala/leetCode/Solution_401.scala
@@ -2,8 +2,8 @@ package leetCode
 
 object Solution_401 {
   def readBinaryWatch(num: Int): List[String] = {
-    def f(n: Int): Int = n.toBinaryString.filter(_ == '1').length
+    def f(n: Int): Int = n.toBinaryString.count(_ == '1')
 
-    (0 until 12).flatMap(h => (0 until 60).filter(m => f(m) + f(h) == num).map(m => h.toString + ":" + "%02d".format(m))).toList
+    (0 until 12).flatMap(h => (0 until 60).filter(m => f(m) + f(h) == num).map(m => "%s:%02d".format(h.toString, m))).toList
   }
 }
