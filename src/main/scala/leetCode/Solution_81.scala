cb4784c3b366e4c84443bf78b58194cc27ae9f64
Justdwiwt <len_master@hotmail.com>
2021-04-07 16:01:09 +0800

    leetCode 81

--- src/main/scala/leetCode/Solution_81.scala
+++ src/main/scala/leetCode/Solution_81.scala
@@ -2,8 +2,10 @@ package leetCode
 
 object Solution_81 {
   def search(nums: Array[Int], target: Int): Boolean = {
-    nums.indices.foreach(i => if (nums(i) == target) return true
-    else if (i < nums.length - 1) if ((nums(i) < target) & (nums(i) > nums(i + 1))) return false)
+    nums.indices.foreach(i =>
+      if (nums(i) == target) return true
+      else if (i < nums.length - 1)
+        if ((nums(i) < target) & (nums(i) > nums(i + 1))) return false)
     false
   }
 }
