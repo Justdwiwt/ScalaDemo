ed1219f793a25a8ab2c3fc3b71a712da1f5ac6de
Justdwiwt <len_master@hotmail.com>
2021-04-08 14:45:10 +0800

    leetCode 1291

--- src/main/scala/leetCode/Solution_1291.scala
+++ src/main/scala/leetCode/Solution_1291.scala
@@ -2,14 +2,18 @@ package leetCode
 
 object Solution_1291 {
   def sequentialDigits(low: Int, high: Int): List[Int] = {
-    var res = List.empty[Int]
-    (1 to 9).foreach(i => {
-      var t = i
-      (i + 1 to 9).foreach(j => {
-        t = t * 10 + j
-        if (t >= low && t <= high) res :+= t
-      })
-    })
-    res.sorted
+    def getInit(n: Int): Int = 1.to(n).reduce(_ * 10 + _)
+
+    def getInc(n: Int): Int = List.fill(n)(1).reduce(_ * 10 + _)
+
+    def digitNum(x: Int): Int = x.toString.length
+
+    def getNext(x: Int): Int = if (x % 10 < 9) x + getInc(digitNum(x)) else getInit(digitNum(x) + 1)
+
+    val res = getInit(digitNum(low))
+
+    def worker(x: Int): Stream[Int] = x #:: worker(getNext(x))
+
+    worker(res).dropWhile(_ < low).takeWhile(_ <= high).toList
   }
 }
