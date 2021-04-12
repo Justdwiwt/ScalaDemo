6b5d091e38a04e0a4fd669ade37624eaf27a7bb7
Justdwiwt <len_master@hotmail.com>
2021-04-12 13:51:10 +0800

    leetCode 315

--- src/main/scala/leetCode/Solution_315.scala
+++ src/main/scala/leetCode/Solution_315.scala
@@ -1,16 +1,25 @@
 package leetCode
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
 
 object Solution_315 {
   def countSmaller(nums: Array[Int]): List[Int] = {
-    var t = 0
-    val res = ListBuffer.fill(nums.length)(0)
-    (nums.length - 2 to 0 by -1).foreach(i => {
-      t = 0
-      (nums.length - 1 until i by -1).foreach(j => if (nums(j) < nums(i)) t += 1)
-      res(i) = t
-    })
-    res.toList
+
+    case class KV(idx: Int, value: Int)
+    implicit object KVOrdering extends Ordering[KV] {
+      override def compare(x: KV, y: KV): Int = {
+        val cmp = Integer.compare(x.value, y.value)
+        if (cmp == 0) Integer.compare(x.idx, y.idx) else cmp
+      }
+    }
+    nums
+      .zipWithIndex
+      .map({ case (value, idx) => KV(idx, value) })
+      .:\((mutable.TreeSet.empty[KV], List.empty[Int]))((ele, acc) => {
+        val (ts: mutable.TreeSet[KV], res: List[Int]) = acc
+        val next = ts + ele
+        val size = next.size - next.from(ele).size
+        (next, size +: res)
+      })._2
   }
 }
