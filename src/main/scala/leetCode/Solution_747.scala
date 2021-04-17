abb8517dcadf1c9b1c2c75a0319b407dce87fedb
Justdwiwt <len_master@hotmail.com>
2021-04-17 14:50:49 +0800

    leetCode 747

--- src/main/scala/leetCode/Solution_747.scala
+++ src/main/scala/leetCode/Solution_747.scala
@@ -2,8 +2,10 @@ package leetCode
 
 object Solution_747 {
   def dominantIndex(nums: Array[Int]): Int = {
-    val idx = nums.indexOf(nums.max)
-    nums.indices.foreach(i => if (idx != i && nums(idx) < 2 * nums(i)) return -1)
-    idx
+    val (mx, idx) = nums.zipWithIndex.maxBy(_._1)
+    nums.zipWithIndex.find({ case (num, i) => (i != idx) && (num * 2) > mx }) match {
+      case Some(_) => -1
+      case None => idx
+    }
   }
 }
