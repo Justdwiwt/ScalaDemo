2a4c0f53f6f69053c19decbab5ed5f86401febb0
Justdwiwt <len_master@hotmail.com>
2021-04-21 14:48:57 +0800

    leetCode 1302

--- src/main/scala/leetCode/Solution_1302.scala
+++ src/main/scala/leetCode/Solution_1302.scala
@@ -1,25 +1,25 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1302 {
-  def deepestLeavesSum(root: TreeNode): Int = root match {
-    case null => 0
-    case _ =>
-      var res = 0
-      val q = new mutable.Queue[TreeNode]()
-      q.enqueue(root)
-      while (q.nonEmpty) {
-        res = 0
-        val size = q.size
-        (0 until size).foreach(_ => {
-          val node = q.head
-          q.dequeue()
-          res += node.value
-          if (node.left != null) q.enqueue(node.left)
-          if (node.right != null) q.enqueue(node.right)
-        })
-      }
-      res
-  }
+  case class LevelWithLeafSum(level: List[TreeNode], leafSum: Int)
+
+  def deepestLeavesSum(root: TreeNode): Int =
+    cal(LevelWithLeafSum(List(root), 0))
+
+  @scala.annotation.tailrec
+  def cal(tree: LevelWithLeafSum): Int =
+    if (tree.level.isEmpty) tree.leafSum
+    else {
+      val next = tree.level./:(LevelWithLeafSum(List.empty[TreeNode], 0))((levelSum, node) => {
+        if (isLeaf(node)) levelSum.copy(leafSum = levelSum.leafSum + node.value)
+        else levelSum.copy(level = add(add(levelSum.level, Option(node.left)), Option(node.right)))
+      })
+      cal(next)
+    }
+
+  def add(list: List[TreeNode], opt: Option[TreeNode]): List[TreeNode] =
+    opt./:(list)(_ :+ _)
+
+  def isLeaf(node: TreeNode): Boolean =
+    Option(node.left).isEmpty && Option(node.right).isEmpty
 }
