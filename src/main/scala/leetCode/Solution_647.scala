8f8f5a9b555d7909cb218c8245967e4d3e9e82be
Justdwiwt <len_master@hotmail.com>
2021-04-23 13:50:31 +0800

    leetCode 647

--- src/main/scala/leetCode/Solution_647.scala
+++ src/main/scala/leetCode/Solution_647.scala
@@ -1,22 +1,8 @@
 package leetCode
 
 object Solution_647 {
-  def countSubstrings(s: String): Int = {
-    val dp = Array.ofDim[Boolean](s.length, s.length)
-    var cnt = 0
-    val ch = s.toCharArray
-    s.indices.foreach(j => (0 to j).foreach(i => {
-      if (j == i && ch(j) == ch(i)) {
-        dp(i)(j) = true
-        cnt += 1
-      } else if (j - i == 1 && ch(i) == ch(j)) {
-        dp(i)(j) = true
-        cnt += 1
-      } else if (j - i > 1 && dp(i + 1)(j - 1) && ch(i) == ch(j)) {
-        dp(i)(j) = true
-        cnt += 1
-      }
-    }))
-    cnt
-  }
+  def countSubstrings(s: String): Int = s
+    .indices
+    .flatMap(i => Seq(i, i + 1).flatMap(j => (i to 0 by -1).zip(j until s.length).takeWhile(p => s(p._1) == s(p._2)).map(_ => true)))
+    .size
 }
