e3f2aa811083f86c64926578e996cdd064130b23
Justdwiwt <len_master@hotmail.com>
2021-04-26 15:47:07 +0800

    leetCode 392

--- src/main/scala/leetCode/Solution_392.scala
+++ src/main/scala/leetCode/Solution_392.scala
@@ -1,14 +1,6 @@
 package leetCode
 
 object Solution_392 {
-  def isSubsequence(s: String, t: String): Boolean = {
-    if (s.length == 0) return true
-    if (s.length > t.length) return false
-    var v = s.indices.head
-    t.indices.withFilter(i => t(i) == s(v)).foreach(_ => {
-      v += 1
-      if (v > s.indices.last) return true
-    })
-    false
-  }
+  def isSubsequence(s: String, t: String): Boolean =
+    t./:(0)((idx, ch) => if (idx < s.length && ch == s(idx)) idx + 1 else idx) == s.length
 }
