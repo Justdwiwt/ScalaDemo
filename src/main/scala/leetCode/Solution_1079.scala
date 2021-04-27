4b80ba16628979790a1c0b2938f94221cb03ad52
Justdwiwt <len_master@hotmail.com>
2021-04-27 11:56:17 +0800

    leetCode 1079

--- src/main/scala/leetCode/Solution_1079.scala
+++ src/main/scala/leetCode/Solution_1079.scala
@@ -1,25 +1,8 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1079 {
-  def numTilePossibilities(tiles: String): Int = {
-    val dict = Array.fill(tiles.length)(0)
-    tiles.indices.foreach(i => dict(i) = tiles(i))
-    dfs("", dict, new mutable.HashSet[String]())
-  }
-
-  def dfs(word: String, dict: Array[Int], set: mutable.HashSet[String]): Int = {
-    var cnt = 0
-    if (word.length != 0 && !set.contains(word)) {
-      set.add(word)
-      cnt += 1
-    }
-    dict.indices.foreach(i => {
-      val d = dict
-      val t = d.drop(i)
-      cnt += dfs(word + t, d, set)
-    })
-    cnt
-  }
+  def numTilePossibilities(tiles: String): Int = List
+    .range(1, tiles.length + 1)
+    .map(i => tiles.toSeq.combinations(i).toList.map(s => s.permutations.toList.length).sum)
+    .sum
 }
