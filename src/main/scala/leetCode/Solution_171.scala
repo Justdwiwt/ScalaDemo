4996d4d5d540dc0596620f258b0c376793d62f07
Justdwiwt <len_master@hotmail.com>
2021-04-30 18:04:58 +0800

    leetCode 171

--- src/main/scala/leetCode/Solution_171.scala
+++ src/main/scala/leetCode/Solution_171.scala
@@ -1,5 +1,6 @@
 package leetCode
 
 object Solution_171 {
-  def titleToNumber(s: String): Int = (0 /: s) ((_: Int) * 26 + _ - 64)
+  def titleToNumber(s: String): Int =
+    s./:(0)(_ * 26 + _ - 64)
 }
