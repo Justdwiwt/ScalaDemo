a1edcb711b077313a44a5f7df029689e15afce3b
Justdwiwt <len_master@hotmail.com>
2021-05-04 13:39:43 +0800

    leetCode 833

--- src/main/scala/leetCode/Solution_833.scala
+++ src/main/scala/leetCode/Solution_833.scala
@@ -2,22 +2,12 @@ package leetCode
 
 object Solution_833 {
   def findReplaceString(S: String, indexes: Array[Int], sources: Array[String], targets: Array[String]): String = {
-    val arr = Array.fill(S.length)(-1)
-    val res = new StringBuilder
-    indexes.indices.foreach(i => {
-      val ix = indexes(i)
-      if (S.substring(ix, ix + sources(i).length).equals(sources(i))) arr(ix) = i
+    var res = S
+    indexes.indices.map(i => Tuple2(indexes(i), i)).sortBy(_._1).reverse.foreach(tup => {
+      val s = sources(tup._2)
+      val t = targets(tup._2)
+      if (res.substring(tup._1, tup._1 + s.length).equals(s)) res = res.substring(0, tup._1) + t + res.substring(tup._1 + s.length)
     })
-    var ix = 0
-    while (ix < S.length) {
-      if (arr(ix) >= 0) {
-        res.append(targets(arr(ix)))
-        ix += sources(arr(ix)).length
-      } else {
-        res.append(S(ix))
-        ix += 1
-      }
-    }
-    res.toString
+    res
   }
 }
