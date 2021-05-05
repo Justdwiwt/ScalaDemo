eaf0aa67c3c85d48a0d8c6e86744d649d8c3b479
Justdwiwt <len_master@hotmail.com>
2021-05-05 13:34:29 +0800

    leetCode 110

--- src/main/scala/leetCode/Solution_110.scala
+++ src/main/scala/leetCode/Solution_110.scala
@@ -1,15 +1,13 @@
 package leetCode
 
 object Solution_110 {
-  def isBalanced(root: TreeNode): Boolean = if (high(root) == -1) false else true
+  def isBalanced(root: TreeNode): Boolean = {
+    def maxDepth(root: TreeNode): Int = {
+      if (root == null) return 0
+      maxDepth(root.left).max(maxDepth(root.right)) + 1
+    }
 
-  def high(root: TreeNode): Int = root match {
-    case null => 0
-    case _ =>
-      val l = high(root.left)
-      if (l == -1) return -1
-      val r = high(root.right)
-      if (r == -1) return -1
-      if (l - r >= -1 && l - r <= 1) l.max(r) + 1 else -1
+    if (root == null) return true
+    (maxDepth(root.left) - maxDepth(root.right)).abs <= 1 && isBalanced(root.left) && isBalanced(root.right)
   }
 }
