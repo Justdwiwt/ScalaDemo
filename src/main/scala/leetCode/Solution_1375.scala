c5c9758e2436bdc59231d72ffbac9265a7dc2dc8
Justdwiwt <len_master@hotmail.com>
2021-05-07 15:03:51 +0800

    leetCode 1375

--- src/main/scala/leetCode/Solution_1375.scala
+++ src/main/scala/leetCode/Solution_1375.scala
@@ -1,13 +1,8 @@
 package leetCode
 
 object Solution_1375 {
-  def numTimesAllBlue(light: Array[Int]): Int = {
-
-    def f(y: Int, n: Int): Int = if (n == light.length || y == n + 1) 1 else 0
-
-    @scala.annotation.tailrec
-    def g(seq: Seq[Int], n: Int, y: Int, acc: Int): Int = if (seq.isEmpty) acc else g(seq.tail, n - 1, y.min(seq.head), f(y, n) + acc)
-
-    g(light.reverse, light.length, light.length + 1, 0)
-  }
+  def numTimesAllBlue(light: Array[Int]): Int = light.indices./:((0, 0))((b, a) => {
+    if (b._2.max(light(a)) == a + 1) (b._1 + 1, a + 1)
+    else (b._1, b._2.max(light(a)))
+  })._1
 }
