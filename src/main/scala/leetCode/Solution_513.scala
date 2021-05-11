e2064fcd66d39fb3379b5be026504d9d4c76ffac
Justdwiwt <len_master@hotmail.com>
2021-05-11 13:33:19 +0800

    leetCode 513

--- src/main/scala/leetCode/Solution_513.scala
+++ src/main/scala/leetCode/Solution_513.scala
@@ -1,18 +1,15 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_513 {
   def findBottomLeftValue(root: TreeNode): Int = {
-    var r = root
-    val q = new mutable.Queue[TreeNode]()
-    q.enqueue(r)
-    while (q.nonEmpty) {
-      r = q.front
-      q.dequeue
-      if (r.right != null) q.enqueue(r.right)
-      if (r.left != null) q.enqueue(r.left)
-    }
-    r.value
+    @scala.annotation.tailrec
+    def f(seq: List[TreeNode], res: Int): Int =
+      if (seq.isEmpty) res
+      else f(seq.flatMap(g), seq.head.value)
+
+    def g(node: TreeNode): List[TreeNode] =
+      Option(node.right)./:(Option(node.left)./:(List.empty[TreeNode])(_ :+ _))(_ :+ _)
+
+    f(List(root), root.value)
   }
 }
