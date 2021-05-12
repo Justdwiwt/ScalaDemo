066c4622f435d2c57f28f549dc4fb6429739dbe0
Justdwiwt <len_master@hotmail.com>
2021-05-12 13:34:07 +0800

    leetCode 1475

--- src/main/scala/leetCode/Solution_1475.scala
+++ src/main/scala/leetCode/Solution_1475.scala
@@ -1,17 +1,7 @@
 package leetCode
 
 object Solution_1475 {
-  def finalPrices(prices: Array[Int]): Array[Int] = {
-    @scala.annotation.tailrec
-    def solve(l: List[Int], min_prices: List[Int] = Nil, acc: List[Int] = Nil): List[Int] = l match {
-      case Nil => acc
-      case h :: t =>
-        min_prices.dropWhile(x => x > h) match {
-          case Nil => solve(t, h :: Nil, h :: acc)
-          case h0 :: t0 => solve(t, h :: h0 :: t0, (h - h0) :: acc)
-        }
-    }
-
-    solve(prices.toList.reverse).toArray
-  }
+  def finalPrices(prices: Array[Int]): Array[Int] = prices
+    .zipWithIndex
+    .map({ case (price, i) => price - prices.drop(i + 1).find(_ <= price).getOrElse(0) })
 }
