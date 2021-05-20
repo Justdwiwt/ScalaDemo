419fd01e0dcc62c08d6bee115e97ad53cbae119e
Justdwiwt <len_master@hotmail.com>
2021-05-20 10:23:13 +0800

    leetCode 692,1824

---
+++ src/main/scala/leetCode/Solution_1824.scala
@@ -0,0 +1,26 @@
+package leetCode
+
+object Solution_1824 {
+  def minSideJumps(obstacles: Array[Int]): Int = {
+    val len = obstacles.length - 1
+
+    @scala.annotation.tailrec
+    def f(i: Int, pa: Array[Int]): Int =
+      if (i == len) pa.min
+      else {
+        val arr = Array.fill(3)(Int.MaxValue)
+        (1 to 3)
+          .withFilter(a => a != obstacles(i))
+          .withFilter(a => pa(a - 1) != Int.MaxValue)
+          .foreach(a => {
+            arr(a - 1) = arr(a - 1).min(pa(a - 1))
+            (1 to 3)
+              .withFilter(o => o != obstacles(i) && a != o)
+              .foreach(o => arr(o - 1) = arr(o - 1).min(arr(a - 1) + 1))
+          })
+        f(i + 1, arr)
+      }
+
+    f(1, Array(1, 0, 1))
+  }
+}
--- src/main/scala/leetCode/Solution_692.scala
+++ src/main/scala/leetCode/Solution_692.scala
@@ -1,7 +1,12 @@
 package leetCode
 
 object Solution_692 {
-  def topKFrequent(words: Array[String], k: Int): List[String] = {
-    words.groupBy(identity).mapValues(_.length).toSeq.sortBy(x => (-x._2, x._1)).take(k).map(_._1).toList
-  }
+  def topKFrequent(words: Array[String], k: Int): List[String] = words
+    .groupBy(identity)
+    .mapValues(_.length)
+    .toSeq
+    .sortBy(x => (-x._2, x._1))
+    .take(k)
+    .map(_._1)
+    .toList
 }
