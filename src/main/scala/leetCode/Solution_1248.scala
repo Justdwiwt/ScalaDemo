eda1afbe64856c4e8e40bd709fda30dd06200a39
Justdwiwt <len_master@hotmail.com>
2021-05-22 14:16:39 +0800

    leetCode 1248

--- src/main/scala/leetCode/Solution_1248.scala
+++ src/main/scala/leetCode/Solution_1248.scala
@@ -1,22 +1,14 @@
 package leetCode
 
 object Solution_1248 {
-  def numberOfSubarrays(A: Array[Int], k: Int): Int = {
-    @scala.annotation.tailrec
-    def f(l: List[Int], acc: List[Int] = Nil, count: Int = 0): List[Int] = l match {
-      case Nil => count :: acc
-      case h :: t => h % 2 match {
-        case 0 => f(t, acc, count + 1)
-        case 1 => f(t, count :: acc)
-      }
-    }
+  def numberOfSubarrays(nums: Array[Int], k: Int): Int = {
+    val pos = -1 +: nums.indices.filter(nums(_) % 2 == 1) :+ nums.length
 
     @scala.annotation.tailrec
-    def g(A: Array[Int], k: Int)(i: Int, acc: Int): Int = {
-      if (i + k < A.length) g(A, k)(i + 1, acc + (A(i + k) + 1) * (A(i) + 1))
-      else acc
-    }
+    def f(acc: Int, l: Int, r: Int): Int =
+      if (pos.size - 2 < k || r == pos.size - 1) acc
+      else f(acc + (pos(l) - pos(l - 1)) * (pos(r + 1) - pos(r)), l + 1, r + 1)
 
-    g(f(A.toList).toArray, k)(0, 0)
+    f(0, 1, k)
   }
 }
