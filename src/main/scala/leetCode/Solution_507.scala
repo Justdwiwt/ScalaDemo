2345e5b940bdf2e02c767561f9db9be8d9c41dbd
Justdwiwt <len_master@hotmail.com>
2021-05-29 13:35:03 +0800

    leetCode 507

--- src/main/scala/leetCode/Solution_507.scala
+++ src/main/scala/leetCode/Solution_507.scala
@@ -1,11 +1,6 @@
 package leetCode
 
 object Solution_507 {
-  def checkPerfectNumber(num: Int): Boolean = {
-    if (num == 1) return false
-    val n = math.sqrt(num).toInt
-    var res = 1
-    (2 to n).foreach(i => if (num % i == 0) res += i + num / i)
-    res == num
-  }
+  def checkPerfectNumber(num: Int): Boolean =
+    (1 until num).filter(i => num % i == 0).sum == num
 }
