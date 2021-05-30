940dcbd8a66c027d09a8eaf0960fe6c6f401707d
Justdwiwt <len_master@hotmail.com>
2021-05-30 12:49:03 +0800

    leetCode 457

--- src/main/scala/leetCode/Solution_457.scala
+++ src/main/scala/leetCode/Solution_457.scala
@@ -1,10 +1,28 @@
 package leetCode
 
 object Solution_457 {
-  def fourSumCount(A: Array[Int], B: Array[Int], C: Array[Int], D: Array[Int]): Int = {
-    val aPlusB = A.flatMap(a => B.map(b => a + b))
-    val sum = aPlusB.groupBy(r => r).map({ case (s: Int, arr: Array[Int]) => (s, arr.length) })
-    val total = C.flatMap(c => D.map(d => sum.getOrElse(-c - d, 0)))
-    total.sum
+  def circularArrayLoop(nums: Array[Int]): Boolean = {
+
+    def f(flag: Boolean, idx: Int): Int = {
+      val direction = nums(idx) >= 0
+      if (flag != direction) return -1
+      var nextIdx = (idx + nums(idx)) % nums.length
+      if (nextIdx < 0) nextIdx += nums.length
+      if (nextIdx == idx) return -1
+      nextIdx
+    }
+
+    nums.zipWithIndex.foreach({ case (num, i) =>
+      val isForward = num >= 0
+      var slow = i
+      var fast = i
+      do {
+        slow = f(isForward, slow)
+        fast = f(isForward, fast)
+        if (fast != -1) fast = f(isForward, fast)
+      } while (slow != -1 && fast != -1 && slow != fast)
+      if (slow != -1 && slow == fast) return true
+    })
+    false
   }
 }
