6c44ca764db3fc3a40000c178216c64fd4f95b39
Justdwiwt <len_master@hotmail.com>
2021-06-01 15:27:45 +0800

    leetCode 474

--- src/main/scala/leetCode/Solution_474.scala
+++ src/main/scala/leetCode/Solution_474.scala
@@ -1,22 +1,21 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_474 {
-  def findMaxForm(strs: Array[String], m: Int, n: Int): Int = {
-    val items = strs map countStr
-    val dp = Array.fill(items.length + 1, m + 1, n + 1)(0)
-    (1 to items.length).foreach(i => (0 to m).foreach(j => (0 to n).foreach(k =>
-      if (items(i - 1)._1 > j || items(i - 1)._2 > k) dp(i)(j)(k) = dp(i - 1)(j)(k)
-      else dp(i)(j)(k) = dp(i - 1)(j)(k) max (dp(i - 1)(j - items(i - 1)._1)(k - items(i - 1)._2) + 1))
-    ))
-    dp(items.length)(m)(n)
+  def cnt(s: String): (Int, Int) = {
+    val m = s.toCharArray.groupBy(identity).mapValues(_.length)
+    (m.getOrElse('0', 0), m.getOrElse('1', 0))
   }
 
-  def countStr(s: String): (Int, Int) = countStr(s.toList, (0, 0))
-
-  @scala.annotation.tailrec
-  def countStr(s: List[Char], acc: (Int, Int)): (Int, Int) = s match {
-    case Nil => acc
-    case '0' :: t => countStr(t, (acc._1 + 1, acc._2))
-    case '1' :: t => countStr(t, (acc._1, acc._2 + 1))
+  def findMaxForm(strs: Array[String], m: Int, n: Int): Int = {
+    val dp = mutable.Map.empty[(Int, Int), Int].withDefaultValue(0)
+    strs.foreach(s => {
+      val (zeros, ones) = cnt(s)
+      (zeros to m).reverse.foreach(z =>
+        (ones to n).reverse.foreach(o =>
+          dp((z, o)) = dp((z, o)).max(dp((z - zeros, o - ones)) + 1)))
+    })
+    dp((m, n))
   }
 }
