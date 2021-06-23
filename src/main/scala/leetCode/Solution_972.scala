cc20cfc57132970271c88aa2e479298f0b9217ae
Justdwiwt <len_master@hotmail.com>
2021-06-23 15:02:14 +0800

    leetCode 972

--- src/main/scala/leetCode/Solution_972.scala
+++ src/main/scala/leetCode/Solution_972.scala
@@ -1,19 +1,43 @@
 package leetCode
 
 object Solution_972 {
-  def isRationalEqual(S: String, T: String): Boolean = {
-    val M = 1e-8
-    (func(S) - func(T)).abs <= M
+  case class Fraction[A] private(numerator: A, denominator: A)(implicit integral: Integral[A]) {
+    private def simplify: Fraction[A] = {
+      import integral.{abs, mkNumericOps, mkOrderingOps, zero}
+      val gcd = Fraction.gcd(numerator, denominator)
+      if (numerator >= zero ^ denominator < zero)
+        new Fraction(abs(numerator / gcd), abs(denominator / gcd))
+      else
+        new Fraction(-abs(numerator / gcd), abs(denominator / gcd))
+    }
   }
 
-  def func(S: String): Double = {
-    if (S.contains("(")) {
-      val dot = S.indexOf(".")
-      val l = S.indexOf("(")
-      val r = S.indexOf(")")
-      val p = S.split('(').head.toDouble
-      val p2 = S.substring(l + 1, r).toDouble / Array.fill(r - l - 1)("9").reduce(_ + _).toDouble / math.pow(10, l - dot - 1)
-      p + p2
-    } else S.toDouble
+  object Fraction {
+    private val Rational = """(\d+)(?:\.(\d*)(?:\((\d+)\))?)?""".r
+
+    def apply[A](numerator: A)(implicit integral: Integral[A]): Fraction[A] = Fraction(numerator, integral.one)
+
+    def apply[A](numerator: A, denominator: A)(implicit integral: Integral[A]): Fraction[A] =
+      new Fraction(numerator, denominator).simplify
+
+    def apply(rational: String): Fraction[BigInt] = rational match {
+      case Rational(integer, null, null) =>
+        Fraction(BigInt(integer))
+      case Rational(integer, nonRepeating, null) =>
+        Fraction(BigInt(integer + nonRepeating), BigInt(10).pow(nonRepeating.length))
+      case Rational(integer, nonRepeating, repeating) =>
+        Fraction(BigInt(integer + nonRepeating + repeating) - BigInt(integer + nonRepeating),
+          BigInt(10).pow(nonRepeating.length + repeating.length) - BigInt(10).pow(nonRepeating.length))
+    }
+
+    @scala.annotation.tailrec
+    private def gcd[A](x: A, y: A)(implicit integral: Integral[A]): A = {
+      import integral.{abs, mkNumericOps, zero}
+      if (x == zero) abs(y)
+      else if (y == zero) abs(x)
+      else gcd(y, x % y)
+    }
   }
+
+  def isRationalEqual(s: String, t: String): Boolean = Fraction(s) == Fraction(t)
 }
