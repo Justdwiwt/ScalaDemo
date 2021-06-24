0126e0367130e117ad7f0ebb4c5ceaa3a1a9dada
Justdwiwt <len_master@hotmail.com>
2021-06-24 15:24:24 +0800

    leetCode 636

--- src/main/scala/leetCode/Solution_636.scala
+++ src/main/scala/leetCode/Solution_636.scala
@@ -1,22 +1,34 @@
 package leetCode
 
 object Solution_636 {
+  trait Entry
+
+  case class StartEntry(func: Int, time: Int) extends Entry
+
+  case class EndEntry(func: Int, time: Int) extends Entry
+
   def exclusiveTime(n: Int, logs: List[String]): Array[Int] = {
-    var st = List.empty[Array[Int]]
-    val res = Array.fill(n)(0)
-    logs.foreach(log => {
-      val split = log.split(":")
-      val id = split.head.toInt
-      val time = split(2).toInt
-      if ("start".equals(split(1))) st ::= Array(id, time)
-      else {
-        val pop = st.last
-        st = st.reverse.tail.reverse
-        var interval = time - pop(1) + 1
-        res(pop.head) += interval
-        if (st.nonEmpty) res(st.head.head) -= interval
-      }
+    val functionTimes = logs.map(_.split(":")).map({
+      case Array(f, "start", t) => StartEntry(f.toInt, t.toInt)
+      case Array(f, _, t) => EndEntry(f.toInt, t.toInt)
     })
+    val res = Array.fill(n)(0)
+    f(functionTimes, Nil, res, 0)
     res
+  }
+
+  @scala.annotation.tailrec
+  def f(list: List[Entry], st: List[Entry], res: Array[Int], prev: Int): Unit = list match {
+    case Nil => ()
+    case StartEntry(headFunction, headTime) :: tail if st.isEmpty =>
+      f(tail, StartEntry(headFunction, headTime) :: st, res, headTime)
+    case StartEntry(headFunction, headTime) :: tail =>
+      val StartEntry(stackFunction, _) = st.head
+      res(stackFunction) = res(stackFunction) + headTime - prev
+      f(tail, StartEntry(headFunction, headTime) :: st, res, headTime)
+    case EndEntry(_, headTime) :: tail =>
+      val StartEntry(stackFunction, _) = st.head
+      res(stackFunction) = res(stackFunction) + headTime - prev + 1
+      f(tail, st.tail, res, headTime + 1)
   }
 }
