d7c8eca5823b2b7f2e01471fd9cbccd5f8bb2c95
Justdwiwt <len_master@hotmail.com>
2021-06-27 15:28:03 +0800

    leetCode 1009

--- src/main/scala/leetCode/Solution_1009.scala
+++ src/main/scala/leetCode/Solution_1009.scala
@@ -1,11 +1,6 @@
 package leetCode
 
 object Solution_1009 {
-  def bitwiseComplement(N: Int): Int = N match {
-    case 0 => 1
-    case _ =>
-      var n = 1
-      while (n <= N) n <<= 1
-      n - 1 - N
-  }
+  def bitwiseComplement(N: Int): Int =
+    ~N & 1.max(Integer.highestOneBit(N) - 1)
 }
