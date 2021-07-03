b12496e273771fc9765dc91122ff9c0114e51b56
Justdwiwt <len_master@hotmail.com>
2021-07-03 14:18:23 +0800

    leetCode 929

--- src/main/scala/leetCode/Solution_929.scala
+++ src/main/scala/leetCode/Solution_929.scala
@@ -1,11 +1,9 @@
 package leetCode
 
 object Solution_929 {
-  def numUniqueEmails(emails: Array[String]): Int = emails.map(func).toSet.size
-
-  def func(s: String): String = removeDot(removePlus(s.split("@")(0))) + "@" + s.split("@")(1)
-
-  def removePlus(in: String): String = if (in.indexOf("+") < 0) in else in.substring(0, in.indexOf("+"))
-
-  def removeDot(in: String): String = in.replace(".", "")
+  def numUniqueEmails(emails: Array[String]): Int = emails
+    .map(mail => mail.split("[@]"))
+    .map(mail => mail.head.split("[+]").head.replace(".", "") + "@" + mail.last)
+    .distinct
+    .length
 }
