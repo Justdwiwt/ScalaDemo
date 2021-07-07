512181e549f8b7c0abdc429d59dc2200d1a585e8
Justdwiwt <len_master@hotmail.com>
2021-07-07 12:53:35 +0800

    leetCode 1338

--- src/main/scala/leetCode/Solution_1338.scala
+++ src/main/scala/leetCode/Solution_1338.scala
@@ -1,20 +1,11 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1338 {
-  def minSetSize(arr: Array[Int]): Int = {
-    val m = new mutable.HashMap[Int, Int]()
-    arr.foreach(i => m.put(i, m.getOrElse(i, 0) + 1))
-    var res = 0
-    val list = m.keySet.toList
-    val t = list.sorted((o1: Int, o2: Int) => m(o2) - m(o1))
-    var len = arr.length
-    t.indices.foreach(i => {
-      len = len - m(t(i))
-      res += 1
-      if (len <= arr.length / 2) return res
-    })
-    res
-  }
+  def minSetSize(arr: Array[Int]): Int = arr
+    .groupBy(c => c)
+    .map(_._2.length)
+    .toSeq
+    .sortWith(_ > _)
+    .scanLeft(0)(_ + _)
+    .indexWhere(_ >= arr.length / 2)
 }
