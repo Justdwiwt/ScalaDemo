62b1dd2f3c7d63201cdf61cf683b7811b6dd24aa
Justdwiwt <len_master@hotmail.com>
2021-07-08 14:10:52 +0800

    leetCode 378

--- src/main/scala/leetCode/Solution_378.scala
+++ src/main/scala/leetCode/Solution_378.scala
@@ -1,14 +1,8 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_378 {
   def kthSmallest(matrix: Array[Array[Int]], k: Int): Int = {
-    val q = new mutable.PriorityQueue[Int]()
-    matrix.indices.foreach(i => matrix.indices.foreach(j => {
-      q.enqueue(matrix(i)(j))
-      if (q.size > k) q.dequeue
-    }))
-    q.head
+    val sorted = matrix.flatten.sorted
+    sorted(sorted.length - (sorted.length - k) - 1)
   }
 }
