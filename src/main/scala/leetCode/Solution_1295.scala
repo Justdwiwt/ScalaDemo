1e6a5d9368c90eed0de38eab45c6a2c41b14d6c1
Justdwiwt <len_master@hotmail.com>
2021-07-16 14:25:43 +0800

    leetCode 1295

--- src/main/scala/leetCode/Solution_1295.scala
+++ src/main/scala/leetCode/Solution_1295.scala
@@ -1,9 +1,6 @@
 package leetCode
 
 object Solution_1295 {
-  def findNumbers(nums: Array[Int]): Int = {
-    var res = 0
-    nums.foreach(i => if (i.toString.length % 2 == 0) res += 1)
-    res
-  }
+  def findNumbers(nums: Array[Int]): Int =
+    nums.count(_.toString.length % 2 == 0)
 }
