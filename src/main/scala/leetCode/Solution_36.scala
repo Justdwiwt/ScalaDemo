062d32fcc1f0011f906132ed86716dc8284182af
Justdwiwt <len_master@hotmail.com>
2021-07-17 13:56:55 +0800

    leetCode 36

--- src/main/scala/leetCode/Solution_36.scala
+++ src/main/scala/leetCode/Solution_36.scala
@@ -1,22 +1,9 @@
 package leetCode
 
 object Solution_36 {
-  def isValidSudoku(board: Array[Array[Char]]): Boolean = {
-    var parts = List[List[Char]]()
-    (0 until 9).foreach(i => parts ::= board(i).toList)
-    (0 until 9).foreach(j => {
-      var tmp = List[Char]()
-      (0 until 9).foreach(i => tmp ::= board(i)(j))
-      parts ::= tmp
-    })
-    val tmp = List((0, 0), (3, 0), (6, 0), (0, 3), (0, 6), (3, 3), (3, 6), (6, 3), (6, 6))
-    tmp.foreach(i => {
-      var tmp = List[Char]()
-      (0 until 3).foreach(j => (0 until 3).foreach(k => tmp ::= board(i._1 + j)(i._2 + k)))
-      parts ::= tmp
-    })
-    parts.map(i => i.filter(_ != '.')).forall(valid)
-  }
-
-  def valid(list: List[Char]): Boolean = list.length == list.distinct.length
+  def isValidSudoku(board: Array[Array[Char]]): Boolean = (0 until 9)
+    .flatMap(i => (0 until 9).withFilter(j => board(i)(j) != '.').map(j => (i, j, j / 3 + ((i / 3) * 3))))
+    .groupBy(p => board(p._1)(p._2))
+    .valuesIterator
+    .forall(g => g.unzip3.productIterator.asInstanceOf[Iterator[Vector[Int]]].forall(_.groupBy(identity).values.forall(_.size == 1)))
 }
