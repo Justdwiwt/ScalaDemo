ce52e8346b63d726df5e37d4b478d547039f4a16
Justdwiwt <len_master@hotmail.com>
2021-07-18 14:22:12 +0800

    leetCode 927

--- src/main/scala/leetCode/Solution_927.scala
+++ src/main/scala/leetCode/Solution_927.scala
@@ -1,30 +1,22 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_927 {
-  def threeEqualParts(array: Array[Int]): Array[Int] = {
-    val power = BigInt(2).pow _
-    if (array.length < 3) return Array(-1, -1)
-    if (array.length == 3) return if (array(0) == array(1) && array(1) == array(2)) Array(0, 2) else Array(-1, -1)
-    var start = 0
-    while (start < array.length && array(start) == 0) start += 1
-    if (start >= array.length) return Array(0, array.length - 2)
-    val preSumTab = Array.ofDim[BigInt](array.length)
-    preSumTab(start) = if (array(start) == 1) power(array.length - 1 - start) else 0
-    (start + 1 until array.length).foreach(i => preSumTab(i) = preSumTab(i - 1) + (if (array(i) == 1) power(array.length - i - 1) else 0))
-    val firstMap = mutable.Map[BigInt, Int]()
-    (start to array.length - 3).foreach(i => {
-      val t: BigInt = preSumTab(i) / power(array.length - i - 1)
-      firstMap += t -> i
-    })
-    (array.length - 1 to start + 2 by -1).foreach(i => {
-      val t: BigInt = preSumTab(array.length - 1) - preSumTab(i - 1)
-      if (firstMap.contains(t)) {
-        val left = firstMap(t)
-        if (((preSumTab(i - 1) - preSumTab(left)) / power(array.length - i)) == t) return Array(left, i)
-      }
-    })
-    Array(-1, -1)
+  def findEndIdx(arr: Array[Int], i: Int, binaryValue: String): Int = {
+    if (arr.drop(i).dropWhile(_ == 0).take(binaryValue.length)./:("")((b, a) => a + b) != binaryValue) return -1
+    arr.drop(i).takeWhile(_ == 0).length + i + binaryValue.length
+  }
+
+  def threeEqualParts(arr: Array[Int]): Array[Int] = {
+    val cnt = arr.count(_ == 1)
+    val n = arr.length
+    if (cnt % 3 != 0) return Array(-1, -1)
+    if (cnt == 0) return Array(0, n - 1)
+    val idxThird = arr.zipWithIndex.filter(n => n._1 == 1).takeRight(cnt / 3).take(1).head._2
+    val binaryValue = arr.takeRight(n - idxThird)./:("")((b, a) => a + b)
+    val res1 = findEndIdx(arr, 0, binaryValue)
+    if (res1 < 0) return Array(-1, -1)
+    val res2 = findEndIdx(arr, res1, binaryValue)
+    if (res2 < 0) return Array(-1, -1)
+    Array(res1 - 1, res2)
   }
 }
