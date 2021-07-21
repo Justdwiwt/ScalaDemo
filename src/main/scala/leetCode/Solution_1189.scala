ca60c824547a8a5596e8417f95c92bd433278095
Justdwiwt <len_master@hotmail.com>
2021-07-21 13:43:54 +0800

    leetCode 1189

--- src/main/scala/leetCode/Solution_1189.scala
+++ src/main/scala/leetCode/Solution_1189.scala
@@ -1,13 +1,9 @@
 package leetCode
 
 object Solution_1189 {
-  def maxNumberOfBalloons(text: String): Int = {
-    val arr = new Array[Int](26)
-    text.toCharArray.foreach(i => arr(i - 97) += 1)
-    arr('l' - 97) /= 2
-    arr('o' - 97) /= 2
-    var res = Int.MaxValue
-    "balon".toCharArray.foreach(i => if (arr(i - 97) < res) res = arr(i - 97))
-    res
-  }
+  def maxNumberOfBalloons(text: String): Int = "balon"
+    .toList
+    .zip(List(1, 1, 2, 2, 1))
+    .map(p => text.count(_ == p._1) / p._2)
+    .min
 }
