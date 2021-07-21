6a3cca5bde5faa0bffd265c496d00186f3d0e924
Justdwiwt <len_master@hotmail.com>
2021-07-21 13:55:45 +0800

    leetCode 784

--- src/main/scala/leetCode/Solution_784.scala
+++ src/main/scala/leetCode/Solution_784.scala
@@ -1,27 +1,11 @@
 package leetCode
 
-import scala.collection.mutable.ListBuffer
-
 object Solution_784 {
-  def letterCasePermutation(S: String): List[String] = {
-    val res = new ListBuffer[String]
-    func(S.toCharArray, 0, res)
-    res.indices.foreach(i => i.toString)
-    res.toList
-  }
+  def letterCasePermutation(S: String): List[String] = f(S.toList, "")
 
-  def func(s: Array[Char], i: Int, res: ListBuffer[String]): Unit = {
-    if (i == s.length) {
-      res.append(s.toString)
-      return
-    }
-    func(s, i + 1, res)
-    if (isAlpha(s(i))) {
-      //      s(i).toInt ^= (1 << 5)   upper <=> lower
-      println(s)
-      func(s, i + 1, res)
-    }
+  def f(S: List[Char], acc: String): List[String] = S match {
+    case Nil => List(acc.reverse)
+    case h :: t if h.isLetter => f(t, h.toUpper + acc) ::: f(t, h.toLower + acc)
+    case h :: t => f(t, h + acc)
   }
-
-  def isAlpha(c: Char): Boolean = (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
 }
