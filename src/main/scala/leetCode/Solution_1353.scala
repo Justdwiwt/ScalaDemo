435686a7cf85d812ecce51fb77a80601836d4a0e
Justdwiwt <len_master@hotmail.com>
2021-07-22 13:49:39 +0800

    leetCode 1353

--- src/main/scala/leetCode/Solution_1353.scala
+++ src/main/scala/leetCode/Solution_1353.scala
@@ -1,20 +1,35 @@
 package leetCode
 
-import scala.collection.mutable
-import scala.util.control.Breaks._
-
 object Solution_1353 {
+  case class Node(start: Int, end: Int, left: Node, right: Node, var flag: Boolean = true)
+
   def maxEvents(events: Array[Array[Int]]): Int = {
-    val t = events.sorted((o1: Array[Int], o2: Array[Int]) => if (o1(1) != o2(1)) o1(1) - o2(1) else o1(0) - o2(0))
-    val s = new mutable.HashSet[Int]()
-    t.foreach(event => breakable {
-      (event(0) to event(1)).foreach(i => {
-        if (!s.contains(i)) {
-          s.add(i)
-          break()
-        }
-      })
-    })
-    s.size
+    val sorted = events.sortWith(_ (1) < _ (1))
+    val mn = events.minBy(_.head)
+    val mx = events.maxBy(_ (1))
+    val root = build(mn.head, mx(1))
+    var res = 0
+    sorted.foreach(x => if (attend(root, x.head, x(1))) res += 1)
+    res
+  }
+
+  def attend(node: Node, start: Int, end: Int): Boolean = {
+    if (node == null || start > node.end || node.start > end || !node.flag) return false
+    if (node.start == node.end) {
+      node.flag = false
+      return true
+    }
+    var res = attend(node.left, start, end)
+    if (!res) res = attend(node.right, start, end)
+    node.flag = node.left.flag || node.right.flag
+    res
+  }
+
+  def build(start: Int, end: Int): Node = {
+    if (start < end) {
+      val a = (start + end) / 2
+      return Node(start, end, build(start, a), build(a + 1, end))
+    }
+    Node(start, end, null, null)
   }
 }
