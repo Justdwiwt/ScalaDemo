85cceb0c588f2290e0e6e0bc0966009cc90c915c
Justdwiwt <len_master@hotmail.com>
2021-07-24 14:30:18 +0800

    leetCode 821

--- src/main/scala/leetCode/Solution_821.scala
+++ src/main/scala/leetCode/Solution_821.scala
@@ -1,18 +1,11 @@
 package leetCode
 
+import scala.collection.mutable.ArrayBuffer
+
 object Solution_821 {
   def shortestToChar(S: String, C: Char): Array[Int] = {
-    val N = S.length
-    val ans = Array.fill(N)(Int.MaxValue)
-    val posE = S.zipWithIndex.filter(_._1 == C).map(_._2)
-    var step = 0
-    while (ans.max == Int.MaxValue) {
-      posE.foreach(idx => {
-        if (idx + step < N) ans(idx + step) = ans(idx + step) min step
-        if (idx - step >= 0) ans(idx - step) = ans(idx - step) min step
-      })
-      step += 1
-    }
-    ans
+    val m = new ArrayBuffer[Int]()
+    S.zipWithIndex.foreach(p => if (p._1 == C) m += p._2)
+    S.zipWithIndex.map(p => m.map(i => (i - p._2).abs).min).toArray
   }
 }
