8b3e8c8e545a97032b40ec9f200c53d2c3c8fa17
Justdwiwt <len_master@hotmail.com>
2021-07-26 15:58:48 +0800

    leetCode 1894

---
+++ src/main/scala/leetCode/Solution_1894.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_1894 {
+  def chalkReplacer(ch: Array[Int], _k: Int): Int = {
+    var k = _k % ch.map(_.toLong).sum
+    ch.indices.foreach(i => if (k >= ch(i)) k -= ch(i) else return i)
+    -1
+  }
+}
