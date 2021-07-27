9878bf3023c038f732ad643d1cd061acdb7bbeeb
Justdwiwt <len_master@hotmail.com>
2021-07-27 15:08:17 +0800

    leetCode 16,476

--- src/main/scala/leetCode/Solution_16.scala
+++ src/main/scala/leetCode/Solution_16.scala
@@ -1,26 +1,34 @@
 package leetCode
 
-import scala.math.abs
-import scala.util.Sorting.quickSort
-
 object Solution_16 {
-  def threeSumClosest(nums: Array[Int], target: Int): Int = {
-    var res: Long = Int.MaxValue
-    quickSort(nums)
-    nums.indices.foreach(i => {
-      var pStart = i + 1
-      var pEnd = nums.length - 1
-      while (pStart < pEnd) {
-        val sum = nums(i) + nums(pStart) + nums(pEnd)
-        if (sum == target) {
-          pStart += 1
-          pEnd -= 1
-        }
-        else if (sum > target) pEnd -= 1
-        else pStart += 1
-        if (abs(sum - target) < abs(res - target)) res = sum
+  def threeSumClosest(nums: Array[Int], target: Int): Int =
+    find(nums, target)
+
+  @scala.annotation.tailrec
+  def find(nums: Array[Int], target: Int, idx: Int = 0, checked: Set[Int] = Set(), closest: Int = Int.MaxValue): Int =
+    if (idx >= nums.length - 1) closest
+    else {
+      val item = nums(idx)
+      if (checked.contains(item)) find(nums, target, idx + 1, checked, closest)
+      else {
+        val newClosest = checkTail(item, nums, target, closest, idx + 1)
+        find(nums, target, idx + 1, checked + item, newClosest)
       }
-    })
-    res.toInt
+    }
+
+  @scala.annotation.tailrec
+  def checkTail(item: Int, nums: Array[Int], target: Int, closest: Int, idx: Int): Int =
+    if (idx >= nums.length) closest
+    else {
+      val next = nums(idx)
+      val foundClosest = checkInArray(item + next, nums, target, closest, idx + 1)
+      checkTail(item, nums, target, foundClosest, idx + 1)
+    }
+
+  @scala.annotation.tailrec
+  def checkInArray(sumPart: Int, array: Array[Int], target: Int, closest: Int, idx: Int): Int = {
+    if (idx >= array.length) closest
+    else if ((target - (sumPart + array(idx))).abs < (target - closest).abs || closest == Int.MaxValue) checkInArray(sumPart, array, target, sumPart + array(idx), idx + 1)
+    else checkInArray(sumPart, array, target, closest, idx + 1)
   }
 }
--- src/main/scala/leetCode/Solution_476.scala
+++ src/main/scala/leetCode/Solution_476.scala
@@ -1,5 +1,6 @@
 package leetCode
 
 object Solution_476 {
-  def findComplement(num: Int): Int = (1 - num % 2) + 2 * (if (num <= 1) 0 else findComplement(num / 2))
+  def findComplement(num: Int): Int =
+    ~num & (Integer.highestOneBit(num) - 1)
 }
