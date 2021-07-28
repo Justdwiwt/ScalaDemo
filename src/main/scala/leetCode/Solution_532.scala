ed5047fe6feabcd6c0d9dd09268eb29fd0907c6e
Justdwiwt <len_master@hotmail.com>
2021-07-28 15:12:44 +0800

    leetCode 532

--- src/main/scala/leetCode/Solution_532.scala
+++ src/main/scala/leetCode/Solution_532.scala
@@ -1,11 +1,14 @@
 package leetCode
 
 object Solution_532 {
-  def findPairs(nums: Array[Int], k: Int): Int =
-    if (k < 0) 0
-    else if (k == 0) nums.groupBy(identity).count(_._2.length > 1)
-    else {
-      val s = nums.toSet
-      s.count(n => s.contains(n + k))
+  def findPairs(nums: Array[Int], k: Int): Int = {
+    @scala.annotation.tailrec
+    def f(remaining: List[Int], pairs: Set[(Int, Int)]): Set[(Int, Int)] = remaining match {
+      case Nil => pairs
+      case head :: tail if tail.contains(head + k) => f(tail, pairs + ((head, head + k)))
+      case _ :: tail => f(tail, pairs)
     }
+
+    f(nums.sorted.toList, Set.empty).size
+  }
 }
