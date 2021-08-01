f88f403628eda1229e1f27a143d2f6b86d10c55c
Justdwiwt <len_master@hotmail.com>
2021-08-01 13:56:21 +0800

    leetCode 1818

--- src/main/scala/leetCode/Solution_1818.scala
+++ src/main/scala/leetCode/Solution_1818.scala
@@ -1,25 +1,32 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1818 {
+  val M = 1000000007
+
   def minAbsoluteSumDiff(nums1: Array[Int], nums2: Array[Int]): Int = {
-    val ts = new mutable.TreeSet[Int]()((x, y) => Integer.compare(x, y))
-    nums1.foreach(ts.add)
+    val sorted = nums1.sorted
 
-    val total: Long = nums1.indices.map(i => math.abs(nums1(i) - nums2(i)).toLong).sum
+    @scala.annotation.tailrec
+    def findBefore(l: Int, r: Int, target: Int): Int =
+      if (l > r) -1
+      else if (sorted(r) <= target) r
+      else {
+        val mid = (l + r) >>> 1
+        if (sorted(mid) <= target) findBefore(mid, r - 1, target)
+        else findBefore(l, mid - 1, target)
+      }
 
-    //    (nums1.indices.map(i => math.min(
-    //      ts.maxBefore(nums2(i)) match {
-    //        case Some(x) => math.abs(x - nums2(i)).toLong
-    //        case _ => Long.MaxValue
-    //      },
-    //      ts.minAfter(nums2(i)) match {
-    //        case Some(x) => math.abs(x - nums2(i)).toLong
-    //        case _ => Long.MaxValue
-    //      }
-    //    ) + total - math.abs(nums1(i) - nums2(i))
-    //    ).min % (math.pow(10, 9) + 7)).toInt
-    0
+    var mx = 0
+    var sum = 0
+    nums1.zip(nums2).foreach({ case (num1, num2) =>
+      val abs = (num1 - num2).abs
+      sum = (sum + abs) % M
+      val before = findBefore(0, sorted.length - 1, num2)
+      if (before != -1) mx = mx.max(abs - (num2 - sorted(before)).abs)
+      if (before != sorted.length - 1) mx = mx.max(abs - (num2 - sorted(before + 1)).abs)
+    })
+
+    val newDiff = sum - mx
+    if (newDiff < 0) M + newDiff else newDiff
   }
 }
