2a824aaf82a9f5e5b101a7b43ebb65a3e5e16da6
Justdwiwt <len_master@hotmail.com>
2021-08-03 15:53:15 +0800

    leetCode 912

--- src/main/scala/leetCode/Solution_912.scala
+++ src/main/scala/leetCode/Solution_912.scala
@@ -1,5 +1,6 @@
 package leetCode
 
 object Solution_912 {
-  def sortArray(nums: Array[Int]): Array[Int] = nums.sorted
+  def sortArray(nums: Array[Int]): Array[Int] =
+    collection.mutable.PriorityQueue(nums: _*)(Ordering.by(-_)).dequeueAll.toArray
 }
