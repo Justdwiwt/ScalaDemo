cbc66de2c72aa255f8068f918719c729ffefd092
Justdwiwt <len_master@hotmail.com>
2021-08-08 14:13:39 +0800

    leetCode 1945

---
+++ src/main/scala/leetCode/Solution_1945.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_1945 {
+  def getLucky(s: String, k: Int): Int = {
+    var res = s.map(_ - 'a' + 1).mkString.toArray.map(_ - '0')
+    (1 until k).foreach(_ => res = res.sum.toString.map(_ - '0').toArray)
+    res.sum
+  }
+}
