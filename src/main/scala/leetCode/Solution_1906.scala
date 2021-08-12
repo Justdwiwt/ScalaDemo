ac8b6ab5c9d1127ad13a00ff64eb06528d5434a4
Justdwiwt <len_master@hotmail.com>
2021-08-12 11:03:25 +0800

    leetCode 954,1899,1901,1906,1937

---
+++ src/main/scala/leetCode/Solution_1906.scala
@@ -0,0 +1,29 @@
+package leetCode
+
+object Solution_1906 {
+  def minDifference(nums: Array[Int], queries: Array[Array[Int]]): Array[Int] = {
+    val arr = Array.ofDim[Int](nums.length, 101)
+    arr.head(nums.head) = 1
+    nums.indices.drop(1).foreach(i => {
+      (1 until 101).foreach(j => arr(i)(j) = arr(i - 1)(j))
+      arr(i)(nums(i)) += 1
+    })
+    queries.map(q => {
+      val l = q.head
+      val r = q(1)
+      var first, second = -1
+      var mn = Int.MaxValue
+      (1 until 101)
+        .filter(i => (arr(r)(i) - (if (l == 0) 0 else arr(l - 1)(i))) > 0)
+        .foreach(i =>
+          if (first == -1) first = i
+          else {
+            second = i
+            mn = mn.min(second - first)
+            first = second
+          }
+        )
+      if (mn == Int.MaxValue) -1 else mn
+    })
+  }
+}
--- src/main/scala/leetCode/Solution_954.scala
+++ src/main/scala/leetCode/Solution_954.scala
@@ -2,24 +2,17 @@ package leetCode
 
 object Solution_954 {
   def canReorderDoubled(arr: Array[Int]): Boolean = {
-    val M = 100000
-    val pos = Array.fill(M + 1)(0)
-    val neg = Array.fill(M + 1)(0)
-    var cnt = 0
-    arr.foreach(v => {
-      if (v > 0) pos(v) += 1
-      else if (v < 0) neg(-v) += 1
-      else cnt += 1
-    })
-    if ((cnt & 1) == 1) return false
-    (1 until M).foreach(i => {
-      if (pos(i) > 0)
-        if (i * 2 > M || pos(i * 2) < pos(i)) return false
-        else pos(i * 2) -= pos(i)
-      if (neg(i) > 0)
-        if (i * 2 > M || neg(i * 2) < neg(i)) return false
-        else neg(i * 2) -= neg(i)
-    })
-    true
+    @scala.annotation.tailrec
+    def f(xs: Array[Int]): Boolean = {
+      if (xs.isEmpty) return true
+      val i = xs.indexOf(xs.head * 2)
+      if (i == -1) false
+      else f(xs.slice(1, i) ++ xs.slice(i + 1, xs.length))
+    }
+
+    val pos = arr.filter(_ >= 0).sorted
+    val neg = arr.filter(_ < 0).sorted.reverse
+    if (pos.length % 2 != 0 || neg.length % 2 != 0) return false
+    f(pos) && f(neg)
   }
 }
