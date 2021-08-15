0bf2243bb31674c2cd59bd67a1f06941b517c399
Justdwiwt <len_master@hotmail.com>
2021-08-15 15:05:50 +0800

    leetCode 1200

--- src/main/scala/leetCode/Solution_1200.scala
+++ src/main/scala/leetCode/Solution_1200.scala
@@ -2,18 +2,9 @@ package leetCode
 
 object Solution_1200 {
   def minimumAbsDifference(arr: Array[Int]): List[List[Int]] = {
-    val t = arr.sorted
-    var res = List[List[Int]]()
-    var diff = t(t.length - 1) - t(0)
-    (1 until t.length).foreach(i => diff = diff.min(t(i) - t(i - 1)))
-    (1 until t.length).foreach(i => {
-      var tmp = List[Int]()
-      if (diff == t(i) - t(i - 1)) {
-        tmp :+= t(i - 1)
-        tmp :+= t(i)
-        res :+= tmp
-      }
-    })
-    res
+    val sorted = arr.sorted
+    val res = sorted.zip(sorted.slice(1, sorted.length))
+    val mn = res.map({ case (a, b) => b - a }).min
+    res.filter({ case (a, b) => (b - a) == mn }).map(t => List(t._1, t._2)).toList
   }
 }
