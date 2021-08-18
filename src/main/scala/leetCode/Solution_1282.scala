3bdbf0930057a6068de3b76c0396f6731b25f7cf
Justdwiwt <len_master@hotmail.com>
2021-08-18 14:40:23 +0800

    leetCode 552,1282

--- src/main/scala/leetCode/Solution_1282.scala
+++ src/main/scala/leetCode/Solution_1282.scala
@@ -1,37 +1,11 @@
 package leetCode
 
-import scala.collection.mutable.ListBuffer
-import scala.util.control.Breaks._
-
 object Solution_1282 {
-  def groupThePeople(groupSizes: Array[Int]): List[List[Int]] = {
-    var res = List.empty[List[Int]]
-    if (groupSizes.length != 0) {
-      groupSizes.indices.foreach(i => {
-        var num = groupSizes(i)
-        breakable {
-          if (num == 0) break
-        }
-        if (num == 1) {
-          var tmp = List(i)
-          res :+= tmp
-        } else {
-          val tmp = ListBuffer(num)
-          num -= 1
-          tmp(num) = i
-          var j = i + 1
-          while (j < groupSizes.length && num > 0) {
-            if (groupSizes(i) == groupSizes(j)) {
-              groupSizes(j) = 0
-              num -= 1
-              tmp(num) = j
-            }
-            j += 1
-          }
-          res :+= tmp.toList
-        }
-      })
-    }
-    res
-  }
+  def groupThePeople(groupSizes: Array[Int]): List[List[Int]] = groupSizes
+    .zipWithIndex
+    ./:(Map.empty[Int, List[Int]])((res, idx) => {
+      val group = idx._2 :: res.getOrElse(idx._1, List.empty)
+      res + (idx._1 -> group)
+    })
+    ./:(List.empty[List[Int]])((res, entry) => res ++ entry._2.grouped(entry._1))
 }
--- src/main/scala/leetCode/Solution_552.scala
+++ src/main/scala/leetCode/Solution_552.scala
@@ -1,49 +1,15 @@
 package leetCode
 
-//import scala.collection.IterableOps
-
 object Solution_552 {
-
-  // scala 2.13
-  //  private implicit class RichIterable[A, CC[_]](lhs: IterableOps[A, CC, _]) {
-  //
-  //    def x[B](rhs: Iterable[B]): CC[(A, B)] = lhs.flatMap(l => rhs.map(l -> _))
-  //  }
-  //
-  //  private object Attendance extends Enumeration {
-  //    type Attendance = Value
-  //    val Present, Absent, Late = Value
-  //  }
-  //
-  //  import Attendance._
-  //
-  //  private lazy val BaseCounts = (values x values)
-  //    .flatMap({
-  //      case (Absent, Absent) => None
-  //      case (Absent, a) => Some((Absent, a, true))
-  //      case (a, Absent) => Some((a, Absent, true))
-  //      case (a1, a2) => Some((a1, a2, false))
-  //    })
-  //    .map(_ -> 1)
-  //    .toMap
-  //
-  //  private def add(x: Int, y: Int) = (x + y) % 1000000007
-  //
-  //  def checkRecord(n: Int): Int = {
-  //    if (n == 0) 1
-  //    else if (n == 1) 3
-  //    else if (n >= 2) (3 to n).foldLeft(BaseCounts) {
-  //      case (counts, _) => counts.toSeq.flatMap({
-  //        case (Late, Late, true) -> count =>
-  //          Seq((Late, Present, true) -> count)
-  //        case (Late, Late, false) -> count =>
-  //          Seq((Late, Present, false), (Late, Absent, true)).map(_ -> count)
-  //        case (_, a, true) -> count =>
-  //          Seq((a, Present, true), (a, Late, true)).map(_ -> count)
-  //        case (_, a, false) -> count =>
-  //          Seq((a, Present, false), (a, Late, false), (a, Absent, true)).map(_ -> count)
-  //      }).groupMapReduce(_._1)(_._2)(add)
-  //    }.values.reduce(add)
-  //    else throw new IllegalArgumentException
-  //  }
+  def checkRecord(n: Int): Int = {
+    val dp = Array.fill(n + 2)(0L)
+    dp(0) = 1
+    dp(1) = 2
+    dp(2) = 4
+    val M = (1e9 + 7).toLong
+    var res = 0L
+    (3 to n).foreach(i => dp(i) = (dp(i - 1) + dp(i - 2) + dp(i - 3)) % M)
+    (0 until n).foreach(i => res = (res + dp(i) * dp(n - 1 - i)) % M)
+    ((res + dp(n)) % M).toInt
+  }
 }
