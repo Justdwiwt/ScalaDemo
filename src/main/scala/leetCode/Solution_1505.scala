ab829c7f9a8c2235d0404c93d8883580f8f14f3f
Justdwiwt <len_master@hotmail.com>
2021-08-19 15:24:38 +0800

    leetCode 1505

--- src/main/scala/leetCode/Solution_1505.scala
+++ src/main/scala/leetCode/Solution_1505.scala
@@ -2,13 +2,36 @@ package leetCode
 
 object Solution_1505 {
   def minInteger(num: String, k: Int): String = {
-    if (k == 0) return num
-    if (k > num.length * (num.length - 1) / 2) return num.sorted
-    ('0' to '9').foreach(c => {
-      val i = num.indexOf(c)
-      if (i >= 0 && i <= k)
-        return c + minInteger(num.substring(0, i) + num.substring(i + 1), k - i)
+    val digitPositions = Array.fill[Int](10)(num.length)
+    num.indices.foreach(i => {
+      val a = num(i) - '0'
+      digitPositions(a) = i.min(digitPositions(a))
     })
-    num
+    val shifts = Array.ofDim[Int](10)
+    val minNumSb = new StringBuilder
+    var t = k
+    var i = 0
+    while (i < num.length) {
+      var d = 0
+      while (digitPositions(d) == num.length || digitPositions(d) - shifts(d) > t) d += 1
+      val digit = d
+      val digitChar = (digit + '0').toChar
+      minNumSb.append(digitChar)
+      t -= digitPositions(d) - shifts(d)
+      d = 0
+      while (d < 10) {
+        if (digitPositions(d) >= digitPositions(digit)) shifts(d) += 1
+        d += 1
+      }
+      var j = digitPositions(digit) + 1
+      while (j < num.length && num(j) != digitChar) {
+        val a = num(j) - '0'
+        if (digitPositions(a) > j) shifts(digit) += 1
+        j += 1
+      }
+      digitPositions(digit) = j
+      i += 1
+    }
+    minNumSb.mkString
   }
 }
