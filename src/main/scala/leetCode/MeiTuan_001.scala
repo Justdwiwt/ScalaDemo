8cd42abad4ffb7ae8088f0dabe7fd5125601d010
Justdwiwt <len_master@hotmail.com>
2021-08-28 17:18:57 +0800

    leetCode 2021美团校招 001,002

---
+++ src/main/scala/leetCode/MeiTuan_001.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+import java.util.Scanner
+
+object MeiTuan_001 {
+  def isValid(userName: String): Boolean =
+    userName.matches("[a-zA-Z]+\\d+[a-zA-Z0-9]*")
+
+  def main(args: Array[String]): Unit = {
+    val sc = new Scanner(System.in)
+    var cnt = sc.nextInt()
+    sc.nextLine()
+    while (cnt != 0) {
+      println(if (isValid(sc.nextLine())) "Accept" else "Wrong")
+      cnt -= 1
+    }
+  }
+}
---
+++ src/main/scala/leetCode/MeiTuan_002.scala
@@ -0,0 +1,54 @@
+package leetCode
+
+import java.io.{BufferedReader, BufferedWriter, InputStreamReader, OutputStreamWriter}
+import java.util
+import scala.util.control.Breaks._
+
+//noinspection ComparingUnrelatedTypes
+object MeiTuan_002 {
+  def main(args: Array[String]): Unit = {
+    val tm = new util.TreeMap[Int, Int]()
+    val ts = new util.TreeSet[Int]()
+    val prefix = Array.fill(50050)(0)
+
+    val br = new BufferedReader(new InputStreamReader(System.in))
+    val bw = new BufferedWriter(new OutputStreamWriter(System.out))
+
+    val n = br.readLine().toInt
+    val arr = br.readLine().split(" ")
+    val query = br.readLine().split(" ")
+
+    br.close()
+
+    (0 until n).foreach(i => prefix(i + 1) = prefix(i) + arr(i).toInt)
+
+    ts.add(0)
+    ts.add(n + 1)
+
+    (0 until n).foreach(i => {
+      val pos = query(i).toInt
+      val left = ts.lower(pos)
+      val right = ts.higher(pos)
+
+      breakable(if (left == null || right == null) break())
+
+      val segSum = prefix(right - 1) - prefix(left)
+      val v = tm.get(segSum)
+
+      if (v != null) {
+        if (v == 1) tm.remove(segSum)
+        else tm.put(segSum, v - 1)
+      }
+
+      val leftSum = prefix(pos - 1) - prefix(left)
+      val rightSum = prefix(right - 1) - prefix(pos)
+
+      ts.add(pos)
+      tm.put(leftSum, tm.getOrDefault(leftSum, 0) + 1)
+      tm.put(rightSum, tm.getOrDefault(right, 0) + 1)
+
+      bw.write(tm.lastKey() + "\n")
+    })
+    bw.close()
+  }
+}
