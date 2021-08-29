28db3faaea9b6f35e61f5fc8bcc63d8f7ed495d9
Justdwiwt <len_master@hotmail.com>
2021-08-29 16:49:35 +0800

    leetCode LCP-28,LCP-29

---
+++ src/main/scala/leetCode/LCP_29.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object LCP_29 {
+  def orchestraLayout(num: Int, xPos: Int, yPos: Int): Int = {
+    val M = xPos.min(yPos).min(num - xPos - 1).min(num - yPos - 1).toLong
+    var start = 1
+    var cnt = ((num * M) - M * M) * 4
+    start = ((cnt + start - 1) % 9 + 1).toInt
+    val n = num - M * 2
+    cnt = (n - 1) * 4
+    var dist = xPos.toLong + yPos.toLong - 2 * M
+    if (xPos == n + M - 1 || (yPos == M && xPos != M)) dist = cnt - dist
+    ((dist + start - 1) % 9 + 1).toInt
+  }
+}
