cc05585b88c949f8cc870792617d07285615d998
Justdwiwt <len_master@hotmail.com>
2021-08-30 14:58:15 +0800

    leetCode LCP-30,LCP-31

---
+++ src/main/scala/leetCode/LCP_30.scala
@@ -0,0 +1,22 @@
+package leetCode
+
+import java.util.PriorityQueue
+
+object LCP_30 {
+  def magicTower(nums: Array[Int]): Int = {
+    val pq = new PriorityQueue[Int]()
+    var res = 0L
+    var cur = 1L
+    val sum = nums.sum.toLong
+    if (sum < 0) return -1
+    nums.foreach(x => {
+      cur += x
+      if (x < 0) pq.offer(x)
+      if (cur <= 0) {
+        cur -= pq.poll()
+        res += 1
+      }
+    })
+    res.toInt
+  }
+}
