57d85c26bb5325e7976dc12217fa7bf45c6ad34c
Justdwiwt <len_master@hotmail.com>
2021-08-31 15:40:43 +0800

    leetCode LCP-32,LCP-33

---
+++ src/main/scala/leetCode/LCP_33.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object LCP_33 {
+  def storeWater(bucket: Array[Int], vat: Array[Int]): Int = {
+    var mx = vat.head
+    bucket.indices.foreach(i => mx = mx.max(vat(i)))
+    if (mx == 0) return 0
+    var res = Int.MaxValue
+    (1 to mx).reverse.foreach(p => {
+      var in = 0
+      bucket.indices.foreach(i => {
+        val need = if (vat(i) % p == 0) vat(i) / p else vat(i) / p + 1
+        in += (if (need - bucket(i) >= 0) need - bucket(i) else 0)
+      })
+      res = res.min(in + p)
+    })
+    res
+  }
+}
