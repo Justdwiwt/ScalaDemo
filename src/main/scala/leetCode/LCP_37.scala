e443bbe365212b811a0e2f980397f2ec85444ac8
Justdwiwt <len_master@hotmail.com>
2021-09-03 15:06:28 +0800

    leetCode LCP-37

---
+++ src/main/scala/leetCode/LCP_37.scala
@@ -0,0 +1,44 @@
+package leetCode
+
+object LCP_37 {
+  def minRecSize(lines: Array[Array[Int]]): Double = {
+    var mxX = Float.MinValue
+    var mxY = Float.MinValue
+    var mnX = Float.MaxValue
+    var mnY = Float.MaxValue
+
+    if (lines.length <= 2) return 0
+
+    val mnDic = Array.fill(10000 + 1)(Int.MaxValue)
+    val mxDix = Array.fill(10000 + 1)(Int.MinValue)
+
+    var ks = Array.emptyIntArray
+
+    lines.foreach(li => {
+      val k = li.head
+      val b = li(1)
+      if (mnDic(k) == Int.MaxValue) ks :+= k
+      mnDic(k) = mnDic(k).min(b)
+      mxDix(k) = mxDix(k).max(b)
+    })
+
+    if (ks.length == 1) return 0
+
+    ks = ks.sorted
+
+    ks.indices.drop(1).foreach(i => {
+      val curK = ks(i)
+      val mnCurB = mnDic(curK)
+      val mxCurB = mxDix(curK)
+      val lastK = ks(i - 1)
+      val mnLastB = mnDic(lastK)
+      val mxLastB = mxDix(lastK)
+      val diff = (lastK - curK).toFloat
+      mnX = mnX.min((mxCurB - mnLastB) / diff)
+      mxX = mxX.max((mnCurB - mxLastB) / diff)
+      mnY = mnY.min((lastK * mxCurB - curK * mnLastB) / diff)
+      mxY = mxY.max((lastK * mnCurB - curK * mxLastB) / diff)
+    })
+    (mxX - mnX) * (mxY - mnY)
+  }
+}
