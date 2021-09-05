9a8f6cf5d8d54258fd922d0c2122fa8be86ebec1
Justdwiwt <len_master@hotmail.com>
2021-09-05 14:59:15 +0800

    leetCode 1805

---
+++ src/main/scala/leetCode/Solution_1805.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_1805 {
+  def numDifferentIntegers(word: String): Int = word
+    .replaceAll("[a-z]", " ")
+    .split(" ")
+    .filter(_.trim.nonEmpty)
+    .map(BigInt(_))
+    .distinct
+    .length
+}
