3500b4d2e28803acf6c9109b98b76478976a11fb
Justdwiwt <len_master@hotmail.com>
2021-09-10 16:46:44 +0800

    leetCode 971

--- src/main/scala/leetCode/Solution_971.scala
+++ src/main/scala/leetCode/Solution_971.scala
@@ -2,30 +2,20 @@ package leetCode
 
 object Solution_971 {
   def flipMatchVoyage(root: TreeNode, voyage: Array[Int]): List[Int] = {
-    val res = solver(root, voyage)
-    if (res.contains(-1)) List(-1) else res
-  }
+    var cnt = 0
 
-  def solver(root: TreeNode, voyage: Array[Int]): List[Int] = root match {
-    case null => List.empty
-    case node: TreeNode if node.value != voyage.head => List(-1)
-    case node: TreeNode if node.left == null && node.right == null => List.empty
-    case node: TreeNode if node.left != null && node.right == null => solver(node.left, voyage.tail)
-    case node: TreeNode if node.left == null && node.right != null => solver(node.right, voyage.tail)
-    case node: TreeNode if node.left != null && node.right != null =>
-      val nl = countNode(root.left)
-      val nr = countNode(root.right)
-      val ml = root.left.value == voyage.tail.head
-      val mr = root.right.value == voyage.tail.head
-      (ml, mr) match {
-        case (true, false) => solver(root.left, voyage.slice(1, nl + 1)) ::: solver(root.right, voyage.slice(nl + 1, voyage.length))
-        case (false, true) => root.value :: solver(root.right, voyage.slice(1, nr + 1)) ::: solver(root.left, voyage.slice(nr + 1, voyage.length))
-        case _ => List(-1)
+    def f(root: TreeNode, res: List[Int]): List[Int] =
+      if (root == null) res
+      else if (root.value != voyage(cnt)) List(-1)
+      else if (root.left != null && root.left.value != voyage(cnt + 1)) {
+        cnt += 1
+        res ++: List(root.value) ++: f(root.right, res) ++: f(root.left, res)
+      } else {
+        cnt += 1
+        res ++: f(root.left, res) ++: f(root.right, res)
       }
-  }
 
-  def countNode(root: TreeNode): Int = root match {
-    case null => 0
-    case _ => 1 + countNode(root.left) + countNode(root.right)
+    val res = f(root, Nil)
+    if (res.contains(-1)) List(-1) else res
   }
 }
