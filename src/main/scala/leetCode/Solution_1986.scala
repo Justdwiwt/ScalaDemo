eb31626995b1b6ef2ea51ad507b8415efe5f6a6f
Justdwiwt <len_master@hotmail.com>
2021-09-17 17:23:20 +0800

    leetCode 1595,1599,1985,1986,1992

---
+++ src/main/scala/leetCode/Solution_1986.scala
@@ -0,0 +1,26 @@
+package leetCode
+
+import scala.Ordering.Implicits._
+
+object Solution_1986 {
+  def minSessions(tasks: Array[Int], sessionTime: Int): Int = {
+    val allTasks = (1 << tasks.length) - 1
+
+    val time = Array.fill(1 << tasks.length)((Int.MaxValue, 0))
+    time(0) = (0, sessionTime)
+
+    (1 to allTasks).foreach(tasksNow => tasks.indices
+      .map({ taskIdx => val task = 1 << taskIdx; (taskIdx, task) })
+      .withFilter({ case (_, task) => (tasksNow & task) != 0 })
+      .foreach({ case (taskIdx, task) =>
+        val (sessions, timeInSession) = time(tasksNow - task)
+        val timeNow =
+          if (timeInSession + tasks(taskIdx) <= sessionTime) (sessions, timeInSession + tasks(taskIdx))
+          else (sessions + 1, tasks(taskIdx))
+        time(tasksNow) = time(tasksNow).min(timeNow)
+      })
+    )
+
+    time(allTasks)._1
+  }
+}
---
+++ src/main/scala/leetCode/Solution_1992.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Solution_1992 {
+  def findFarmland(land: Array[Array[Int]]): Array[Array[Int]] = {
+    var res: List[Array[Int]] = List.empty[Array[Int]]
+    land.indices.foreach(i => land.head.indices
+      .withFilter(j => land(i)(j) == 1 && (i == 0 || land(i - 1)(j) == 0) && (j == 0 || land(i)(j - 1) == 0))
+      .foreach(j => {
+        var ri = i
+        while (ri + 1 < land.length && land(ri + 1)(j) == 1) ri += 1
+        var rj = j
+        while (rj + 1 < land(0).length && land(i)(rj + 1) == 1) rj += 1
+        res = Array(i, j, ri, rj) :: res
+      })
+    )
+    res.toArray
+  }
+}
