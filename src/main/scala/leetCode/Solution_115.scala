0ec97eba93d943fc5ff39ddc0ee68fa5b6701c93
Justdwiwt <len_master@hotmail.com>
2021-09-19 17:08:42 +0800

    leetCode 115

--- src/main/scala/leetCode/Solution_115.scala
+++ src/main/scala/leetCode/Solution_115.scala
@@ -2,11 +2,9 @@ package leetCode
 
 object Solution_115 {
   def numDistinct(s: String, t: String): Int = {
-    val dp = Array.fill(t.length + 1, s.length + 1)(0)
-    s.indices.foreach(i => dp(0)(i) = 1)
-    (1 to t.length).foreach(i => dp(i)(0) = 0)
-    (1 to t.length).foreach(i => (1 to s.length).foreach(j =>
-      dp(i)(j) = dp(i)(j - 1) + (if (t(i - 1) == s(j - 1)) dp(i - 1)(j - 1) else 0)))
-    dp(t.length)(s.length)
+    val dp = Array.ofDim[Int](t.length + 1)
+    dp(0) = 1
+    s.foreach(c => (1 to t.length).reverse.withFilter(i => t(i - 1) == c).foreach(i => dp(i) += dp(i - 1)))
+    dp.last
   }
 }
