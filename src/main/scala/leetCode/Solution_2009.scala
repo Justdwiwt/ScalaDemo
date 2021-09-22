a3234c6f76dee66e895a3060aeed3bbad48d04ee
Justdwiwt <len_master@hotmail.com>
2021-09-22 16:41:46 +0800

    leetCode 2009

---
+++ src/main/scala/leetCode/Solution_2009.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Solution_2009 {
+  def minOperations(nums: Array[Int]): Int = {
+    val sorted = nums.distinct.sorted
+    sorted.indices./:(nums.length, 0) { case ((mn, end), start) =>
+      val newEnd = (end until sorted.length).find(i => sorted(i) >= sorted(start) + nums.length).getOrElse(sorted.length)
+      val need = nums.length - (newEnd - start)
+      (mn.min(need), newEnd)
+    }._1
+  }
+}
