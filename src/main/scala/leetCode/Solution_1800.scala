a2816998a1586a17185b3e3ba9d4e79dc3d81f55
Justdwiwt <len_master@hotmail.com>
2021-09-27 16:09:16 +0800

    leetCode 1800

---
+++ src/main/scala/leetCode/Solution_1800.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_1800 {
+  def maxAscendingSum(nums: Array[Int]): Int = nums
+    .indices
+    .drop(1)
+    ./:(nums.head, nums.head)((b, a) =>
+      if (nums(a) > nums(a - 1)) (b._1 + nums(a), b._2.max(b._1 + nums(a)))
+      else (nums(a), b._2.max(nums(a))))
+    ._2
+}
