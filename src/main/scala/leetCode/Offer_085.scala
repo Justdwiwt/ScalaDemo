e9cc25aff80739e030c82f7252c92e46a3b27fe3
Justdwiwt <len_master@hotmail.com>
2021-09-30 00:39:38 +0800

    leetCode 剑指Offer II 081,082,083,084,085

---
+++ src/main/scala/leetCode/Offer_085.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Offer_085 {
+  def generateParenthesis(n: Int): List[String] = n match {
+    case 0 => List("")
+    case n => (0 until n)
+      .toList
+      .flatMap(m => generateParenthesis(m)
+        .flatMap(x => generateParenthesis(n - 1 - m)
+          .map(y => "(" ++ x ++ ")" ++ y)))
+  }
+}
