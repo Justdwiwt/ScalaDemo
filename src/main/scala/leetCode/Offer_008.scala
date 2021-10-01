85744d1a0dcb4130b640db9eb1140456bbc5387a
Justdwiwt <len_master@hotmail.com>
2021-10-01 19:29:43 +0800

    leetCode 剑指Offer II 007,008,009

---
+++ src/main/scala/leetCode/Offer_008.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Offer_008 {
+  def minSubArrayLen(s: Int, nums: Array[Int]): Int = {
+
+    @annotation.tailrec
+    def func(left: Int, hi: Int, sum: Int, mn: Int): Int =
+      if (hi >= nums.length) {
+        if (left < nums.length && sum - nums(left) >= s) func(left + 1, hi, sum - nums(left), mn.min(hi - left))
+        else if (mn != Int.MaxValue || sum >= s) mn.min(hi - left)
+        else 0
+      }
+      else if (sum < s) func(left, hi + 1, sum + nums(hi), mn)
+      else func(left + 1, hi, sum - nums(left), mn.min(hi - left))
+
+    func(0, 0, 0, Int.MaxValue)
+  }
+}
