24b359b40790b97d5f2502b49b703331fc9a15c7
Justdwiwt <len_master@hotmail.com>
2021-10-02 18:24:22 +0800

    leetCode 剑指Offer II 010,011,012,013

---
+++ src/main/scala/leetCode/Offer_012.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Offer_012 {
+  def pivotIndex(nums: Array[Int]): Int = nums
+    .scanLeft(0)((sum, x) => sum + x)
+    .drop(1)
+    .zipWithIndex
+    .indexWhere({ case (l, idx) => nums.scanRight(0)((sum, x) => sum + x).dropRight(1)(idx) == l })
+}
