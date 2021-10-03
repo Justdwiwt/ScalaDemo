be10ea5854cbcc7d4987c39eec318d997bedfc1d
Justdwiwt <len_master@hotmail.com>
2021-10-03 19:05:50 +0800

    leetCode 剑指Offer II 014,015,016

---
+++ src/main/scala/leetCode/Offer_016.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Offer_016 {
+  def lengthOfLongestSubstring(s: String): Int = s
+    .toSeq
+    ./:((0, Seq.empty[Char])) { case ((mx, cur), c) =>
+      if (cur.contains(c)) mx.max(cur.size) -> (c +: cur.takeWhile(_ != c))
+      else mx.max(cur.size + 1) -> (c +: cur)
+    }._1
+}
