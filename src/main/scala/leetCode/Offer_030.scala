62b74684da761b84dc5ed159efe20ae77d4eccab
Justdwiwt <len_master@hotmail.com>
2021-10-08 18:39:22 +0800

    leetCode 剑指Offer II 030,031,032

---
+++ src/main/scala/leetCode/Offer_030.scala
@@ -0,0 +1,34 @@
+package leetCode
+
+import scala.collection.mutable
+import scala.util.Random
+
+object Offer_030 {
+  class RandomizedSet {
+    private val indices = mutable.Map.empty[Int, Int]
+    private val values = mutable.ListBuffer.empty[Int]
+
+    def insert(value: Int): Boolean = indices.get(value) match {
+      case Some(_) => false
+      case None =>
+        values.append(value)
+        indices.update(value, values.length - 1)
+        true
+    }
+
+    def remove(value: Int): Boolean = indices.get(value) match {
+      case None => false
+      case Some(idx) =>
+        val last = values.last
+        values.remove(values.length - 1)
+        indices.remove(value)
+        if (idx != values.length) {
+          values.update(idx, last)
+          indices.update(last, idx)
+        }
+        true
+    }
+
+    def getRandom(): Int = values(Random.nextInt(values.length))
+  }
+}
