e906086908557ba8a0049c286ed835562c225873
Justdwiwt <len_master@hotmail.com>
2021-10-10 19:00:56 +0800

    leetCode 剑指Offer II 036,037,038

---
+++ src/main/scala/leetCode/Offer_037.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Offer_037 {
+  def asteroidCollision(asteroids: Array[Int]): Array[Int] = {
+    @scala.annotation.tailrec
+    def f(arr: List[Int], ss: List[Int]): Array[Int] = (arr, ss) match {
+      case (Nil, ss) => ss.reverse.toArray
+      case (a :: tail, Nil) => f(tail, List(a))
+      case (a :: tail, ss) if a > 0 || ss.head < 0 => f(tail, a :: ss)
+      case (a :: tail, ss) if ss.head > -a => f(tail, ss)
+      case (a :: tail, ss) if ss.head == -a => f(tail, ss.tail)
+      case (_, ss) => f(arr, ss.tail)
+    }
+
+    f(asteroids.toList, Nil)
+  }
+}
