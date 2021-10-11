45399cccebdd0d2b05819f0d961444de45492aea
Justdwiwt <len_master@hotmail.com>
2021-10-11 19:39:43 +0800

    leetCode 剑指Offer II 039,040

---
+++ src/main/scala/leetCode/Offer_039.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Offer_039 {
+  def largestRectangleArea(heights: Array[Int]): Int = {
+    val seq = heights.toSeq :+ 0
+    seq.zipWithIndex./:((List.empty[(Int, Int, Int)], seq.head))({ case ((stack, mxH), (h, idx)) =>
+      val (hi, lo) = stack.span(_._1 > h)
+      val newMx = hi.map(Function.tupled((h: Int, lo: Int, _: Int) => (idx - lo) * h)).fold(mxH)(math.max)
+      val newLo = lo.headOption.map(_._3 + 1).getOrElse(0)
+      ((h, newLo, idx) :: lo, newMx)
+    })._2
+  }
+}
