d58f8becd284f72075a6118f1d3b7bb2a8f464c4
Justdwiwt <len_master@hotmail.com>
2021-10-12 16:08:18 +0800

    leetCode 346,919

--- src/main/scala/leetCode/Solution_346.scala
+++ src/main/scala/leetCode/Solution_346.scala
@@ -1,23 +1,23 @@
 package leetCode
 
-object Solution_346 {
+import scala.collection.mutable
 
+object Solution_346 {
   class MovingAverage(_size: Int) {
 
     private val size = _size
     private var sum = 0.0
-    private var q = List.empty[Int]
+    private val q = mutable.Queue.empty[Int]
 
     def next(`val`: Int): Double = {
-      q ::= `val`
-      if (q.size > size) {
-        sum -= q.head
-        q = q.tail
-      }
+      q += `val`
       sum += `val`
+      if (q.size > size) {
+        sum -= q.front
+        q.dequeue()
+      }
       sum / q.size
     }
 
   }
-
 }
