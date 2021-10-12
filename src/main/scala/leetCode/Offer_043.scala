aae6d8d6eb655ce7777b1bf52e491989d58472a4
Justdwiwt <len_master@hotmail.com>
2021-10-12 16:08:39 +0800

    leetCode 剑指Offer II 041,042,043

---
+++ src/main/scala/leetCode/Offer_043.scala
@@ -0,0 +1,31 @@
+package leetCode
+
+import java.util
+
+object Offer_043 {
+  class CBTInserter(_root: TreeNode) {
+    private val arr = new util.LinkedList[TreeNode]()
+    private val q = new util.LinkedList[TreeNode]()
+    q.add(_root)
+    while (!q.isEmpty) {
+      val t = q.remove()
+      if (t.left != null) q.add(t.left)
+      if (t.right != null) q.add(t.right)
+      arr.add(t)
+    }
+
+    def insert(v: Int): Int = {
+      val node = new TreeNode(v)
+      arr.add(node)
+      val p = arr.get(arr.size() / 2 - 1)
+      if (p.left == null) p.left = node
+      else p.right = node
+      p.value
+    }
+
+    def get_root(): TreeNode = {
+      arr.get(0)
+    }
+
+  }
+}
