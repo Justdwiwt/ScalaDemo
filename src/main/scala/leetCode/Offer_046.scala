cfbe7347af6d303df427d0a81a25e771c6f6d90b
Justdwiwt <len_master@hotmail.com>
2021-10-13 15:55:46 +0800

    leetCode 剑指Offer II 044,045,046

---
+++ src/main/scala/leetCode/Offer_046.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Offer_046 {
+  def rightSideView(root: TreeNode): List[Int] =
+    f(Option(root).toSeq)
+
+  @scala.annotation.tailrec
+  def f(seq: Seq[TreeNode], list: List[Int] = Nil): List[Int] =
+    if (seq.isEmpty) list
+    else f(seq.flatMap(node => Seq(Option(node.left), Option(node.right)).flatten), list :+ seq.last.value)
+}
