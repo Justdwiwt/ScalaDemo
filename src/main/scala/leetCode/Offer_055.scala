1f584068d57c13f051786699cd9885a9a4c0a7fb
Justdwiwt <len_master@hotmail.com>
2021-10-16 17:26:19 +0800

    leetCode 剑指Offer II 053,054,055

---
+++ src/main/scala/leetCode/Offer_055.scala
@@ -0,0 +1,21 @@
+package leetCode
+
+object Offer_055 {
+  class BSTIterator(_root: TreeNode) {
+    private var stack = pushLefts(_root, Nil)
+
+    @scala.annotation.tailrec
+    private def pushLefts(node: TreeNode, acc: List[TreeNode]): List[TreeNode] =
+      if (node == null) acc
+      else pushLefts(node.left, node :: acc)
+
+    def next(): Int = stack match {
+      case x :: xs =>
+        stack = pushLefts(x.right, xs)
+        x.value
+      case Nil => throw new NoSuchElementException
+    }
+
+    def hasNext(): Boolean = stack.nonEmpty
+  }
+}
