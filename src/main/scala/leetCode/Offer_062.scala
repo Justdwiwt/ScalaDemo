0bf4a701dbb87d48e682ca73764756f186895ae0
Justdwiwt <len_master@hotmail.com>
2021-10-19 17:16:27 +0800

    leetCode 剑指Offer II 062,063,064

---
+++ src/main/scala/leetCode/Offer_062.scala
@@ -0,0 +1,54 @@
+package leetCode
+
+object Offer_062 {
+  class Trie() {
+
+    /** Initialize your data structure here. */
+    case class STrie(term: Boolean, m: Map[Char, STrie])
+
+    var tree: STrie = STrie(term = false, Map())
+
+    private def insert(word: List[Char], t: STrie): STrie = word match {
+      case Nil => STrie(term = true, t.m)
+      case x :: xs => t.m.get(x) match {
+        case None => STrie(t.term, t.m + (x -> insert(xs, STrie(term = false, Map()))))
+        case Some(sub) => STrie(t.term, t.m + (x -> insert(xs, sub)))
+      }
+    }
+
+    @scala.annotation.tailrec
+    private def search(word: List[Char], t: STrie): Boolean = word match {
+      case Nil => t.term
+      case x :: xs => t.m.get(x) match {
+        case None => false
+        case Some(sub) => search(xs, sub)
+      }
+    }
+
+    @scala.annotation.tailrec
+    private def startWith(word: List[Char], t: STrie): Boolean = word match {
+      case Nil => true
+      case x :: xs => t.m.get(x) match {
+        case None => false
+        case Some(sub) => startWith(xs, sub)
+      }
+    }
+
+    /** Inserts a word into the trie. */
+    def insert(word: String) {
+      val newTree = insert(word.toList, tree)
+      tree = newTree
+    }
+
+    /** Returns if the word is in the trie. */
+    def search(word: String): Boolean = {
+      search(word.toList, tree)
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    def startsWith(prefix: String): Boolean = {
+      startWith(prefix.toList, tree)
+    }
+
+  }
+}
---
+++ src/main/scala/leetCode/Offer_064.scala
@@ -0,0 +1,27 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Offer_064 {
+  class MagicDictionary() {
+    private val m = mutable.Map[Int, mutable.Set[String]]()
+
+    def buildDict(dict: Array[String]) {
+      dict.foreach(i => {
+        val len = i.length
+        if (m.contains(len)) m(len).add(i)
+        else m += len -> mutable.Set[String](i)
+      })
+    }
+
+    def search(word: String): Boolean = {
+      if (!m.contains(word.length)) return false
+      m(word.length).foreach(i => {
+        var diff = 0
+        word.indices.foreach(j => if (i(j) != word(j)) diff += 1)
+        if (diff == 1) return true
+      })
+      false
+    }
+  }
+}
