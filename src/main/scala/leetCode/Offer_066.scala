b4de5292fa4fa5e0aabca687e5d383534c06f521
Justdwiwt <len_master@hotmail.com>
2021-10-20 15:10:15 +0800

    leetCode 剑指Offer II 065,066,067

---
+++ src/main/scala/leetCode/Offer_066.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Offer_066 {
+  class MapSum() {
+
+    val m = mutable.Map.empty[String, Int]
+
+    def insert(key: String, value: Int): m.type = m += (key -> value)
+
+    def sum(prefix: String): Int = m.filterKeys(_.startsWith(prefix)).values.sum
+
+  }
+}
