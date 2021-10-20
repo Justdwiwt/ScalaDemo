bccb915f1f56cf0bbb3ea90a524074c4e8a01ae5
Justdwiwt <len_master@hotmail.com>
2021-10-20 17:16:18 +0800

    leetCode 416

--- src/main/scala/leetCode/Solution_416.scala
+++ src/main/scala/leetCode/Solution_416.scala
@@ -2,21 +2,12 @@ package leetCode
 
 object Solution_416 {
   def canPartition(nums: Array[Int]): Boolean = {
-    val sum = nums.sum
-    if ((sum & 1) > 0) false
-    else {
-      val x = nums.sortBy(a => -a)
-      val t = sum / 2
-
-      def f(idx: Int, target: Int): Boolean = {
-        if (x(idx) > target) false
-        else if (x(idx) < target) {
-          (idx + 1 until x.length).foreach(i => if (f(i, target - x(idx))) return true)
-          false
-        } else true
-      }
-
-      f(0, t)
-    }
+    val s = nums.sum
+    if ((s & 1) == 1) return false
+    val m = s / 2 + 1
+    val dp = Array.fill[Boolean](m)(false)
+    dp(0) = true
+    nums.foreach(num => (1 until m).reverse.foreach(i => if (num <= i) dp(i) = dp(i) || dp(i - num)))
+    dp(m - 1)
   }
 }
