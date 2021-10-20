c49e4a4f6e3645176ac6218318ffec2bb6027563
Justdwiwt <len_master@hotmail.com>
2021-10-20 17:24:40 +0800

    leetCode 剑指Offer II 102,103,104,105,106

---
+++ src/main/scala/leetCode/Offer_102.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Offer_102 {
+  def findTargetSumWays(nums: Array[Int], S: Int): Int = {
+    def f(nums: Array[Int], start: Int, S: Int): Int = {
+      if (start == nums.length) return if (S == 0) 1 else 0
+      f(nums, start + 1, S + nums(start)) + f(nums, start + 1, S - nums(start))
+    }
+
+    f(nums, 0, S)
+  }
+}
