2ef99d14781e54f9a74d34411236dd23c3dbe870
Justdwiwt <len_master@hotmail.com>
2021-10-26 15:20:05 +0800

    leetCode 剑指Offer II 107,108,109,110

---
+++ src/main/scala/leetCode/Offer_108.scala
@@ -0,0 +1,30 @@
+package leetCode
+
+object Offer_108 {
+  def ladderLength(beginWord: String, endWord: String, wordList: List[String]): Int = {
+    var q = scala.collection.immutable.Queue[String](beginWord)
+    var m = Map[String, Int](wordList.toStream.map((_, Integer.MAX_VALUE)): _*) + (beginWord -> 1)
+    while (q.nonEmpty) {
+      val curWord = q.dequeue._1
+      val curDist = m.getOrElse(curWord, Integer.MAX_VALUE)
+      q = q.dequeue._2
+      if (curWord == endWord) return m(endWord)
+      val ch = curWord.toCharArray
+      ('a' to 'z').foreach(t => {
+        ch.indices.foreach(i => {
+          val old = ch(i)
+          ch(i) = t
+          val nextNode = ch./:("")(_ + _)
+          m.get(nextNode).foreach(d => {
+            if (d > curDist + 1) {
+              m = m + (nextNode -> (curDist + 1))
+              q = q.enqueue(nextNode)
+            }
+          })
+          ch(i) = old
+        })
+      })
+    }
+    0
+  }
+}
---
+++ src/main/scala/leetCode/Offer_110.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Offer_110 {
+  def allPathsSourceTarget(graph: Array[Array[Int]]): List[List[Int]] = {
+    def f(path: List[Int]): Stream[List[Int]] = graph(path.head)
+      .toStream
+      .flatMap(h => f(h :: path))
+      .#::(path)
+
+    f(List(0))
+      .filter(_.head == graph.length - 1)
+      .map(_.reverse)
+      .toList
+  }
+}
