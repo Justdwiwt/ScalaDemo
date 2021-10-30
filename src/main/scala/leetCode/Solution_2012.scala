a54c422eb1432289ff47c98fbf046e211ee101a6
Justdwiwt <len_master@hotmail.com>
2021-10-30 19:51:41 +0800

    leetCode 2012

---
+++ src/main/scala/leetCode/Solution_2012.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_2012 {
+  def sumOfBeauties(nums: Array[Int]): Int = {
+    val (forward, _) = nums.indices.drop(1).dropRight(1)./:((List.empty[Int], nums(0))) {
+      case ((acc, curMx), i) =>
+        if (nums(i) > curMx) (2 :: acc, nums(i))
+        else if (nums(i) > nums(i - 1)) (1 :: acc, curMx)
+        else (0 :: acc, curMx)
+    }
+    val (_, sum, _) = nums.indices.drop(1).dropRight(1).:\((forward, 0, nums(nums.length - 1))) {
+      case (i, (acc, curSum, curMn)) =>
+        if (nums(i) < curMn) (acc.tail, curSum + 2.min(acc.head), nums(i))
+        else if (nums(i) < nums(i + 1)) (acc.tail, curSum + 1.min(acc.head), curMn)
+        else (acc.tail, curSum, curMn)
+    }
+    sum
+  }
+}
