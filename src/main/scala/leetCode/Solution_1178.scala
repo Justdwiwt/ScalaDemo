1e0115ab97be28d5334c77e6cfbe64c2982e9c63
Justdwiwt <len_master@hotmail.com>
2021-11-15 14:00:37 +0800

    leetCode 1178,1413

--- src/main/scala/leetCode/Solution_1178.scala
+++ src/main/scala/leetCode/Solution_1178.scala
@@ -1,41 +1,21 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1178 {
   def findNumOfValidWords(words: Array[String], puzzles: Array[String]): List[Int] = {
-    class Node(_v: Char) {
-      val v: Char = _v
-      val children: mutable.Map[Char, Node] = mutable.Map[Char, Node]()
-      var word = ""
-    }
+    def bitmask(s: String): Int =
+      s.toSet./:(0)((bitmask, char) => bitmask | (1 << (char - 'a')))
 
-    val cntM = mutable.Map[String, Int]()
-    words.foreach(w => cntM += w -> (cntM.getOrElse(w, 0) + 1))
+    val wordBitmaskCounts = words.groupBy(bitmask).mapValues(_.length).toMap.withDefaultValue(0)
 
-    def f(root: Node, set: Set[Char], ch: Char): Int = {
-      var cnt = 0
+    puzzles.map(puzzle => {
+      val first = 1 << (puzzle.head - 'a')
+      val mask = bitmask(puzzle.tail)
 
-      def g(root: Node, set: Set[Char], ch: Char, flag: Boolean): Unit = {
-        if (root.v != ' ' && !set.contains(root.v)) return
-        val newFlag = if (root.v == ch) true else flag
-        if (root.word.length > 0 && newFlag) cnt += cntM(root.word)
-        root.children.keySet.foreach(c => g(root.children(c), set, ch, newFlag))
-      }
+      def count(submask: Int): Int =
+        if (submask == 0) 0
+        else wordBitmaskCounts(submask | first) + count((submask - 1) & mask)
 
-      g(root, set, ch, flag = false)
-      cnt
-    }
-
-    val root = new Node(' ')
-    words.foreach(w => {
-      var runner = root
-      (0 until w.length).foreach(i => {
-        if (!runner.children.contains(w.charAt(i))) runner.children += w.charAt(i) -> new Node(w.charAt(i))
-        runner = runner.children(w(i))
-        if (i == w.length - 1) runner.word = w
-      })
-    })
-    puzzles.map({ p => f(root, p.toCharArray.toSet, p.head) }).toList
+      wordBitmaskCounts(first) + count(mask)
+    }).toList
   }
 }
--- src/main/scala/leetCode/Solution_1413.scala
+++ src/main/scala/leetCode/Solution_1413.scala
@@ -2,5 +2,5 @@ package leetCode
 
 object Solution_1413 {
   def minStartValue(nums: Array[Int]): Int =
-    (1 - nums.scanLeft(0)(_ + _).drop(1).min).max(1)
+    1 - nums.scanLeft(0)(_ + _).min
 }
