3f35bf62500f15448552a54a4dcd9a9795a8090b
Justdwiwt <len_master@hotmail.com>
2021-11-24 16:07:40 +0800

    leetCode 2080

---
+++ src/main/scala/leetCode/Solution_2080.scala
@@ -0,0 +1,33 @@
+package leetCode
+
+object Solution_2080 {
+  final case class Solver(arrMap: Array[Map[Int, Int]])
+
+  object Solver {
+    def apply(_arr: Array[Int]): Solver = {
+      val arrMap = _arr.scanLeft(Map.empty[Int, Int]) { case (map, number) =>
+        map.get(number) match {
+          case Some(value) => map.updated(number, value + 1)
+          case None => map.updated(number, 1)
+        }
+      }
+      Solver(arrMap.tail)
+    }
+
+    def query(solver: Solver)(left: Int, right: Int, value: Int): Int = {
+      val rightVal = solver.arrMap(right).getOrElse(value, 0)
+      val leftVal = if (left == 0) 0
+      else solver.arrMap(left - 1).getOrElse(value, 0)
+      rightVal - leftVal
+    }
+  }
+
+  class RangeFreqQuery(_arr: Array[Int]) {
+    private val solver: Solver = Solver(_arr)
+
+    def query(left: Int, right: Int, value: Int): Int = {
+      Solver.query(solver)(left, right, value)
+    }
+
+  }
+}
