4b235637de67c007ae8cd6071f42c06aafa97cc3
Justdwiwt <len_master@hotmail.com>
2021-11-29 18:39:27 +0800

    leetCode 786

---
+++ src/main/scala/leetCode/Solution_786.scala
@@ -0,0 +1,29 @@
+package leetCode
+
+object Solution_786 {
+  def kthSmallestPrimeFraction(arr: Array[Int], k: Int): Array[Int] = {
+    var l = 0.0
+    var r = 1.0
+    while (true) {
+      val mid = (l + r) / 2
+      var i = -1
+      var cnt = 0
+      var x = 0
+      var y = 1
+      arr.indices.drop(1).foreach(j => {
+        while (arr(i + 1).toDouble / arr(j) < mid) {
+          i += 1
+          if (arr(i) * y > arr(j) * x) {
+            x = arr(i)
+            y = arr(j)
+          }
+        }
+        cnt += i + 1
+      })
+      if (cnt == k) return Array(x, y)
+      if (cnt < k) l = mid
+      else r = mid
+    }
+    Array.empty
+  }
+}
