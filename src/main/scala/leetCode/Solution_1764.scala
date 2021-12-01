44bb046243ec9c558e16553a4b250d3f0b49bb02
Justdwiwt <len_master@hotmail.com>
2021-12-01 16:54:59 +0800

    leetCode 1764

---
+++ src/main/scala/leetCode/Solution_1764.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_1764 {
+  def canChoose(groups: Array[Array[Int]], nums: Array[Int]): Boolean = {
+    val matchIdx = groups./:(0)((pos, group) => {
+      if (pos != -1) {
+        val idx = nums.indexOfSlice(group, pos)
+        if (idx == -1) -1 else idx + group.length
+      } else -1
+    })
+    matchIdx != -1
+  }
+}
