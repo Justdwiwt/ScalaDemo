7ade4ca980ee6ecaebc18f7d221c0a2c9c7e9f7a
Justdwiwt <len_master@hotmail.com>
2021-12-07 15:48:45 +0800

    leetCode 2085

---
+++ src/main/scala/leetCode/Solution_2085.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2085 {
+  def countWords(words1: Array[String], words2: Array[String]): Int = {
+    val words1Set = words1.groupBy(identity).mapValues(_.length).toArray.filter(_._2 == 1).map(_._1).toSet
+    val words2Set = words2.groupBy(identity).mapValues(_.length).toArray.filter(_._2 == 1).map(_._1).toSet
+    words1Set.intersect(words2Set).size
+  }
+}
