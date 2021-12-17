bda046fcc849c108fa879727cf8a0c477e6c853b
Justdwiwt <len_master@hotmail.com>
2021-12-18 02:03:21 +0800

    leetCode 1763

---
+++ src/main/scala/leetCode/Solution_1763.scala
@@ -0,0 +1,22 @@
+package leetCode
+
+object Solution_1763 {
+  def longestNiceSubstring(s: String): String = {
+    @scala.annotation.tailrec
+    def f(left: Int, right: Int, pairs: Map[Char, Boolean], substr: String): String = {
+      if (left == s.length) substr
+      else if (right == s.length) f(left + 1, left + 1, Map.empty[Char, Boolean], substr)
+      else {
+        val rChar = s.charAt(right)
+        val newPairs = if (pairs.contains(rChar) && pairs(rChar)) pairs
+        else if (rChar.isUpper && pairs.contains(rChar.toLower)) pairs + (rChar.toLower -> true)
+        else if (rChar.isLower && pairs.contains(rChar.toUpper)) pairs + (rChar.toUpper -> true)
+        else pairs + (rChar -> false)
+        val newSubstr = if (newPairs.forall(_._2) && right - left + 1 > substr.length) s.substring(left, right + 1) else substr
+        f(left, right + 1, newPairs, newSubstr)
+      }
+    }
+
+    f(0, 0, Map.empty[Char, Boolean], "")
+  }
+}
