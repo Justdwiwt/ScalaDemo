fc50152106927b8ac983ec873662313717a3356e
Justdwiwt <len_master@hotmail.com>
2021-12-21 00:41:59 +0800

    leetCode 2095,2100,2101,2109,2110

---
+++ src/main/scala/leetCode/Solution_2109.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2109 {
+  def addSpaces(s: String, spaces: Array[Int]): String = {
+    val res = new StringBuilder(s)
+    spaces.indices.reverse.foreach(i => res.insert(spaces(i), ' '))
+    res.mkString
+  }
+}
