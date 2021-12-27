a27d3d8c6ea3348ebc632978b72a890dbb01222c
Justdwiwt <len_master@hotmail.com>
2021-12-28 00:28:38 +0800

    leetCode 472

--- src/main/scala/leetCode/Solution_472.scala
+++ src/main/scala/leetCode/Solution_472.scala
@@ -1,39 +1,61 @@
 package leetCode
 
 import scala.collection.mutable
+import scala.math.Ordering._
 
 object Solution_472 {
-
   class TrieNode {
-    var isWord = false
-    var next = new mutable.HashMap[Char, TrieNode]()
+    var isWord: Boolean = false
+    val child = mutable.HashMap.empty[Char, TrieNode]
 
-    def add(str: String): Unit = {
-      var node = this
-      str.indices.foreach(i => {
-        if (!node.next.contains(str(i))) node.next += str(i) -> new TrieNode
-        node = node.next(str(i))
-      })
-      node.isWord = true
+    override def toString: String = {
+      isWord.toString + " " + child.toString
     }
   }
 
-  def findAllConcatenatedWordsInADict(words: Array[String]): List[String] = {
-    val root = new TrieNode
-    words.foreach(w => if (!w.equals("")) root.add(w))
-    var res = List.empty[String]
-    words.foreach(w => if (dfs(w.toCharArray, 0, root)) res :+= w)
-
-    def dfs(str: Array[Char], start: Int, root: TrieNode): Boolean = {
-      var node = root
-      (start until str.length).foreach(i => {
-        if (!node.next.contains(str(i))) return false
-        node = node.next(str(i))
-        if (node.isWord && dfs(str, i + 1, root)) return true
-      })
-      node.isWord && start != 0
+  object LenghtOrdering extends StringOrdering {
+    override def compare(x: String, y: String): Int = {
+      x.length.compare(y.length)
     }
+  }
 
-    res
+  private var root: TrieNode = _
+
+  def add(word: String): Unit = {
+    var tmp = root
+    word.foreach(ch => {
+      if (!tmp.child.contains(ch)) {
+        val newNode = new TrieNode
+        tmp.child(ch) = newNode
+      }
+      tmp = tmp.child(ch)
+    })
+    tmp.isWord = true
+  }
+
+  def doesExists(word: String, root: TrieNode, index: Int): Boolean = {
+    var temp = root
+    (index until word.length).foreach(i => {
+      val ch = word(i)
+      if (!temp.child.contains(ch)) return false
+      if (temp.child(ch).isWord) {
+        if (i == word.length - 1) return true
+        if (doesExists(word, root, i + 1)) return true
+      }
+      temp = temp.child(ch)
+    })
+    false
+  }
+
+  def findAllConcatenatedWordsInADict(words: Array[String]): List[String] = {
+    root = new TrieNode
+    val sortedWords = words.sorted(LenghtOrdering)
+    val res = mutable.ListBuffer.empty[String]
+    sortedWords.foreach(word => {
+      if (doesExists(word, root, 0)) res.append(word)
+      else add(word)
+    })
+    res.toList
+
   }
 }
