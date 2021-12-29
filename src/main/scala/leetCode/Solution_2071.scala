f8723d918a608ab253723ddea358dc38e58cdf25
Justdwiwt <len_master@hotmail.com>
2021-12-30 01:48:04 +0800

    leetCode 2071,2097,2116,2117,2122

---
+++ src/main/scala/leetCode/Solution_2071.scala
@@ -0,0 +1,40 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2071 {
+  //  def maxTaskAssign(tasks: Array[Int], workers: Array[Int], pills: Int, strength: Int): Int = {
+  //    tasks.sortInPlace()
+  //    workers.sortInPlace()
+  //
+  //    def can(n: Int): Boolean = {
+  //      val ts = tasks.take(n).to(mutable.Stack)
+  //      val ws = workers.drop(workers.length - n)
+  //      val tl = mutable.ArrayDeque.empty[Int]
+  //      var left = pills
+  //      ws.foreach(w => {
+  //        if (tl.isEmpty) tl.append(ts.pop())
+  //        if (w >= tl.head) tl.removeHead()
+  //        else {
+  //          if (left == 0) return false
+  //          while (ts.nonEmpty && ts.head <= w + strength) {
+  //            tl.append(ts.pop())
+  //          }
+  //          if (tl.last > w + strength) return false
+  //          tl.removeLast()
+  //          left -= 1
+  //        }
+  //      })
+  //      true
+  //    }
+  //
+  //    var lo = 0
+  //    var hi = tasks.length.min(workers.length)
+  //    while (lo != hi) {
+  //      val mid = (lo + hi + 1) / 2
+  //      if (can(mid)) lo = mid
+  //      else hi = mid - 1
+  //    }
+  //    lo
+  //  }
+}
---
+++ src/main/scala/leetCode/Solution_2117.scala
@@ -0,0 +1,20 @@
+package leetCode
+
+object Solution_2117 {
+  def abbreviateProduct(left: Int, right: Int): String = {
+    val M = 1e12.toLong
+    var (pre, suf, c) = (1L, 1L, 0L)
+    (left.toLong until (right + 1).toLong).foreach(n => {
+      pre *= n
+      suf *= n
+      while (suf % 10 == 0) {
+        c += 1
+        suf /= 10
+      }
+      if (suf > M) suf = suf % M
+      while (pre > M) pre /= 10
+    })
+    if (suf < 10000000000L) suf.toString + "e" + c
+    else pre.toString.take(5) + "..." + suf.toString.takeRight(5) + "e" + c
+  }
+}
