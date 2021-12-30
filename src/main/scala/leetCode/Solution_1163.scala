20ed682478bbf27c4dbb1faaf6cfa1d4720279c5
Justdwiwt <len_master@hotmail.com>
2021-12-31 01:53:17 +0800

    leetCode 1155,1163,1292

---
+++ src/main/scala/leetCode/Solution_1163.scala
@@ -0,0 +1,30 @@
+package leetCode
+
+object Solution_1163 {
+  def lastSubstring(s: String): String = {
+    var mxV = s.head - 'a'
+    var mxIdx = 0
+    s.indices.drop(1).foreach(i => {
+      val c = s(i)
+      if (c - 'a' > mxV) {
+        mxV = c - 'a'
+        mxIdx = i
+      } else if (c - 'a' == mxV) {
+        val idx = getMxIdx(mxIdx, i, s)
+        if (idx == -1) return s.substring(mxIdx)
+        mxIdx = idx
+      }
+    })
+    s.substring(mxIdx)
+  }
+
+  def getMxIdx(cur: Int, idx: Int, s: String): Int = {
+    var i = 1
+    while (idx + i < s.length) {
+      if (s(cur + i) > s(idx + i)) return cur
+      else if (s(cur + i) < s(idx + i)) return idx
+      else i += 1
+    }
+    -1
+  }
+}
