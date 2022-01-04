9034820477476e097bd8343ead3b303e51313190
Justdwiwt <len_master@hotmail.com>
2022-01-04 18:52:23 +0800

    leetCode 913,2115

--- src/main/scala/leetCode/Solution_913.scala
+++ src/main/scala/leetCode/Solution_913.scala
@@ -1,38 +1,79 @@
 package leetCode
 
-import scala.util.control.Breaks._
-
 object Solution_913 {
-  def catMouseGame(graph: Array[Array[Int]]): Int = {
-    val dp = Array.fill(2 * graph.length, graph.length, graph.length)(-1)
-    func(graph, 0, 1, 2, dp)
+  sealed trait Player {
+    def start: Int
   }
 
-  def func(graph: Array[Array[Int]], t: Int, x: Int, y: Int, dp: Array[Array[Array[Int]]]): Int = {
-    if (t == graph.length * 2) return 0
-    if (x == y) return 2
-    if (x == 0) return 1
-    if (dp(t)(x)(y) != -1) return dp(t)(x)(y)
-    val flag = t % 2 == 0
-    if (flag) {
-      var catWin = true
-      graph(x).indices.foreach(i => {
-        val next = func(graph, t + 1, graph(x)(i), y, dp)
-        if (next == 1) dp(t)(x)(y) = 1
-        else if (next != 2) catWin = false
-      })
-      if (catWin) 2 else 0
-    } else {
-      var mouseWin = true
-      graph(y).indices.foreach(i => {
-        breakable {
-          if (graph(y)(i) == 0) break
-        }
-        val next = func(graph, t + 1, x, graph(y)(i), dp)
-        if (next == 2) dp(t)(x)(y) = 2
-        else if (next != 1) mouseWin = false
-      })
-      if (mouseWin) 1 else 0
+  case object Cat extends Player {
+    override def start: Int = 2
+  }
+
+  case object Mouse extends Player {
+    override def start: Int = 1
+  }
+
+  sealed trait Result {
+    def toInt: Int
+  }
+
+  case object Draw extends Result {
+    override def toInt: Int = 0
+  }
+
+  final case class Win(player: Player) extends Result {
+    override def toInt: Int = player match {
+      case Cat => 2
+      case Mouse => 1
     }
+  }
+
+  class Lazy[+A](f: => A) {
+    private lazy val elem = f
+
+    def get: A = elem
+  }
+
+  object Lazy {
+    def apply[A](f: => A) = new Lazy(f)
+  }
+
+  private val Hole = 0
+
+  def catMouseGame(graph: Array[Array[Int]]): Int = {
+    lazy val res: IndexedSeq[IndexedSeq[IndexedSeq[Lazy[Result]]]] = IndexedSeq
+      .tabulate(2 * graph.length + 1, graph.length, graph.length) {
+        case (turn, _, _) if turn == 2 * graph.length => Lazy(Draw)
+        case (_, cat, mouse) if cat == mouse => Lazy(Win(Cat))
+        case (_, _, Hole) => Lazy(Win(Mouse))
+        case (turn, cat, mouse) if turn % 2 == 0 =>
+          @scala.annotation.tailrec
+          def playOptimally(moves: Seq[Int], result: Result = Win(Cat)): Result = moves match {
+            case Seq(head, tail@_*) => res(turn + 1)(cat)(head).get match {
+              case Win(Mouse) => Win(Mouse)
+              case Draw => playOptimally(tail, Draw)
+              case _ => playOptimally(tail, result)
+            }
+            case _ => result
+          }
+
+          Lazy(playOptimally(graph(mouse).toSeq))
+
+        case (turn, cat, mouse) =>
+          @scala.annotation.tailrec
+          def playOptimally(moves: Seq[Int], result: Result = Win(Mouse)): Result = moves match {
+            case Seq(Hole, tail@_*) => playOptimally(tail, result)
+            case Seq(head, tail@_*) => res(turn + 1)(head)(mouse).get match {
+              case Win(Cat) => Win(Cat)
+              case Draw => playOptimally(tail, Draw)
+              case _ => playOptimally(tail, result)
+            }
+            case _ => result
+          }
+
+          Lazy(playOptimally(graph(cat).toSeq))
+      }
+
+    res.head(Cat.start)(Mouse.start).get.toInt
   }
 }
