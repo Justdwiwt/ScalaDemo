ea893543464ca19fb84734c25a7afbce8a3d1270
Justdwiwt <len_master@hotmail.com>
2022-01-11 14:56:13 +0800

    leetCode 2129

--- src/main/scala/leetCode/Solution_2129.scala
+++ src/main/scala/leetCode/Solution_2129.scala
@@ -1,14 +1,8 @@
 package leetCode
 
 object Solution_2129 {
-  def capitalizeTitle(title: String): String = {
-    val t = title.split(" ")
-    val res = new StringBuilder(f(t.head))
-    t.indices.drop(1).foreach(i => res.append(" ").append(f(t(i))))
-    res.toString()
-  }
-
-  def f(s: String): String =
-    if (s.length > 2) new StringBuilder(s.substring(0, 1).toUpperCase()).append(s.substring(1).toLowerCase()).toString()
-    else s.toLowerCase()
+  def capitalizeTitle(title: String): String = title
+    .split(' ')
+    .map(w => if (w.length <= 2) w.map(_.toLower) else w.take(1).toUpperCase + w.drop(1).toLowerCase)
+    .mkString(" ")
 }
