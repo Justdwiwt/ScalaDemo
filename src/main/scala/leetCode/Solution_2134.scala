11f4b6ceb4b5a421277c6f86eaeb0decb093f29b
Justdwiwt <len_master@hotmail.com>
2022-01-17 18:48:50 +0800

    leetCode 2134

--- src/main/scala/leetCode/Solution_2134.scala
+++ src/main/scala/leetCode/Solution_2134.scala
@@ -2,14 +2,10 @@ package leetCode
 
 object Solution_2134 {
   def minSwaps(nums: Array[Int]): Int = {
-    val target = nums.count(_ == 1)
-    var cur = 0
-    var mx = 0
-    (0 until nums.length + target).foreach(i => {
-      cur += nums(i % nums.length)
-      if (i >= target) cur -= nums(i - target)
-      mx = mx.max(cur)
-    })
-    target - mx
+    val arr = nums ++ nums
+    val cnt = nums.count(_ == 1)
+    val dp = Array.fill(arr.length)(0)
+    dp.indices.foreach(i => dp(i) = arr(i) + dp.lift(i - 1).getOrElse(0))
+    nums.indices.map(i => cnt - (dp(i + cnt) - dp(i))).min
   }
 }
