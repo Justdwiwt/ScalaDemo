0bc16aae2469ecc397646847874bb718d0d358f8
Justdwiwt <len_master@hotmail.com>
2022-01-20 17:50:29 +0800

    leetCode 2126

--- src/main/scala/leetCode/Solution_2126.scala
+++ src/main/scala/leetCode/Solution_2126.scala
@@ -1,10 +1,7 @@
 package leetCode
 
 object Solution_2126 {
-  def asteroidsDestroyed(mass: Int, asteroids: Array[Int]): Boolean = {
-    var cnt = mass.toLong
-    val sorted = asteroids.sorted
-    sorted.foreach(i => if (cnt >= i) cnt += i else return false)
-    true
-  }
+  def asteroidsDestroyed(mass: Int, asteroids: Array[Int]): Boolean = asteroids
+    .sorted
+    ./:(mass.toLong)((m, a) => if (a > m) return false else m + a) > 0
 }
