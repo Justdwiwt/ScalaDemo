4d7ae92c128601c129cccd16c75b0b3b2b68885d
Justdwiwt <len_master@hotmail.com>
2022-01-22 16:04:13 +0800

    leetCode 946

--- src/main/scala/leetCode/Solution_946.scala
+++ src/main/scala/leetCode/Solution_946.scala
@@ -1,18 +1,12 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_946 {
   def validateStackSequences(pushed: Array[Int], popped: Array[Int]): Boolean = {
-    val st = new mutable.Stack[Int]()
-    var j = 0
-    pushed.foreach(x => {
-      st.push(x)
-      while (st.nonEmpty && j < pushed.length && st.head == popped(j)) {
-        st.pop()
-        j += 1
-      }
-    })
-    j == pushed.length
+    @scala.annotation.tailrec
+    def f(stack: Seq[Int], idx: Int): (Seq[Int], Int) =
+      if (stack.headOption.forall(_ != popped(idx))) (stack, idx)
+      else f(stack.tail, idx + 1)
+
+    pushed./:(Seq.empty[Int], 0)({ case ((stack, idx), n) => f(n +: stack, idx) })._1.isEmpty
   }
 }
