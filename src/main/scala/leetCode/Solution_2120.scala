cb5b00870925a32ef415584ab9222d9bfc26e368
Justdwiwt <len_master@hotmail.com>
2022-01-27 15:01:53 +0800

    leetCode 2120

--- src/main/scala/leetCode/Solution_2120.scala
+++ src/main/scala/leetCode/Solution_2120.scala
@@ -1,26 +1,20 @@
 package leetCode
 
 object Solution_2120 {
-  def executeInstructions(n: Int, startPos: Array[Int], s: String): Array[Int] = {
-    def dfs(s: String, i: Int): Int = {
-      var res = 0
-      val pos = Array[Int](startPos.head, startPos(1))
-      (i until s.length).foreach(j => {
-        s(j) match {
-          case 'L' => pos(1) -= 1
-          case 'R' => pos(1) += 1
-          case 'U' => pos(0) -= 1
-          case 'D' => pos(0) += 1
+  def executeInstructions(n: Int, startPos: Array[Int], s: String): Array[Int] = s
+    .indices
+    .map(i => {
+      Iterator.iterate((startPos.head, startPos.last, i)) { case (x, y, i) =>
+        if (i == s.length) (x, y, i + 1)
+        else s(i) match {
+          case 'R' => (x, y + 1, i + 1)
+          case 'L' => (x, y - 1, i + 1)
+          case 'D' => (x + 1, y, i + 1)
+          case 'U' => (x - 1, y, i + 1)
         }
-        if (pos.head < 0 || pos(1) == n || pos.head == n || pos(1) < 0)
-          return res
-        res += 1
-      })
-      res
-    }
-
-    val res = Array.fill(s.length)(0)
-    s.indices.foreach(i => res(i) = dfs(s, i))
-    res
-  }
+      }
+        .drop(1)
+        .takeWhile { case (x, y, i) => i <= s.length && x >= 0 && x < n && y >= 0 && y < n }
+        .size
+    }).toArray
 }
