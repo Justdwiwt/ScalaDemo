5e537c4c88c4c3509d38f666c75586a00886410d
Justdwiwt <len_master@hotmail.com>
2022-01-30 14:43:27 +0800

    leetCode 884

--- src/main/scala/leetCode/Solution_884.scala
+++ src/main/scala/leetCode/Solution_884.scala
@@ -1,7 +1,11 @@
 package leetCode
 
 object Solution_884 {
-  def uncommonFromSentences(A: String, B: String): Array[String] = {
-    (A + " " + B).split(" ").map((_, 1)).groupBy(_._1).filter(_._2.length == 1).keys.toArray
-  }
+  def uncommonFromSentences(A: String, B: String): Array[String] = (A + " " + B)
+    .split(" ")
+    .map((_, 1))
+    .groupBy(_._1)
+    .filter(_._2.length == 1)
+    .keys
+    .toArray
 }
