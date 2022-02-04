14503cdc6e8c9620d0ffc6f30ef96dd96870b1db
Justdwiwt <len_master@hotmail.com>
2022-02-04 18:46:29 +0800

    leetCode 2139

---
+++ src/main/scala/leetCode/Solution_2139.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2139 {
+  def minMoves(target: Int, maxDoubles: Int): Int = {
+    @scala.annotation.tailrec
+    def f(n: Int, doubles: Int, moves: Int): Int =
+      if (n == 1) moves
+      else if (doubles == maxDoubles) moves + n - 1
+      else if (n % 2 == 1) f(n - 1, doubles, moves + 1)
+      else f(n / 2, doubles + 1, moves + 1)
+
+    f(n = target, doubles = 0, moves = 0)
+  }
+}
