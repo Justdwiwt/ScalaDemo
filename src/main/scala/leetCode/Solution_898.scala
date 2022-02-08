0db9e4298262c2a8e1d9494e02e5e62d7d717395
Justdwiwt <len_master@hotmail.com>
2022-02-08 14:43:27 +0800

    leetCode 898

--- src/main/scala/leetCode/Solution_898.scala
+++ src/main/scala/leetCode/Solution_898.scala
@@ -1,18 +1,9 @@
 package leetCode
 
 object Solution_898 {
-  def subarrayBitwiseORs(A: Array[Int]): Int = {
-    if (A.length == 1) return 1
-    var dp = Array.ofDim[Int](A.length)
-    dp.indices.foreach(i => dp(i) = A(i))
-    var res = Set[Int]()
-    res ++= dp
-    (2 to A.length).foreach(len => {
-      val tmp = Array.ofDim[Int](A.length - len + 1)
-      (0 to A.length - len).foreach(i => tmp(i) = dp(i) | A(i + len - 1))
-      dp = tmp
-      res ++= dp
-    })
-    res.size
-  }
+  def subarrayBitwiseORs(arr: Array[Int]): Int = arr
+    .scanLeft(Array.empty[Int])((pre, n) => (pre :+ 0).map(_ | n).distinct)
+    .flatten
+    .distinct
+    .length
 }
