98d0dcaed3ef9bbbe3caf32450732947c5e3622c
Justdwiwt <len_master@hotmail.com>
2022-02-10 00:32:35 +0800

    leetCode 1447

--- src/main/scala/leetCode/Solution_1447.scala
+++ src/main/scala/leetCode/Solution_1447.scala
@@ -1,12 +1,10 @@
 package leetCode
 
 object Solution_1447 {
-  def simplifiedFractions(n: Int): List[String] = {
-    var res = List.empty[String]
-    (2 to n).foreach(b => (1 until b).foreach(a => if (gcd(a, b) == 1) res :+= (a.toString + "/" + b.toString)))
-    res
-  }
-
-  @scala.annotation.tailrec
-  def gcd(x: Int, y: Int): Int = if (y == 0) x else gcd(y, x % y)
+  def simplifiedFractions(n: Int): List[String] = (2 to n)
+    .flatMap(x => (1 until x).map(n => (n / x.toFloat, n + "/" + x)))
+    .groupBy(_._1)
+    .values
+    .map(_.head._2)
+    .toList
 }
