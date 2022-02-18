8bc597f1684c5ca3c588bf6bf1fcfe6d43f4bdf2
Justdwiwt <len_master@hotmail.com>
2022-02-18 17:09:44 +0800

    leetCode 1968,2141,2145

---
+++ src/main/scala/leetCode/Solution_1968.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_1968 {
+  def rearrangeArray(nums: Array[Int]): Array[Int] = {
+    val sorted = nums.sorted
+    (sorted.indices.drop(1).dropRight(1) by 2).foreach(i => {
+      val t = sorted(i)
+      sorted(i) = sorted(i + 1)
+      sorted(i + 1) = t
+    })
+    sorted
+  }
+}
