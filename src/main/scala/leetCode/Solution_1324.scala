172c593a32781fba0d40c19798330bff9bdf3ab3
Justdwiwt <len_master@hotmail.com>
2022-02-20 14:44:57 +0800

    leetCode 1324,1366,1701

--- src/main/scala/leetCode/Solution_1324.scala
+++ src/main/scala/leetCode/Solution_1324.scala
@@ -5,7 +5,7 @@ object Solution_1324 {
     def f(s: String): String = s.replaceAll("\\s+$", "")
 
     val splitWords = s.split(" ").map(_.toCharArray)
-    (0 until splitWords.map(_.length).max).foldLeft(List.empty[String])((a, b) => {
+    (0 until splitWords.map(_.length).max)./:(List.empty[String])((a, b) => {
       f(splitWords.map(_.lift(b).getOrElse(' ')).mkString) :: a
     }).reverse
   }
