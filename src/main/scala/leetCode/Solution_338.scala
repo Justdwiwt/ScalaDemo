2def334955ea2f32fe7999c6aa152c815fd5a8e2
Justdwiwt <len_master@hotmail.com>
2022-03-01 15:51:22 +0800

    leetCode 338

--- src/main/scala/leetCode/Solution_338.scala
+++ src/main/scala/leetCode/Solution_338.scala
@@ -1,9 +1,15 @@
 package leetCode
 
 object Solution_338 {
-  def countBits(num: Int): Array[Int] = {
-    val res = Array.fill(num + 1)(0)
-    (1 to num).foreach(i => res(i) = res(i >> 1) + (i & 1))
-    res
+  def countBits(n: Int): Array[Int] = {
+    @scala.annotation.tailrec
+    def f(in: Int, out: Int = 0): Int =
+      if (in == 0) out
+      else f(in >>> 1, out + (in & 1))
+
+    def count(n: Int): Stream[Int] =
+      f(n) #:: count(n + 1)
+
+    count(0).take(n + 1).toArray
   }
 }
