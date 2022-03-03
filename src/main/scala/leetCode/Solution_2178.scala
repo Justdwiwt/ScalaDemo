24741957d613441b96dbae526034c7b9ea980a3c
Justdwiwt <len_master@hotmail.com>
2022-03-03 18:30:42 +0800

    leetCode 2178

---
+++ src/main/scala/leetCode/Solution_2178.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_2178 {
+  def maximumEvenSplit(finalSum: Long): List[Long] = {
+    @scala.annotation.tailrec
+    def f(nums: List[Long], sum: Long, next: Long): List[Long] =
+      if (sum + next > finalSum) (finalSum - sum + nums.head) +: nums.tail
+      else f(next +: nums, sum + next, next + 2)
+
+    if (finalSum % 2 == 1) List.empty
+    else f(List.empty, sum = 0, next = 2)
+  }
+}
