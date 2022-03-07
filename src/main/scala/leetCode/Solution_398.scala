f1a91644e0e565f9364837fc1ba69ce7d87fd73e
Justdwiwt <len_master@hotmail.com>
2022-03-07 18:20:24 +0800

    leetCode 398

--- src/main/scala/leetCode/Solution_398.scala
+++ src/main/scala/leetCode/Solution_398.scala
@@ -4,7 +4,7 @@ object Solution_398 {
 
   class Solution(_nums: Array[Int]) {
 
-    def pick(target: Int): Int = _nums.foldLeft((-1, 1, 0)) {
+    def pick(target: Int): Int = _nums./:((-1, 1, 0)) {
       case ((idx, cnt, i), num) =>
         if (num == target) {
           if (util.Random.nextInt(cnt) == 0) (i, cnt + 1, i + 1)
