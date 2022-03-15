f1f67dba2b5329746b45aa2c6507111567439eac
Justdwiwt <len_master@hotmail.com>
2022-03-15 19:47:01 +0800

    leetCode 2200

--- src/main/scala/leetCode/Solution_2200.scala
+++ src/main/scala/leetCode/Solution_2200.scala
@@ -1,17 +1,16 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2200 {
   def findKDistantIndices(nums: Array[Int], key: Int, k: Int): List[Int] = {
-    val st = mutable.HashSet.empty[Int]
-    var res = List.empty[Int]
-    nums.indices.foreach(i => if (nums(i) == key) {
-      val start = 0.max(i - k)
-      val end = (nums.length - 1).min(i + k)
-      (start to end).foreach(j => st += j)
-    })
-    st.foreach(res ::= _)
-    res.sorted
+    @scala.annotation.tailrec
+    def f(idx: Int, acc: List[Int]): List[Int] = {
+      if (idx == nums.length) return acc.reverse
+      if (nums(idx) == key) {
+        val n = (acc.headOption.getOrElse(-1) + 1).max(idx - k).max(0) to (idx + k).min(nums.length - 1)
+        f(idx + 1, n./:(acc)((a, x) => x :: a))
+      } else f(idx + 1, acc)
+    }
+
+    f(0, Nil)
   }
 }
