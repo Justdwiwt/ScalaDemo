e20b53cb27a9443f9e5c9f92bf81d341d4467418
Justdwiwt <len_master@hotmail.com>
2022-03-22 17:50:35 +0800

    leetCode 2207

---
+++ src/main/scala/leetCode/Solution_2207.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_2207 {
+  def maximumSubsequenceCount(text: String, pattern: String): Long = {
+    def f(s: String): Long = {
+      var t = 0L
+      var res = 0L
+      s.foreach(c => {
+        if (c == pattern(1)) res += t
+        if (c == pattern.head) t += 1
+      })
+      res
+    }
+
+    f(pattern.head + text).max(f(text + pattern(1)))
+  }
+}
