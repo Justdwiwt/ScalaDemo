bd47f7bb7a75a0cfba64fbe330923e5b778521ab
Justdwiwt <len_master@hotmail.com>
2022-03-23 17:32:02 +0800

    leetCode 2211

---
+++ src/main/scala/leetCode/Solution_2211.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2211 {
+  def countCollisions(directions: String): Int = directions.indexWhere(_ != 'L') match {
+    case -1 => 0
+    case h => directions.substring(h, directions.lastIndexWhere(_ != 'R') + 1).count(_ != 'S')
+  }
+}
