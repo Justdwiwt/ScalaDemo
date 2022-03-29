0df7689626a8209d2e5b170466615373ab3faae9
Justdwiwt <len_master@hotmail.com>
2022-03-29 16:59:51 +0800

    leetCode 2217

---
+++ src/main/scala/leetCode/Solution_2217.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2217 {
+  def kthPalindrome(queries: Array[Int], intLength: Int): Array[Long] = {
+    val arr = Iterator.fill((intLength - 1) / 2)(10L).product
+    queries.map(x => if (x > arr * 9) -1 else (arr + x - 1).toString match {
+      case s => (s.dropRight(intLength & 1) ++ s.reverse).toLong
+    })
+  }
+}
