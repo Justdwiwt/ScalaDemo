874b37b059c59488e1cdc7dbbdd4c94b3064e086
Justdwiwt <len_master@hotmail.com>
2022-04-02 16:17:54 +0800

    leetCode 420

--- src/main/scala/leetCode/Solution_420.scala
+++ src/main/scala/leetCode/Solution_420.scala
@@ -1,84 +1,51 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_420 {
   def strongPasswordChecker(s: String): Int = {
-    var add = 0.max(6 - s.length)
-    var delete = 0.max(s.length - 20)
+    val pq = new java.util.PriorityQueue[Int]((a: Int, b: Int) => a % 3 - b % 3)
+    if (s.isEmpty) return 6
+    var need1 = 1
+    var need2 = 1
+    var need3 = 1
+    var cnt = 1
+    var len = s.length
+    if (s.head.isLower) need1 = 0
+    if (s.head.isUpper) need2 = 0
+    if (s.head.isDigit) need3 = 0
+    (1 until len).foreach(i => {
+      if (s(i).isLower) need1 = 0
+      if (s(i).isUpper) need2 = 0
+      if (s(i).isDigit) need3 = 0
+      if (s(i) != s(i - 1)) {
+        if (cnt >= 3) pq.add(cnt)
+        cnt = 1
+      } else cnt += 1
+    })
+    if (cnt >= 3) pq.add(cnt)
+    val need = need1 + need2 + need3
     var res = 0
-
-    var d = 1
-    var a = 1
-    var A = 1
-
-    val ab = mutable.ArrayBuffer[Int]()
-
-    var same = 0
-    (0 to s.length).foreach(i => {
-      if (i == s.length) {
-        if (same > 1) ab += same
-      } else if (i == 0 || (s(i) == s(i - 1))) {
-        same += 1
-      } else {
-        if (same > 1) ab += same
-        same = 1
-      }
-
-      if (i < s.length) {
-        if (s(i).isDigit) d = 0
-        if (s(i).isLower) a = 0
-        if (s(i).isUpper) A = 0
-      }
-    })
-
-    var missing = d + a + A
-    val arr = ab.toArray
-
-    arr.indices.foreach(i => if (arr(i) % 3 == 0) {
-      if (delete >= 1) {
-        delete -= 1
-        res += 1
-        arr(i) = arr(i) - 1
-      }
-    } else if (arr(i) % 3 == 1) {
-      if (delete >= 2) {
-        delete -= 2
-        res += 2
-        arr(i) = arr(i) - 2
-      }
-    })
-
-    arr.indices.foreach(i => {
-      val need = arr(i) - 2
-      if (need <= delete) {
-        arr(i) = arr(i) - need
-        delete -= need
-        res += need
-      } else {
-        arr(i) = arr(i) - delete
-        res += delete
-        delete = 0
-      }
-    })
-
-    arr.indices.withFilter(i => arr(i) > 2).foreach(i => {
-      while (arr(i) > 2 && add > 0) {
-        add -= 1
-        missing -= 1
-        arr(i) -= 2
-        res += 1
-      }
-      missing -= arr(i) / 3
-      res += arr(i) / 3
-    })
-
-    while (missing > 0) {
-      if (add > 0) add -= 1
-      res += 1
-      missing -= 1
+    if (len < 6) {
+      if (len == 5) {
+        if (need >= 2 || cnt == 5) return need
+        return 1
+      } else return 6 - len
     }
-
-    add + delete + res
+    while (!pq.isEmpty && len > 20) {
+      var now = pq.poll()
+      res += 1
+      len -= 1
+      now -= 1
+      if (now >= 3) pq.add(now)
+    }
+    if (len > 20) res += len - 20 + 0.max(need)
+    else {
+      var cunt = 0
+      while (!pq.isEmpty) {
+        val now = pq.poll()
+        cunt += now / 3
+      }
+      res += cunt.max(need)
+    }
+    res
   }
 }
