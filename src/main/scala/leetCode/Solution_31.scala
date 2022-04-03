f167700b4b760044d0530cffaf9b062dd51214a9
Justdwiwt <len_master@hotmail.com>
2022-04-03 19:54:15 +0800

    leetCode 31

--- src/main/scala/leetCode/Solution_31.scala
+++ src/main/scala/leetCode/Solution_31.scala
@@ -1,34 +1,31 @@
 package leetCode
 
 object Solution_31 {
+  case class SplitResult(init: List[Int], rest: List[Int])
+
+  @scala.annotation.tailrec
+  def splitAtReversal(prev: Int, rest: List[Int], acc: List[Int]): SplitResult =
+    if (rest.isEmpty) SplitResult(rest, acc)
+    else if (rest.head < prev) SplitResult(rest, acc :+ prev)
+    else splitAtReversal(rest.head, rest.tail, acc :+ prev)
+
+  case class ReversalResult(item: Int, list: List[Int])
+
+  def getReversal(item: Int, list: List[Int]): ReversalResult = {
+    val (replacement, replacementIndex) = list.zipWithIndex.filter { case (a, _) => a > item }.minBy { case (a, _) => a }
+    ReversalResult(replacement, list.slice(0, replacementIndex) ::: List(item) ::: list.slice(replacementIndex + 1, list.length))
+  }
+
   def nextPermutation(nums: Array[Int]): Unit = {
-    @annotation.tailrec
-    def reverse(i: Int, j: Int): Unit = {
-      if (i < j) {
-        val t = nums(i)
-        nums(i) = nums(j)
-        nums(j) = t
-        reverse(i + 1, j - 1)
-      }
+    val list = nums.toList.reverse
+    val SplitResult(leadAndLast, toRev) = splitAtReversal(list.head, list.tail, List())
+    val result = if (leadAndLast.isEmpty) list
+    else {
+      val itemToReplace = leadAndLast.reverse.last
+      val constants = leadAndLast.reverse.init
+      val ReversalResult(newItem, replaceList) = getReversal(itemToReplace, toRev)
+      constants ::: List(newItem) ::: replaceList
     }
-
-    def swap(i: Int, j: Int): Unit = {
-      val t = nums(i)
-      nums(i) = nums(j)
-      nums(j) = t
-    }
-
-    nums.indices.drop(1).reverse.find(r => {
-      val x = r - 1
-      nums(x) < nums(r)
-    }) match {
-      case None =>
-        reverse(0, nums.indices.last)
-      case Some(r) =>
-        val x = r - 1
-        val Some(y) = (r until nums.length).reverse.find(y => nums(y) > nums(x))
-        swap(x, y)
-        reverse(r, nums.indices.last)
-    }
+    result.indices.foreach(i => nums(i) = result(i))
   }
 }
