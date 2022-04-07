b29828621b3aed22952baea0ed543d86ac8a9847
Justdwiwt <len_master@hotmail.com>
2022-04-07 18:16:30 +0800

    leetCode 1046

--- src/main/scala/leetCode/Solution_1046.scala
+++ src/main/scala/leetCode/Solution_1046.scala
@@ -1,11 +1,16 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1046 {
   def lastStoneWeight(stones: Array[Int]): Int = {
-    val pq = mutable.PriorityQueue[Int]() ++ stones
-    while (pq.size > 1) pq += (pq.dequeue - pq.dequeue)
-    if (pq.isEmpty) 0 else pq.dequeue
+    @scala.annotation.tailrec
+    def f(lst: List[Int]): Int = lst match {
+      case List(n) => n
+      case h :: s :: tl =>
+        if (h == s) f(tl)
+        else f(((h - s).abs +: tl).sortWith(_ > _))
+      case _ => 0
+    }
+
+    f(stones.toList.sortWith(_ > _))
   }
 }
