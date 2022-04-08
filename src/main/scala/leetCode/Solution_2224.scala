34d07c35c5f5ccf9ee017924357dff6715e60a82
Justdwiwt <len_master@hotmail.com>
2022-04-08 19:58:10 +0800

    leetCode 2224

---
+++ src/main/scala/leetCode/Solution_2224.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2224 {
+  def convertTime(current: String, correct: String): Int = Array(60, 15, 5, 1)./:(0, (minutes(current) - minutes(correct)).abs) {
+    case ((ops, minutesLeft), unit) => (ops + minutesLeft / unit, minutesLeft % unit)
+  }._1
+
+  def minutes(s: String): Int =
+    s.take(2).toInt * 60 + s.takeRight(2).toInt
+}
