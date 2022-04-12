40132aab867fb9f9310ee5e4479cf5491e194d30
Justdwiwt <len_master@hotmail.com>
2022-04-12 18:30:54 +0800

    leetCode 2233

---
+++ src/main/scala/leetCode/Solution_2233.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2233 {
+  def maximumProduct(nums: Array[Int], k: Int): Int = {
+    val pq = mutable.PriorityQueue(nums: _*).reverse
+    (0 until k).foreach(_ => pq += (pq.dequeue() + 1))
+    pq.toArray.map(_.toLong).reduce(_ * _ % 1000000007).toInt
+  }
+}
