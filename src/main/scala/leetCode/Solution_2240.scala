d526a6c7669dbcbdc76dcf6c036ae0f76a85b371
Justdwiwt <len_master@hotmail.com>
2022-04-19 18:07:14 +0800

    leetCode 2240

---
+++ src/main/scala/leetCode/Solution_2240.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2240 {
+  def waysToBuyPensPencils(total: Int, cost1: Int, cost2: Int): Long = (0 to total / cost1)
+    .iterator
+    .map(n => (total - n.toLong * cost1) / cost2 + 1)
+    .sum
+}
