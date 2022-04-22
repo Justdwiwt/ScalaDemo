016cf39550d93379055f163a075c574d97f82a19
Justdwiwt <len_master@hotmail.com>
2022-04-22 18:09:47 +0800

    leetCode 396

--- src/main/scala/leetCode/Solution_396.scala
+++ src/main/scala/leetCode/Solution_396.scala
@@ -3,8 +3,8 @@ package leetCode
 object Solution_396 {
   def maxRotateFunction(A: Array[Int]): Int = {
     val sum = A.sum
-    val seed = ((0, 0) /: A) { case ((s, k), n) => (s + k * n, k + 1) }._1
-    (A.indices :\ (seed, seed)) { case (i, (m, s)) =>
+    val seed = A./:(0, 0) { case ((s, k), n) => (s + k * n, k + 1) }._1
+    A.indices.:\(seed, seed) { case (i, (m, s)) =>
       val newS = s + sum - A(i) * A.length
       (m.max(newS), newS)
     }._1
