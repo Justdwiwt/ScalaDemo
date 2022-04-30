4aa94359b21d9b19d1d1b8df4618541a38e7ed8c
Justdwiwt <len_master@hotmail.com>
2022-04-30 16:49:59 +0800

    leetCode 1898,2059

--- src/main/scala/leetCode/Solution_1898.scala
+++ src/main/scala/leetCode/Solution_1898.scala
@@ -1,32 +1,26 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1898 {
-  def maximumRemovals(s: String, p: String, remo: Array[Int]): Int = {
-    val st = mutable.HashSet.empty[Int]
-    var last = -1
-    var start = 0
-    var end = remo.length - 1
-    var mx = 0
-    while (start <= end) {
-      val mid = (end + start) >> 1
-      if (last < mid) (last + 1 to mid).foreach(st add remo(_))
-      else (last to mid + 1 by -1).foreach(st remove remo(_))
-      last = mid
-      if ( {
-        var i, j = 0
-        while (i < s.length && j < p.length) {
-          if (s.charAt(i) == p.charAt(j) && !st.contains(i)) j += 1
-          i += 1
+  def maximumRemovals(s: String, p: String, removable: Array[Int]): Int = {
+    def isSubsequence(n: Int): Boolean = {
+      val removed = removable.iterator.take(n).toSet
+      Iterator
+        .iterate(0 -> 0) {
+          case (i, j) if removed.contains(i) || s(i) != p(j) => (i + 1, j)
+          case (i, j) => (i + 1, j + 1)
         }
-        j == p.length
-      }) {
-        mx = mx.max(mid + 1)
-        start = mid + 1
-      }
-      else end = mid - 1
+        .find { case (i, j) => i == s.length || j == p.length }
+        .exists(_._2 == p.length)
     }
-    mx
+
+    @scala.annotation.tailrec
+    def search(start: Int, end: Int): Int =
+      if (start + 1 == end) start
+      else {
+        val mid = (start + end) / 2
+        if (isSubsequence(mid)) search(mid, end) else search(start, mid)
+      }
+
+    search(0, removable.length + 1)
   }
 }
--- src/main/scala/leetCode/Solution_2059.scala
+++ src/main/scala/leetCode/Solution_2059.scala
@@ -1,24 +1,27 @@
 package leetCode
 
-import scala.collection.mutable
+import scala.collection.immutable.BitSet
 
 object Solution_2059 {
+  private val Range = 0 to 1000
+
   def minimumOperations(nums: Array[Int], start: Int, goal: Int): Int = {
-    val arr = Array.fill(1001)(-1)
-    arr(start) = 0
-    val q = mutable.Queue(start)
-    while (q.nonEmpty) {
-      val cur = q.dequeue()
-      val d = arr(cur)
-      nums.foreach(num =>
-        Array(num + cur, cur - num, cur ^ num).foreach(next =>
-          if (next == goal) return d + 1
-          else if (0 <= next && next <= 1000 && arr(next) == -1) {
-            arr(next) = d + 1
-            q.enqueue(next)
-          })
-      )
-    }
-    -1
+    @scala.annotation.tailrec
+    def f(xs: Set[Int], visited: Set[Int], numOperations: Int): Int =
+      if (xs.isEmpty) -1
+      else xs
+        .iterator
+        .flatMap(x => nums
+          .iterator
+          .flatMap(num => Iterator(x + num, x - num, x ^ num).filter(!xs.contains(_)).filter(!visited.contains(_))))
+        .partition(Range.contains) match {
+        case (inRange, _) if Range.contains(goal) =>
+          val newXs = inRange.toSet
+          if (newXs.contains(goal)) numOperations + 1 else f(newXs, visited | xs, numOperations + 1)
+        case (_, notInRange) if notInRange.contains(goal) => numOperations + 1
+        case (inRange, _) => f(inRange.toSet, visited | xs, numOperations + 1)
+      }
+
+    if (start == goal) 0 else f(BitSet(start).filter(Range.contains), BitSet.empty, 0)
   }
 }
