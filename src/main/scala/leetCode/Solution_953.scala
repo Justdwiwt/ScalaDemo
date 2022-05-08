f119ae6827c17a41dadbe868b416ba6667b4decd
Justdwiwt <len_master@hotmail.com>
2022-05-08 18:01:50 +0800

    leetCode 953

--- src/main/scala/leetCode/Solution_953.scala
+++ src/main/scala/leetCode/Solution_953.scala
@@ -3,5 +3,7 @@ package leetCode
 object Solution_953 {
   def isAlienSorted(words: Array[String], order: String): Boolean = words
     .map(_.toList.map(c => (order.indexOf(c.toString) + 'a').toChar).mkString)
-    .toList.sliding(2).forall({ case List(a, b) => a <= b })
+    .toList
+    .sliding(2)
+    .forall({ case List(a, b) => a <= b; case _ => false })
 }
