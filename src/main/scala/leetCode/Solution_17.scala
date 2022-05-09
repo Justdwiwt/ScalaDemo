3d77d4409143168c2a9e7ef3ddc70d0aa04c7e74
Justdwiwt <len_master@hotmail.com>
2022-05-09 14:01:51 +0800

    leetCode 17

--- src/main/scala/leetCode/Solution_17.scala
+++ src/main/scala/leetCode/Solution_17.scala
@@ -1,15 +1,17 @@
 package leetCode
 
 object Solution_17 {
-  def letterCombinations(digits: String): List[String] = {
-    if (digits.isEmpty) return List.empty
-    val diff = Array("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
+  private val diff = Array("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz")
 
-    def f(s: String): IndexedSeq[String] = {
-      if (s.isEmpty) IndexedSeq("")
-      else diff(s.head - '2').flatMap(i => f(s.tail).map(j => i + j))
+  def getResult(idx: Int, path: String, digits: String): List[String] =
+    if (idx == digits.length)
+      List(path)
+    else {
+      val d = digits(idx).asDigit
+      diff(d - 2).map(s => getResult(idx + 1, path + s, digits)).reduceRight(_ ++ _)
     }
 
-    f(digits).toList
-  }
+  def letterCombinations(digits: String): List[String] =
+    if (digits == "") List()
+    else getResult(idx = 0, path = "", digits = digits)
 }
