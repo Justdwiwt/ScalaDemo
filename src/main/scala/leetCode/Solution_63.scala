573ebf8657b5bdb5f4833f2713a3c053936ee1b1
Justdwiwt <len_master@hotmail.com>
2022-05-20 12:10:27 +0800

    leetCode 63

--- src/main/scala/leetCode/Solution_63.scala
+++ src/main/scala/leetCode/Solution_63.scala
@@ -1,14 +1,14 @@
 package leetCode
 
 object Solution_63 {
-  def uniquePathsWithObstacles(obstacleGrid: Array[Array[Int]]): Int = {
-    if (obstacleGrid.isEmpty || obstacleGrid(0).isEmpty || (obstacleGrid(0)(0) == 1)) return 0
-    val dp = Array.fill(obstacleGrid(0).length)(0L)
-    dp(0) = 1
-    obstacleGrid.indices.foreach(i => obstacleGrid(0).indices.foreach(j =>
-      if (obstacleGrid(i)(j) == 1) dp(j) = 0
-      else if (j > 0) dp(j) += dp(j - 1)
-    ))
-    dp(obstacleGrid(0).length - 1).toInt
-  }
+  def uniquePathsWithObstacles(obstacleGrid: Array[Array[Int]]): Int = obstacleGrid
+    .iterator
+    ./:(1 +: Vector.fill(obstacleGrid.head.length - 1)(0))((counts, obstacles) => counts
+      .iterator
+      .zip(obstacles.iterator)
+      .scanLeft(0) { case (left, (up, obs)) => if (obs == 1) 0 else left + up }
+      .drop(1)
+      .toVector
+    )
+    .last
 }
