d9bba9b37be978336161c2d2e94fb2255f0b0399
Justdwiwt <len_master@hotmail.com>
2022-05-23 14:32:26 +0800

    leetCode 2278

--- src/main/scala/leetCode/Solution_2278.scala
+++ src/main/scala/leetCode/Solution_2278.scala
@@ -2,7 +2,6 @@ package leetCode
 
 object Solution_2278 {
   def percentageLetter(s: String, letter: Char): Int = {
-    val cnt = s.count(_ == letter)
-    100 * cnt / s.length
+    s.count(_ == letter) * 100 / s.length
   }
 }
