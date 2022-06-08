e3b4aed322c668eb0d33bd0654b0d738dfa4969d
Justdwiwt <len_master@hotmail.com>
2022-06-09 00:29:50 +0800

    leetCode 2296

--- src/main/scala/leetCode/Solution_2296.scala
+++ src/main/scala/leetCode/Solution_2296.scala
@@ -3,49 +3,47 @@ package leetCode
 import scala.collection.mutable
 
 object Solution_2296 {
+  class TextEditor() {
 
-}
+    var sb = new mutable.StringBuilder()
+    var cur = 0
 
-class TextEditor() {
-
-  var sb = new mutable.StringBuilder()
-  var cur = 0
-
-  def addText(text: String) {
-    sb.insert(cur, text)
-    cur += text.length
-  }
-
-  def deleteText(k: Int): Int = {
-    if (cur <= k) {
-      val old = cur
-      sb.replace(0, old, "")
-      cur = 0
-      old
-    } else {
-      sb.replace(cur - k, cur, "")
-      cur -= k
-      k
+    def addText(text: String): Unit = {
+      sb.insert(cur, text)
+      cur += text.length
     }
-  }
 
-  def cursorLeft(k: Int): String = {
-    if (cur >= k) {
-      cur -= k
+    def deleteText(k: Int): Int = {
+      if (cur <= k) {
+        val old = cur
+        sb.replace(0, old, "")
+        cur = 0
+        old
+      } else {
+        sb.replace(cur - k, cur, "")
+        cur -= k
+        k
+      }
+    }
+
+    def cursorLeft(k: Int): String = {
+      if (cur >= k) {
+        cur -= k
+        if (cur >= 10) sb.substring(cur - 10, cur)
+        else sb.substring(0, cur)
+      } else {
+        cur = 0
+        ""
+      }
+    }
+
+    def cursorRight(k: Int): String = {
+      val length = sb.length
+      if (cur + k <= length) cur += k
+      else cur = length
       if (cur >= 10) sb.substring(cur - 10, cur)
       else sb.substring(0, cur)
-    } else {
-      cur = 0
-      ""
     }
-  }
 
-  def cursorRight(k: Int): String = {
-    val length = sb.length
-    if (cur + k <= length) cur += k
-    else cur = length
-    if (cur >= 10) sb.substring(cur - 10, cur)
-    else sb.substring(0, cur)
   }
-
 }
