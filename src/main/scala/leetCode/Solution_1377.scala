57eccd95d6a4c337c40a344fbe1602b1c65867b3
Justdwiwt <len_master@hotmail.com>
2022-06-12 14:02:27 +0800

    leetCode 1377

--- src/main/scala/leetCode/Solution_1377.scala
+++ src/main/scala/leetCode/Solution_1377.scala
@@ -1,36 +1,23 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_1377 {
-  //  def frogPosition(n: Int, edges: Array[Array[Int]], t: Int, target: Int): Double =
-  //    frogPosition(Edges(edges), t, target)
-  //
-  //  def frogPosition(edges: Edges, time: Int, target: Int): Double = {
-  //    def dfs(vertex: Int, time: Int, probability: Double, visited: Set[Int]): Double = {
-  //      val neighbors = edges.neighbors(vertex).filterNot(visited.contains)
-  //      if (time == 0 || neighbors.isEmpty) {
-  //        if (vertex == target) probability
-  //        else 0.0
-  //      } else {
-  //        LazyList(neighbors: _*)
-  //          .map(dfs(_, time - 1, probability / neighbors.length, visited + vertex))
-  //          .find(_ > 0.0)
-  //          .getOrElse(0.0)
-  //      }
-  //    }
-  //
-  //    dfs(1, time, 1.0, Set())
-  //  }
-  //
-  //  final class Edges(map: Map[Int, Array[Int]]) {
-  //    def neighbors(u: Int): Array[Int] = map.getOrElse(u, Array())
-  //  }
-  //
-  //  object Edges {
-  //    def apply(edges: Array[Array[Int]]): Edges = new Edges(edges
-  //      .flatMap {
-  //        case Array(u, v) => Iterable(u -> v, v -> u)
-  //        case _ => None
-  //      }
-  //      .groupMap(_._1)(_._2))
-  //  }
+  def frogPosition(n: Int, edges: Array[Array[Int]], t: Int, target: Int): Double = {
+    val m = mutable.Map.empty[Int, Set[Int]]
+    edges.foreach { case Array(a, b) =>
+      m += a -> (m.getOrElse(a, Set.empty) + b)
+      m += b -> (m.getOrElse(b, Set.empty) + a)
+    }
+
+    def dfs(probability: Double, jumps: Int, vertex: Int, prev: Int): Double = {
+      lazy val next = m.getOrElse(vertex, Set.empty) - prev
+      if (prev == vertex) 0
+      else if (next.nonEmpty && jumps > 0) next.map(dfs(probability / next.size, jumps - 1, _, vertex)).sum
+      else if (vertex == target) probability
+      else 0
+    }
+
+    dfs(1, t, 1, 0)
+  }
 }
