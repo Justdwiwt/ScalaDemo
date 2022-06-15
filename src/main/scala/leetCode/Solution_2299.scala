673c08d938edd573174e2dcf111743cf9a52ac26
Justdwiwt <len_master@hotmail.com>
2022-06-15 14:10:35 +0800

    leetCode 2299

---
+++ src/main/scala/leetCode/Solution_2299.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2299 {
+  def strongPasswordCheckerII(password: String): Boolean = Some(password)
+    .filter(_.length >= 8)
+    .filter(_.exists(_.isUpper))
+    .filter(_.exists(_.isLower))
+    .filter(_.exists(_.isDigit))
+    .filter(_.exists("!@#$%^&*()-+".toList.contains(_)))
+    .exists(!_.toList.sliding(2).exists {
+      case x :: xs :: _ => x == xs
+      case _ => false
+    })
+}
