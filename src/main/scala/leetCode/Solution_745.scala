6af2dfcb8e91c8791df3cd600f32f9387a60fd45
Justdwiwt <len_master@hotmail.com>
2022-06-18 12:47:27 +0800

    leetCode 745

--- src/main/scala/leetCode/Solution_745.scala
+++ src/main/scala/leetCode/Solution_745.scala
@@ -3,17 +3,43 @@ package leetCode
 import scala.collection.mutable
 
 object Solution_745 {
-
   class WordFilter(_words: Array[String]) {
+    class TrieNode {
+      val children = mutable.HashMap.empty[Char, TrieNode]
+      var wordIdx: Int = -1
+    }
 
-    private val m = new mutable.HashMap[String, Int]()
+    def buildTrie(charList: List[Char], curNode: TrieNode, wordIndex: Int): TrieNode = charList match {
+      case Nil =>
+        curNode.wordIdx = wordIndex
+        curNode
+      case c :: tail =>
+        curNode.wordIdx = wordIndex
+        if (!curNode.children.contains(c)) curNode.children += ((c, new TrieNode()))
+        buildTrie(tail, curNode.children(c), wordIndex)
+    }
 
-    _words.indices.foreach(k => _words(k).indices.foreach(i => _words(k).indices.foreach(j =>
-      m(_words(k).substring(0, i) + "#" + _words(k).substring(_words(k).length - j)) = k)))
+    def shiftList(charList: List[Char], i: Int): List[Char] =
+      charList.drop(charList.length - i) ++ List('#') ++ charList
 
-    def f(prefix: String, suffix: String): Int = if (m.contains(prefix + "#" + suffix)) m(prefix + "#" + suffix) else -1
+    lazy val root: TrieNode = {
+      val root = new TrieNode()
+      _words.zipWithIndex.foreach(pair => {
+        val (word, i) = pair
+        val wordList = word.toList
+        (1 to wordList.length).foreach(j => buildTrie(shiftList(wordList, j), root, i))
+      })
+      root
+    }
 
+    def f(prefix: String, suffix: String): Int = {
+      @scala.annotation.tailrec
+      def search(charList: List[Char], curNode: TrieNode): Int = charList match {
+        case Nil => curNode.wordIdx
+        case c :: tail => if (curNode.children.contains(c)) search(tail, curNode.children(c)) else -1
+      }
+
+      search((suffix + "#" + prefix).toList, root)
+    }
   }
-
 }
-
