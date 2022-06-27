07301c2286ca338bed8b41e77060fa8ea3134dce
Justdwiwt <len_master@hotmail.com>
2022-06-27 15:11:54 +0800

    leetCode 2309

--- src/main/scala/leetCode/Solution_2309.scala
+++ src/main/scala/leetCode/Solution_2309.scala
@@ -1,16 +1,9 @@
 package leetCode
 
 object Solution_2309 {
-  def greatestLetter(s: String): String = {
-    var k = 0L
-    var mask = ((1L << 32) + 1) << 25
-    s.toCharArray.foreach(c => k |= (1L << (c - 'A')))
-    var idx = 25
-    while (idx >= 0) {
-      if ((k & mask) == mask) return "" + (idx + 'A').toChar
-      idx -= 1
-      mask >>= 1
-    }
-    ""
-  }
+  def greatestLetter(s: String): String = ('Z'.toInt to 'A'.toInt by -1)
+    .map(_.toChar)
+    .find(c => s.contains(c) && s.contains(c.toLower))
+    .map(_.toString)
+    .getOrElse("")
 }
