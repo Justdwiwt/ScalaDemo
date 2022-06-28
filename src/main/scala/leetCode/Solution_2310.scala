a39ec0a8f5b8b0114539ed0ff8a5c3992bca29bb
Justdwiwt <len_master@hotmail.com>
2022-06-28 10:08:44 +0800

    leetCode 2310

--- src/main/scala/leetCode/Solution_2310.scala
+++ src/main/scala/leetCode/Solution_2310.scala
@@ -1,9 +1,7 @@
 package leetCode
 
 object Solution_2310 {
-  def minimumNumbers(num: Int, k: Int): Int = {
-    if (num == 0) return 0
-    (1 to 10).foreach(i => if ((num - k * i) >= 0 && (num - k * i) % 10 == 0) return i)
-    -1
-  }
+  def minimumNumbers(num: Int, k: Int): Int =
+    if (num == 0) 0
+    else (1 to 10).find(i => i * k % 10 == num % 10 && i * k <= num).getOrElse(-1)
 }
