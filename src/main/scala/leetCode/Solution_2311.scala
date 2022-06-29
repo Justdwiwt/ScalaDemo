28271bf0322e9ba96b0362ae4a6f7a110ae16c2e
Justdwiwt <len_master@hotmail.com>
2022-06-29 15:00:56 +0800

    leetCode 2311

---
+++ src/main/scala/leetCode/Solution_2311.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_2311 {
+  def longestSubsequence(s: String, k: Int): Int = {
+    @scala.annotation.tailrec
+    def f(i: Int, value: Int, pow: Int, cnt: Int): Int =
+      if (i < 0 || value + pow > k) s.count(_ == '0') + cnt
+      else if (s(i) == '1') f(i - 1, value + pow, pow << 1, cnt + 1)
+      else f(i - 1, value, pow << 1, cnt)
+
+    f(i = s.indices.last, value = 0, pow = 1, cnt = 0)
+  }
+}
