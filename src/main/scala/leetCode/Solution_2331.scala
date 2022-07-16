3e775e8046425cb0219d8c414eae144d56abec3b
Justdwiwt <len_master@hotmail.com>
2022-07-16 14:08:25 +0800

    leetCode 2331

---
+++ src/main/scala/leetCode/Solution_2331.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2331 {
+  def evaluateTree(root: TreeNode): Boolean = root.value match {
+    case 0 => false
+    case 1 => true
+    case 2 => evaluateTree(root.left) || evaluateTree(root.right)
+    case 3 => evaluateTree(root.left) && evaluateTree(root.right)
+  }
+}
