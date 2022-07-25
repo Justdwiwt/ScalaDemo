7e8272e0bee8555849126543563a8ab33e0e68d9
Justdwiwt <len_master@hotmail.com>
2022-07-25 13:59:08 +0800

    leetCode 2341

--- src/main/scala/leetCode/Solution_2341.scala
+++ src/main/scala/leetCode/Solution_2341.scala
@@ -1,9 +1,8 @@
 package leetCode
 
 object Solution_2341 {
-  def numberOfPairs(nums: Array[Int]): Array[Int] = {
-    val arr = Array.fill(101)(0)
-    nums.foreach(arr(_) += 1)
-    Array(arr.map(_ / 2).sum, arr.map(_ % 2).sum)
-  }
+  def numberOfPairs(nums: Array[Int]): Array[Int] = nums
+    .groupBy(identity)
+    .mapValues(_.length)
+    ./:(Array(0, 0)) { case (res, (_, cnt)) => Array(res.head + cnt / 2, res(1) + cnt % 2) }
 }
