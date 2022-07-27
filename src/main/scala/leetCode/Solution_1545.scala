49000c1070cc57fed606bff2f5bd1cf6fcd7e6e1
Justdwiwt <len_master@hotmail.com>
2022-07-27 13:58:39 +0800

    leetCode 1545

--- src/main/scala/leetCode/Solution_1545.scala
+++ src/main/scala/leetCode/Solution_1545.scala
@@ -2,10 +2,11 @@ package leetCode
 
 object Solution_1545 {
   def findKthBit(n: Int, k: Int): Char = {
-    if (n == 1 || k == 1) return '0'
-    if (k == math.pow(2, n - 1)) return '1'
-    if (k < math.pow(2, n - 1)) findKthBit(n - 1, k)
-    else if (findKthBit(n, math.pow(2, n).toInt - k) == '1') '0'
-    else '1'
+    @scala.annotation.tailrec
+    def f(n: Int, k: Int, N: Int, s: String): Char =
+      if (n == N) s(k - 1)
+      else f(n + 1, k, N, s + "1" + s.reverse.map(49 - _.toInt).mkString)
+
+    f(1, k, n, "0")
   }
 }
