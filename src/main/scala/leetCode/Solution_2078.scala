b20e11085356199619dc64f7e3830d6cd6909798
Justdwiwt <len_master@hotmail.com>
2022-07-27 14:34:16 +0800

    leetCode 2078,2079

--- src/main/scala/leetCode/Solution_2078.scala
+++ src/main/scala/leetCode/Solution_2078.scala
@@ -1,9 +1,10 @@
 package leetCode
 
 object Solution_2078 {
-  def maxDistance(colors: Array[Int]): Int = {
-    var res = 0
-    colors.indices.dropRight(1).foreach(i => (i + 1 until colors.length).withFilter(j => colors(i) != colors(j)).foreach(j => res = res.max(j - i)))
-    res
-  }
+  def maxDistance(colors: Array[Int]): Int = colors
+    .indices
+    .flatMap(i => ((i + 1) until colors.length)
+      .withFilter(j => colors(j) != colors(i))
+      .map(j => j - i))
+    .max
 }
--- src/main/scala/leetCode/Solution_2079.scala
+++ src/main/scala/leetCode/Solution_2079.scala
@@ -1,17 +1,11 @@
 package leetCode
 
 object Solution_2079 {
-  def wateringPlants(plants: Array[Int], cap: Int): Int = {
-    var cur = cap
-    plants.indices.map(i => {
-      val t = if (cur >= plants(i)) {
-        cur -= plants(i)
-        1
-      } else {
-        cur = cap - plants(i)
-        2 * i + 1
-      }
-      t
-    }).sum
-  }
+  def wateringPlants(plants: Array[Int], capacity: Int): Int = plants
+    .zip(Stream.from(1))
+    ./:((capacity, 0))((acc, cur) => {
+      if (cur._1 <= acc._1) (acc._1 - cur._1, acc._2 + 1)
+      else (capacity - cur._1, acc._2 + (cur._2 - 1) * 2 + 1)
+    })
+    ._2
 }
