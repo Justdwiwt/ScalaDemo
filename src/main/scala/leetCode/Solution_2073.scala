bb1c76e824a51b6e0e160ed507ee1b96864178b3
Justdwiwt <len_master@hotmail.com>
2022-07-29 14:24:48 +0800

    leetCode 2073

--- src/main/scala/leetCode/Solution_2073.scala
+++ src/main/scala/leetCode/Solution_2073.scala
@@ -1,18 +1,13 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2073 {
-  def timeRequiredToBuy(tickets: Array[Int], k: Int): Int = {
-    val q = mutable.Queue.empty[(Int, Int)]
-    tickets.zipWithIndex.foreach({ case (v, i) => q += ((v, i)) })
-    var res = 0
-    while (q.nonEmpty) {
-      val cur = q.dequeue
-      res += 1
-      if (cur._1 == 1 && cur._2 == k) return res
-      if (cur._1 > 1) q += ((cur._1 - 1, cur._2))
-    }
-    res
-  }
+  def timeRequiredToBuy(tickets: Array[Int], k: Int): Int = tickets
+    .indices
+    .filter(_ <= k)
+    .map(i => tickets(i).min(tickets(k)))
+    .sum + tickets
+    .indices
+    .filter(_ > k)
+    .map(i => tickets(i).min(tickets(k) - 1))
+    .sum
 }
