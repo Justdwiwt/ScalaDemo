758bcb59e38bc96df389a76dca17fc5548550fbe
Justdwiwt <len_master@hotmail.com>
2022-07-31 15:57:12 +0800

    leetCode 2347

---
+++ src/main/scala/leetCode/Solution_2347.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_2347 {
+  def bestHand(ranks: Array[Int], suits: Array[Char]): String =
+    if (suits.min == suits.max) "Flush"
+    else ranks.groupBy(identity).mapValues(_.length).values.max match {
+      case 5 | 4 | 3 => "Three of a Kind"
+      case 2 => "Pair"
+      case _ => "High Card"
+    }
+}
