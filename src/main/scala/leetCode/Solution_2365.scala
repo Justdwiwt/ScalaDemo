5b20191a99b8cbc59addd03d54ab10f2ca341cc9
Justdwiwt <len_master@hotmail.com>
2022-08-11 13:44:34 +0800

    leetCode 2365

---
+++ src/main/scala/leetCode/Solution_2365.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_2365 {
+  def taskSchedulerII(tasks: Array[Int], space: Int): Long =
+    f(tasks.toList, Map.empty, 1, space)
+
+  @scala.annotation.tailrec
+  def f(tasks: List[Int], breakMap: Map[Int, Long], day: Long, space: Int): Long = tasks match {
+    case Nil => day - 1
+    case h :: tail =>
+      val breakUntil = breakMap.getOrElse(h, 0L)
+      if (day > breakUntil) f(tail, breakMap.updated(h, day + space), day + 1, space)
+      else f(tasks, breakMap, breakUntil + 1, space)
+  }
+}
