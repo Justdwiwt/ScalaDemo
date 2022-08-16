f54e56694b1b0be4cc6e874e98cf6a1bda75b66b
Justdwiwt <len_master@hotmail.com>
2022-08-16 14:33:53 +0800

    leetCode 2369

---
+++ src/main/scala/leetCode/Solution_2369.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2369 {
+  def validPartition(nums: Array[Int]): Boolean = {
+    val dp = Array.fill(nums.length + 1)(false)
+    dp(0) = true
+    (2 to nums.length).foreach(i => {
+      dp(i) |= nums(i - 1) == nums(i - 2) && dp(i - 2)
+      dp(i) |= i > 2 && nums(i - 1) == nums(i - 2) && nums(i - 2) == nums(i - 3) && dp(i - 3)
+      dp(i) |= i > 2 && nums(i - 1) == nums(i - 2) + 1 && nums(i - 2) == nums(i - 3) + 1 && dp(i - 3)
+    })
+    dp.last
+  }
+}
