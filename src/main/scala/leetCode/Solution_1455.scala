ad2f7efdcb8aa430f63c572a6972ace4b2631ced
Justdwiwt <len_master@hotmail.com>
2022-08-21 14:14:58 +0800

    leetCode 1455

--- src/main/scala/leetCode/Solution_1455.scala
+++ src/main/scala/leetCode/Solution_1455.scala
@@ -1,7 +1,11 @@
 package leetCode
 
 object Solution_1455 {
-  def isPrefixOfWord(sentence: String, searchWord: String): Int = {
-    sentence.split(' ').iterator.zipWithIndex.find(_._1.startsWith(searchWord)).map(_._2 + 1).getOrElse(-1)
-  }
+  def isPrefixOfWord(sentence: String, searchWord: String): Int = sentence
+    .split(' ')
+    .iterator
+    .zipWithIndex
+    .find(_._1.startsWith(searchWord))
+    .map(_._2 + 1)
+    .getOrElse(-1)
 }
