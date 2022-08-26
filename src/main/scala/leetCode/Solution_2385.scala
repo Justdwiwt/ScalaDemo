b0a8a14742327ebb3f247991c79eca5c4e2e701e
Justdwiwt <len_master@hotmail.com>
2022-08-26 14:36:06 +0800

    leetCode 2385

---
+++ src/main/scala/leetCode/Solution_2385.scala
@@ -0,0 +1,36 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2385 {
+  def amountOfTime(root: TreeNode, start: Int): Int = {
+    val adjacent = buildAdjacent(root)
+    val visited = scala.collection.mutable.Set(start)
+    var visit = List(start)
+    var minutes = -1
+    while (visit.nonEmpty) {
+      visit = visit.flatMap(adjacent(_)).filter(visited.add)
+      minutes += 1
+    }
+    minutes
+  }
+
+  def buildAdjacent(root: TreeNode): mutable.Map[Int, List[Int]] = {
+    val adjacent = scala.collection.mutable.Map.empty[Int, List[Int]].withDefaultValue(List.empty)
+
+    def dfs(parent: TreeNode): Unit = {
+      def go(child: TreeNode): Unit =
+        if (child != null) {
+          adjacent(parent.value) ::= child.value
+          adjacent(child.value) ::= parent.value
+          dfs(child)
+        }
+
+      go(parent.left)
+      go(parent.right)
+    }
+
+    dfs(root)
+    adjacent
+  }
+}
