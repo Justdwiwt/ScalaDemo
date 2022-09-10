f5ba6d2c5741bd6bc857f0b8e423e8519e623835
Justdwiwt <len_master@hotmail.com>
2022-09-10 14:05:03 +0800

    leetCode 2399

--- src/main/scala/leetCode/Solution_2399.scala
+++ src/main/scala/leetCode/Solution_2399.scala
@@ -2,12 +2,12 @@ package leetCode
 
 object Solution_2399 {
   def checkDistances(s: String, distance: Array[Int]): Boolean = {
-    val arr = Array.fill(26)(-1)
-    s.indices.foreach(i => {
-      val a = s(i) - 'a'
-      if (arr(a) == -1) arr(a) = i
-      else if (i - arr(a) - 1 != distance(a)) return false
-    })
-    true
+    def f(c: Char): Boolean = {
+      val i = s.indexOf(c)
+      val j = s.indexOf(c, i + 1)
+      distance(c - 97) == j - i - 1
+    }
+
+    s.distinct.forall(f)
   }
 }
