1ad1a7d49ead17b8a5d92e0ec4d3146456a2ddaf
Justdwiwt <len_master@hotmail.com>
2022-09-17 14:30:21 +0800

    leetCode 2389

--- src/main/scala/leetCode/Solution_2389.scala
+++ src/main/scala/leetCode/Solution_2389.scala
@@ -2,23 +2,11 @@ package leetCode
 
 object Solution_2389 {
   def answerQueries(nums: Array[Int], queries: Array[Int]): Array[Int] = {
-    val res = Array.fill(queries.length)(0)
-    val sorted = nums.sorted
-    nums.indices.drop(1).foreach(i => sorted(i) += sorted(i - 1))
-    queries.indices.foreach(i => res(i) = f(sorted, queries(i)))
-    res
-  }
+    val pre = nums.sorted.scanLeft(0)(_ + _)
 
-  def f(nums: Array[Int], find: Int): Int = {
-    var l = 0
-    var r = nums.length - 1
-    while (l < r) {
-      val m = (l + r) >>> 1
-      if (nums(m) == find) return m + 1
-      if (nums(m) < find) l = m + 1
-      else r = m
-    }
-    if (nums(l) <= find) l + 1
-    else l
+    def f(target: Int): Int =
+      pre.takeWhile(_ <= target).length - 1
+
+    queries.map(f)
   }
 }
