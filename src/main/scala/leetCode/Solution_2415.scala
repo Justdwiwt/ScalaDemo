17b0da5d1d1caeb3f718a2a02585a815bc0a468d
Justdwiwt <len_master@hotmail.com>
2022-09-26 15:33:16 +0800

    leetCode 2413,2414,2415

---
+++ src/main/scala/leetCode/Solution_2415.scala
@@ -0,0 +1,21 @@
+package leetCode
+
+object Solution_2415 {
+  def reverseOddLevels(root: TreeNode): TreeNode = {
+    reverse(root.left, root.right, flag = true)
+    root
+  }
+
+  def swap(l: TreeNode, r: TreeNode): Unit = {
+    val t = l.value
+    l.value = r.value
+    r.value = t
+  }
+
+  def reverse(l: TreeNode, r: TreeNode, flag: Boolean): Unit = {
+    if (l == null) return
+    if (flag) swap(l, r)
+    reverse(l.left, r.right, !flag)
+    reverse(l.right, r.left, !flag)
+  }
+}
