4d58d0ae453ea98f0fdbc76fef266921fce0d957
Justdwiwt <len_master@hotmail.com>
2022-10-04 14:40:15 +0800

    leetCode 2428

---
+++ src/main/scala/leetCode/Solution_2428.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2428 {
+  def maxSum(grid: Array[Array[Int]]): Int = (0 until (grid.length - 2))
+    .flatMap(i => (0 until (grid.head.length - 2))
+      .map(j => grid(i + 0)(j)
+        + grid(i + 0)(j + 1)
+        + grid(i + 0)(j + 2)
+        + grid(i + 1)(j + 1)
+        + grid(i + 2)(j)
+        + grid(i + 2)(j + 1)
+        + grid(i + 2)(j + 2)))
+    .max
+}
