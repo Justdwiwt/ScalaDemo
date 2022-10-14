56d668f0d7dc7a2bbe3fa6b6c83c2522f1fd2d98
Justdwiwt <len_master@hotmail.com>
2022-10-14 13:02:34 +0800

    leetCode 2128

---
+++ src/main/scala/leetCode/Solution_2128.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2128 {
+  def removeOnes(grid: Array[Array[Int]]): Boolean = {
+    grid.indices.drop(1).foreach(i => grid.head.indices.drop(1).foreach(j =>
+      if ((grid(i)(j) + grid.head(j)) % 2 != (grid(i).head + grid.head.head) % 2) return false
+    ))
+    true
+  }
+}
