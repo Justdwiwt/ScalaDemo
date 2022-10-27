0e4db9a13aa64aab43075c98c172235fdb7f6717
Justdwiwt <len_master@hotmail.com>
2022-10-27 17:18:31 +0800

    leetCode 2441

---
+++ src/main/scala/leetCode/Solution_2441.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2441 {
+  def findMaxK(nums: Array[Int]): Int = {
+    val filtered = nums.filter(n => n > 0 && nums.contains(-n))
+    if (filtered.isEmpty) -1 else filtered.max
+  }
+}
