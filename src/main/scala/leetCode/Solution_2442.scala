6daa132a198a36921b7d66ebbf4655f079a3e490
Justdwiwt <len_master@hotmail.com>
2022-10-28 16:59:41 +0800

    leetCode 2442

---
+++ src/main/scala/leetCode/Solution_2442.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2442 {
+  def reverse(i: Int): Int = i.toString.reverse.toInt
+
+  def countDistinctIntegers(nums: Array[Int]): Int =
+    (nums ++ nums.map(reverse)).distinct.length
+}
