62ad00719cda97c8bab06d97a9b4d473cd45bba3
Justdwiwt <len_master@hotmail.com>
2022-10-30 15:17:33 +0800

    leetCode 2453

---
+++ src/main/scala/leetCode/Solution_2453.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2453 {
+  def destroyTargets(nums: Array[Int], space: Int): Int = nums
+    .sorted
+    .groupBy(_ % space)
+    .values
+    .maxBy(n => (n.length, -n.head))
+    .head
+}
