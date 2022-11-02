f059d4addaa0567b6c0d1d46944d55a602ece81d
Justdwiwt <len_master@hotmail.com>
2022-11-02 18:12:04 +0800

    leetCode 1062

---
+++ src/main/scala/leetCode/Solution_1062.scala
@@ -0,0 +1,20 @@
+package leetCode
+
+object Solution_1062 {
+  def longestRepeatingSubstring(s: String): Int = {
+    var res = 0
+    var t = 0
+    s.indices.drop(1).foreach(i => {
+      t = 0
+      (i until s.length).foreach(j => {
+        if (s(j) == s(j - i)) t += 1
+        else {
+          res = res.max(t)
+          t = 0
+        }
+        res = res.max(t)
+      })
+    })
+    res
+  }
+}
