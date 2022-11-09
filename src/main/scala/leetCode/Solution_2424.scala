a91e0473db00a4217b2d22abd7a6f96856e3c66f
Justdwiwt <len_master@hotmail.com>
2022-11-09 14:14:41 +0800

    leetCode 2424

---
+++ src/main/scala/leetCode/Solution_2424.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Solution_2424 {
+  class LUPrefix(_n: Int) {
+
+    val st = scala.collection.mutable.Set.empty[Int]
+    var mx = 0
+
+    def upload(video: Int) {
+      st.add(video)
+      while (st.contains(mx + 1))
+        mx += 1
+    }
+
+    def longest(): Int = mx
+
+  }
+}
