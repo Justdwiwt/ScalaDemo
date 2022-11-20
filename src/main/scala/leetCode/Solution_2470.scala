21d29f62c73c4f7536ccbcb91287533076a7fd07
Justdwiwt <len_master@hotmail.com>
2022-11-20 15:16:46 +0800

    leetCode 2470

--- src/main/scala/leetCode/Solution_2470.scala
+++ src/main/scala/leetCode/Solution_2470.scala
@@ -1,17 +1,24 @@
 package leetCode
 
-import java.math.BigInteger
+
+import scala.math.Integral.Implicits._
 
 object Solution_2470 {
+  @scala.annotation.tailrec
+  def gcd[T: Integral](a: T, b: T): T = if (b == 0) a else gcd(b, a % b)
+
+  def lcm[T: Integral](a: T, b: T): T = a / gcd(a, b) * b
+
   def subarrayLCM(nums: Array[Int], k: Int): Int = {
-    var res = 0
-    nums.indices.foreach(i => {
-      var lcm = BigInteger.ONE
-      (i until nums.length).foreach(j => {
-        lcm = lcm.multiply(BigInteger.valueOf(nums(j))).divide(lcm.gcd(BigInteger.valueOf(nums(j))))
-        if (lcm.doubleValue == k) res += 1
-      })
-    })
-    res
+    def f(j: Int, currLcm: Int): Int =
+      if (j == nums.length) 0
+      else {
+        val newLcm = lcm(currLcm, nums(j))
+        if (newLcm > k) 0
+        else if (newLcm == k) 1 + f(j + 1, newLcm)
+        else f(j + 1, newLcm)
+      }
+
+    nums.indices./:(0)((cnt, i) => cnt + f(j = i, currLcm = 1))
   }
 }
