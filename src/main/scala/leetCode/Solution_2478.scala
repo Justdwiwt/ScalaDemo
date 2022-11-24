d723917b3cd40cd6a876e80ba4f71156762b6369
Justdwiwt <len_master@hotmail.com>
2022-11-24 15:11:38 +0800

    leetCode 2478

--- src/main/scala/leetCode/Solution_2478.scala
+++ src/main/scala/leetCode/Solution_2478.scala
@@ -1,25 +1,22 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_2478 {
   def beautifulPartitions(s: String, k: Int, minLength: Int): Int = {
-    val M = (1e9 + 7).toInt
-    val n = s.length
-    if (isPrime(s(n - 1)) || !isPrime(s.head)) return 0
-    val ch = s.toCharArray
-    val dp = Array.fill(k + 1, n + 1)(0L)
-    dp(0)(0) = 1
-    (1 to k).foreach(i => {
-      var now = 0L
-      (minLength to n).foreach(j => {
-        if (isPrime(ch(j - minLength))) now = (now + dp(i - 1)(j - minLength)) % M
-        if (!isPrime(ch(j - 1))) dp(i)(j) = now
-      })
-    })
-    dp(k)(n).toInt
-  }
+    def prime(ch: Char): Boolean = "2357".contains(ch)
 
-  def isPrime(c: Char): Boolean = {
-    val num = c - '0'
-    num == 2 || num == 3 || num == 5 || num == 7
+    def jump(atStart: Boolean): Int = if (atStart) minLength - 1 else 1
+
+    val m = mutable.Map.empty[(Int, Boolean, Int), Long]
+
+    def dfs(i: Int, atStart: Boolean, k: Int): Long = m.getOrElseUpdate((i, atStart, k),
+      if (i == s.length) if (k == 0) 1 else 0
+      else if (i > s.length || k == 0 || (!prime(s(i)) && atStart)) 0
+      else if (prime(s(i))) dfs(i + jump(atStart), atStart = false, k)
+      else (dfs(i + 1, atStart = true, k - 1) + dfs(i + 1, atStart, k)) % 1000000007
+    )
+
+    dfs(i = 0, atStart = true, k).toInt
   }
 }
