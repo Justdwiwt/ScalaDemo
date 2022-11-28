1ffef72a0e9e5ffc8f47bc774e79282d59d5bbff
Justdwiwt <len_master@hotmail.com>
2022-11-28 15:19:58 +0800

    leetCode 1072

--- src/main/scala/leetCode/Solution_1072.scala
+++ src/main/scala/leetCode/Solution_1072.scala
@@ -1,7 +1,11 @@
 package leetCode
 
 object Solution_1072 {
-  def maxEqualRowsAfterFlips(matrix: Array[Array[Int]]): Int = {
-    matrix.map(_.toList).map({ case h :: t => t.map(_ ^ h) }).groupBy(x => x).values.map(_.length).max
-  }
+  def maxEqualRowsAfterFlips(matrix: Array[Array[Int]]): Int = (matrix.filter(_.head == 1) ++ matrix.filter(_.head == 0)
+    .map(_.map(x => (x - 1).abs)))
+    .map(_.toList)
+    .groupBy(identity)
+    .mapValues(_.length)
+    .values
+    .max
 }
