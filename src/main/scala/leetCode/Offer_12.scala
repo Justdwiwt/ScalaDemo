858c36f873b00e9e7c41582eea8347adfb448e7c
Justdwiwt <len_master@hotmail.com>
2022-12-05 15:47:34 +0800

    leetCode 剑指Offer 12

--- src/main/scala/leetCode/Offer_12.scala
+++ src/main/scala/leetCode/Offer_12.scala
@@ -2,22 +2,19 @@ package leetCode
 
 object Offer_12 {
   def exist(board: Array[Array[Char]], word: String): Boolean = {
-    if (board == null || board.isEmpty || board(0).isEmpty || word == null || word.isEmpty) return true
-    board.indices.foreach(r => board(0).indices.foreach(c => if (board(r)(c) == word(0)) if (f(board, word, r, c, 0)) return true))
+
+    def dfs(y: Int, x: Int, pos: Int): Boolean =
+      if (pos == word.length) true
+      else if (y < 0 || y == board.length || x < 0 || x == board(0).length || board(y)(x) != word(pos)) false
+      else {
+        val char = board(y)(x)
+        board(y)(x) = ' '
+        val exist = dfs(y, x + 1, pos + 1) || dfs(y, x - 1, pos + 1) || dfs(y + 1, x, pos + 1) || dfs(y - 1, x, pos + 1)
+        board(y)(x) = char
+        exist
+      }
+
+    board.indices.foreach(y => board.head.indices.foreach(x => if (dfs(y, x, 0)) return true))
     false
   }
-
-  def f(board: Array[Array[Char]], word: String, r: Int, c: Int, start: Int): Boolean = {
-    if (start == word.length) return true
-    if (!g(board, r, c) || board(r)(c) != word(start)) return false
-    board(r)(c) = '#'
-    val res = f(board, word, r + 1, c, start + 1) ||
-      f(board, word, r, c + 1, start + 1) ||
-      f(board, word, r - 1, c, start + 1) ||
-      f(board, word, r, c - 1, start + 1)
-    board(r)(c) = word(start)
-    res
-  }
-
-  def g(board: Array[Array[Char]], r: Int, c: Int): Boolean = r >= 0 && r < board.length && c >= 0 && c < board(0).length
 }
