655e40a48ef85bb36cbdff5e2f73d3041394e746
Justdwiwt <len_master@hotmail.com>
2022-12-08 14:54:52 +0800

    leetCode 2289,2471,2488

--- src/main/scala/leetCode/Solution_2289.scala
+++ src/main/scala/leetCode/Solution_2289.scala
@@ -1,21 +1,19 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2289 {
   def totalSteps(nums: Array[Int]): Int = {
-    var res = 0
-    val st = mutable.Stack[Int]()
-    val dp = Array.fill(nums.length)(0)
-    nums.indices.foreach(i => {
-      var mx = 0
-      while (st.nonEmpty && nums(st.head) <= nums(i))
-        mx = mx.max(dp(st.pop()))
-      if (st.nonEmpty)
-        dp(i) = mx + 1
-      res = res.max(dp(i))
-      st.push(i)
-    })
-    res
+    val M = Int.MaxValue / 2
+
+    @scala.annotation.tailrec
+    def f(nums: List[Int], stack: List[(Int, Int)], res: Int): Int = nums match {
+      case Nil => res
+      case x :: xs =>
+        val (l, r) = stack.span(_._1 <= x)
+        val step = if (l == Nil) 1 else l.map(_._2).max + 1
+        f(xs, (x, step) :: r, res.max(if (step >= M) 0 else step))
+
+    }
+
+    f(nums.drop(1).toList, List((nums.head, M)), 0)
   }
 }
