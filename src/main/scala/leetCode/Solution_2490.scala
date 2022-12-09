910324ecdc9a05a0bdc59d4f5ecd1e0b0eec2f73
Justdwiwt <len_master@hotmail.com>
2022-12-09 16:01:34 +0800

    leetCode 2490

--- src/main/scala/leetCode/Solution_2490.scala
+++ src/main/scala/leetCode/Solution_2490.scala
@@ -1,8 +1,9 @@
 package leetCode
 
 object Solution_2490 {
-  def isCircularSentence(sentence: String): Boolean = {
-    sentence.indices.foreach(i => if (sentence(i) == ' ' && (sentence(i - 1) != sentence(i + 1))) return false)
-    sentence.head == sentence(sentence.length - 1)
-  }
+  def isCircularSentence(sentence: String): Boolean = sentence
+    .split(" ")
+    .sliding(2)
+    .filter(_.length == 2)
+    .forall(p => p.head.last == p(1).head) && sentence.head == sentence.last
 }
