30228ef1a42e7ff2ec8844094d39bc3356ebcbbb
Justdwiwt <len_master@hotmail.com>
2022-12-14 14:47:44 +0800

    leetCode 1052

--- src/main/scala/leetCode/Solution_1052.scala
+++ src/main/scala/leetCode/Solution_1052.scala
@@ -1,16 +1,18 @@
 package leetCode
 
 object Solution_1052 {
-  def maxSatisfied(customers: Array[Int], grumpy: Array[Int], X: Int): Int = {
-    var p = 0
-    var cnt = 0
-    var mxCnt = 0
-    grumpy.indices.foreach(i => {
-      if (grumpy(i) == 0) cnt += customers(i)
-      else p += customers(i)
-      if (i >= X) p -= grumpy(i - X) * customers(i - X)
-      mxCnt = mxCnt.max(p)
-    })
-    cnt + mxCnt
+  def maxSatisfied(customers: Array[Int], grumpy: Array[Int], minutes: Int): Int = {
+    val zipped = (customers :+ 0).zip(grumpy :+ 0)
+    zipped
+      .map(x => x._1 * (1 - x._2))
+      .sum + zipped
+      .take(minutes)
+      .map(x => x._1 * x._2)
+      .sum + zipped
+      .sliding(zipped.length - minutes, minutes)
+      .toArray
+      .transpose
+      .scanLeft(0) { case (cur, Array((oc, og), (nc, ng))) => cur + nc * ng - oc * og }
+      .max
   }
 }
