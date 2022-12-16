179584b07c8e13c1c364162e7427856f5142019a
Justdwiwt <len_master@hotmail.com>
2022-12-16 14:41:58 +0800

    leetCode 1813

--- src/main/scala/leetCode/Solution_1813.scala
+++ src/main/scala/leetCode/Solution_1813.scala
@@ -2,18 +2,15 @@ package leetCode
 
 object Solution_1813 {
   def areSentencesSimilar(sentence1: String, sentence2: String): Boolean = {
-    val l1 = sentence1.split("\\s+").toVector
-    val l2 = sentence2.split("\\s+").toVector
-    val (x, y) = f(l1, l2)
-    x.isEmpty || y.isEmpty
-  }
+    @scala.annotation.tailrec
+    def f(s1: List[String], s2: List[String]): Boolean = (s1, s2) match {
+      case (Nil, _) => true
+      case (_, Nil) => true
+      case (h1 +: r1, h2 +: r2) if h1 == h2 => f(r1, r2)
+      case (r1 :+ t1, r2 :+ t2) if t1 == t2 => f(r1, r2)
+      case _ => false
+    }
 
-  @scala.annotation.tailrec
-  def f(l1: Vector[String], l2: Vector[String]): (Vector[String], Vector[String]) = (l1, l2) match {
-    case (Vector(), _) => (l1, l2)
-    case (_, Vector()) => (l1, l2)
-    case (_, _) => if (l1.head == l2.head) f(l1.tail, l2.tail)
-    else if (l1.last == l2.last) f(l1.dropRight(1), l2.dropRight(1))
-    else (l1, l2)
+    f(sentence1.split(" ").toList, sentence2.split(" ").toList)
   }
 }
