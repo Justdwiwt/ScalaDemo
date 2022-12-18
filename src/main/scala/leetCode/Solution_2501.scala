25109335be08fd07ecbd7f79c0e74e8c2b1e4e8c
Justdwiwt <len_master@hotmail.com>
2022-12-18 15:07:26 +0800

    leetCode 2501

---
+++ src/main/scala/leetCode/Solution_2501.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2501 {
+  def longestSquareStreak(nums: Array[Int]): Int = {
+    val m = mutable.Map.empty[Int, Int]
+    nums.sortBy(ele => ele * -1)./:(-1)((totalCount, item) => {
+      val updatedSeqCount = m.getOrElse(item * item, 0) + 1
+      m.update(item, updatedSeqCount)
+      totalCount.max(if (updatedSeqCount == 1) -1 else updatedSeqCount)
+    })
+  }
+}
