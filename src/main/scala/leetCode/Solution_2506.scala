a4aabf026f070fa7cd4869e02579bde77af89842
Justdwiwt <len_master@hotmail.com>
2022-12-20 19:42:20 +0800

    leetCode 2506

--- src/main/scala/leetCode/Solution_2506.scala
+++ src/main/scala/leetCode/Solution_2506.scala
@@ -1,17 +1,9 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2506 {
-  def similarPairs(words: Array[String]): Int = {
-    val m = mutable.HashMap.empty[Int, Int]
-    var res = 0
-    words.foreach(w => {
-      var t = 0
-      w.indices.foreach(i => t |= 1 << (w(i) - 'a'))
-      res += m.getOrElse(t, 0)
-      m += t -> (m.getOrElse(t, 0) + 1)
-    })
-    res
-  }
+  def similarPairs(words: Array[String]): Int = words
+    .groupBy(_.toSet.mkString.sorted)
+    .values
+    .map(list => list.length * (list.length - 1) / 2)
+    .sum
 }
