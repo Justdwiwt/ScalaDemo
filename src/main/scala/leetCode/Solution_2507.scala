ca2550e694ae9b579a04180262497999fc464663
Justdwiwt <len_master@hotmail.com>
2022-12-21 18:34:46 +0800

    leetCode 2507

---
+++ src/main/scala/leetCode/Solution_2507.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_2507 {
+  private val primes = (2 to 100000).filter(p => BigInt(p).isProbablePrime(10))
+
+  @scala.annotation.tailrec
+  def smallestValue(n: Int): Int = {
+    val f = factors(n)
+    val sum = f.sum
+    if (sum == n) n else smallestValue(sum)
+  }
+
+  private def factors(i: Int): List[Int] =
+    if (primes.contains(i)) List(i)
+    else {
+      val p = primes.find(i % _ == 0).get
+      p :: factors(i / p)
+    }
+}
