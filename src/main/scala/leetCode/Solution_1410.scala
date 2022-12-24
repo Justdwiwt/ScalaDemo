9f665478d30aae9c2b854dcf754694eced89d347
Justdwiwt <len_master@hotmail.com>
2022-12-24 16:07:35 +0800

    leetCode 1410

--- src/main/scala/leetCode/Solution_1410.scala
+++ src/main/scala/leetCode/Solution_1410.scala
@@ -1,11 +1,17 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_1410 {
-  def entityParser(text: String): String = text
-    .replace("&quot;", "\"")
-    .replace("&apos;", "'")
-    .replace("&gt;", ">")
-    .replace("&lt;", "<")
-    .replace("&frasl;", "/")
-    .replace("&amp;", "&")
+  def entityParser(text: String): String = {
+    val diff = mutable.HashMap(
+      "&quot;" -> """\"""",
+      "&apos;" -> "'",
+      "&gt;" -> ">",
+      "&lt;" -> "<",
+      "&frasl;" -> "/"
+    )
+
+    diff.keySet./:(text)((b, key) => b.replaceAll(key, diff.getOrElse(key, ""))).replaceAll("&amp;", "&")
+  }
 }
