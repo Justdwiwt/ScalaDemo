5221def75d37bfd872f7222706ba0f909de5dda1
Justdwiwt <len_master@hotmail.com>
2022-12-27 15:54:17 +0800

    leetCode 2027

--- src/main/scala/leetCode/Solution_2027.scala
+++ src/main/scala/leetCode/Solution_2027.scala
@@ -1,15 +1,6 @@
 package leetCode
 
 object Solution_2027 {
-  def minimumMoves(s: String): Int = {
-    var res, i = 0
-    while (i < s.length) {
-      if (s(i) == 'X') {
-        i += 2
-        res += 1
-      }
-      i += 1
-    }
-    res
-  }
+  def minimumMoves(s: String): Int =
+    ((s.length - s.replaceAll("X.?.?", "").length) / 3.0).ceil.toInt
 }
