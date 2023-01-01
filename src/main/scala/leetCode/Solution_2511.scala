8e43e0e1d19c1953b7506945fa16b662f411935d
Justdwiwt <len_master@hotmail.com>
2023-01-01 15:27:31 +0800

    leetCode 2511

---
+++ src/main/scala/leetCode/Solution_2511.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2511 {
+  def captureForts(forts: Array[Int]): Int = forts./:(2, 0, 0) { case ((prev, len, prevMax), curr) =>
+    if (curr == 0) (prev, len + 1, prevMax)
+    else if (prev != curr && prev != 2) (curr, 0, prevMax.max(len))
+    else (curr, 0, prevMax)
+  }._3
+}
