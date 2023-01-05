12dd4551f5b677f14c7fe72dc5cef57af8666ee0
Justdwiwt <len_master@hotmail.com>
2023-01-05 19:36:16 +0800

    leetCode 1803

---
+++ src/main/scala/leetCode/Solution_1803.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_1803 {
+  def countPairs(nums: Array[Int], low: Int, high: Int): Int = {
+    var res = 0
+    nums.indices.dropRight(1).foreach(i => {
+      val x = nums(i)
+      (i + 1 until nums.length).foreach(j => {
+        val r = nums(j) ^ x
+        if (r >= low && r <= high) res += 1
+      })
+    })
+    res
+  }
+}
