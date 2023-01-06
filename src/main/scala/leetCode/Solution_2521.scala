53f7435309330211cc00c71d1efb9d0070c6ff68
Justdwiwt <len_master@hotmail.com>
2023-01-06 18:53:42 +0800

    leetCode 2521

--- src/main/scala/leetCode/Solution_2521.scala
+++ src/main/scala/leetCode/Solution_2521.scala
@@ -1,27 +1,11 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2521 {
-  private val primes = mutable.Map.empty[Int, mutable.Set[Int]]
-  primes(2) = mutable.Set(2)
-  primes(3) = mutable.Set(3)
-  (4 to 1000).foreach(i => primes(i) = getAllPrimes(i))
-
-  private def getAllPrimes(number: Int): mutable.Set[Int] = {
-    val primes = mutable.Set.empty[Int]
-    var current = number
-    (2 to (number / 2) + 1).foreach(i => while (current % i == 0) {
-      primes += i
-      current = current / i
-    })
-    if (primes.isEmpty) primes += number
-    primes
-  }
-
   def distinctPrimeFactors(nums: Array[Int]): Int = {
-    val unqiuePrimes = mutable.Set.empty[Int]
-    nums.foreach(num => unqiuePrimes ++= primes(num))
-    unqiuePrimes.size
+    lazy val primes: Stream[Int] = 2 #:: Stream.iterate[Int](3)(x => Stream
+      .iterate(x + 2)(_ + 2)
+      .find(i => primes.takeWhile(p => p * p <= i).forall(i % _ > 0))
+      .get)
+    nums.flatMap(x => primes.takeWhile(_ <= x).filter(x % _ == 0).toSet).toSet.size
   }
 }
