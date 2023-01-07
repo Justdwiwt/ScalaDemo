59ed255e3ee341eaf0bf8f247e7e8a3cd9b32b2a
Justdwiwt <len_master@hotmail.com>
2023-01-07 19:18:04 +0800

    leetCode 2520

--- src/main/scala/leetCode/Solution_2520.scala
+++ src/main/scala/leetCode/Solution_2520.scala
@@ -1,12 +1,7 @@
 package leetCode
 
 object Solution_2520 {
-  def countDigits(num: Int): Int = {
-    @scala.annotation.tailrec
-    def f(n: Int, res: Int): Int =
-      if (n == 0) res
-      else f(n / 10, if (num % (n % 10) == 0) res + 1 else res)
-
-    f(num, res = 0)
-  }
+  def countDigits(num: Int): Int = num
+    .toString
+    .count(x => num % (x - '0') == 0)
 }
