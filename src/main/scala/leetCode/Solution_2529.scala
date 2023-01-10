b882808d9db37325e78207ef9f27580dd715adea
Justdwiwt <len_master@hotmail.com>
2023-01-10 18:45:49 +0800

    leetCode 2529

--- src/main/scala/leetCode/Solution_2529.scala
+++ src/main/scala/leetCode/Solution_2529.scala
@@ -1,13 +1,7 @@
 package leetCode
 
 object Solution_2529 {
-  def maximumCount(nums: Array[Int]): Int = {
-    var pos = 0
-    var neg = 0
-    nums.foreach(n => {
-      if (n > 0) pos += 1
-      else if (n < 0) neg += 1
-    })
-    pos.max(neg)
-  }
+  def maximumCount(nums: Array[Int]): Int = nums
+    .count(_ > 0)
+    .max(nums.count(_ < 0))
 }
