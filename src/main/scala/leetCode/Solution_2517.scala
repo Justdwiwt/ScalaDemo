71cba51efb30d55af86e26470e8cf9272ae874bf
Justdwiwt <len_master@hotmail.com>
2023-01-13 20:15:21 +0800

    leetCode 2517

--- src/main/scala/leetCode/Solution_2517.scala
+++ src/main/scala/leetCode/Solution_2517.scala
@@ -2,28 +2,31 @@ package leetCode
 
 object Solution_2517 {
   def maximumTastiness(price: Array[Int], k: Int): Int = {
-    val sorted = price.sorted
-    val mx = sorted(sorted.length - 1) - sorted.head
-    var mn = Int.MaxValue
-    if (k == 2) return mx
-    sorted.indices.drop(1).foreach(i => mn = mn.min(sorted(i) - sorted(i - 1)))
-    var l = mn
-    var r = mx
-    while (l < r) {
-      val mid = (l + r) >> 1
-      if (check(sorted, mid) >= k) l = mid + 1
-      else r = mid
-    }
-    l
-  }
+    val list = price.sorted.toList
 
-  private def check(pos: Array[Int], m: Int): Int = {
-    var num = 1
-    var head = pos.head
-    pos.indices.drop(1).foreach(i => if (pos(i) - head > m) {
-      num += 1
-      head = pos(i)
-    })
-    num
+    def f(n: Int): Boolean = {
+      @scala.annotation.tailrec
+      def g(ls: List[Int], curr: Int): Boolean = {
+        if (curr == 0) true
+        else ls match {
+          case Nil => false
+          case x :: _ => g(ls.dropWhile(_ - x < n), curr - 1)
+        }
+      }
+
+      g(list, k)
+    }
+
+    @scala.annotation.tailrec
+    def solve(l: Int, h: Int): Int = {
+      if (l == h) l
+      else {
+        val m = (l + h + 1) / 2
+        if (f(m)) solve(m, h)
+        else solve(l, m - 1)
+      }
+    }
+
+    solve(0, 1000000005)
   }
 }
