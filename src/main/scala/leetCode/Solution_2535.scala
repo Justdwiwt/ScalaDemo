c4fbde4bda70f9185aeac47edf249cefddede5cd
Justdwiwt <len_master@hotmail.com>
2023-01-17 15:23:23 +0800

    leetCode 2535

--- src/main/scala/leetCode/Solution_2535.scala
+++ src/main/scala/leetCode/Solution_2535.scala
@@ -1,8 +1,6 @@
 package leetCode
 
 object Solution_2535 {
-  def differenceOfSum(nums: Array[Int]): Int = {
-    val digits = nums.flatMap(_.toString).map(_ - '0')
-    (nums.sum - digits.sum).abs
-  }
+  def differenceOfSum(nums: Array[Int]): Int =
+    nums.sum - nums.map(n => n.toString.map(_ - '0').sum).sum
 }
