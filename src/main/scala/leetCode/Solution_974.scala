a12f62a95071fd472d69960c83c6784ff6586efc
Justdwiwt <len_master@hotmail.com>
2023-01-19 21:15:43 +0800

    leetCode 974

--- src/main/scala/leetCode/Solution_974.scala
+++ src/main/scala/leetCode/Solution_974.scala
@@ -1,10 +1,12 @@
 package leetCode
 
 object Solution_974 {
-  def subarraysDivByK(A: Array[Int], K: Int): Int = A.scanLeft(0)(_ + _)./:(Map[Int, Int](), 0: Int) {
-    case ((pre, cur), sum) =>
-      val M = (sum % K + K) % K
-      if (pre.contains(M)) (pre + (M -> (pre.getOrElse(M, 0) + 1)), cur + pre.getOrElse(M, 0))
-      else (pre + (M -> pre.getOrElse(M, 1)), cur)
-  }._2
+  def subarraysDivByK(A: Array[Int], K: Int): Int = A
+    .scanLeft(0)(_ + _)
+    .map(x => (x % K + K) % K)
+    .groupBy(identity)
+    .mapValues(_.length)
+    .values
+    .map(x => x * (x - 1) / 2)
+    .sum
 }
