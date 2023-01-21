879843ffa2ec2b26d0bd479841165904db354f93
Justdwiwt <len_master@hotmail.com>
2023-01-21 15:09:23 +0800

    leetCode 2537

---
+++ src/main/scala/leetCode/Solution_2537.scala
@@ -0,0 +1,24 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2537 {
+  def countGood(nums: Array[Int], k: Int): Long = {
+    var len = nums.length
+    var res = 0L
+    var m = mutable.HashMap.empty[Int, Int]
+    var l, r, cnt = 0
+    while (r < len) {
+      cnt += m.getOrElse(nums(r), 0)
+      m += nums(r) -> (m.getOrElse(nums(r), 0) + 1)
+      r += 1
+      while (cnt >= k) {
+        res += len - r + 1
+        m += nums(l) -> (m.getOrElse(nums(l), 0) - 1)
+        cnt -= m(nums(l))
+        l += 1
+      }
+    }
+    res
+  }
+}
