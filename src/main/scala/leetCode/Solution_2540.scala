d10c833da0226a3152c84a927bbde1dc7341c903
Justdwiwt <len_master@hotmail.com>
2023-01-24 15:58:35 +0800

    leetCode 2540

--- src/main/scala/leetCode/Solution_2540.scala
+++ src/main/scala/leetCode/Solution_2540.scala
@@ -2,12 +2,7 @@ package leetCode
 
 object Solution_2540 {
   def getCommon(nums1: Array[Int], nums2: Array[Int]): Int = {
-    var i, j = 0
-    while (i < nums1.length && j < nums2.length) {
-      if (nums1(i) < nums2(j)) i += 1
-      else if (nums1(i) == nums2(j)) return nums1(i)
-      else j += 1
-    }
-    -1
+    val res = nums1.intersect(nums2)
+    if (res.isEmpty) -1 else res.min
   }
 }
