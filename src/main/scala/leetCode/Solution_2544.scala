4f788a7797aa805e3a0d02ede4e22f52ccffc5fa
Justdwiwt <len_master@hotmail.com>
2023-01-26 19:44:36 +0800

    leetCode 2544

---
+++ src/main/scala/leetCode/Solution_2544.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2544 {
+  def alternateDigitSum(n: Int): Int = n
+    .toString
+    .toVector
+    .map(_ - '0')
+    .sliding(2, 2)
+    ./:(0) {
+      case (s, Vector(x, y)) => s + x - y
+      case (s, Vector(x)) => s + x
+      case (s, _) => s
+    }
+}
