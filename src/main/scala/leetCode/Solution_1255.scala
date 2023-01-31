0f156c2b6f6e469050dbd472fcd134c0aafe16be
Justdwiwt <len_master@hotmail.com>
2023-01-31 21:53:42 +0800

    leetCode 1255

--- src/main/scala/leetCode/Solution_1255.scala
+++ src/main/scala/leetCode/Solution_1255.scala
@@ -1,31 +1,29 @@
 package leetCode
 
-import scala.util.control.Breaks._
-
 object Solution_1255 {
   def maxScoreWords(words: Array[String], letters: Array[Char], score: Array[Int]): Int = {
-    val l = Array.fill(26)(0)
-    letters.foreach(i => l(i - 'a') += 1)
-    var res = 0
-    (0 until (1 << words.length)).foreach(i => {
-      val v = group(words, i)
-      var t = 0
-      breakable {
-        (0 until 26).foreach(j => {
-          if (l(j) < v(j)) {
-            t = 0
-            break
-          } else t += v(j) * score(j)
-        })
-      }
-      res = res.max(t)
-    })
-    res
-  }
 
-  def group(words: Array[String], bit: Int): Array[Int] = {
-    val res = Array.fill(26)(0)
-    words.indices.foreach(i => if ((bit & (1 << i)) > 0) words(i).foreach(j => res(j - 'a') += 1))
-    res
+    def createCounter(letters: String): Array[Int] = letters./:(Array.fill(26)(0))((acc, x) => {
+      acc(x - 97) += 1
+      acc
+    })
+
+    def canBeCreated(x1: Array[Int], x2: Array[Int]): Boolean = x1.zip(x2)./:(true)((res, x) => {
+      if (x._1 < x._2) false
+      else res
+    })
+
+    val lettersCounter = createCounter(letters.mkString)
+
+    var combinations = Array[String]("")
+
+    words.indices.foreach(x => {
+      combinations = combinations.flatMap(y => {
+        if (canBeCreated(lettersCounter, createCounter(y + words(x)))) Array(y, y + words(x))
+        else Array(y)
+      })
+    })
+
+    combinations./:(0)((mx, x) => mx.max(x./:(0)((acc, y) => acc + score(y - 97))))
   }
 }
