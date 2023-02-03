f69b524f6ccec8c2fddd460937ed9a0dc8d2b176
Justdwiwt <len_master@hotmail.com>
2023-02-03 21:08:56 +0800

    leetCode 2550

--- src/main/scala/leetCode/Solution_2550.scala
+++ src/main/scala/leetCode/Solution_2550.scala
@@ -2,14 +2,7 @@ package leetCode
 
 object Solution_2550 {
   def monkeyMove(n: Int): Int = {
-    val M = 1000000007
-
-    @annotation.tailrec
-    def f(base: Long, pow: Int, res: Long): Int =
-      if (pow == 0) res.toInt
-      else if (pow % 2 == 0) f((base * base) % M, pow / 2, res)
-      else f((base * base) % M, pow / 2, (res * base) % M)
-
-    java.lang.Math.floorMod(f(2, n, 1) - 2, M)
+    val M = 1000000007: BigInt
+    ((2: BigInt).modPow(n, M) - 2).mod(M).toInt
   }
 }
