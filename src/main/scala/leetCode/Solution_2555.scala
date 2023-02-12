621ced1bdee4442c7c817fed9cd32ebb7526173d
Justdwiwt <len_master@hotmail.com>
2023-02-12 14:52:25 +0800

    leetCode 2555

--- src/main/scala/leetCode/Solution_2555.scala
+++ src/main/scala/leetCode/Solution_2555.scala
@@ -1,17 +1,12 @@
 package leetCode
 
 object Solution_2555 {
-  def maximizeWin(prizePositions: Array[Int], k: Int): Int = {
-    var l = 0
-    var r = 0
-    var res = 0
-    val arr = Array.fill(prizePositions.length + 1)(0)
-    while (r < prizePositions.length) {
-      while (prizePositions(r) - prizePositions(l) > k) l += 1
-      res = res.max(r - l + 1 + arr(l))
-      arr(r + 1) = arr(r).max(r - l + 1)
-      r += 1
-    }
-    res
+  def maximizeWin(prizes: Array[Int], k: Int): Int = {
+    val dp = Array.fill(prizes.length + 1)(0)
+    prizes.indices./:(0, 0) { case ((j, res), i) =>
+      val newJ = Iterator.iterate(j)(_ + 1).dropWhile(j => prizes(j) < prizes(i) - k).next()
+      dp(i + 1) = dp(i).max(i - newJ + 1)
+      (newJ, res.max(i - newJ + 1 + dp(newJ)))
+    }._2
   }
 }
