97e34ad9bac24dbd8590519295a7bb40f2b28fa7
Justdwiwt <len_master@hotmail.com>
2023-02-13 15:37:31 +0800

    leetCode 2562

---
+++ src/main/scala/leetCode/Solution_2562.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2562 {
+  def findTheArrayConcVal(nums: Array[Int]): Long = (nums.take(nums.length / 2) :+ 0)
+    .map(_.toString)
+    .zip(nums.drop(nums.length / 2).map(_.toString).reverse)
+    .map(n => (n._1 + n._2).toLong)
+    .sum
+}
