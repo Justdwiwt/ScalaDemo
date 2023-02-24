f54e123163fd0c5c2f8f9932245e85d065b85c76
Justdwiwt <len_master@hotmail.com>
2023-02-24 15:37:11 +0800

    leetCode 2567

---
+++ src/main/scala/leetCode/Solution_2567.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2567 {
+  def minimizeSum(nums: Array[Int]): Int = {
+    val sorted = nums.sorted
+    (sorted.last - sorted(2)).min(sorted(sorted.length - 3) - sorted.head).min(sorted(sorted.length - 2) - sorted(1))
+  }
+}
