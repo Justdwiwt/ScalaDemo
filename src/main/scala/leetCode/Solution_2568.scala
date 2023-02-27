b5f7137d77362d49aecb24faa7c34e59f0819bad
Justdwiwt <len_master@hotmail.com>
2023-02-27 15:41:10 +0800

    leetCode 2568

--- src/main/scala/leetCode/Solution_2568.scala
+++ src/main/scala/leetCode/Solution_2568.scala
@@ -2,9 +2,7 @@ package leetCode
 
 object Solution_2568 {
   def minImpossibleOR(nums: Array[Int]): Int = {
-    var res = 0
-    nums.foreach(n => if ((n & (n - 1)) == 0) res |= n)
-    res = ~res
-    res & -res
+    val st = nums.toSet
+    (0 to 32).map(i => 1 << i).dropWhile(st.contains).head
   }
 }
