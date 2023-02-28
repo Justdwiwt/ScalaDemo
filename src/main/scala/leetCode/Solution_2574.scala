fd7e853066b61e0fd6e7ddca0c49deeebf28e859
Justdwiwt <len_master@hotmail.com>
2023-02-28 15:39:14 +0800

    leetCode 2574

---
+++ src/main/scala/leetCode/Solution_2574.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2574 {
+  def leftRigthDifference(nums: Array[Int]): Array[Int] = nums
+    .scanLeft(0)(_ + _)
+    .init
+    .zip(nums.scanRight(0)(_ + _).tail)
+    .map(n => (n._2 - n._1).abs)
+}
