a0014cce8377aca917b830ff32ff911340609a65
Justdwiwt <len_master@hotmail.com>
2023-03-03 14:54:54 +0800

    leetCode 1487

--- src/main/scala/leetCode/Solution_1487.scala
+++ src/main/scala/leetCode/Solution_1487.scala
@@ -4,26 +4,20 @@ import scala.collection.mutable
 
 object Solution_1487 {
   def getFolderNames(names: Array[String]): Array[String] = {
-    var arr = mutable.ArrayBuffer.empty[String]
-    val m = new mutable.HashMap[String, Int]()
-    names.indices.foreach(i => {
-      var t = names(i)
-      if (m.contains(t)) {
-        var num = m(t)
-        num += 1
-        var str = t + "(" + num + ")"
-        while (m.contains(str)) {
-          num += 1
-          str = t + "(" + num + ")"
+    def f(pfx: mutable.LinkedHashSet[String], name: String): String = {
+      if (pfx.contains(name)) {
+        var s = 1
+        var n = name + "(" + s + ")"
+        while (pfx.contains(n)) {
+          s += 1
+          n = name + "(" + s + ")"
         }
-        m.put(t, num)
-        arr += str
-        m.put(str, 0)
-      } else {
-        m.put(t, 0)
-        arr += t
-      }
-    })
-    arr.toArray
+        n
+      } else name
+    }
+
+    val buffer = names./:(mutable.LinkedHashSet[String]()) { case (buffer, name) => buffer += f(buffer, name) }
+
+    buffer.toArray
   }
 }
