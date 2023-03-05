44bd6ffd287ebfc421529704e071befc93e50aca
Justdwiwt <len_master@hotmail.com>
2023-03-05 14:12:45 +0800

    leetCode 1345

--- src/main/scala/leetCode/Solution_1345.scala
+++ src/main/scala/leetCode/Solution_1345.scala
@@ -1,30 +1,19 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_1345 {
   def minJumps(arr: Array[Int]): Int = {
-    val m = mutable.Map.empty[Int, List[Int]].withDefaultValue(Nil)
-    arr.indices.foreach(i => m(arr(i)) = i :: m(arr(i)))
-    val visited = Array.fill(arr.length)(false)
-    visited(0) = true
-    val q = mutable.Queue[Int](0)
-    arr.indices.foreach(step =>
-      q.indices.foreach(_ => {
-        val x = q.dequeue()
-        if (x == arr.length - 1) return step
-        if (x - 1 > 0 && !visited(x - 1)) {
-          q += x - 1
-          visited(x - 1) = true
-        }
-        (x + 1 :: m(arr(x)))
-          .withFilter(j => !visited(j))
-          .foreach(j => {
-            q += j
-            visited(j) = true
-          })
-        m.remove(arr(x))
-      }))
-    arr.length
+    val initVal2ind = arr.zipWithIndex.groupBy(x => x._1).mapValues(_.map(_._2).toSet).toMap.withDefaultValue(Set())
+
+    @scala.annotation.tailrec
+    def bfs(front: Set[Int], vis: Set[Int], dis: Int, val2ind: Map[Int, Set[Int]]): Int = {
+      if (front.contains(arr.length - 1)) dis
+      else {
+        val next = front.flatMap(x => (val2ind(arr(x)) + (x + 1) + (x - 1)).filter(x => x >= 0 && x < arr.length && !vis.contains(x)))
+        val newVal2ind = front./:(val2ind)((v, x) => v + (arr(x) -> Set()))
+        bfs(next, vis ++ next, dis + 1, newVal2ind)
+      }
+    }
+
+    bfs(Set(0), Set(0), 0, initVal2ind)
   }
 }
