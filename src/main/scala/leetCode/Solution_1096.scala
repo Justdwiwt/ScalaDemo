33de2b326aed223dd3c82d307848a02036bb01ba
Justdwiwt <len_master@hotmail.com>
2023-03-07 00:19:43 +0800

    leetCode 1096

--- src/main/scala/leetCode/Solution_1096.scala
+++ src/main/scala/leetCode/Solution_1096.scala
@@ -1,32 +1,50 @@
 package leetCode
 
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-
 object Solution_1096 {
   def braceExpansionII(expression: String): List[String] = {
-    val st = new mutable.Stack[ArrayBuffer[String]]()
-    var prev = new ArrayBuffer[String]()
-    var cur = new ArrayBuffer[String]()
-    expression.foreach({
-      case '{' =>
-        st.push(prev)
-        st.push(cur)
-        prev = new ArrayBuffer[String]()
-        cur = new ArrayBuffer[String]()
-      case '}' =>
-        val tmp = prev ++ cur
-        cur = st.pop
-        if (cur.isEmpty) cur += ""
-        cur = cur.flatMap(x => tmp.map(y => x + y))
-        prev = st.pop
-      case ',' =>
-        prev ++= cur
-        cur = new ArrayBuffer[String]()
-      case c =>
-        if (cur.isEmpty) cur += ""
-        cur = cur.map(x => x + c)
-    })
-    (prev ++ cur).toSet.toList.sorted
+    @scala.annotation.tailrec
+    def toPip(idx: Int, openBraceCnt: Int, subExpression: String, returnValue: List[String]): List[String] =
+      if (idx >= expression.length)
+        if (subExpression != "") returnValue :+ subExpression
+        else returnValue
+      else expression(idx) match {
+        case '{' => toPip(idx + 1, openBraceCnt + 1, subExpression + '{', returnValue)
+        case '}' if openBraceCnt == 1 => toPip(idx + 1, openBraceCnt - 1, "", returnValue :+ subExpression.drop(1))
+        case '}' => toPip(idx + 1, openBraceCnt - 1, subExpression + '}', returnValue)
+        case c if openBraceCnt == 0 => toPip(idx + 1, openBraceCnt, "", returnValue :+ c.toString)
+        case c => toPip(idx + 1, openBraceCnt, subExpression + c, returnValue)
+      }
+
+    @scala.annotation.tailrec
+    def cal(expression: String, idx: Int, openBraceCnt: Int, subExpression: String, returnValue: List[String]): List[String] =
+      if (idx >= expression.length)
+        if (subExpression == "") returnValue
+        else if (subExpression.contains('{')) returnValue ++ braceExpansionII(subExpression)
+        else returnValue :+ subExpression
+      else expression(idx) match {
+        case ',' if openBraceCnt == 0 =>
+          val updatedReturnValue: List[String] = returnValue ++ {
+            if (subExpression.contains('{')) braceExpansionII(subExpression)
+            else List(subExpression)
+          }
+          cal(expression, idx + 1, 0, "", updatedReturnValue)
+        case c =>
+          val updatedOpenBraceCount = openBraceCnt + {
+            if (c == '{') 1
+            else if (c == '}') -1
+            else 0
+          }
+          cal(expression, idx + 1, updatedOpenBraceCount, subExpression + c, returnValue)
+      }
+
+    def fromPip(pipelineSteps: List[Set[String]]): Set[String] = pipelineSteps.headOption match {
+      case Some(cur: Set[String]) => cur.flatMap(c => fromPip(pipelineSteps.tail).map(rest => c + rest))
+      case None => Set("")
+    }
+
+    val pip: List[Set[String]] = toPip(0, 0, "", List.empty)
+      .map((exp: String) => cal(exp, 0, 0, "", List.empty).toSet)
+
+    fromPip(pip).toList.sorted
   }
 }
