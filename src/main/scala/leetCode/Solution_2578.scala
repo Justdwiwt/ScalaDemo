e8ef7c461212df540198fed3b76b48c157a5621d
Justdwiwt <len_master@hotmail.com>
2023-03-09 19:25:36 +0800

    leetCode 2578

---
+++ src/main/scala/leetCode/Solution_2578.scala
@@ -0,0 +1,21 @@
+package leetCode
+
+object Solution_2578 {
+  def splitNum(num: Int): Int = num
+    .toString
+    .toList
+    .sorted
+    .zipWithIndex
+    .filter(_._2 % 2 == 0)
+    .map(_._1)
+    .mkString
+    .toInt + num
+    .toString
+    .toList
+    .sorted
+    .zipWithIndex
+    .filter(_._2 % 2 == 1)
+    .map(_._1)
+    .mkString
+    .toInt
+}
