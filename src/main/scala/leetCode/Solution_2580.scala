3045c69b9f28f855d19339887b49a070d8afa906
Justdwiwt <len_master@hotmail.com>
2023-03-18 13:42:11 +0800

    leetCode 2580

--- src/main/scala/leetCode/Solution_2580.scala
+++ src/main/scala/leetCode/Solution_2580.scala
@@ -1,19 +1,11 @@
 package leetCode
 
 object Solution_2580 {
-  final val M = 1000000007
-
-  def countWays(ranges: Array[Array[Int]]): Int = {
-    val sorted = ranges.sorted((a: Array[Int], b: Array[Int]) => a.head - b.head)
-
-    var mx = sorted.head(1)
-    var res = 2
-    sorted.indices.drop(1).foreach(i => {
-      val l = sorted(i).head
-      val r = sorted(i)(1)
-      if (l > mx) res = (res * 2) % M
-      mx = mx.max(r)
-    })
-    res
-  }
+  def countWays(ranges: Array[Array[Int]]): Int = ranges
+    .sortBy(_.head)
+    ./:(1, -1) { case ((res, last), Array(l, r)) =>
+      if (last >= l) (res, last.max(r))
+      else (res * 2 % 1000000007, last.max(r))
+    }
+    ._1
 }
