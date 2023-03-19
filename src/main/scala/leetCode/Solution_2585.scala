732950cc9ad27a5bf1f83cd7412f2799f4bc0f6c
Justdwiwt <len_master@hotmail.com>
2023-03-19 14:23:10 +0800

    leetCode 2585

--- src/main/scala/leetCode/Solution_2585.scala
+++ src/main/scala/leetCode/Solution_2585.scala
@@ -2,20 +2,15 @@ package leetCode
 
 object Solution_2585 {
   def waysToReachTarget(target: Int, types: Array[Array[Int]]): Int = {
-    val M = (1e9 + 7).toInt
-    val dp = Array.fill(types.length + 1, target + 1)(0)
-    dp(0)(0) = 1
-    (1 to types.length).foreach(i => {
-      val t = types(i - 1)(1)
+    val dp = Array.fill(types.length + 1)(new Array[Long](target + 1))
+    dp(0)(target) = 1
+    types.indices.foreach(i => {
+      val Array(cnt, mark) = types(i)
       (0 to target).foreach(j => {
-        var k = 0
-        while (k <= types(i - 1).head && j - t * k >= 0) {
-          dp(i)(j) += dp(i - 1)(j - k * t)
-          dp(i)(j) = dp(i)(j) % M
-          k += 1
-        }
+        val idx = (0 to cnt).map(c => j + c * mark).takeWhile(_ <= target).map(dp(i)(_))
+        dp(i + 1)(j) = idx./:(0L)((acc, x) => (acc + x) % 1000000007)
       })
     })
-    dp(types.length)(target)
+    dp.last.head.toInt
   }
 }
