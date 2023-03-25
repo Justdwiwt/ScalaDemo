cfeed6b7ce6236651fe3b8a0f7faf3d9d912030a
Justdwiwt <len_master@hotmail.com>
2023-03-25 19:29:13 +0800

    leetCode 2598

---
+++ src/main/scala/leetCode/Solution_2598.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2598 {
+  //  def findSmallestInteger(nums: Array[Int], value: Int): Int = {
+  //    val modules = scala.collection.mutable.Map.empty[Int, Int].withDefaultValue(0)
+  //    nums.foreach(i => modules(java.lang.Math.floorMod(i, value)) += 1)
+  //    val mexs = LazyList.from(0).dropWhile(i => {
+  //      val ind = i % value
+  //      modules(ind) -= 1
+  //      modules(ind) >= 0
+  //    })
+  //    mexs.head
+  //  }
+}
