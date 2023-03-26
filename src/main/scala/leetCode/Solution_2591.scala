27627e22f9d38f322f37120763be6639ab938d76
Justdwiwt <len_master@hotmail.com>
2023-03-26 18:32:03 +0800

    leetCode 2591

--- src/main/scala/leetCode/Solution_2591.scala
+++ src/main/scala/leetCode/Solution_2591.scala
@@ -1,17 +1,10 @@
 package leetCode
 
 object Solution_2591 {
-  def distMoney(money: Int, children: Int): Int =
-    if (money < children) -1
-    else f(money, children)
-
-  private def f(money: Int, children: Int): Int =
-    if (money == 12 && children == 2) 0
-    else if (children == 1) if (money == 8) 1 else 0
-    else {
-      val nMoney = money - 8
-      val nChildren = children - 1
-      if (nMoney < nChildren) 0
-      else 1 + f(nMoney, nChildren)
-    }
+  def distMoney(money: Int, children: Int): Int = money - children match {
+    case cur if cur < 0 => -1
+    case cur if cur / 7 == children && cur % 7 == 0 => children
+    case cur if cur / 7 == children - 1 && cur % 7 == 3 => children - 2
+    case cur => (children - 1).min(cur / 7)
+  }
 }
