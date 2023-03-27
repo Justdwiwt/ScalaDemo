6f278e672a777ca6f852d42006e978d05c8ab60d
Justdwiwt <len_master@hotmail.com>
2023-03-27 16:10:16 +0800

    leetCode 2592

--- src/main/scala/leetCode/Solution_2592.scala
+++ src/main/scala/leetCode/Solution_2592.scala
@@ -1,17 +1,6 @@
 package leetCode
 
 object Solution_2592 {
-  def maximizeGreatness(nums: Array[Int]): Int = {
-    val sorted = nums.sorted.toList
-    f(sorted, sorted, 0)
-  }
-
-  @scala.annotation.tailrec
-  private def f(nums: List[Int], perm: List[Int], acc: Int): Int = (nums, perm) match {
-    case (_, Nil) => acc
-    case (n :: nTail, p :: pTail) =>
-      if (p > n) f(nTail, pTail, acc + 1)
-      else f(nums, pTail, acc)
-    case _ => ???
-  }
+  def maximizeGreatness(nums: Array[Int]): Int =
+    nums.length - nums.groupBy(identity).mapValues(_.length).values.max
 }
