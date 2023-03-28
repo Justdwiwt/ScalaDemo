aa8b55f41f5d0fd3520828a57e04ee1f145bfc43
Justdwiwt <len_master@hotmail.com>
2023-03-28 19:12:31 +0800

    leetCode 2593

--- src/main/scala/leetCode/Solution_2593.scala
+++ src/main/scala/leetCode/Solution_2593.scala
@@ -1,14 +1,14 @@
 package leetCode
 
 object Solution_2593 {
-  def findScore(nums: Array[Int]): Long =
-    f(nums.zipWithIndex.sorted.toList, Set.empty, 0)
+  def findScore(nums: Array[Int]): Long = {
+    @scala.annotation.tailrec
+    def f(lists: List[(Int, Int)], marked: Set[Int], res: Long): Long = lists match {
+      case Nil => res
+      case (_, index) :: tail if marked.contains(index) => f(tail, marked, res)
+      case (num, index) :: tail => f(tail, marked ++ List(index - 1, index + 1), res + num)
+    }
 
-  @scala.annotation.tailrec
-  private def f(nums: List[(Int, Int)], marked: Set[Int], score: Long): Long = nums match {
-    case Nil => score
-    case (num, index) :: tail =>
-      if (marked.contains(index)) f(tail, marked, score)
-      else f(tail, marked ++ List(index - 1, index + 1), score + num)
+    f(nums.zipWithIndex.sorted.toList, Set.empty, 0L)
   }
 }
