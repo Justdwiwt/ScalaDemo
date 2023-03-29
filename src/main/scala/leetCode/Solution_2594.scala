94b72e14ff51a64debf91d328dd7a8ce813fa4d8
Justdwiwt <len_master@hotmail.com>
2023-03-29 19:13:35 +0800

    leetCode 2594

--- src/main/scala/leetCode/Solution_2594.scala
+++ src/main/scala/leetCode/Solution_2594.scala
@@ -1,22 +1,16 @@
 package leetCode
 
 object Solution_2594 {
-  case class Mech(rank: Int, repaired: Int) {
-    private val nRepaired = repaired + 1
-    val cost: Long = rank.toLong * nRepaired * nRepaired
-  }
+  def repairCars(ranks: Array[Int], cars: Int): Long = {
+    @scala.annotation.tailrec
+    def f(l: Long, r: Long): Long =
+      if (l >= r) l
+      else {
+        val mid = (l + r) >>> 1
+        val cur = ranks.map(r => math.sqrt(mid.toDouble / r).toLong).sum
+        if (cur < cars) f(mid + 1, r) else f(l, mid)
+      }
 
-  implicit val ord: Ordering[Mech] = Ordering.by(-_.cost)
-
-  def repairCars(ranks: Array[Int], totalCars: Int): Long = {
-    var time = 0L
-    //    val mechs = scala.collection.mutable.PriorityQueue.empty[Int](ranks.map(r => Mech(r, 0): _*)
-    val mechs = scala.collection.mutable.PriorityQueue.empty[Mech]
-    (1 to totalCars).foreach(_ => {
-      val mech = mechs.dequeue()
-      time = time.max(mech.cost)
-      mechs.enqueue(Mech(mech.rank, mech.repaired + 1))
-    })
-    time
+    f(l = 1L, r = ranks.head.toLong * cars * cars)
   }
 }
