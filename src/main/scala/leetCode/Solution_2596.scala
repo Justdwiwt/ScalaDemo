f74f5b47e01d355db708be934ff9ca70cedf0045
Justdwiwt <len_master@hotmail.com>
2023-04-01 19:38:31 +0800

    leetCode 2596

--- src/main/scala/leetCode/Solution_2596.scala
+++ src/main/scala/leetCode/Solution_2596.scala
@@ -1,19 +1,9 @@
 package leetCode
 
 object Solution_2596 {
-  private final val diff: Seq[Seq[Int]] = Seq(
-    Seq(-1, -2), Seq(1, -2),
-    Seq(-2, -1), Seq(2, -1),
-    Seq(-2, 1), Seq(2, 1),
-    Seq(-1, 2), Seq(1, 2)
-  )
-
-  private def isValid(grid: Array[Array[Int]], x: Int, y: Int, expectedPos: Int): Boolean = {
-    if (x >= grid.length || y >= grid.head.length || x < 0 || y < 0 || grid(x)(y) != expectedPos) return false
-    if (expectedPos == grid.length * grid.head.length - 1) return true
-    diff.exists(delta => isValid(grid, x + delta.head, y + delta(1), expectedPos + 1))
+  def checkValidGrid(grid: Array[Array[Int]]): Boolean = {
+    if (grid.head.head != 0) return false
+    val res = grid.map(_.zipWithIndex.toList).zipWithIndex.flatMap(n => n._1.map(m => (m._1, m._2, n._2))).toList.sortBy(_._1).map(n => (n._2, n._3))
+    res.zip(res.tail).forall(n => ((n._2._2 - n._1._2).abs == 1 && (n._2._1 - n._1._1).abs == 2) || ((n._2._2 - n._1._2).abs == 2 && (n._2._1 - n._1._1).abs == 1))
   }
-
-  def checkValidGrid(grid: Array[Array[Int]]): Boolean =
-    isValid(grid, 0, 0, 0)
 }
