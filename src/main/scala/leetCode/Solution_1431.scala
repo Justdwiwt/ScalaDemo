560e79c52a7aeed80115e56aa8c9425446ec4950
Justdwiwt <len_master@hotmail.com>
2023-04-17 20:42:56 +0800

    leetCode 1431

--- src/main/scala/leetCode/Solution_1431.scala
+++ src/main/scala/leetCode/Solution_1431.scala
@@ -1,6 +1,6 @@
 package leetCode
 
 object Solution_1431 {
-  def kidsWithCandies(candies: Array[Int], extraCandies: Int): Array[Boolean] =
-    candies.map(_ + extraCandies >= candies.max)
+  def kidsWithCandies(candies: Array[Int], extraCandies: Int): List[Boolean] =
+    candies.map(_ + extraCandies >= candies.max).toList
 }
