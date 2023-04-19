18b3a1d6284ead8c75ee84f823de54a75b58a714
Justdwiwt <len_master@hotmail.com>
2023-04-19 19:22:13 +0800

    leetCode 2483

---
+++ src/main/scala/leetCode/Solution_2483.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2483 {
+  def bestClosingTime(customers: String): Int = {
+    val y = customers.map(n => if (n == 'Y') 1 else 0).mkString.toList.map(_.asDigit).scanRight(0)(_ + _)
+    val n = customers.map(n => if (n == 'N') 1 else 0).mkString.toList.map(_.asDigit).scanLeft(0)(_ + _)
+    y.zip(n).map(n => n._2 + n._1).zipWithIndex.minBy(n => (n._1, n._2))._2
+  }
+}
