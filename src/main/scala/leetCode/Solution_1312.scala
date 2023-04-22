77a61a193273a97f0c8647e1361ec5504d53f470
Justdwiwt <len_master@hotmail.com>
2023-04-22 20:41:46 +0800

    leetCode 1312

--- src/main/scala/leetCode/Solution_1312.scala
+++ src/main/scala/leetCode/Solution_1312.scala
@@ -1,12 +1,19 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_1312 {
   def minInsertions(s: String): Int = {
-    val dp = Array.fill(s.length)(Array.fill(s.length)(0))
-    s.indices.reverse.foreach(i => (i + 1 until s.length).foreach(j => {
-      dp(i)(j) = 1 + dp(i + 1)(j).min(dp(i)(j - 1))
-      if (s(i) == s(j)) dp(i)(j) = dp(i)(j).min(dp(i + 1)(j - 1))
-    }))
-    dp(0)(s.length - 1)
+    def mem[I, O](f: I => O): mutable.HashMap[I, O] = new mutable.HashMap[I, O]() {
+      override def apply(k: I): O = getOrElseUpdate(k, f(k))
+    }
+
+    lazy val dp: ((Int, Int)) => Int = mem {
+      case (i, j) if i >= j => 0
+      case (i, j) if s(i) == s(j) => dp(i + 1, j - 1)
+      case (i, j) => (dp(i + 1, j) + 1).min(dp(i, j - 1) + 1).min(dp(i + 1, j - 1) + 2)
+    }
+
+    dp(0, s.length - 1)
   }
 }
