73dda57d73055137a442161321f69c5324a4de41
Justdwiwt <len_master@hotmail.com>
2023-04-23 20:26:41 +0800

    leetCode 1416

--- src/main/scala/leetCode/Solution_1416.scala
+++ src/main/scala/leetCode/Solution_1416.scala
@@ -1,30 +1,20 @@
 package leetCode
 
 object Solution_1416 {
-  def getDPs(s: String, k: Int): Array[Long] = {
-    val arr = s.toCharArray.map(_.asDigit)
-    val dps = new Array[Long](arr.length + 1)
-    dps(dps.length - 1) = 1
-    ((arr.length - 1) to(0, -1)).foreach { i =>
-      if (arr(i) != 0) {
-        var acc = 0L
-        var curr = 0L
-        var j = 0
-        var flag = false
-        while (i + j < arr.length && !flag) {
-          curr = curr * 10 + arr(i + j)
-          if (curr <= k) acc = (acc + dps(i + j + 1)) % 1000000007
-          else flag = true
-          j += 1
-        }
-        dps(i) = acc
-      } else dps(i) = 0
-    }
-    dps
-  }
-
   def numberOfArrays(s: String, k: Int): Int = {
-    val dp = getDPs(s, k)
-    dp(0).toInt
+    val n = s.length
+    val dp = Array.ofDim[Long](n + 1)
+    dp(n) = 1
+    val arr = s.toCharArray.map(_.asDigit)
+    (n - 1 to 0 by -1).withFilter(i => arr(i) != 0).foreach(i => {
+      var num = arr(i).toLong
+      var j = i + 1
+      while (1 <= num && num <= k && j < n + 1) {
+        dp(i) = (dp(i) + dp(j)) % 1000000007
+        if (j < n) num = 10 * num + arr(j)
+        j += 1
+      }
+    })
+    dp.head.toInt
   }
 }
