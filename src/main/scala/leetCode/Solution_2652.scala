49c7ab58c47e9f9a80aba6fe9a213b8bc3ef7778
Justdwiwt <len_master@hotmail.com>
2023-05-03 19:13:18 +0800

    leetCode 2652

--- src/main/scala/leetCode/Solution_2652.scala
+++ src/main/scala/leetCode/Solution_2652.scala
@@ -1,9 +1,6 @@
 package leetCode
 
 object Solution_2652 {
-  def sumOfMultiples(n: Int): Int = {
-    var res = 0
-    (1 to n).foreach(i => if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) res += i)
-    res
-  }
+  def sumOfMultiples(n: Int): Int =
+    (1 to n).filter(x => x % 3 == 0 || x % 5 == 0 || x % 7 == 0).sum
 }
