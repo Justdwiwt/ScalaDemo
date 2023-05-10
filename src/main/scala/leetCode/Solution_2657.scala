2124cc97f4c00609e98460b65972d45ac825cffc
Justdwiwt <len_master@hotmail.com>
2023-05-10 18:55:39 +0800

    leetCode 2657

---
+++ src/main/scala/leetCode/Solution_2657.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Solution_2657 {
+  def findThePrefixCommonArray(A: Array[Int], B: Array[Int]): Array[Int] = {
+    val idx = A.map(B.indexOf(_))
+
+    def count(i: Int): Int =
+      idx.take(i + 1).count(_ <= i)
+
+    A.indices.map(count).toArray
+  }
+}
