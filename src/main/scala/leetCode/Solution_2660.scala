ae53b5e032368c22218d0d4517bb4bf7b62f88bc
Justdwiwt <len_master@hotmail.com>
2023-05-12 18:41:39 +0800

    leetCode 2660

---
+++ src/main/scala/leetCode/Solution_2660.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_2660 {
+  def isWinner(player1: Array[Int], player2: Array[Int]): Int = {
+    var p1 = 0
+    var p2 = 0
+    player1.indices.foreach(i => {
+      p1 += player1(i)
+      if ((i > 0 && player1(i - 1) == 10) || (i > 1 && player1(i - 2) == 10)) p1 += player1(i)
+      p2 += player2(i)
+      if ((i > 0 && player2(i - 1) == 10) || (i > 1 && player2(i - 2) == 10)) p2 += player2(i)
+    })
+    if (p1 > p2) 1 else if (p2 > p1) 2 else 0
+  }
+}
