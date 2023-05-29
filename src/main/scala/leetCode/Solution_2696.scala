b0ef0aeb5294dede0dd556900ff1fd1798cb20f9
Justdwiwt <len_master@hotmail.com>
2023-05-29 19:54:56 +0800

    leetCode 2696

---
+++ src/main/scala/leetCode/Solution_2696.scala
@@ -0,0 +1,11 @@
+package leetCode
+
+object Solution_2696 {
+  @scala.annotation.tailrec
+  def minLength(s: String): Int = s.contains("AB") || s.contains("CD") match {
+    case false => s.length
+    case true if s.contains("AB") => minLength(s.replaceAll("AB", ""))
+    case true if s.contains("CD") => minLength(s.replaceAll("CD", ""))
+    case _ => s.length
+  }
+}
