bab78442bc0862941056591bab8981d5ca71f7e5
Justdwiwt <len_master@hotmail.com>
2023-06-13 00:47:30 +0800

    leetCode 2718

--- src/main/scala/leetCode/Solution_2718.scala
+++ src/main/scala/leetCode/Solution_2718.scala
@@ -1,26 +1,20 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2718 {
   def matrixSumQueries(n: Int, queries: Array[Array[Int]]): Long = {
-    var res = 0L
-    var row = n
-    var col = n
-    val stRow = mutable.HashSet.empty[Int]
-    val stCol = mutable.HashSet.empty[Int]
-    queries.indices.reverse.foreach(i => {
-      if (!stRow.contains(queries(i)(1)) && queries(i).head == 0) {
-        res += row * queries(i)(2)
-        col -= 1
-        stRow += queries(i)(1)
-      }
-      if (!stCol.contains(queries(i)(1)) && queries(i).head == 1) {
-        res += col * queries(i)(2)
-        row -= 1
-        stCol += queries(i)(1)
-      }
-    })
-    res
+    val row = Array.fill(n)(false)
+    val col = Array.fill(n)(false)
+    queries.:\(0L, 0, 0)((cur, acc) => {
+      val ty = cur(0)
+      val id = cur(1)
+      val value = cur(2)
+      if (ty == 0 && !row(id)) {
+        row(id) = true
+        (acc._1 + (n - acc._3) * value, acc._2 + 1, acc._3)
+      } else if (ty == 1 && !col(id)) {
+        col(id) = true
+        (acc._1 + (n - acc._2) * value, acc._2, acc._3 + 1)
+      } else acc
+    })._1
   }
 }
