ddb6b41ca132d17a91c6767754efbd80c238970d
Justdwiwt <len_master@hotmail.com>
2023-07-08 20:37:08 +0800

    leetCode 2761

---
+++ src/main/scala/leetCode/Solution_2761.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_2761 {
+  def findPrimePairs(n: Int): List[List[Int]] =
+    if (n % 2 == 1)
+      if (n > 2 && BigInt(n - 2).isProbablePrime(10)) List(List(2, n - 2)) else List()
+    else {
+      val res = mutable.ListBuffer[List[Int]]()
+      (2 to n / 2).foreach(i => if (BigInt(i).isProbablePrime(10) && BigInt(n - i).isProbablePrime(10)) res += List(i, n - i))
+      res.toList
+    }
+}
