a3efdf61b135fc03b67afe05067e6dda23b49435
Justdwiwt <len_master@hotmail.com>
2023-07-12 18:27:55 +0800

    leetCode 2760

--- src/main/scala/leetCode/Solution_2760.scala
+++ src/main/scala/leetCode/Solution_2760.scala
@@ -1,15 +1,20 @@
 package leetCode
 
 object Solution_2760 {
-  def longestAlternatingSubarray(nums: Array[Int], threshold: Int): Int = {
-    var start = 0
-    var res = 0
-    nums.indices.foreach(i => {
-      if (nums(i) > threshold) start = i + 1
-      else if (i != 0 && nums(i - 1) % 2 == nums(i) % 2) start = i
-      if (start == i && nums(i) % 2 != 0) start = i + 1
-      res = res.max(i - start + 1)
-    })
-    res
-  }
+  def longestAlternatingSubarray(nums: Array[Int], threshold: Int): Int = Iterator
+    .iterate((0, 0, 0)) { case (mx, cur, i) =>
+      if (nums(i) > threshold) (mx, 0, i + 1)
+      else {
+        val newLen =
+          if (cur == 0) if (nums(i) % 2 == 0) 1 else 0
+          else if (nums(i) % 2 == nums(i - 1) % 2)
+            if (nums(i) % 2 == 0) 1
+            else 0
+          else cur + 1
+        (mx.max(newLen), newLen, i + 1)
+      }
+    }
+    .dropWhile { case (_, _, i) => i < nums.length }
+    .next()
+    ._1
 }
