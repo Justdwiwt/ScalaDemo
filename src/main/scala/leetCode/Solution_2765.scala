e577a43b49507e3f7ca824e243ed9d117f326c9c
Justdwiwt <len_master@hotmail.com>
2023-07-13 18:10:30 +0800

    leetCode 2765

---
+++ src/main/scala/leetCode/Solution_2765.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_2765 {
+  def alternatingSubarray(nums: Array[Int]): Int = {
+    val n = nums.length
+
+    @scala.annotation.tailrec
+    def f(res: Int, i: Int, j: Int): Int = {
+      if (i == n) return res
+      if (j < n && nums(j) == (nums(i) + (j - i) % 2)) f(res.max(j - i + 1), i, j + 1)
+      else f(res, i + 1, i + 2)
+    }
+
+    val res = f(0, 0, 1)
+    if (res > 1) res else -1
+  }
+}
