0fa023e7e607ee278ca5765ba84ba6e4034d0274
Justdwiwt <len_master@hotmail.com>
2023-07-20 18:32:41 +0800

    leetCode 2770

---
+++ src/main/scala/leetCode/Solution_2770.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_2770 {
+  def maximumJumps(nums: Array[Int], target: Int): Int = {
+    val dp = Array.fill(nums.length)(0)
+    dp(0) = 1
+    nums.indices.drop(1).foreach(i => (0 until i).foreach(j => {
+      if ((nums(i) - nums(j)) <= target && (nums(i) - nums(j)) >= -target && dp(j) != 0)
+        dp(i) = dp(i).max(dp(j) + 1)
+    }))
+    dp(nums.length - 1) - 1
+  }
+}
