9398d87a83f7da857b6a8f97fac1c32dbf51090f
Justdwiwt <len_master@hotmail.com>
2023-07-22 22:07:13 +0800

    leetCode 2767

--- src/main/scala/leetCode/Solution_2767.scala
+++ src/main/scala/leetCode/Solution_2767.scala
@@ -6,7 +6,7 @@ object Solution_2767 {
     dp(0) = 0
     s
       .indices
-      .withFilter(i => (s.charAt(i) != '0'))
+      .withFilter(i => s.charAt(i) != '0')
       .foreach(i => (i until s.length)./:(0)((cur, j) => {
         val t = cur * 2 + s.charAt(j) - '0'
         if (15625 % t == 0) dp(j + 1) = dp(j + 1).min(dp(i) + 1)
