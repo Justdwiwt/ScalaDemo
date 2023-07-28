4807661df5864fc6cdf3ab2cb337f0c1ba923683
Justdwiwt <len_master@hotmail.com>
2023-07-28 15:49:35 +0800

    leetCode 2790

---
+++ src/main/scala/leetCode/Solution_2790.scala
@@ -0,0 +1,17 @@
+package leetCode
+
+object Solution_2790 {
+  def maxIncreasingGroups(usageLimits: List[Int]): Int = {
+    val sorted = usageLimits.sorted
+    var available = 0L
+    var group = 0
+    sorted.foreach(u => {
+      available += u
+      if (available > group) {
+        group += 1
+        available -= group
+      }
+    })
+    group
+  }
+}
