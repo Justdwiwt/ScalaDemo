9a123ada247e2dcd01bf88cc9afb29c79b53fff2
Justdwiwt <len_master@hotmail.com>
2023-07-29 19:20:22 +0800

    leetCode 2784

--- src/main/scala/leetCode/Solution_2784.scala
+++ src/main/scala/leetCode/Solution_2784.scala
@@ -1,10 +1,6 @@
 package leetCode
 
 object Solution_2784 {
-  def isGood(nums: Array[Int]): Boolean = {
-    val sorted = nums.sorted
-    if (sorted.length - 1 != sorted.last) return false
-    sorted.indices.dropRight(1).foreach(i => if (sorted(i) != i + 1) return false)
-    true
-  }
+  def isGood(nums: Array[Int]): Boolean =
+    nums.sorted.toSeq == (1 until nums.length) :+ nums.length - 1
 }
