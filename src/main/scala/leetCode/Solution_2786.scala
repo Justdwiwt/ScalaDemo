fd857e7d053e7442d81127f6602e5b3807cb00a4
Justdwiwt <len_master@hotmail.com>
2023-07-30 20:43:43 +0800

    leetCode 2786

--- src/main/scala/leetCode/Solution_2786.scala
+++ src/main/scala/leetCode/Solution_2786.scala
@@ -2,20 +2,9 @@ package leetCode
 
 object Solution_2786 {
   def maxScore(nums: Array[Int], x: Int): Long = {
-    val INF = Long.MaxValue / 2
-    var a = -INF
-    var b = -INF
-    if ((nums.head & 1) == 1) a = nums.head
-    else b = nums.head
-    nums.indices.drop(1).foreach(i => {
-      val n = nums(i)
-      var na = a
-      var nb = b
-      if ((n & 1) == 1) na = a.max(b - x) + n
-      else nb = b.max(a - x) + n
-      a = na
-      b = nb
-    })
+    val h = nums.head.toLong
+    val i = if (h % 2 == 0) (h, h - x) else (h - x, h)
+    val (a, b) = nums.tail./:(i) { case ((even, odd), num) => if (num % 2 == 0) (even.max(odd - x) + num, odd) else (even, odd.max(even - x) + num) }
     a.max(b)
   }
 }
