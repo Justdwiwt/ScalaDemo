47c9221e01d3715c9e60753b84102e8a7e66bc8d
Justdwiwt <len_master@hotmail.com>
2023-07-31 20:06:17 +0800

    leetCode 2788

--- src/main/scala/leetCode/Solution_2788.scala
+++ src/main/scala/leetCode/Solution_2788.scala
@@ -1,13 +1,7 @@
 package leetCode
 
 object Solution_2788 {
-  def splitWordsBySeparator(words: List[String], separator: Char): List[String] = {
-    var res = List.empty[String]
-    val ss = "\\" + separator.toString
-    words.foreach(w => {
-      val s = w.split(ss)
-      s.foreach(t => if (t.nonEmpty) res ::= t)
-    })
-    res.reverse
-  }
+  def splitWordsBySeparator(words: List[String], separator: Char): List[String] = words
+    .flatMap(_.split(separator))
+    .filter(_.nonEmpty)
 }
