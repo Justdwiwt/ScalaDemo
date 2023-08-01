7c2d37ccd4395cace870819f1290684bf5fea2cc
Justdwiwt <len_master@hotmail.com>
2023-08-01 18:38:12 +0800

    leetCode 2789

--- src/main/scala/leetCode/Solution_2789.scala
+++ src/main/scala/leetCode/Solution_2789.scala
@@ -1,12 +1,6 @@
 package leetCode
 
 object Solution_2789 {
-  def maxArrayValue(nums: Array[Int]): Long = {
-    var cur = nums(nums.length - 1).toLong
-    (nums.length - 2 to 0 by -1).foreach(i => {
-      if (nums(i) <= cur) cur += nums(i)
-      else cur = nums(i)
-    })
-    cur
-  }
+  def maxArrayValue(nums: Array[Int]): Long =
+    nums.:\(0L) { case (n, x) => if (n > x) n else x + n }
 }
