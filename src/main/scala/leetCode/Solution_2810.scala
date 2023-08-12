a06ee23f019f02e1a20657d6890b3b3e17f0274d
Justdwiwt <len_master@hotmail.com>
2023-08-12 19:22:00 +0800

    leetCode 2810

--- src/main/scala/leetCode/Solution_2810.scala
+++ src/main/scala/leetCode/Solution_2810.scala
@@ -1,9 +1,8 @@
 package leetCode
 
 object Solution_2810 {
-  def finalString(s: String): String = {
-    var res = new StringBuilder
-    s.toCharArray.foreach(c => if ('i' == c) res = res.reverse else res.append(c))
-    res.mkString
-  }
+  def finalString(s: String): String = s
+    ./:(List.empty[Char])((acc, c) => if (c == 'i') acc.reverse else c :: acc)
+    .reverse
+    .mkString
 }
