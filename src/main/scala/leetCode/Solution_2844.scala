1cdb8deee535c908ba8f78d616f85600279df24c
Justdwiwt <len_master@hotmail.com>
2023-09-13 20:14:33 +0800

    leetCode 2844

--- src/main/scala/leetCode/Solution_2844.scala
+++ src/main/scala/leetCode/Solution_2844.scala
@@ -5,13 +5,12 @@ object Solution_2844 {
     f(num.reverse.toList, found0 = false, found5 = false, 0)
 
   @scala.annotation.tailrec
-  private def f(chars: List[Char], found0: Boolean, found5: Boolean, rm: Int): Int =
-    chars match {
-      case Nil => rm - (if (found0) 1 else 0)
-      case ('2' | '7') :: tail if found5 => rm - 1
-      case ('5' | '0') :: tail if found0 => rm - 1
-      case '0' :: tail => f(tail, found0 = true, found5 = found5, rm + 1)
-      case '5' :: tail => f(tail, found0, found5 = true, rm + 1)
-      case _ :: tail => f(tail, found0, found5, rm + 1)
-    }
+  private def f(chars: List[Char], found0: Boolean, found5: Boolean, rm: Int): Int = chars match {
+    case Nil => rm - (if (found0) 1 else 0)
+    case ('2' | '7') :: _ if found5 => rm - 1
+    case ('5' | '0') :: _ if found0 => rm - 1
+    case '0' :: tail => f(tail, found0 = true, found5 = found5, rm + 1)
+    case '5' :: tail => f(tail, found0, found5 = true, rm + 1)
+    case _ :: tail => f(tail, found0, found5, rm + 1)
+  }
 }
