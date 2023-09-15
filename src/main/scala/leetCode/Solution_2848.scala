6130331d7d750af094aa96627aeb141297071b66
Justdwiwt <len_master@hotmail.com>
2023-09-15 20:27:24 +0800

    leetCode 2848

---
+++ src/main/scala/leetCode/Solution_2848.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2848 {
+  def numberOfPoints(nums: List[List[Int]]): Int = nums
+    .collect { case List(start, end) => (start to end).toSet }
+    .reduce(_ ++ _)
+    .size
+}
