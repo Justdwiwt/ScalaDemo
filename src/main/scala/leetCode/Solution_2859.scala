38510dd846357697bc26a88f067a1da5d605073a
Justdwiwt <len_master@hotmail.com>
2023-09-18 19:21:39 +0800

    leetCode 2859

---
+++ src/main/scala/leetCode/Solution_2859.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2859 {
+  def sumIndicesWithKSetBits(nums: List[Int], k: Int): Int = nums
+    .zipWithIndex
+    .collect { case (n, i) if i.toBinaryString.count(_ == '1') == k => n }
+    .sum
+}
