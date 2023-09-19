b9e9980b77ca25bd7c461b6ce4ea5364c42dd049
Justdwiwt <len_master@hotmail.com>
2023-09-19 18:51:59 +0800

    leetCode 2860

---
+++ src/main/scala/leetCode/Solution_2860.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2860 {
+  def countWays(nums: List[Int]): Int = {
+    val sorted = nums.sorted
+    val zipped = sorted.zip((sorted :+ Int.MaxValue).zipWithIndex.tail)
+    (if (nums.forall(_ > 0)) 1 else 0) + zipped.count { case (l, (r, i)) => l < i && i < r }
+  }
+}
