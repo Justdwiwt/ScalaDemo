c5d916da09207f9de2d0b4de169f173e4f541810
Justdwiwt <len_master@hotmail.com>
2023-09-24 20:36:00 +0800

    leetCode 2856

--- src/main/scala/leetCode/Solution_2856.scala
+++ src/main/scala/leetCode/Solution_2856.scala
@@ -1,16 +1,13 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_2856 {
   def minLengthAfterRemovals(nums: List[Int]): Int = {
-    val len = nums.length
-    var res = nums.length
-    var i = 0
-    var j = (len + 1) / 2
-    while (i < len / 2 && j < len) {
-      if (nums(i) < nums(j)) res -= 2
-      i += 1
-      j += 1
-    }
-    res
+    val m = mutable.HashMap.empty[Int, Int]
+    nums.foreach(num => m += num -> (m.getOrElse(num, 0) + 1))
+    val mx = m.values.max
+    val n = nums.length
+    (n & 1).max((mx << 1) - n)
   }
 }
