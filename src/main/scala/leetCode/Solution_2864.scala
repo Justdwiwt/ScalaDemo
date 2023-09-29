6ac2fb32ab218c294e9c7b5d244820d8c80b2fd6
Justdwiwt <len_master@hotmail.com>
2023-09-29 22:42:53 +0800

    leetCode 2864

---
+++ src/main/scala/leetCode/Solution_2864.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2864 {
+  def maximumOddBinaryNumber(s: String): String = {
+    val ones = s.count(_ == '1')
+    "1" * (ones - 1) + "0" * (s.length() - ones) + "1"
+  }
+}
