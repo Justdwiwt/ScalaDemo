91aa2f1ec094b5eda854c75bb0f34d0d53ea0047
Justdwiwt <len_master@hotmail.com>
2023-09-30 20:31:18 +0800

    leetCode 2865

---
+++ src/main/scala/leetCode/Solution_2865.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+object Solution_2865 {
+  def maximumSumOfHeights(maxHeights: List[Int]): Long =
+    f(Nil, maxHeights, 0)
+
+  @scala.annotation.tailrec
+  private def f(left: List[Int], right: List[Int], max: Long): Long = right match {
+    case Nil => max
+    case height :: tail =>
+      val sums = g(height, left) + g(height, tail) + height
+      f(height :: left, tail, max.max(sums))
+  }
+
+  private def g(init: Int, heights: List[Int]): Long = heights./:((0L, init)) { case ((sum, prev), height) =>
+    val min = prev.min(height)
+    (sum + min, min)
+  }._1
+}
