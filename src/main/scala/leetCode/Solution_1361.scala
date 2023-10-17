6cf9b948f2c9c9474d79921f3fdf3cbf4eae1950
Justdwiwt <len_master@hotmail.com>
2023-10-17 21:35:08 +0800

    leetCode 1361

--- src/main/scala/leetCode/Solution_1361.scala
+++ src/main/scala/leetCode/Solution_1361.scala
@@ -1,19 +1,25 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_1361 {
   def validateBinaryTreeNodes(n: Int, leftChild: Array[Int], rightChild: Array[Int]): Boolean = {
-    val diff = Array.fill(n)(0)
-    (0 until n).foreach(i => {
-      if (~leftChild(i) > 0) diff(leftChild(i)) += 1
-      if (~rightChild(i) > 0) diff(rightChild(i)) += 1
-    })
-    var cnt = 0
-    (0 until n).foreach(i => {
-      if (diff(i) > 0) {
-        if (diff(i) > 1) return false
-        cnt += 1
-      } else if (n != 1 && leftChild(i) == -1 && leftChild(i) == -1) return false
-    })
-    cnt + 1 == n
+    val st = leftChild.filter(_ != -1).toSet ++ rightChild.filter(_ != -1).toSet
+    if (st.size != n - 1) false
+    else {
+      var visited = Set.empty[Int]
+      val q = mutable.Queue((0 until n).find(!st.contains(_)).get)
+      var res = true
+      while (q.nonEmpty && res) {
+        val cur = q.dequeue()
+        if (visited.contains(cur)) res = false
+        else {
+          visited += cur
+          if (leftChild(cur) != -1) q += leftChild(cur)
+          if (rightChild(cur) != -1) q += rightChild(cur)
+        }
+      }
+      res && visited.size == n
+    }
   }
 }
