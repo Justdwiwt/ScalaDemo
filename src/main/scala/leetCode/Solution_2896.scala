1383a6f888fa004dabdf98040ed253612a7c594c
Justdwiwt <len_master@hotmail.com>
2023-10-19 19:57:00 +0800

    leetCode 2896

---
+++ src/main/scala/leetCode/Solution_2896.scala
@@ -0,0 +1,18 @@
+package leetCode
+
+object Solution_2896 {
+  def minOperations(s1: String, s2: String, x: Int): Int = {
+    if (s1 == s2) return 0
+    var p = List.empty[Int]
+    s1.indices.foreach(i => if (s1.charAt(i) != s2.charAt(i)) p :+= i)
+    if (p.size % 2 != 0) return -1
+    var f0 = 0
+    var f1 = x
+    p.indices.drop(1).foreach(i => {
+      val newF = (f1 + x).min(f0 + (p(i) - p(i - 1)) * 2)
+      f0 = f1
+      f1 = newF
+    })
+    f1 / 2
+  }
+}
