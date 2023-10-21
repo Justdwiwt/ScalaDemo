f6b0b6dd6ccf6ac89c519b9af7c70ed354bd847d
Justdwiwt <len_master@hotmail.com>
2023-10-21 22:23:27 +0800

    leetCode 1425

--- src/main/scala/leetCode/Solution_1425.scala
+++ src/main/scala/leetCode/Solution_1425.scala
@@ -2,27 +2,15 @@ package leetCode
 
 object Solution_1425 {
   def constrainedSubsetSum(nums: Array[Int], k: Int): Int = {
-    if (nums.isEmpty) return 0
-    var res = nums(0)
-    var pos = 0
-    var sums = Array.empty[Int]
-    sums :+= res
-    (1 until nums.length).foreach(i => {
-      var mx = nums(i)
-      if (i - pos < k) mx = if (res > 0) mx + res else mx
-      else {
-        var j = i - 1
-        while (j >= 0 && j >= i - k) {
-          if (mx < sums(j) + nums(i)) mx = sums(j) + nums(i)
-          j -= 1
-        }
+    var dq = Array.emptyIntArray
+    nums.indices.foreach(i => {
+      if (dq.nonEmpty) nums(i) = nums(i) + nums(dq.head)
+      while (dq.nonEmpty && (i - dq.head >= k || nums(i) >= nums(dq.last))) {
+        if (nums(i) >= nums(dq.last)) dq.drop(dq.last)
+        else dq.drop(0)
       }
-      sums :+= mx
-      if (res < mx) {
-        res = mx
-        pos = i
-      }
+      if (nums(i) > 0) dq :+= 0
     })
-    res
+    nums.max
   }
 }
