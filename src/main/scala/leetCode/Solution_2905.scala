d52d15923602f3c502ac860f83192ae490476d6b
Justdwiwt <len_master@hotmail.com>
2023-10-29 18:52:42 +0800

    leetCode 2905

---
+++ src/main/scala/leetCode/Solution_2905.scala
@@ -0,0 +1,15 @@
+package leetCode
+
+object Solution_2905 {
+  def findIndices(nums: Array[Int], indexDifference: Int, valueDifference: Int): Array[Int] = {
+    var mxIdx, mnIdx = 0
+    (indexDifference until nums.length).foreach(j => {
+      val i = j - indexDifference
+      if (nums(i) > nums(mxIdx)) mxIdx = i
+      else if (nums(i) < nums(mnIdx)) mnIdx = i
+      if (nums(mxIdx) - nums(j) >= valueDifference) return Array(mxIdx, j)
+      if (nums(j) - nums(mnIdx) >= valueDifference) return Array(mnIdx, j)
+    })
+    Array(-1, -1)
+  }
+}
