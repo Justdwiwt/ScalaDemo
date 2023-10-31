b639b34cc8dd7459caf184ec3b033fb4be474a83
Justdwiwt <len_master@hotmail.com>
2023-10-31 21:05:17 +0800

    leetCode 2903

--- src/main/scala/leetCode/Solution_2903.scala
+++ src/main/scala/leetCode/Solution_2903.scala
@@ -1,15 +1,11 @@
 package leetCode
 
 object Solution_2903 {
-  def findIndices(nums: Array[Int], indexDifference: Int, valueDifference: Int): Array[Int] = {
-    var mxIdx, mnIdx = 0
-    (indexDifference until nums.length).foreach(j => {
-      val i = j - indexDifference
-      if (nums(i) > nums(mxIdx)) mxIdx = i
-      else if (nums(i) < nums(mnIdx)) mnIdx = i
-      if (nums(mxIdx) - nums(j) >= valueDifference) return Array(mxIdx, j)
-      if (nums(j) - nums(mnIdx) >= valueDifference) return Array(mnIdx, j)
-    })
-    Array(-1, -1)
-  }
+  def findIndices(nums: Array[Int], indexDifference: Int, valueDifference: Int): Array[Int] = scala
+    .util
+    .Try(nums.indices.flatMap(i => (i + indexDifference until nums.length)
+      .withFilter(j => (nums(j) - nums(i)).abs >= valueDifference)
+      .map(j => Array(i, j)))
+      .head)
+    .getOrElse(Array(-1, -1))
 }
