731687b936ee78fee1637eabaaa1cffd0837b9db
Justdwiwt <len_master@hotmail.com>
2023-11-06 20:57:26 +0800

    leetCode 2913

---
+++ src/main/scala/leetCode/Solution_2913.scala
@@ -0,0 +1,9 @@
+package leetCode
+
+object Solution_2913 {
+  def sumCounts(nums: List[Int]): Int = nums
+    .indices
+    .flatMap(i => (i + 1 to nums.length).map(j => nums.slice(i, j).distinct.length))
+    .map(x => x * x)
+    .sum
+}
