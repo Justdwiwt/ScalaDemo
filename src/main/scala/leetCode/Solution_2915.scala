89b1a7de461ad34b55fcc504d0a6bb8babfac4b1
Justdwiwt <len_master@hotmail.com>
2023-11-08 12:53:10 +0800

    leetCode 2915

---
+++ src/main/scala/leetCode/Solution_2915.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Solution_2915 {
+  def lengthOfLongestSubsequence(nums: List[Int], target: Int): Int = {
+    val arr = Array.fill(target + 1)(Int.MinValue)
+    arr(0) = 0
+    var s = 0
+    nums.foreach(x => {
+      s = target.min(s + x)
+      (s to x by -1).foreach(j => arr(j) = arr(j).max(arr(j - x) + 1))
+    })
+    if (arr(target) > 0) arr(target) else -1
+  }
+}
