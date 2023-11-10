08262fc9717ae4fac80e5f75328f14e99b9f82d6
Justdwiwt <len_master@hotmail.com>
2023-11-10 20:36:25 +0800

    leetCode 1156

--- src/main/scala/leetCode/Solution_1156.scala
+++ src/main/scala/leetCode/Solution_1156.scala
@@ -1,33 +1,32 @@
 package leetCode
 
+import scala.collection.mutable
+
 object Solution_1156 {
-
-  case class E(ch: Char, n: Int)
-
-  @scala.annotation.tailrec
-  def parser(l: List[Char], xs: List[E] = Nil): List[E] = l match {
-    case Nil => xs
-    case h :: t => xs match {
-      case Nil => parser(t, E(h, 1) :: Nil)
-      case E(y, n) :: t2 => if (y == h) parser(t, E(y, n + 1) :: t2) else parser(t, E(h, 1) :: xs)
-    }
-  }
-
   def maxRepOpt1(text: String): Int = {
-    val m = scala.collection.mutable.HashMap[Char, Int]()
-    text.foreach(ch => m += ch -> (m.getOrElse(ch, 0) + 1))
+    val m = mutable.Map.empty[Char, Int]
+    text.indices.foreach(i => {
+      val c = text.charAt(i)
+      m += c -> (m.getOrElse(c, 0) + 1)
+    })
 
-    @scala.annotation.tailrec
-    def solve(xs: List[E], acc: Int): Int = xs match {
-      case E(a, n1) :: E(_, n2) :: E(c, n3) :: _ =>
-        if (n2 == 1 && a == c) solve(xs.tail, acc.max(m(a).min(n1 + n3 + 1)))
-        else solve(xs.tail, acc.max(m(a).min(n1 + 1)))
-      case E(a, n1) :: E(_, n2) :: Nil => acc.max(m(a).min(n1 + 1)).max(m(a).min(n2 + 1))
-      case E(_, n1) :: Nil => acc.max(n1)
-      case Nil => acc
+    var res = 0
+    var i = 0
+    while (i < text.length) {
+      var j = i
+      while (j < text.length && text.charAt(j) == text.charAt(i))
+        j += 1
+      val cur = j - i
+
+      if (cur < m.getOrElse(text.charAt(i), 0) && (j < text.length || i > 0))
+        res = res.max(cur + 1)
+
+      var k = j + 1
+      while (k < text.length && text.charAt(k) == text.charAt(i))
+        k += 1
+      res = res.max((k - i).min(m.getOrElse(text.charAt(i), 0)))
+      i = j
     }
-
-    solve(parser(text.toList, Nil), 0)
+    res
   }
-
 }
