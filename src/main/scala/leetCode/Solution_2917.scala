2cacac21f931281f3da8341e51016b385f5a0a45
Justdwiwt <len_master@hotmail.com>
2023-11-17 19:00:30 +0800

    leetCode 2917

---
+++ src/main/scala/leetCode/Solution_2917.scala
@@ -0,0 +1,13 @@
+package leetCode
+
+object Solution_2917 {
+  def findKOr(nums: Array[Int], k: Int): Int = nums
+    .flatMap(_.toBinaryString.reverse.zipWithIndex)
+    .filter(_._1 == '1')
+    .groupBy(_._2)
+    .mapValues(_.length)
+    .filter(_._2 >= k)
+    .map(n => math.pow(2, n._1))
+    .sum
+    .toInt
+}
