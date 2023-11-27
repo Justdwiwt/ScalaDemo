d479627e31e05e223684c7fc67c03da0f4323305
Justdwiwt <len_master@hotmail.com>
2023-11-27 18:22:36 +0800

    leetCode 2931

---
+++ src/main/scala/leetCode/Solution_2931.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2931 {
+  def maxSpending(values: Array[Array[Int]]): Long = {
+    val (m, n) = (values.length, values.head.length)
+    values.flatten.sorted.zip(1 to m * n).map { case (v, day) => v.toLong * day }.sum
+  }
+}
