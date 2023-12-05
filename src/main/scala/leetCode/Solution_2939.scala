867c72cacc08f219784c441c410bd98b2c595585
Justdwiwt <len_master@hotmail.com>
2023-12-05 18:43:16 +0800

    leetCode 2939

--- src/main/scala/leetCode/Solution_2939.scala
+++ src/main/scala/leetCode/Solution_2939.scala
@@ -1,38 +1,15 @@
 package leetCode
 
 object Solution_2939 {
-  def maximumXorProduct(_a: Long, _b: Long, n: Int): Int = {
-    var M = (1e9 + 7).toInt
-    var a = _a
-    var b = _b
-    if (b > a) {
-      val t = a
-      a = b
-      b = t
-    }
+  def maximumXorProduct(a: Long, b: Long, n: Int): Int = {
+    val M = 1000000007
 
-    var mask = (1L << n) - 1
-    var ax = a & (~mask)
-    var bx = b & (~mask)
-    a &= mask
-    b &= mask
+    @scala.annotation.tailrec
+    def dfs(a: Long, b: Long, bt: Long): Long =
+      if (n == 0 || bt == 0) ((a % M) * (b % M)) % M
+      else if ((a.min(b) & bt) > 0) dfs(a, b, bt >> 1)
+      else dfs(a ^ bt, b ^ bt, bt >> 1)
 
-    var left = a ^ b
-    var one = mask ^ left
-
-    ax |= one
-    bx |= one
-
-    if (left > 0 && ax == bx) {
-      var l = java.lang.Long.highestOneBit(left)
-      ax |= l
-      left ^= l
-    }
-
-    bx |= left
-
-    ax %= M
-    bx %= M
-    (ax * bx % M).toInt
+    dfs(a, b, 1L << (n - 1)).toInt
   }
 }
