cea723878dd8f18ce53d2ddf7c3b51ae3f9956ed
Justdwiwt <len_master@hotmail.com>
2023-12-16 18:15:46 +0800

    leetCode 1436

--- src/main/scala/leetCode/Solution_1436.scala
+++ src/main/scala/leetCode/Solution_1436.scala
@@ -1,8 +1,8 @@
 package leetCode
 
 object Solution_1436 {
-  def destCity(paths: List[List[String]]): String = {
-    val diff = paths.map(_.head)
-    paths.map({ case List(_, x) => x }).filterNot(diff.contains).head
-  }
+  def destCity(paths: List[List[String]]): String = paths
+    .map(_(1))
+    .filter(ds => paths.forall(p => p.head != ds))
+    .head
 }
