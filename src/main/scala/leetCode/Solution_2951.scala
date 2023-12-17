fed58a6c013c4042ba7002516168b57f0d4aa540
Justdwiwt <len_master@hotmail.com>
2023-12-17 18:09:05 +0800

    leetCode 2951

---
+++ src/main/scala/leetCode/Solution_2951.scala
@@ -0,0 +1,10 @@
+package leetCode
+
+object Solution_2951 {
+  def findPeaks(mountain: Array[Int]): List[Int] = mountain
+    .indices
+    .drop(1)
+    .dropRight(1)
+    .filter(i => mountain(i) > mountain(i - 1) && mountain(i) > mountain(i + 1))
+    .toList
+}
