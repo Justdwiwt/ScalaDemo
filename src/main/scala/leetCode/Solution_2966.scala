712ef184efb1fa07e55eebcfaf25acf780975556
Justdwiwt <len_master@hotmail.com>
2023-12-27 20:39:04 +0800

    leetCode 2966

---
+++ src/main/scala/leetCode/Solution_2966.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_2966 {
+  def divideArray(nums: Array[Int], k: Int): Array[Array[Int]] = {
+    val res = nums.sorted.sliding(3, 3).toArray
+    if (res.forall(n => (n(2) - n.head) <= k)) res else Array.empty
+  }
+}
