b604a0bfd86d7b988c98cd9a84b8eecf834c8550
Justdwiwt <len_master@hotmail.com>
2024-01-03 20:54:42 +0800

    leetCode 2980

--- src/main/scala/leetCode/Solution_2980.scala
+++ src/main/scala/leetCode/Solution_2980.scala
@@ -1,9 +1,6 @@
 package leetCode
 
 object Solution_2980 {
-  def hasTrailingZeros(nums: Array[Int]): Boolean = {
-    var cnt = 0
-    nums.indices.foreach(i => if ((nums(i) & 1) == 0) cnt += 1)
-    cnt >= 2
-  }
+  def hasTrailingZeros(nums: Array[Int]): Boolean =
+    nums.map(n => (n + 1) % 2).sum > 1
 }
