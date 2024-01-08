5773970910225e57b954c0da36cab10edfc45afb
Justdwiwt <len_master@hotmail.com>
2024-01-08 20:34:36 +0800

    leetCode 10031

---
+++ src/main/scala/leetCode/Solution_10031.scala
@@ -0,0 +1,19 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_10031 {
+  def missingInteger(nums: Array[Int]): Int = {
+    val st = mutable.HashSet.empty[Int]
+    nums.foreach(a => st += a)
+    var res = nums.head
+    val n = nums.length
+    var i = 1
+    while (i < n && nums(i) == nums(i - 1) + 1) {
+      res += nums(i)
+      i += 1
+    }
+    while (st.contains(res)) res += 1
+    res
+  }
+}
