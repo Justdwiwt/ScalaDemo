dc4d42d67eab364b6bceabce8451c054fa3670bd
Justdwiwt <len_master@hotmail.com>
2024-01-09 21:27:04 +0800

    leetCode 2996

--- src/main/scala/leetCode/Solution_2996.scala
+++ src/main/scala/leetCode/Solution_2996.scala
@@ -2,7 +2,7 @@ package leetCode
 
 import scala.collection.mutable
 
-object Solution_10031 {
+object Solution_2996 {
   def missingInteger(nums: Array[Int]): Int = {
     val st = mutable.HashSet.empty[Int]
     nums.foreach(a => st += a)
