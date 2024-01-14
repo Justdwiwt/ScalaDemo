7a31e2938bc4ff41e9dea67c05f0de351251f341
Justdwiwt <len_master@hotmail.com>
2024-01-14 17:05:51 +0800

    leetCode 2996

--- src/main/scala/leetCode/Solution_2996.scala
+++ src/main/scala/leetCode/Solution_2996.scala
@@ -1,19 +1,17 @@
 package leetCode
 
-import scala.collection.mutable
-
 object Solution_2996 {
   def missingInteger(nums: Array[Int]): Int = {
-    val st = mutable.HashSet.empty[Int]
-    nums.foreach(a => st += a)
-    var res = nums.head
-    val n = nums.length
-    var i = 1
-    while (i < n && nums(i) == nums(i - 1) + 1) {
-      res += nums(i)
-      i += 1
+    @scala.annotation.tailrec
+    def f(nums: List[Int], cur: Int): Int = nums match {
+      case Nil => cur
+      case head :: Nil => cur + head
+      case head :: next :: tail if next == head + 1 => f(next :: tail, cur + head)
+      case head :: _ => cur + head
     }
-    while (st.contains(res)) res += 1
-    res
+
+    val prefixSum = f(nums.toList, 0)
+
+    Stream.from(prefixSum).find(!nums.contains(_)).getOrElse(prefixSum)
   }
 }
