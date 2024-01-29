a2364ad52314f6d65b485098b6f915ca6513e3cb
Justdwiwt <len_master@hotmail.com>
2024-01-29 19:02:35 +0800

    Cracking the Coding Interview 16.10, 16.15, 16.21

---
+++ src/main/scala/leetCode/Code_16_10.scala
@@ -0,0 +1,12 @@
+package leetCode
+
+object Code_16_10 {
+  def maxAliveYear(birth: Array[Int], death: Array[Int]): Int = {
+    val arr = Array.fill(2001)(0)
+    (1900 to 2000).foreach(i => {
+      birth.foreach(j => if (i >= j) arr(i) += 1)
+      death.foreach(k => if (i > k) arr(i) -= 1)
+    })
+    arr.indexOf(arr.max)
+  }
+}
---
+++ src/main/scala/leetCode/Code_16_21.scala
@@ -0,0 +1,14 @@
+package leetCode
+
+object Code_16_21 {
+  def findSwapValues(array1: Array[Int], array2: Array[Int]): Array[Int] = {
+    val st = array2.toSet
+    val s1 = array1.sum
+    val s2 = array2.sum
+    var diff = s1 - s2
+    if (diff % 2 != 0) return Array.empty
+    diff /= 2
+    array1.foreach(n => if (st.contains(n - diff)) return Array(n, n - diff))
+    Array.empty
+  }
+}
