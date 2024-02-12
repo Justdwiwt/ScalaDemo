4390d2c56c43f6f2b7fb756037a6cd4c33bd9b43
Justdwiwt <len_master@hotmail.com>
2024-02-12 20:59:36 +0800

    leetCode 3021

---
+++ src/main/scala/leetCode/Solution_3021.scala
@@ -0,0 +1,8 @@
+package leetCode
+
+object Solution_3021 {
+  def flowerGame(n: Int, m: Int): Long = f(n, m)
+
+  private def f(n: Long, m: Long): Long =
+    (n >> 1) * (m + 1 >> 1) + (n + 1 >> 1) * (m >> 1)
+}
