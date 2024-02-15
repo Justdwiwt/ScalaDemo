940736db472a7f0b4bd6037fca5d6edd8feb2a78
Justdwiwt <len_master@hotmail.com>
2024-02-16 00:56:00 +0800

    leetCode 3026

---
+++ src/main/scala/leetCode/Solution_3026.scala
@@ -0,0 +1,22 @@
+package leetCode
+
+import scala.collection.mutable
+
+object Solution_3026 {
+  def maximumSubarraySum(nums: Array[Int], k: Int): Long = {
+    var prefSum = 0L
+    var maxGoodSum = Long.MinValue
+    val minPrefSum = mutable.HashMap[Long, Long]().withDefaultValue(Long.MaxValue)
+
+    nums.foreach(x => {
+      prefSum += x
+      minPrefSum(x) = minPrefSum(x).min(prefSum)
+
+      maxGoodSum = (Seq(x + k, x - k).collect { case seek if minPrefSum.contains(seek) =>
+        prefSum - minPrefSum(seek) + seek
+      } :+ maxGoodSum).max
+    })
+
+    if (maxGoodSum == Long.MinValue) 0 else maxGoodSum
+  }
+}
