fe1dd220c0f29b4f09484ca59646857d6df76a6a
Justdwiwt <len_master@hotmail.com>
2024-03-05 11:00:12 +0800

    leetCode 1222

--- src/main/scala/leetCode/Solution_1222.scala
+++ src/main/scala/leetCode/Solution_1222.scala
@@ -1,35 +1,44 @@
 package leetCode
 
-import scala.collection.mutable.ListBuffer
-import scala.util.control.Breaks._
+import spire.math.Algebraic.apply
 
 object Solution_1222 {
-
-  private val N = 8
-
   def queensAttacktheKing(queens: Array[Array[Int]], king: Array[Int]): List[List[Int]] = {
-    var res = List[List[Int]]()
-    val Q = ListBuffer.fill(N, N)(0)
-    queens.foreach(i => Q(i.head)(i(1)) = 1)
-    val dr = Array(1, 1, 0, -1, -1, -1, 0, 1)
-    val dc = Array(0, 1, 1, 1, 0, -1, -1, -1)
-    (0 until N).foreach(i => {
-      var r = king(0)
-      var c = king(1)
-      breakable {
-        while (flag(r, c)) {
-          if (Q(r)(c) > 0) {
-            res :+= List(r, c)
-            break
-          }
-          r += dr(i)
-          c += dc(i)
-        }
-      }
-    })
-    res
+    val Array(xk, yk) = king
+
+    def canAttack: Array[Int] => Boolean = {
+      case queen@Array(xq, yq) =>
+        val xAttack = (xq == xk) && !blockedPathX(queen)
+        lazy val yAttack = (yq == yk) && !blockedPathY(queen)
+        lazy val diagAttack = (math.abs(xq - xk) == math.abs(yq - yk)) && !blockedPathDiag(queen)
+        xAttack || yAttack || diagAttack
+    }
+
+    def blockedPathX: Array[Int] => Boolean = {
+      case Array(xq, yq) =>
+        val ys = yq.min(yk) + 1 until yq.max(yk)
+        queens.exists { case Array(x, y) => x == xq && ys.contains(y) }
+    }
+
+    def blockedPathY: Array[Int] => Boolean = {
+      case Array(xq, yq) =>
+        val xs = xq.min(xk) + 1 until xq.max(xk)
+        queens.exists { case Array(x, y) => xs.contains(x) && y == yq }
+    }
+
+    def blockedPathDiag: Array[Int] => Boolean = {
+      case Array(xq, yq) =>
+        val dx = (xk - xq).sign
+        val dy = (yk - yq).sign
+        val xs = xq until xk by dx
+        val ys = yq until yk by dy
+        val xys = xs.zip(ys).toSet.diff(Set((xq, yq)))
+        queens.exists { case Array(x, y) => xys.contains(x -> y) }
+    }
+
+    queens
+      .filter(canAttack)
+      .map(_.toList)
+      .toList
   }
-
-  def flag(r: Int, c: Int): Boolean = 0 <= r && r < N && 0 <= c && c < N
-
 }
