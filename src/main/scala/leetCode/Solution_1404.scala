c9bc77ae951d49680684fb842ad93733acf7fd19
Justdwiwt <len_master@hotmail.com>
2024-03-05 11:13:56 +0800

    leetCode 1404

--- src/main/scala/leetCode/Solution_1404.scala
+++ src/main/scala/leetCode/Solution_1404.scala
@@ -1,22 +1,8 @@
 package leetCode
 
-import scala.util.control.Breaks._
-
 object Solution_1404 {
   def numSteps(s: String): Int = {
-    if (s.isEmpty) return 0
-    var res = 0
-    var cur = 0
-    breakable {
-      (0 until s.length).reverse.foreach(i => {
-        if (i == 0 && cur == 0) break()
-        if (s(i) - '0' != cur) {
-          cur = 1
-          res += 1
-        }
-        res += 1
-      })
-    }
-    res
+    val res = (s.length - 1 to 1 by -1).foldLeft(0, 0)((acc, i) => if (s(i) - '0' + acc._2 == 1) (acc._1 + 2, 1) else (acc._1 + 1, acc._2))
+    res._2 + res._1
   }
 }
