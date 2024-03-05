e0e6747d1ec88f5a3da78a705028c57cc6497a8b
Justdwiwt <len_master@hotmail.com>
2024-03-05 10:44:41 +0800

    leetCode 1432

--- src/main/scala/leetCode/Solution_1432.scala
+++ src/main/scala/leetCode/Solution_1432.scala
@@ -1,32 +1,13 @@
 package leetCode
 
-import scala.util.control.Breaks._
-
 object Solution_1432 {
   def maxDiff(num: Int): Int = {
-    val one = 1.toString.charAt(0)
-    val zero = 0.toString.charAt(0)
-    val nine = 9.toString.charAt(0)
-    var flag = true
-    var s1 = num.toString
-    var s2 = num.toString
-    breakable {
-      s1.indices.foreach(i => {
-        if (i == 0) {
-          if (s1(i) == one) flag = false
-          else if (s1(i) != nine) {
-            s1 = s1.replace(s1(i), nine)
-            break()
-          }
-        } else {
-          if ((flag && s1(i) != nine) || (!flag && s1(i) != zero && s1(i) != one)) {
-            s1 = if (flag) s1.replace(s1(i), nine) else s1.replace(s1(i), zero)
-            break()
-          }
-        }
-      })
+    val digits = num.toString
+    val max = digits.find(_ != '9').fold(digits)(d => digits.replaceAll(d.toString, "9"))
+    val min = digits.head match {
+      case '1' => digits.find(_ > '1').fold(digits)(d => digits.replaceAll(d.toString, "0"))
+      case d => digits.replaceAll(d.toString, "1")
     }
-    s2 = if (flag) s2.replace(s2(0), one) else s2.replace(s2(0), nine)
-    (s1.toInt - s2.toInt).abs
+    max.toInt - min.toInt
   }
 }
