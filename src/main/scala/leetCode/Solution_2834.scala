8c4ffa4bb64a75009e168c38aabab0a76ab28ba1
Justdwiwt <len_master@hotmail.com>
2024-03-08 11:37:15 +0800

    leetCode 2834

--- src/main/scala/leetCode/Solution_2834.scala
+++ src/main/scala/leetCode/Solution_2834.scala
@@ -1,8 +1,8 @@
 package leetCode
 
 object Solution_2834 {
-  def minimumPossibleSum(n: Int, target: Int): Long = {
-    val m = n.min(target / 2)
-    ((1 + m).toLong * m / 2) + (2L * target + n - m - 1) * (n - m) / 2
+  def minimumPossibleSum(n: Int, k: Int): Int = {
+    val m = BigInt(n).min(BigInt(k) / 2)
+    ((m * (m + 1) + (n - m - 1 + k * 2) * (n - m)) / 2 % 1000000007).toInt
   }
 }
