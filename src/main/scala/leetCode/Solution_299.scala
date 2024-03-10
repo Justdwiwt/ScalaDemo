948f0afb2b706f85b1ecb7aa5e83a5dadb65ac41
Justdwiwt <len_master@hotmail.com>
2024-03-10 10:21:56 +0800

    leetCode 299

--- src/main/scala/leetCode/Solution_299.scala
+++ src/main/scala/leetCode/Solution_299.scala
@@ -2,18 +2,9 @@ package leetCode
 
 object Solution_299 {
   def getHint(secret: String, guess: String): String = {
-    var a = 0
-    var b = 0
-    val m = Array.fill(10)(0)
-    val n = Array.fill(10)(0)
-    secret.indices.foreach(i => {
-      if (secret(i) == guess(i)) a += 1
-      else {
-        m(secret(i) - '0') += 1
-        n(guess(i) - '0') += 1
-      }
-    })
-    (0 until 10).foreach(i => b += n(i).min(m(i)))
-    a + "A" + b + "B"
+    lazy val a = secret.zip(guess).count { case (s, g) => s == g }
+    lazy val List(s, g) = List(secret, guess).map(_.groupBy(identity).mapValues(_.length).toMap)
+    lazy val b = s.map { case (k, v) => g.getOrElse(k, 0).min(v) }.sum - a
+    a.toString + "A" + b + "B"
   }
 }
