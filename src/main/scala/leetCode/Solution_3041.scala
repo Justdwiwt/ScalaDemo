7d78b667965a4de9403731b4038ebfe154897140
Justdwiwt <len_master@hotmail.com>
2024-03-12 10:44:30 +0800

    leetCode 3041

--- src/main/scala/leetCode/Solution_3041.scala
+++ src/main/scala/leetCode/Solution_3041.scala
@@ -1,18 +1,9 @@
 package leetCode
 
 object Solution_3041 {
-  def maxSelectedElements(nums: Array[Int]): Int = {
-    val sorted = nums.sorted
-    val mx = sorted.last
-    val dp = Array.fill(mx + 2)(0)
-    dp(sorted.head) = 1
-    dp(sorted.head + 1) = 1
-    var res = 1
-    nums.indices.drop(1).foreach(i => {
-      dp(sorted(i) + 1) = dp(sorted(i)) + 1
-      dp(sorted(i)) = dp(sorted(i) - 1) + 1
-      res = res.max(dp(sorted(i)).max(dp(sorted(i) + 1)))
-    })
-    res
-  }
+  def maxSelectedElements(nums: Array[Int]): Int = nums
+    .sorted
+    ./:(Map.empty[Int, Int].withDefaultValue(0))((dp, n) => dp.updated(n + 1, dp(n) + 1).updated(n, dp(n - 1) + 1))
+    .values
+    .max
 }
