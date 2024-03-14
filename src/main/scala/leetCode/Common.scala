a279a17b8140d5060bc5d0065d95e85eda0323a6
Justdwiwt <len_master@hotmail.com>
2024-03-14 11:32:03 +0800

    update Common

--- src/main/scala/leetCode/Common.scala
+++ src/main/scala/leetCode/Common.scala
@@ -1,5 +1,7 @@
 package leetCode
 
+import scala.math.Integral.Implicits._
+
 object Common {
 
   def lowerBound(xs: Array[Int], x: Int): Int = {
@@ -66,4 +68,77 @@ object Common {
 
   def tuple2ToList[T](t: (T, T)): List[T] = List(t._1, t._2)
 
+  def powMod(base: Int, pow: Int, mod: Int): Int = {
+    @scala.annotation.tailrec
+    def dfs(base: Long, pow: Int, res: Long): Int =
+      if (pow == 0) res.toInt
+      else if (pow % 2 == 0) dfs((base * base) % mod, pow / 2, res)
+      else dfs((base * base) % mod, pow / 2, (res * base) % mod)
+
+    dfs(base, pow, res = 1L)
+  }
+
+  @scala.annotation.tailrec
+  def gcd[T: Integral](a: T, b: T): T =
+    if (b == 0) a else gcd(b, a % b)
+
+  def lcm[T: Integral](a: T, b: T): T =
+    a / gcd(a, b) * b
+
+  def bisectLeft(nums: collection.Seq[Int], n: Int): Int = {
+    @scala.annotation.tailrec
+    def dfs(l: Int, r: Int): Int =
+      if (l >= r) l
+      else {
+        val mid = (l + r) >>> 1
+        if (nums(mid) < n) dfs(mid + 1, r)
+        else dfs(l, mid)
+      }
+
+    dfs(l = 0, r = nums.length)
+  }
+
+  def characterCounts(s: String, upper: Boolean = false): Seq[Int] = {
+    val (start, end) = if (upper) ('A'.toInt, 'Z'.toInt) else ('a'.toInt, 'z'.toInt)
+    val asciiOffset = start
+    val charCountLength = end - asciiOffset + 1
+    val charCounts = Array.fill(charCountLength)(0)
+    s.foreach(char => charCounts(char.toInt - asciiOffset) += 1)
+    charCounts.toSeq
+  }
+
+  def neighbours[T](x: Int, y: Int, grid: Array[Array[T]]): Seq[(Int, Int)] = Seq((-1, 0), (0, -1), (0, 1), (1, 0)).collect {
+    case (dx, dy) if x + dx >= 0 && x + dx < grid.length && y + dy >= 0 && y + dy < grid.head.length => (x + dx, y + dy)
+  }
+
+  def isPalindrome(s: String): Boolean = {
+    @scala.annotation.tailrec
+    def dfs(l: Int, r: Int): Boolean =
+      l >= r || (s(l) == s(r) && dfs(l + 1, r - 1))
+
+    dfs(l = 0, r = s.length - 1)
+  }
+
+  def sieveOfEratosthenes(n: Int): Seq[Int] = {
+    val arr = Array.tabulate(n)(i => i)
+    (2 to math.sqrt(n).toInt).foreach(i => if (arr(i) == i) (i * i until n by i).foreach(j => if (arr(j) > i) arr(j) = i))
+    arr.toSeq
+  }
+
+  def seqToLinkedList(elements: Seq[Int]): ListNode =
+    elements.:\[ListNode](null)((value, next) => new ListNode(value, next))
+
+  def linkedListToSeq(head: ListNode): Seq[Int] =
+    if (head == null) Seq.empty
+    else head.x +: linkedListToSeq(head.next)
+
+  class ListNode(_x: Int = 0, _next: ListNode = null) {
+    var next: ListNode = _next
+    var x: Int = _x
+  }
+
+  class Node(var _value: Int) {
+    var value: Int = _value
+    var children: List[Node] = Nil
+  }
 }
