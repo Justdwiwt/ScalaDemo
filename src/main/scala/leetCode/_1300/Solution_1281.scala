6dfa773020725c20881b2d863d400edb9368f6b8
Justdwiwt <len_master@hotmail.com>
2024-03-28 17:01:56 +0800

    leetCode 1266, 1267, 1281, 1287, 1289

--- src/main/scala/leetCode/_1300/Solution_1281.scala
+++ src/main/scala/leetCode/_1300/Solution_1281.scala
@@ -1,8 +1,11 @@
 package leetCode._1300
 
 object Solution_1281 {
-  def subtractProductAndSum(n: Int): Int = {
-    val t = n.toString.map(_ - '0')
-    t.product - t.sum
-  }
+  def subtractProductAndSum(n: Int): Int = n
+    .toString
+    .map(_.asDigit)
+    .product - n
+    .toString
+    .map(_.asDigit)
+    .sum
 }
--- src/main/scala/leetCode/_1300/Solution_1289.scala
+++ src/main/scala/leetCode/_1300/Solution_1289.scala
@@ -1,14 +1,14 @@
 package leetCode._1300
 
 object Solution_1289 {
-  def minFallingPathSum(arr: Array[Array[Int]]): Int = {
-    (0 to arr.length - 2).reverse.foreach(i => arr.indices.foreach(j => {
-      var mn = Int.MaxValue
-      arr.indices.foreach(k => if (k != j) mn = mn.min(arr(i + 1)(k)))
-      arr(i)(j) += mn
-    }))
-    var res = Int.MaxValue
-    arr.indices.foreach(i => res = res.min(arr(0)(i)))
-    res
+  def minFallingPathSum(matrix: Array[Array[Int]]): Int = {
+    def minExcept(arr: Array[Int], exIdx: Int): Int =
+      arr.indices.view.collect { case x if x != exIdx => arr(x) }.min
+
+    def step(s: Array[Int], ns: Array[Int]): Array[Int] = {
+      ns.indices.map(i => minExcept(s, i) + ns(i)).toArray
+    }
+
+    matrix.tail.foldLeft(matrix.head)(step).min
   }
 }
