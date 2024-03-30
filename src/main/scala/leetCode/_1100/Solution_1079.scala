2ff4cd4a0b625e975655a3e7fefe908891ba4459
Justdwiwt <len_master@hotmail.com>
2024-03-30 20:34:33 +0800

    leetCode 1078, 1079, 1089

--- src/main/scala/leetCode/_1100/Solution_1079.scala
+++ src/main/scala/leetCode/_1100/Solution_1079.scala
@@ -3,6 +3,6 @@ package leetCode._1100
 object Solution_1079 {
   def numTilePossibilities(tiles: String): Int = List
     .range(1, tiles.length + 1)
-    .map(i => tiles.toSeq.combinations(i).toList.map(s => s.permutations.toList.length).sum)
+    .map(tiles.toSeq.combinations(_).toList.map(_.permutations.toList.length).sum)
     .sum
 }
--- src/main/scala/leetCode/_1100/Solution_1089.scala
+++ src/main/scala/leetCode/_1100/Solution_1089.scala
@@ -2,13 +2,14 @@ package leetCode._1100
 
 object Solution_1089 {
   def duplicateZeros(arr: Array[Int]): Unit = {
-    var cnt = arr.count(_ == 0)
-    (arr.length - 1 until -1 by -1).foreach(i => {
-      if (arr(i) == 0) {
-        cnt -= 1
-        if (i + cnt < arr.length) arr(i + cnt) = 0
-        if (i + cnt + 1 < arr.length) arr(i + cnt + 1) = 0
-      } else if (i + cnt < arr.length) arr(i + cnt) = arr(i)
-    })
+    val lastIndex = arr.lastIndexWhere(_ => true)
+    (lastIndex until -1 by -1).foreach(i => if (arr(i).equals(0)) f(arr, i, lastIndex))
   }
+
+  @scala.annotation.tailrec
+  private def f(arr: Array[Int], lIndex: Int, rIndex: Int): Unit =
+    if (rIndex > lIndex) {
+      arr(rIndex) = arr(rIndex - 1)
+      f(arr, lIndex, rIndex - 1)
+    }
 }
