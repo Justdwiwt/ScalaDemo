a803affebf5770077d4119443716a14a44586492
Justdwiwt <len_master@hotmail.com>
2024-04-04 12:06:00 +0800

    leetCode 599, 600

--- src/main/scala/leetCode/_600/Solution_599.scala
+++ src/main/scala/leetCode/_600/Solution_599.scala
@@ -2,7 +2,7 @@ package leetCode._600
 
 object Solution_599 {
   def findRestaurant(list1: Array[String], list2: Array[String]): Array[String] = List(list1, list2)
-    .reduce((a, b) => a intersect b)
+    .reduce(_.intersect(_))
     .groupBy(e => list1.indexOf(e) + list2.indexOf(e))
     ./:(Int.MaxValue -> Array[String]()) { case (min, p) => if (min._1 <= p._1) min else p }._2
 }
--- src/main/scala/leetCode/_600/Solution_600.scala
+++ src/main/scala/leetCode/_600/Solution_600.scala
@@ -1,22 +1,13 @@
 package leetCode._600
 
 object Solution_600 {
-  def findIntegers(num: Int): Int = {
-    var res = 0
-    var k = 31
-    var pre = 0
-    val f = new Array[Int](32)
-    f(0) = 1
-    f(1) = 2
-    (2 until 31).foreach(i => f(i) = f(i - 2) + f(i - 1))
-    while (k >= 0) {
-      if ((num & (1 << k)) > 0) {
-        res += f(k)
-        if (pre > 0) return res
-        pre = 1
-      } else pre = 0
-      k -= 1
-    }
-    res + 1
+  def findIntegers(n: Int): Int =
+    f(n, 1, 1) + 1
+
+  private def f(max: Int, n: Int, bit: Int): Int = {
+    if (n == max) 1
+    else if (n > max) 0
+    else if (bit == 1) 1 + f(max, n << 1, 0)
+    else 1 + f(max, (n << 1) + 1, 1) + f(max, n << 1, 0)
   }
 }
