2cc71486c7a262d221b4d44f45089452bc5b341f
Justdwiwt <len_master@hotmail.com>
2024-04-05 17:02:25 +0800

    leetCode 497, 498, 500

--- src/main/scala/leetCode/_500/Solution_497.scala
+++ src/main/scala/leetCode/_500/Solution_497.scala
@@ -1,24 +1,29 @@
 package leetCode._500
 
 import scala.collection.immutable
+import scala.collection.immutable.SortedMap
+import scala.util.Random
 
 object Solution_497 {
-  //  class Solution(_rects: Array[Array[Int]]) {
-  //    val random = scala.util.Random
-  //
-  //    def getRnd(mx: Int): Int = random.nextInt.abs % mx
-  //
-  //    val prefixSums = immutable.SortedMap.from(_rects.map(r => (r(2) - r(0) + 1) * (r(3) - r(1) + 1))
-  //      ./:(List(0)) { case (list, area) => (list.head + area) +: list }
-  //      .reverse
-  //      .tail
-  //      .zipWithIndex
-  //    )
-  //    val sum = prefixSums.keys.last
-  //
-  //    def pick(): Array[Int] = {
-  //      val r = _rects(prefixSums.iteratorFrom(getRnd(sum + 1)).next._2)
-  //      Array(r(0) + getRnd(r(2) - r(0) + 1), r(1) + getRnd(r(3) - r(1) + 1))
-  //    }
-  //  }
+  class Solution(_rects: Array[Array[Int]]) {
+    val random: Random.type = scala.util.Random
+
+    private def getRnd(mx: Int): Int =
+      random.nextInt.abs % mx
+
+    val prefixSums: SortedMap[Int, Int] = immutable.SortedMap(_rects
+      .map(r => (r(2) - r(0) + 1) * (r(3) - r(1) + 1))
+      .foldLeft(List(0)) { case (list, area) => (list.head + area) +: list }
+      .reverse
+      .tail
+      .zipWithIndex: _*
+    )
+
+    val sum: Int = prefixSums.keys.last
+
+    def pick(): Array[Int] = {
+      val r = _rects(prefixSums.iteratorFrom(getRnd(sum + 1)).next._2)
+      Array(r.head + getRnd(r(2) - r.head + 1), r(1) + getRnd(r(3) - r(1) + 1))
+    }
+  }
 }
--- src/main/scala/leetCode/_500/Solution_498.scala
+++ src/main/scala/leetCode/_500/Solution_498.scala
@@ -1,13 +1,23 @@
 package leetCode._500
 
 object Solution_498 {
-  def findDiagonalOrder(A: Array[Array[Int]]): Array[Int] = {
-    if (A.isEmpty) return Array.empty[Int]
-    val arr = Array.fill(A.head.length + A.length - 1)(List.empty[Int])
-    A.indices.foreach(i => A.head.indices.foreach(j => arr(i + j) ::= A(i)(j)))
-    (0 to A.head.length + A.length - 2).toArray.flatMap(i => i % 2 match {
-      case 0 => arr(i)
-      case 1 => arr(i).reverse
-    })
+  private case class Pos(i: Int, j: Int, diff: Int)
+
+  def findDiagonalOrder(mat: Array[Array[Int]]): Array[Int] = {
+    val n = mat.head.length
+
+    val diags = mat.indices.flatMap(i => mat(i).indices.map(j => Pos(i, j, n - i - j) -> mat(i)(j)))
+
+    val positionList: List[List[Pos]] = diags
+      .groupBy { case (Pos(_, _, d), _) => d }
+      .toList
+      .sortBy { case (d, _) => -d }
+      .map { case (_, l) => l.toList.map { case (p, _) => p } }
+
+
+    positionList.zipWithIndex.flatMap {
+      case (l, i) if i % 2 == 0 => l.sortBy { case Pos(_, j, _) => j }.map(p => mat(p.i)(p.j))
+      case (l, _) => l.sortBy { case Pos(_, j, _) => -j }.map(p => mat(p.i)(p.j))
+    }.toArray
   }
 }
