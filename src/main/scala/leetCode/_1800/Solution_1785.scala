883b2886958750bc2d535af54297338f920f9a4b
Justdwiwt <len_master@hotmail.com>
2024-04-13 11:16:55 +0800

    leetCode 1785

--- src/main/scala/leetCode/_1800/Solution_1785.scala
+++ src/main/scala/leetCode/_1800/Solution_1785.scala
@@ -1,14 +1,6 @@
 package leetCode._1800
 
 object Solution_1785 {
-  def minElements(nums: Array[Int], limit: Int, goal: Int): Int = {
-    val sum = nums.map(_.toLong).sum
-    val needed = (goal.toLong - sum).abs
-    if (needed == 0) 0
-    else {
-      val res = needed / limit.toLong
-      val extra = if (needed % limit.toLong == 0) 0L else 1L
-      (res + extra).toInt
-    }
-  }
+  def minElements(nums: Array[Int], limit: Int, goal: Int): Int =
+    (((goal - nums.foldLeft(0L)(_ + _)).abs + limit - 1) / limit).toInt
 }
