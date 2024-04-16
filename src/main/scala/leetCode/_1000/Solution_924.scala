328ba8a7ab0f11323b1c3dc97f28764b654ba5a1
Justdwiwt <len_master@hotmail.com>
2024-04-16 10:04:16 +0800

    leetCode 924

--- src/main/scala/leetCode/_1000/Solution_924.scala
+++ src/main/scala/leetCode/_1000/Solution_924.scala
@@ -2,19 +2,19 @@ package leetCode._1000
 
 object Solution_924 {
 
-  case class Node(res: Int, size: Int, vis: Set[Int])
+  private case class Node(res: Int, size: Int, vis: Set[Int])
 
   def minMalwareSpread(graph: Array[Array[Int]], initial: Array[Int]): Int =
-    (Node(0, -1, Set.empty[Int]) /: initial.sorted) (g(graph.map(_.zipWithIndex.filter(_._1 == 1).map(_._2)), initial.toSet)).res
+    initial.sorted./:(Node(0, -1, Set.empty[Int]))(g(graph.map(_.zipWithIndex.filter(_._1 == 1).map(_._2)), initial.toSet)).res
 
-  def g(graph: Array[Array[Int]], initial: Set[Int])(d: Node, n: Int): Node = {
+  private def g(graph: Array[Array[Int]], initial: Set[Int])(d: Node, n: Int): Node = {
     val segInfo = f(graph, Set(n), d.vis ++ (initial - n), Set.empty[Int])
     if (segInfo._1 > d.size) Node(n, segInfo._1, d.vis ++ segInfo._2)
     else Node(d.res, d.size, d.vis ++ segInfo._2)
   }
 
   @scala.annotation.tailrec
-  def f(graph: Array[Array[Int]], queue: Set[Int], vis: Set[Int], visSeg: Set[Int]): (Int, Set[Int]) =
+  private def f(graph: Array[Array[Int]], queue: Set[Int], vis: Set[Int], visSeg: Set[Int]): (Int, Set[Int]) =
     if (queue.isEmpty) (visSeg.size, visSeg)
     else {
       val visSegNew = visSeg ++ queue
