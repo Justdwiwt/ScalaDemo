df1392c665395eee37629958078d66409a9c46c7
Justdwiwt <len_master@hotmail.com>
2024-05-10 13:24:20 +0800

    leetCode 3139

--- src/main/scala/leetCode/_3200/Solution_3139.scala
+++ src/main/scala/leetCode/_3200/Solution_3139.scala
@@ -1,45 +1,41 @@
 package leetCode._3200
 
 object Solution_3139 {
-  val mod: Int = 1000000007
+  val M: Int = 1000000007
 
   def minCostToEqualizeArray(nums: Array[Int], cost1: Int, cost2: Int): Int = {
-    var max = 0
-    var min = nums(0)
-    var sum: Long = 0
-    for (x <- nums) {
-      if (x < min) min = x
-      if (x > max) max = x
-      sum += x
+    val (min, max, sum) = nums.foldLeft((nums.head, nums.head, 0L)) {
+      case ((currentMin, currentMax, currentSum), x) =>
+        (currentMin.min(x), currentMax.max(x), currentSum + x)
     }
 
-    val n: Int = nums.length
-    sum = max.toLong * n - sum
+    val n = nums.length
+    val totalIncrement = max.toLong * n - sum
 
-    if (cost1 * 2 <= cost2 || n <= 2) {
-      return (sum * cost1 % mod).toInt
-    }
+    if (cost1 * 2 <= cost2 || n <= 2) return (totalIncrement * cost1 % M).toInt
 
-    val maxDiff: Long = max - min
-    var sum1: Long = math.max(sum % 2, maxDiff * 2 - sum)
-    val sum2: Long = sum - sum1
-    var ans: Long = sum2 / 2 * cost2
+    val maxDiff = max - min
+    val initialSum1 = (totalIncrement % 2).max(maxDiff * 2 - totalIncrement)
+    val sum2 = totalIncrement - initialSum1
+    var ans = sum2 / 2 * cost2
 
-    if (cost2 * (n - 1) >= cost1 * (n - 2)) {
-      ans += sum1 / (n - 1) * (n - 1) * cost1
-      sum1 %= n - 1
-    } else {
-      ans += sum1 / (n - 2) * (n - 1) * cost2
-      sum1 %= n - 2
-    }
+    val (sum1, additionalCost) =
+      if (cost2 * (n - 1) >= cost1 * (n - 2)) (initialSum1 % (n - 1), initialSum1 / (n - 1) * (n - 1) * cost1)
+      else (initialSum1 % (n - 2), initialSum1 / (n - 2) * (n - 1) * cost2)
 
-    val option1: Long = sum1 * cost1
-    var option2: Long = (sum1 + n) / 2 * cost2
-    if ((sum1 + n) % 2 == 1)
-      option2 += (if (n % 2 == 1) math.min(cost1, (n + 1) / 2 * cost2) else cost1)
+    ans += additionalCost
 
-    ans += math.min(option1, option2)
+    val option1 = sum1 * cost1
+    val option2Base = (sum1 + n) / 2 * cost2
+    val option2 = option2Base + (
+      if ((sum1 + n) % 2 == 1)
+        if (n % 2 == 1) cost1.min((n + 1) / 2 * cost2)
+        else cost1
+      else 0
+      )
 
-    (ans % mod).toInt
+    ans += option1.min(option2)
+
+    (ans % M).toInt
   }
 }
