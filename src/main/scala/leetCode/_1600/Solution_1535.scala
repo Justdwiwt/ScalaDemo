be3ee565dcf7e373223c35fe0daab0e782acdf19
Justdwiwt <len_master@hotmail.com>
2024-05-19 09:55:28 +0800

    leetCode 1535

--- src/main/scala/leetCode/_1600/Solution_1535.scala
+++ src/main/scala/leetCode/_1600/Solution_1535.scala
@@ -2,15 +2,14 @@ package leetCode._1600
 
 object Solution_1535 {
   def getWinner(arr: Array[Int], k: Int): Int = {
-    var res = arr.head
-    var cnt = 0
-    arr.indices.drop(1).withFilter(_ => cnt < k).foreach(i => {
-      if (arr(i) < res) cnt += 1
-      else {
-        res = arr(i)
-        cnt = 1
+    @scala.annotation.tailrec
+    def f(h0: Int, it: Iterator[Int], rem: Int): Int =
+      if (rem == 0 || !it.hasNext) h0 else {
+        lazy val h1 = it.next()
+        lazy val (_, mx) = if (h0 < h1) (h0, h1) else (h1, h0)
+        f(mx, it, if (mx == h0) rem - 1 else k - 1)
       }
-    })
-    res
+
+    if (k > arr.length) arr.max else f(arr.head, arr.iterator.drop(1), k)
   }
 }
