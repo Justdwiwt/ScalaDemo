e6aa55d9448461d23f4934b262f5a74c13b784e9
Justdwiwt <len_master@hotmail.com>
2024-05-21 10:09:21 +0800

    leetCode 3155

--- src/main/scala/leetCode/_3200/Solution_3155.scala
+++ src/main/scala/leetCode/_3200/Solution_3155.scala
@@ -1,10 +1,16 @@
 package leetCode._3200
 
 object Solution_3155 {
-  // fixme: case 572/574 wrong answer
+  private def ceilDivide(a: Long, b: Long): Long =
+    (a + b - 1) / b
+
   def maxUpgrades(count: Array[Int], upgrade: Array[Int], sell: Array[Int], money: Array[Int]): Array[Int] = count
     .zip(upgrade)
     .zip(sell)
     .zip(money)
-    .map { case (((i, j), p), q) => i.min((q + p * i) / (j + p)) }
+    .map {
+      case (((c, u), s), m) =>
+        val maxUpgrade = c.min(c - ceilDivide(c.toLong * u - m, u + s).toInt)
+        maxUpgrade
+    }
 }
