0fb9c5201f2c805e1f5d8f330fd8f33dcb4b2145
Justdwiwt <len_master@hotmail.com>
2024-05-23 10:32:11 +0800

    leetCode 2831

--- src/main/scala/leetCode/_2900/Solution_2831.scala
+++ src/main/scala/leetCode/_2900/Solution_2831.scala
@@ -1,20 +1,28 @@
 package leetCode._2900
 
+import scala.collection.mutable
+
 object Solution_2831 {
   def longestEqualSubarray(nums: List[Int], k: Int): Int = {
-    val n = nums.length
-    var list = Array.empty[List[Int]]
-    (0 to n).foreach(_ => list :+= List.empty[Int])
-    nums.indices.foreach(i => list(nums(i)) :+= i)
     var res = 0
-    (1 to n).foreach(u => {
-      var q = list(u)
-      var i = 0
-      var j = 0
-      while (i < q.length) {
-        while ((q(i) - q(j)) - (i - j) > k) j += 1
-        res = res.max(i - j + 1)
-        i += 1
+    val q = mutable.Map.empty[Int, mutable.Queue[Int]]
+
+    nums.zipWithIndex.foreach { case (c, i) =>
+      if (!q.contains(c)) q(c) = mutable.Queue.empty[Int]
+      q(c).enqueue(i)
+    }
+
+    q.keys.foreach(c => {
+      val lst = mutable.Queue.empty[Int]
+      var s = 0
+      q(c).zipWithIndex.foreach { case (i, _) =>
+        if (lst.nonEmpty) s += i - lst.last - 1
+        lst.enqueue(i)
+        while (s > k) {
+          val i0 = lst.dequeue()
+          s -= lst.front - i0 - 1
+        }
+        res = res.max(lst.length)
       }
     })
     res
