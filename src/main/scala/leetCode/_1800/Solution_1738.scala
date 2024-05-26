a642a186cd38a040715fc4a310fe4da099449832
Justdwiwt <len_master@hotmail.com>
2024-05-26 09:49:07 +0800

    leetCode 1738

--- src/main/scala/leetCode/_1800/Solution_1738.scala
+++ src/main/scala/leetCode/_1800/Solution_1738.scala
@@ -2,13 +2,13 @@ package leetCode._1800
 
 object Solution_1738 {
   def kthLargestValue(matrix: Array[Array[Int]], k: Int): Int = {
-    val dp = Array.ofDim[Int](matrix.length + 1, matrix.head.length + 1)
-    val pq = new java.util.PriorityQueue[Int]
-    (1 to matrix.length).foreach(i => (1 to matrix.head.length).foreach(j => {
+    val dp = Array.fill(matrix.length + 1, matrix.head.length + 1)(0)
+
+    val values = (1 to matrix.length).flatMap(i => (1 to matrix.head.length).map(j => {
       dp(i)(j) = dp(i - 1)(j) ^ dp(i)(j - 1) ^ dp(i - 1)(j - 1) ^ matrix(i - 1)(j - 1)
-      pq.offer(dp(i)(j))
-      while (pq.size() > k) pq.poll()
+      dp(i)(j)
     }))
-    pq.peek()
+
+    values.sorted(Ordering[Int].reverse).apply(k - 1)
   }
 }
