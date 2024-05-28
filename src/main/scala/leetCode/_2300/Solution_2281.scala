05c6f2e6c2d0e682c138fde2b3db76ee84b0cbac
Justdwiwt <len_master@hotmail.com>
2024-05-28 18:14:46 +0800

    leetCode 2281

--- src/main/scala/leetCode/_2300/Solution_2281.scala
+++ src/main/scala/leetCode/_2300/Solution_2281.scala
@@ -13,22 +13,20 @@ object Solution_2281 {
     val st = ListBuffer.empty[Int]
     st.append(-1)
 
-    strength.indices
-      .foreach(i => {
-        while (st.size > 1 && strength(st.last) >= strength(i))
-          right(st.remove(st.size - 1)) = i
-        left(i) = st.last
-        st.append(i)
-      })
+    strength.indices.foreach(i => {
+      while (st.size > 1 && strength(st.last) >= strength(i))
+        right(st.remove(st.size - 1)) = i
+      left(i) = st.last
+      st.append(i)
+    })
 
     var s = 0L
 
     val ss = Array.fill(n + 2)(0L)
-    (1 to n)
-      .foreach(i => {
-        s += strength(i - 1)
-        ss(i + 1) = (ss(i) + s) % M
-      })
+    (1 to n).foreach(i => {
+      s += strength(i - 1)
+      ss(i + 1) = (ss(i) + s) % M
+    })
 
     var res = 0L
 
@@ -38,6 +36,7 @@ object Solution_2281 {
       val tot = ((i - l + 1).toLong * (ss(r + 2) - ss(i + 1)) - (r - i + 1).toLong * (ss(i + 1) - ss(l))) % M
       res = (res + strength(i) * tot) % M
     })
+
     ((res + M) % M).toInt
   }
 }
