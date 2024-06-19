c11df3079eccfc3be9da5c9f8c57cf204bb6f64d
Justdwiwt <len_master@hotmail.com>
2024-06-19 09:20:20 +0800

    leetCode 1482

--- src/main/scala/leetCode/_1500/Solution_1482.scala
+++ src/main/scala/leetCode/_1500/Solution_1482.scala
@@ -1,25 +1,28 @@
 package leetCode._1500
 
+import scala.collection.mutable
+
 object Solution_1482 {
   def minDays(bloomDay: Array[Int], m: Int, k: Int): Int = {
-    if (k * m > bloomDay.length) return -1
-    f(bloomDay, m, k, 1, bloomDay.max)._3
-  }
+    def countBouquets(day: Int): Int = bloomDay
+      .indices
+      .iterator
+      .filter(bloomDay(_) <= day)
+      .foldLeft(List[(Int, Int)]()) {
+        case ((start, end) :: tail, i) if i == end + 1 => (start, end + 1) :: tail
+        case (ranges, i) => (i, i) :: ranges
+      }
+      .map { case (start, end) => (end - start + 1) / k }
+      .sum
 
-  @scala.annotation.tailrec
-  private def f(bloomDay: Array[Int], m: Int, k: Int, left: Int, right: Int): (Int, Int, Int) = {
-    if (left >= right && isDayOkay(bloomDay, m, k, left)) return (0, 0, left)
-    if (left >= right) return (0, 0, bloomDay.max)
-    val mid = (left + right) >>> 1
-    if (isDayOkay(bloomDay, m, k, mid)) f(bloomDay, m, k, left, mid)
-    else f(bloomDay, m, k, mid + 1, right)
-  }
+    @scala.annotation.tailrec
+    def search(seq: mutable.IndexedSeq[Int]): Int = seq.size match {
+      case 1 if countBouquets(seq.head) >= m => seq.head
+      case 0 | 1 => -1
+      case n if countBouquets(seq((n - 1) / 2)) >= m => search(seq.take((n - 1) / 2 + 1))
+      case n => search(seq.drop((n - 1) / 2 + 1))
+    }
 
-  private def isDayOkay(bloomDay: Array[Int], m: Int, k: Int, currDay: Int): Boolean = bloomDay./:((m, k)) {
-    case ((0, _), _) => (0, 0)
-    case ((cur, _), d) if d > currDay => (cur, k)
-    case ((cur, currK), _) if currK > 1 => (cur, currK - 1)
-    case ((cur, 1), _) => (cur - 1, k)
-    case ((cur, currK), _) => (cur, currK)
-  }._1 == 0
+    search(bloomDay.distinct.sorted)
+  }
 }
