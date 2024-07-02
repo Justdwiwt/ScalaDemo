d08a05e19805e3df3a1c140d7d622a709a0c9717
Justdwiwt <len_master@hotmail.com>
2024-07-02 09:17:35 +0800

    leetCode 716

--- src/main/scala/leetCode/_800/Solution_716.scala
+++ src/main/scala/leetCode/_800/Solution_716.scala
@@ -1,37 +1,72 @@
 package leetCode._800
 
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 object Solution_716 {
 
   class MaxStack {
-    private val stack = mutable.Stack[Int]()
-    private val max = mutable.Stack[Int]()
-    private val buf = mutable.Stack[Int]()
+
+    class Node(val value: Int) {
+      var prev: Node = _
+      var next: Node = _
+    }
+
+    private class DoubleLinkedList {
+      private val head: Node = new Node(0)
+      val tail: Node = new Node(0)
+      head.next = tail
+      tail.prev = head
+
+      def addTop(node: Node): Unit = {
+        node.next = tail
+        node.prev = tail.prev
+        tail.prev.next = node
+        tail.prev = node
+      }
+
+      def delTop(): Unit = {
+        tail.prev.prev.next = tail
+        tail.prev = tail.prev.prev
+      }
+
+      def delete(node: Node): Unit = {
+        node.prev.next = node.next
+        node.next.prev = node.prev
+      }
+    }
+
+    private val dll = new DoubleLinkedList()
+    val map: mutable.TreeMap[Int, ArrayBuffer[Node]] = mutable.TreeMap.empty[Int, ArrayBuffer[Node]]
 
     def push(x: Int): Unit = {
-      if (max.isEmpty || x >= max.top) max.push(x)
-      stack.push(x)
+      val newNode = new Node(x)
+      dll.addTop(newNode)
+      map.getOrElseUpdate(x, ArrayBuffer()).append(newNode)
     }
 
     def pop(): Int = {
-      if (stack.top == max.top) max.pop()
-      stack.pop()
+      val valTop = top()
+      dll.delTop()
+      val nodes = map(valTop)
+      nodes.remove(nodes.size - 1)
+      if (nodes.isEmpty) map.remove(valTop)
+      valTop
     }
 
-    def top(): Int =
-      stack.top
+    private def top(): Int =
+      dll.tail.prev.value
 
     def peekMax(): Int =
-      max.top
+      map.lastKey
 
     def popMax(): Int = {
-      while (stack.top != max.top) buf.push(stack.pop())
-
-      val ret = pop()
-      while (buf.nonEmpty) push(buf.pop())
-
-      ret
+      val maxVal = map.lastKey
+      val nodes = map(maxVal)
+      val node = nodes.remove(nodes.size - 1)
+      if (nodes.isEmpty) map.remove(maxVal)
+      dll.delete(node)
+      maxVal
     }
   }
 
