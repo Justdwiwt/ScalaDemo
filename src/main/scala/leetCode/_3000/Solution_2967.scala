9a71e065593991b711881244029ce11171df6a52
Justdwiwt <len_master@hotmail.com>
2024-07-20 12:14:05 +0800

    leetCode 2967

--- src/main/scala/leetCode/_3000/Solution_2967.scala
+++ src/main/scala/leetCode/_3000/Solution_2967.scala
@@ -1,32 +1,50 @@
 package leetCode._3000
 
-//fixme: case 543/648 accuracy overflow
 object Solution_2967 {
-  def minimumCost(nums: Array[Int]): Long = {
-    val sortedNums = nums.sorted.map(BigInt(_))
-    val midIndex = sortedNums.length / 2
-    val mid = sortedNums(midIndex)
+  private val pal: List[Int] = {
+    def generatePalindromes(base: Int): Seq[Int] = {
+      val oddLengthPalindromes = (base until base * 10).flatMap(i => {
+        val s = i.toString
+        val palindrome = s + s.reverse.tail
+        Seq(palindrome.toInt)
+      })
 
-    if (isPalindrome(mid)) cost(sortedNums, mid).toLong
-    else {
-      var l = mid - 1
-      var r = mid + 1
-      while (l > BigInt(1) && !isPalindrome(l)) l -= 1
-      while (r < BigInt("1000000000") && !isPalindrome(r)) r += 1
+      val evenLengthPalindromes = if (base <= 1000) (base until base * 10).flatMap(i => {
+        val s = i.toString
+        val palindrome = s + s.reverse
+        Seq(palindrome.toInt)
+      }) else Seq.empty[Int]
 
-      cost(sortedNums, if (l > 1 && r < BigInt("1000000000")) {
-        if ((mid - l) < (r - mid)) l else r
-      } else if (l > 1) l else r)
-    }.toLong
+      oddLengthPalindromes ++ evenLengthPalindromes
+    }
+
+    val palindromes = {
+      val bases = Iterator.iterate(1)(_ * 10).takeWhile(_ <= 10000).toSeq
+      bases.flatMap(generatePalindromes)
+    }
+
+    (palindromes :+ 1000000001).distinct.sorted.toList
   }
 
-  private def cost(nums: Array[BigInt], tar: BigInt): BigInt =
-    nums.map(a => (a - tar).abs).sum
+  def minimumCost(nums: Array[Int]): Long = {
+    val sortedNums = nums.sorted
+    val n = sortedNums.length
+    val medianIndex = (n - 1) / 2
+    val target = sortedNums(medianIndex)
+    val index = lowerBound(target)
 
-  private def isPalindrome(x: BigInt): Boolean =
-    if (x < 0) false
-    else {
-      val str = x.toString
-      str == str.reverse
-    }
+    val cost1 = getCost(sortedNums, index)
+    val cost2 = if (index > 0) getCost(sortedNums, index - 1) else Long.MaxValue
+    cost1.min(cost2)
+  }
+
+  private def lowerBound(target: Int): Int = pal.indexWhere(_ >= target) match {
+    case -1 => pal.length
+    case idx => idx
+  }
+
+  private def getCost(nums: Array[Int], index: Int): Long = {
+    val target = pal(index)
+    nums.map(num => (num - target).abs.toLong).sum
+  }
 }
