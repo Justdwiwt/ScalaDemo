f2de9834d59d7393b8c9c69a7e887e599592809e
Justdwiwt <len_master@hotmail.com>
2024-08-01 09:10:29 +0800

    leetCode LCP-40

--- src/main/scala/leetCode/LCP/LCP_40.scala
+++ src/main/scala/leetCode/LCP/LCP_40.scala
@@ -3,22 +3,22 @@ package leetCode.LCP
 object LCP_40 {
   def maxmiumScore(cards: Array[Int], cnt: Int): Int = {
     val sorted = cards.sorted
-    var sum = 0
-    var t = cnt
-    var idx = cards.length - 1
-    while (t > 0) {
-      sum += sorted(idx)
-      idx -= 1
-      t -= 1
+    val n = sorted.length
+    val initialSum = sorted.takeRight(cnt).sum
+
+    if (initialSum % 2 == 0) initialSum
+    else {
+      val x = sorted(n - cnt)
+      val res = replaceSum(sorted, cnt, initialSum, x)
+      val replacementSum = (n - cnt + 1 until n).collectFirst {
+        case i if sorted(i) % 2 != x % 2 => replaceSum(sorted, cnt, initialSum, sorted(i))
+      }
+
+      res.max(replacementSum.getOrElse(0))
     }
-    if (sum % 2 == 0) return sum
-    (idx to 0 by -1).foreach(i => (idx + 1 until cards.length).foreach(j => {
-      sum -= sorted(j)
-      sum += sorted(i)
-      if (sum % 2 == 0) return sum
-      sum -= sorted(i)
-      sum += sorted(j)
-    }))
-    0
   }
+
+  private def replaceSum(cards: Array[Int], cnt: Int, s: Int, x: Int): Int = (cards.length - cnt - 1 to 0 by -1)
+    .collectFirst { case i if cards(i) % 2 != x % 2 => s - x + cards(i) }
+    .getOrElse(0)
 }
