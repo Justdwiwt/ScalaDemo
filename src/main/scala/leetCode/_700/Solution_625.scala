da121272dd3f6c5d0946044aaeca8fb3f106d9a3
Justdwiwt <len_master@hotmail.com>
2024-08-05 19:44:17 +0800

    leetCode 625

--- src/main/scala/leetCode/_700/Solution_625.scala
+++ src/main/scala/leetCode/_700/Solution_625.scala
@@ -1,18 +1,22 @@
 package leetCode._700
 
 object Solution_625 {
-  def smallestFactorization(a: Int): Long = {
-    var res = 0L
-    var n = 1L
-    if (a < 10) return a
-    var t = a
-    (2 to 9).reverse.foreach(i => while (t % i == 0) {
-      res = i * n + res
-      if (res > Int.MaxValue) return 0
-      n *= 10
-      t /= i
-    })
-    if (t == 1) return res.toInt
-    0
+  def smallestFactorization(a: Int): Int = {
+    @scala.annotation.tailrec
+    def f(t: Int, res: Long, n: Long, divisors: List[Int]): Long = divisors match {
+      case Nil => if (t == 1) res else 0
+      case i :: tail =>
+        if (t % i == 0) {
+          val newRes = i * n + res
+          if (newRes > Int.MaxValue) 0
+          else f(t / i, newRes, n * 10, divisors)
+        } else f(t, res, n, tail)
+    }
+
+    if (a < 10) a
+    else {
+      val res = f(a, 0L, 1L, (2 to 9).reverse.toList)
+      if (res > Int.MaxValue) 0 else res.toInt
+    }
   }
 }
