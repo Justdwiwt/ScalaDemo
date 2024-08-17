e67c7009b4878e5946081f143c574af6b4c66ff3
Justdwiwt <len_master@hotmail.com>
2024-08-17 11:25:16 +0800

    leetCode 3250

---
+++ src/main/scala/leetCode/_3300/Solution_3250.scala
@@ -0,0 +1,38 @@
+package leetCode._3300
+
+object Solution_3250 {
+  private val M = BigInt(10).pow(9) + 7
+
+  def countOfPairs(nums: Array[Int]): Int = {
+    val n = nums.length
+
+    if (n == 1) return comb(nums.last + n, n).toInt
+
+    val initPre = nums.last
+
+    val (isValid, finalPre) = nums
+      .reverse
+      .sliding(2)
+      .foldLeft((true, initPre)) {
+        case ((isValid, pre), Array(a, b)) =>
+          if (!isValid) (isValid, pre)
+          else if (a > b + pre) (false, pre)
+          else if (a > b) (isValid, pre - (a - b))
+          else (isValid, pre)
+        case ((_, pre), _) => (false, pre)
+      }
+
+    if (isValid && finalPre >= 0) (comb(finalPre + n, n) % M).toInt else 0
+  }
+
+  private def comb(n: Int, k: Int): BigInt =
+    if (k > n) BigInt(0)
+    else {
+      val numerator = factorial(n)
+      val denominator = factorial(k) * factorial(n - k)
+      numerator / denominator
+    }
+
+  private def factorial(x: Int): BigInt =
+    (BigInt(1) to BigInt(x)).product
+}
