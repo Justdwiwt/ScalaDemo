1dc478952ed4922cd0a87a217d14f66466606a04
Justdwiwt <len_master@hotmail.com>
2024-08-26 09:18:31 +0800

    leetCode 690

--- src/main/scala/leetCode/_700/Solution_690.scala
+++ src/main/scala/leetCode/_700/Solution_690.scala
@@ -2,25 +2,14 @@ package leetCode._700
 
 object Solution_690 {
 
-  class Employee() {
+  class Employee {
     var id = 0
     var importance = 0
     var subordinates: List[Nothing] = List()
   }
 
   def getImportance(employees: List[Employee], id: Int): Int = {
-    val empLookup = employees./:(Map[Int, Employee]())((m, e) => m + (e.id -> e))
-
-    def dfs(employeeId: Int, acc: Int): Int = {
-      val e = empLookup(employeeId)
-      e.subordinates./:(e.importance) {
-        case (acc, id) =>
-          val sub = empLookup(id).importance
-          acc + dfs(id, sub)
-        case _ => acc
-      }
-    }
-
-    dfs(id, 0)
+    val e = employees.find(_.id == id).get
+    e.importance + e.subordinates.map(getImportance(employees, _)).sum
   }
 }
