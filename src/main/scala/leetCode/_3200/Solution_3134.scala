86b9f7fb42880f05cfa2240fcab782596bb4b527
Justdwiwt <len_master@hotmail.com>
2024-08-27 09:48:32 +0800

    leetCode 3134

--- src/main/scala/leetCode/_3200/Solution_3134.scala
+++ src/main/scala/leetCode/_3200/Solution_3134.scala
@@ -3,37 +3,38 @@ package leetCode._3200
 import scala.collection.mutable
 
 object Solution_3134 {
+  private def getLessOrEqualMedianCount(nums: Array[Int], median: Int): Long = {
+    val countMap = mutable.Map.empty[Int, Int]
+    var l = 0
+    var res = 0L
+
+    nums.indices.foreach(r => {
+      countMap(nums(r)) = countMap.getOrElse(nums(r), 0) + 1
+      while (countMap.size > median) {
+        countMap(nums(l)) -= 1
+        if (countMap(nums(l)) == 0) countMap.remove(nums(l))
+        l += 1
+      }
+      res += (r - l + 1)
+    })
+    res
+  }
+
   def medianOfUniquenessArray(nums: Array[Int]): Int = {
     val n = nums.length
-    val k = (n.toLong * (n + 1) / 2 + 1) / 2
+    val total: Long = n.toLong * (n + 1) / 2
+    val target: Long = (total + 1) / 2
 
-    def check(nums: Array[Int], upper: Int, k: Long): Boolean = {
-      var cnt = 0L
-      var l = 0
-      val freq = mutable.HashMap.empty[Int, Int]
-      var r = 0
-      while (r < nums.length) {
-        freq(nums(r)) = freq.getOrElse(nums(r), 0) + 1
-        while (freq.size > upper) {
-          val out = nums(l)
-          l += 1
-          if (freq(out) == 1) freq.remove(out)
-          else freq(out) -= 1
-        }
-        cnt += r - l + 1
-        if (cnt >= k) return true
-        r += 1
+    @scala.annotation.tailrec
+    def f(left: Int, right: Int): Int =
+      if (left > right) left
+      else {
+        val median = (left + right) / 2
+        val count = getLessOrEqualMedianCount(nums, median)
+        if (count < target) f(median + 1, right)
+        else f(left, median - 1)
       }
-      false
-    }
 
-    var l = 0
-    var r = n
-    while (l + 1 < r) {
-      val mid = (l + r) >>> 1
-      if (check(nums, mid, k)) r = mid
-      else l = mid
-    }
-    r
+    f(1, n)
   }
 }
