357bf3973badf899b473e49b3f8b94bc5a6d9a16
Justdwiwt <len_master@hotmail.com>
2024-08-28 09:35:06 +0800

    leetCode 1905

--- src/main/scala/leetCode/_2000/Solution_1905.scala
+++ src/main/scala/leetCode/_2000/Solution_1905.scala
@@ -1,17 +1,74 @@
 package leetCode._2000
 
 object Solution_1905 {
-  def countSubIslands(g1: Array[Array[Int]], g2: Array[Array[Int]]): Int = {
-    def f(i: Int, j: Int): Boolean = {
-      if (i < 0 || i >= g1.length || j < 0 || j >= g1.head.length || g2(i)(j) <= 0) return true
-      if (g2(i)(j) == 1 && g1(i)(j) != 1) return false
-      g2(i)(j) = -1
-      Seq((0, 1), (0, -1), (1, 0), (-1, 0)).map(d => f(i + d._1, j + d._2)).reduce(_ & _)
-    }
+  def countSubIslands(grid1: Array[Array[Int]], grid2: Array[Array[Int]]): Int = {
+    val (djs1, djs2) = (new DJS[(Int, Int)], new DJS[(Int, Int)])
+    grid1.indices.foreach(i => grid1.head.indices.foreach(j => {
+      if (grid1(i)(j) != 0) djs1.add((i, j))
+      if (grid2(i)(j) != 0) djs2.add((i, j))
+    }))
+    grid1.indices.foreach(i => grid1.head.indices.foreach(j => {
+      if ((i + 1) < grid1.length && grid1(i)(j) + grid1(i + 1)(j) == 2) djs1.join((i, j), (i + 1, j))
+      if ((j + 1) < grid1.head.length && grid1(i)(j) + grid1(i)(j + 1) == 2) djs1.join((i, j), (i, j + 1))
+      if ((i + 1) < grid2.length && grid2(i)(j) + grid2(i + 1)(j) == 2) djs2.join((i, j), (i + 1, j))
+      if ((j + 1) < grid2.head.length && grid2(i)(j) + grid2(i)(j + 1) == 2) djs2.join((i, j), (i, j + 1))
+    }))
 
-    g1
-      .indices
-      .map(i => g1.head.indices.count(j => g2(i)(j) == 1 && f(i, j)))
-      .sum
+    djs2
+      .djs
+      .keys
+      .toList
+      .map(x => djs2.getRoot(x) -> x)
+      .groupBy(_._1)
+      .mapValues(_.map(_._2))
+      .filter { case (_, v) => v.forall { case (i, j) => grid1(i)(j) + grid2(i)(j) == 2 } }
+      .count { case (_, v) =>
+        lazy val r0 = djs1.getRoot(v.head._1, v.head._2)
+        v.tail.isEmpty || v.tail.forall { case (i, j) => djs1.getRoot(i, j) == r0 }
+      }
   }
+}
+
+class DJS[A] {
+  val djs = collection.mutable.Map.empty[A, A]
+
+  @scala.annotation.tailrec
+  private def _getRoot(a: A, depth: Int = 0): (A, Int) =
+    if (djs(a) == a) (a, depth)
+    else _getRoot(djs(a), depth + 1)
+
+  def add(a: A): DJS[A] = {
+    djs(a) = a
+    this
+  }
+
+  @scala.annotation.tailrec
+  private def _addWithParent(a: A, p: A): DJS[A] = {
+    val oldP = djs(a)
+    djs(a) = p
+    if (oldP != a) _addWithParent(oldP, p)
+    else this
+  }
+
+  def getRoot(a: A): A =
+    _getRoot(a: A)._1
+
+  def join(a: A, b: A): DJS[A] = {
+    if (!djs.contains(a)) add(a)
+    if (!djs.contains(b)) add(b)
+    lazy val (ra, rb) = (_getRoot(a), _getRoot(b))
+    if (ra._1 != rb._1) {
+      if (ra._2 < rb._2) {
+        _addWithParent(rb._1, ra._1)
+        _addWithParent(b, ra._1)
+      }
+      else {
+        _addWithParent(ra._1, rb._1)
+        _addWithParent(a, rb._1)
+      }
+    }
+    this
+  }
+
+  override def toString: String = djs.toString
 }
