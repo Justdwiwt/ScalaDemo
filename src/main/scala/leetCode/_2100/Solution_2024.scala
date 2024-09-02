bd7fc6aca65f0495c7b158880df0034cac8ca1b9
Justdwiwt <len_master@hotmail.com>
2024-09-02 09:21:49 +0800

    leetCode 2024

--- src/main/scala/leetCode/_2100/Solution_2024.scala
+++ src/main/scala/leetCode/_2100/Solution_2024.scala
@@ -2,20 +2,19 @@ package leetCode._2100
 
 object Solution_2024 {
   def maxConsecutiveAnswers(answerKey: String, k: Int): Int = {
-    var cntT = 0
-    var cntF = 0
-    var p = 0
-    var res = 0
-    answerKey.indices.foreach(i => {
-      if (answerKey(i) == 'T') cntT += 1
-      else cntF += 1
-      while (cntF.min(cntT) > k) {
-        if (answerKey(p) == 'T') cntT -= 1
-        else cntF -= 1
-        p += 1
-      }
-      res = res.max(1 + i - p)
-    })
-    res
+    @scala.annotation.tailrec
+    def f(l: Int, r: Int, maxLen: Int)(TF: (Int, Int)): Int =
+      if ((TF._1 min TF._2) <= k) {
+        if (r == answerKey.length) maxLen.max(r - l)
+        else f(l, r + 1, maxLen.max(r - l))(answerKey(r) match {
+          case 'T' => (TF._1 + 1, TF._2)
+          case 'F' => (TF._1, TF._2 + 1)
+        })
+      } else f(l + 1, r, maxLen)(answerKey(l) match {
+        case 'T' => (TF._1 - 1, TF._2)
+        case 'F' => (TF._1, TF._2 - 1)
+      })
+
+    f(0, 0, 0)(0, 0)
   }
 }
