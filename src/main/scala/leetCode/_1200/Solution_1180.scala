e39a8b66c930662fbda14c260ea5fa5daae9500d
Justdwiwt <len_master@hotmail.com>
2024-09-15 11:35:15 +0800

    leetCode 1180

--- src/main/scala/leetCode/_1200/Solution_1180.scala
+++ src/main/scala/leetCode/_1200/Solution_1180.scala
@@ -1,17 +1,14 @@
 package leetCode._1200
 
 object Solution_1180 {
-  def countLetters(s: String): Int = {
-    var cnt = 1
-    var res = 0
-    s.indices.drop(1).foreach(i => {
-      if (s(i) == s(i - 1)) cnt += 1
-      else {
-        res += cnt * (cnt + 1) / 2
-        cnt = 1
-      }
-    })
-    res += cnt * (cnt + 1) / 2
-    res
+  def countLetters(s: String): Int = s
+    .indices
+    .drop(1)
+    .foldLeft((1, 0)) { case ((cnt, res), i) =>
+      if (s(i) == s(i - 1)) (cnt + 1, res)
+      else (1, res + cnt * (cnt + 1) / 2)
+    }
+  match {
+    case (cnt, res) => res + cnt * (cnt + 1) / 2
   }
 }
