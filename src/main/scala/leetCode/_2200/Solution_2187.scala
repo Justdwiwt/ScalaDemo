c982dcdc70e8a3665b1462ba5adbc8b5ebed5491
Justdwiwt <len_master@hotmail.com>
2024-10-05 13:38:03 +0800

    leetCode 2187

--- src/main/scala/leetCode/_2200/Solution_2187.scala
+++ src/main/scala/leetCode/_2200/Solution_2187.scala
@@ -2,16 +2,19 @@ package leetCode._2200
 
 object Solution_2187 {
   def minimumTime(time: Array[Int], totalTrips: Int): Long = {
-    var l = 1L
-    val sorted = time.sorted
-    var r = sorted.head * totalTrips.toLong
-    while (l < r) {
-      val m = (l + r) / 2L
-      var sum = 0L
-      time.foreach(sum += m / _)
-      if (sum < totalTrips) l = m + 1
-      else r = m
+    lazy val sorted = time.toVector.sorted
+
+    def check(t: Long): Boolean =
+      sorted.takeWhile(_ <= t).map(t / _).sum >= totalTrips
+
+    @scala.annotation.tailrec
+    def f(lo: Long, hi: Long): Long = {
+      lazy val mid = (hi + lo) >> 1
+      if (lo + 1 >= hi) hi
+      else if (check(mid)) f(lo, mid)
+      else f(mid, hi)
     }
-    l
+
+    f(0L, sorted.last * totalTrips.toLong * sorted.size)
   }
 }
