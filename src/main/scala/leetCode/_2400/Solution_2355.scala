3e0c5d29675915f486d6ed8e01ad281d1a0e8f04
Justdwiwt <len_master@hotmail.com>
2024-10-13 09:31:30 +0800

    leetCode 2355

--- src/main/scala/leetCode/_2400/Solution_2355.scala
+++ src/main/scala/leetCode/_2400/Solution_2355.scala
@@ -1,20 +1,20 @@
 package leetCode._2400
 
-import java.util
-
 object Solution_2355 {
   def maximumBooks(books: Array[Int]): Long = {
     val n = books.length
     var res = 0L
-    val dp = Array.fill(n)(0L)
+    val dp = new Array[Long](n)
     dp(0) = books.head
-    val st = new util.LinkedList[Int]()
+    var stack = List.empty[Int]
+
     books.indices.foreach(i => {
-      while (!st.isEmpty && books(st.peek()) - st.peek() > books(i) - i) st.pop()
-      val j = if (st.isEmpty) -1 else st.peek()
-      st.push(i)
-      val cnt = books(i).min(i - j)
-      val sum = (((books(i) - cnt + 1) + books(i)) * cnt / 2).toLong
+      while (stack.nonEmpty && books(stack.head) - stack.head > books(i) - i)
+        stack = stack.tail
+      val j = if (stack.isEmpty) -1 else stack.head
+      stack = i :: stack
+      val count = books(i).min(i - j)
+      val sum = ((books(i) - count + 1L) + books(i)) * count / 2
       dp(i) = sum + (if (j == -1) 0 else dp(j))
       res = res.max(dp(i))
     })
