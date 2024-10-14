f5d8046220223b077311bb5bdaceb8ce46c03044
Justdwiwt <len_master@hotmail.com>
2024-10-14 21:28:08 +0800

    leetCode 3315

---
+++ src/main/scala/leetCode/_3400/Solution_3315.scala
@@ -0,0 +1,12 @@
+package leetCode._3400
+
+object Solution_3315 {
+  def minBitwiseArray(nums: List[Int]): Array[Int] = {
+    nums.map {
+      case 2 => -1
+      case x =>
+        val t = ~x
+        x ^ ((t & -t) >> 1)
+    }
+  }.toArray
+}
