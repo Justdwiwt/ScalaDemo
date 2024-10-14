d3900b3373b2e88ff9c00c2c07c4d45616a68754
Justdwiwt <len_master@hotmail.com>
2024-10-14 21:44:22 +0800

    leetCode 887

--- src/main/scala/leetCode/_900/Solution_887.scala
+++ src/main/scala/leetCode/_900/Solution_887.scala
@@ -1,30 +1,27 @@
 package leetCode._900
 
-import scala.util.control.Breaks._
-
 object Solution_887 {
   def superEggDrop(K: Int, N: Int): Int = {
     var left = 1
     var right = N
     while (left < right) {
       val mid = left + (right - left) / 2
-      if (func(mid, K, N) < N) left = mid + 1
+      if (f(mid, K, N) < N) left = mid + 1
       else right = mid
     }
     right
   }
 
-  def func(x: Int, k: Int, n: Int): Int = {
-    var res = 0
-    var r = 1
-    breakable {
-      (1 to k).foreach(i => {
-        r *= x - i + 1
-        r /= i
-        res += r
-        if (res >= n) break
-      })
-    }
-    res
+  private def f(x: Int, k: Int, n: Int): Int = {
+    @scala.annotation.tailrec
+    def loop(i: Int, res: Int, r: Int): Int =
+      if (i > k || res >= n) res
+      else {
+        val newR = r * (x - i + 1) / i
+        val newRes = res + newR
+        loop(i + 1, newRes, newR)
+      }
+
+    loop(1, 0, 1)
   }
 }
