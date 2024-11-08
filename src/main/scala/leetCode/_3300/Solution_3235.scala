115192509c24f5c31d0037025edd993124daac89
Justdwiwt <len_master@hotmail.com>
2024-11-08 21:13:17 +0800

    leetCode 3235

--- src/main/scala/leetCode/_3300/Solution_3235.scala
+++ src/main/scala/leetCode/_3300/Solution_3235.scala
@@ -1,41 +1,45 @@
 package leetCode._3300
 
 object Solution_3235 {
-  private class UnionFind(size: Int) {
-    private val fa = Array.tabulate(size)(identity)
+  def canReachCorner(xCorner: Int, yCorner: Int, circles: Array[Array[Int]]): Boolean = {
+    val visited = Array.fill(circles.length)(false)
 
-    def find(x: Int): Int = {
-      if (fa(x) != x) fa(x) = find(fa(x))
-      fa(x)
+    def pointInCircle(px: Long, py: Long, x: Long, y: Long, r: Long): Boolean =
+      (x - px) * (x - px) + (y - py) * (y - py) <= r * r
+
+    def circleIntersectsTopLeftOfRectangle(x: Int, y: Int, r: Int, xCorner: Int, yCorner: Int): Boolean =
+      (x.abs <= r && 0 <= y && y <= yCorner) ||
+        (0 <= x && x <= xCorner && (y - yCorner).abs <= r) ||
+        pointInCircle(x, y, 0, yCorner, r)
+
+    def circleIntersectsBottomRightOfRectangle(x: Int, y: Int, r: Int, xCorner: Int, yCorner: Int): Boolean = {
+      (y.abs <= r && 0 <= x && x <= xCorner) ||
+        (0 <= y && y <= yCorner && (x - xCorner).abs <= r) ||
+        pointInCircle(x, y, xCorner, 0, r)
     }
 
-    def merge(x: Int, y: Int): Unit =
-      fa(find(x)) = find(y)
-  }
-
-  def canReachCorner(x: Int, y: Int, circles: Array[Array[Int]]): Boolean = {
-    val n = circles.length
-    val uf = new UnionFind(n + 2)
-
-    def mergeCircles(i: Int): Unit = {
-      val c = circles(i)
-      val (ox, oy, r) = (c.head, c(1), c(2))
-
-      if (ox <= r || oy + r >= y) uf.merge(i, n)
-      if (oy <= r || ox + r >= x) uf.merge(i, n + 1)
-
-      (0 until i).foreach(j => {
-        val q = circles(j)
-        if ((ox - q.head).toLong * (ox - q.head) + (oy - q(1)).toLong * (oy - q(1)) <= (r + q(2)).toLong * (r + q(2)))
-          uf.merge(i, j)
-      })
+    def circlesIntersectInRectangle(x1: Long, y1: Long, r1: Long, x2: Long, y2: Long, r2: Long, xCorner: Long, yCorner: Long): Boolean = {
+      (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) <= (r1 + r2) * (r1 + r2) &&
+        x1 * r2 + x2 * r1 < (r1 + r2) * xCorner &&
+        y1 * r2 + y2 * r1 < (r1 + r2) * yCorner
     }
 
-    circles.indices.foldLeft(true)((res, i) =>
-      if (res) {
-        mergeCircles(i)
-        uf.find(n) != uf.find(n + 1)
-      } else false
-    )
+    def dfs(i: Int): Boolean = {
+      val Array(x1, y1, r1) = circles(i)
+      if (circleIntersectsBottomRightOfRectangle(x1, y1, r1, xCorner, yCorner)) true
+      else {
+        visited(i) = true
+        circles.indices.exists(j => {
+          val Array(x2, y2, r2) = circles(j)
+          !visited(j) && circlesIntersectInRectangle(x1, y1, r1, x2, y2, r2, xCorner, yCorner) && dfs(j)
+        })
+      }
+    }
+
+    !circles.indices.exists { i =>
+      val Array(x, y, r) = circles(i)
+      pointInCircle(0, 0, x, y, r) || pointInCircle(xCorner, yCorner, x, y, r) ||
+        (!visited(i) && circleIntersectsTopLeftOfRectangle(x, y, r, xCorner, yCorner) && dfs(i))
+    }
   }
 }
