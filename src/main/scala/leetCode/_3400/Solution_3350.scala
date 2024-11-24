e9acf5b49cd35d3608b897e89b3d6195c4fd60dc
Justdwiwt <len_master@hotmail.com>
2024-11-24 22:04:07 +0800

    leetCode 3350

--- src/main/scala/leetCode/_3400/Solution_3350.scala
+++ src/main/scala/leetCode/_3400/Solution_3350.scala
@@ -2,13 +2,17 @@ package leetCode._3400
 
 object Solution_3350 {
   def maxIncreasingSubarrays(nums: List[Int]): Int = {
-    nums.zipWithIndex.foldLeft((0, 0, 0)) { case ((res, preCnt, cnt), (x, i)) =>
-      val newCnt = cnt + 1
-      val newAns = if (i == nums.length - 1 || x >= nums(i + 1)) {
-        res.max((newCnt / 2).max(preCnt.min(newCnt)))
-      } else res
-      if (i == nums.length - 1 || x >= nums(i + 1)) (newAns, newCnt, 0)
-      else (newAns, preCnt, newCnt)
-    }._1
+    @scala.annotation.tailrec
+    def f(arr: Array[Int], idx: Int, cnt: Int, preCnt: Int, res: Int): Int =
+      if (idx >= arr.length) res
+      else {
+        val newCnt = cnt + 1
+        if (idx == arr.length - 1 || arr(idx) >= arr(idx + 1)) {
+          val newRes = res.max((newCnt / 2).max(preCnt.min(newCnt)))
+          f(arr, idx + 1, 0, newCnt, newRes)
+        } else f(arr, idx + 1, newCnt, preCnt, res)
+      }
+
+    f(nums.toArray, 0, 0, 0, 0)
   }
 }
