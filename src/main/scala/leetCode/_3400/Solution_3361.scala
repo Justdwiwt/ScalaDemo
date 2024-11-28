cddf7de809ad9c4d2625ab98f6bbef5a147b97fe
Justdwiwt <len_master@hotmail.com>
2024-11-29 00:09:48 +0800

    leetCode 3361

--- src/main/scala/leetCode/_3400/Solution_3361.scala
+++ src/main/scala/leetCode/_3400/Solution_3361.scala
@@ -2,13 +2,21 @@ package leetCode._3400
 
 object Solution_3361 {
   def shiftDistance(s: String, t: String, nextCost: Array[Int], previousCost: Array[Int]): Long = {
-    s.indices.foldLeft(0L) { (sum, i) =>
-      val (si, ti) = (s(i) - 'a', t(i) - 'a')
-      val nDist = Math.floorMod(ti - si, 26)
-      val pDist = Math.floorMod(si - ti, 26)
-      val nC = (0 until nDist).foldLeft(0L)((s, i) => s + previousCost((si + i) % 26))
-      val pC = (0 until pDist).foldLeft(0L)((s, i) => s + previousCost((ti + i + 1) % 26))
-      sum + nC.min(pC)
-    }
+    val nextArr = Range(0, 52).scanLeft(0L)((acc, i) => acc + nextCost(i % 26)).toArray
+    nextArr.zipWithIndex.map { case (a, i) => (i, (i % 26 + 'a').toChar, a) }
+    val prevArr = Range(0, 51).scanRight(0L)((i, acc) => acc + previousCost(i % 26)).tail.toArray
+    prevArr.zipWithIndex.map { case (a, i) => (i, (i % 26 + 'a').toChar, a) }
+
+    def next(a: Char, b: Char): Long =
+      if (a == b) 0
+      else if (a < b) nextArr(b - 'a') - nextArr(a - 'a')
+      else nextArr(b + 26 - 'a') - nextArr(a - 'a')
+
+    def prev(a: Char, b: Char): Long =
+      if (a == b) 0
+      else if (b < a) prevArr(b - 'a') - prevArr(a - 'a')
+      else prevArr(b - 'a') - prevArr(a + 26 - 'a')
+
+    s.zip(t).foldLeft(0L) { case (acc, (a, b)) => acc + next(a, b).min(prev(a, b)) }
   }
 }
