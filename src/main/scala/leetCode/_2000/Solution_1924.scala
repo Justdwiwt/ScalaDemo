8ce5ce73ec866c2b97c8e33eff0e0a19e3424013
Justdwiwt <len_master@hotmail.com>
2024-12-28 12:34:12 +0800

    leetCode 1924, 1956

--- src/main/scala/leetCode/_2000/Solution_1924.scala
+++ src/main/scala/leetCode/_2000/Solution_1924.scala
@@ -1,43 +1,68 @@
 package leetCode._2000
 
+import scala.math._
+import scala.util.Random
+
 object Solution_1924 {
-  // fixme: case 85/92 accuracy overflow
-  def outerTrees(trees: Array[Array[Int]]): Array[Double] = {
-    var r = 0.0
-    var centerX = trees.head.head.toDouble
-    var centerY = trees.head(1).toDouble
-    trees.indices.foreach(i => if (dis(trees(i).head, centerX, trees(i)(1), centerY) > r * r) {
-      centerX = trees(i).head.toDouble
-      centerY = trees(i)(1).toDouble
-      r = 0
-      (0 until i).foreach(j => if (dis(trees(j).head, centerX, trees(j)(1), centerY) > r * r) {
-        centerX = (trees(i).head + trees(j).head) / 2.0
-        centerY = (trees(i)(1) + trees(j)(1)) / 2.0
-        r = math.sqrt(dis(trees(j).head, centerX, trees(j)(1), centerY))
-        (0 until j).foreach(k => if (dis(trees(k).head, centerX, trees(k)(1), centerY) > r * r) {
-          val tmp = getCircle(trees, i, j, k)
-          centerX = tmp.head
-          centerY = tmp(1)
-          r = tmp(2)
-        })
-      })
-    })
-    Array(centerX, centerY, r)
+  case class Point(x: Double, y: Double) {
+    def dist2(q: Point): Double = pow(x - q.x, 2) + pow(y - q.y, 2)
   }
 
-  private def dis(x1: Double, x2: Double, y1: Double, y2: Double): Double =
-    (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+  def outerTrees(trees: Array[Array[Int]]): Array[Double] = {
+    if (trees.length <= 1) {
+      if (trees.isEmpty) return Array(0.0, 0.0, 0.0)
+      return Array(trees.head.head.toDouble, trees.head(1).toDouble, 0.0)
+    }
 
-  private def getCircle(trees: Array[Array[Int]], idx1: Int, idx2: Int, idx3: Int): Array[Double] = {
-    val P1 = trees(idx1)(1) - trees(idx3)(1)
-    val P2 = trees(idx1)(1) - trees(idx2)(1)
-    val Q1 = trees(idx1).head - trees(idx3).head
-    val Q2 = trees(idx1).head - trees(idx2).head
-    val A = (trees(idx1).head * trees(idx1).head - trees(idx2).head * trees(idx2).head) + (trees(idx1)(1) * trees(idx1)(1) - trees(idx2)(1) * trees(idx2)(1))
-    val B = (trees(idx1).head * trees(idx1).head - trees(idx3).head * trees(idx3).head) + (trees(idx1)(1) * trees(idx1)(1) - trees(idx3)(1) * trees(idx3)(1))
-    val C = 2 * (trees(idx1).head - trees(idx2).head) * (trees(idx1)(1) - trees(idx3)(1)) - 2 * (trees(idx1).head - trees(idx3).head) * (trees(idx1)(1) - trees(idx2)(1))
-    val x = (P1 * A - P2 * B) / C
-    val y = (Q2 * B - Q1 * A) / C
-    Array(x, y, math.sqrt(dis(trees(idx1).head, x, trees(idx1)(1), y)))
+    val shuffledTrees = Random.shuffle(trees.toSeq).map(arr => Point(arr.head.toDouble, arr(1).toDouble)).toArray
+
+    var o = shuffledTrees.head
+    var r2 = 0.0
+
+    shuffledTrees.indices.foreach(i => {
+      val p = shuffledTrees(i)
+      if (o.dist2(p) > r2 + 1e-9) {
+        o = p
+        r2 = 0.0
+        (0 until i).foreach(j => {
+          val q = shuffledTrees(j)
+          if (o.dist2(q) > r2 + 1e-9) {
+            o = Point((p.x + q.x) / 2, (p.y + q.y) / 2)
+            r2 = o.dist2(p)
+            (0 until j).foreach(k => {
+              val x = shuffledTrees(k)
+              if (o.dist2(x) > r2 + 1e-9) {
+                val tmp = getCircle(p, q, x)
+                o = Point(tmp.head, tmp(1))
+                r2 = o.dist2(p)
+              }
+            })
+          }
+        })
+      }
+    })
+    Array(o.x, o.y, sqrt(r2))
+  }
+
+  private def getCircle(a: Point, b: Point, c: Point): Array[Double] = {
+    val a1 = b.x - a.x
+    val b1 = b.y - a.y
+    val a2 = c.x - a.x
+    val b2 = c.y - a.y
+    val c1 = a1 * a1 + b1 * b1
+    val c2 = a2 * a2 + b2 * b2
+    val d = 2 * (a1 * b2 - a2 * b1)
+    if (abs(d) < 1e-9) {
+      val ab2 = a.dist2(b)
+      val bc2 = b.dist2(c)
+      val ca2 = c.dist2(a)
+      if (ab2 >= bc2 && ab2 >= ca2) Array((a.x + b.x) / 2, (a.y + b.y) / 2, sqrt(ab2) / 2)
+      else if (bc2 >= ab2 && bc2 >= ca2) Array((b.x + c.x) / 2, (b.y + c.y) / 2, sqrt(bc2) / 2)
+      else Array((c.x + a.x) / 2, (c.y + a.y) / 2, sqrt(ca2) / 2)
+    } else {
+      val centerX = a.x + (c1 * b2 - c2 * b1) / d
+      val centerY = a.y + (a1 * c2 - a2 * c1) / d
+      Array(centerX, centerY, sqrt(a.dist2(Point(centerX, centerY))))
+    }
   }
 }
--- src/main/scala/leetCode/_2000/Solution_1956.scala
+++ src/main/scala/leetCode/_2000/Solution_1956.scala
@@ -1,113 +1,27 @@
 package leetCode._2000
 
 object Solution_1956 {
-  // fixme: case 15/70 wrong answer
   def minDayskVariants(points: Array[Array[Int]], k: Int): Int = {
-    val N = points.length
-    var res = Int.MaxValue
-    val nPoints = Array.ofDim[Int](k, 2)
-    val distX = Array.fill(k)(0)
-    val start = (1 << k) - 1
-    val end = (1 << N) - (1 << (N - k))
-    var x1 = 1000000000
-    var y1 = 1000000000
-    var x2 = 1
-    var y2 = 1
-
-    points.indices.foreach(i => {
-      x1 = x1.min(points(i).head)
-      x2 = x2.max(points(i).head)
-      y1 = y1.min(points(i)(1))
-      y2 = y2.max(points(i)(1))
-    })
-
-    (start to end).foreach(i => {
-      if (count(i, N, k) != k) ()
-      val digit = i
-      var idx = 0
-      points.indices.foreach(j => if (((1 << j) & digit) != 0) {
-        nPoints(idx) = points(j)
-        idx += 1
+    val n = points.length
+    if (n < k) return -1
+    val cp = points.map { case Array(x, y) => (x + y, x - y) }.sortBy(_._1)
+    val minDis = (0 to n - k).flatMap(i => {
+      (k to n - i).map(j => {
+        val xDis = cp(i + j - 1)._1 - cp(i)._1
+        if (xDis < Int.MaxValue) {
+          val yAxis = (0 until j).map(t => cp(i + t)._2).sorted
+          (0 to j - k).map(t => {
+            val yDis = yAxis(t + k - 1) - yAxis(t)
+            val yStart = yAxis(t)
+            val xStart = cp(i)._1
+            var dis = xDis.max(yDis)
+            if (dis % 2 == 0 && xDis == yDis && (xStart + yStart) % 2 != 0)
+              dis += 1
+            dis
+          }).reduceOption(_.min(_)).getOrElse(Int.MaxValue)
+        } else Int.MaxValue
       })
-      val a = prob(nPoints)
-      var tmp1 = a / 2
-      if (a % 2 == 1) tmp1 += 1
-      if (tmp1 > res) ()
-      val tmp = dist2(nPoints, distX, x1, x2, y1, y2)
-      if (res > tmp) res = tmp
-    })
-    res
-  }
-
-  private def prob(points: Array[Array[Int]]): Int = {
-    var res = 0
-    points.indices.foreach(i => (i + 1 until points.length).foreach(j => {
-      val tmp = (points(i).head - points(j).head).abs + (points(i)(1) - points(j)(1)).abs
-      if (res < tmp) res = tmp
-    }))
-    res
-  }
-
-  private def dist2(points: Array[Array[Int]], distX: Array[Int], x1: Int, x2: Int, y1: Int, y2: Int): Int = {
-    var left = x1
-    var right = x2
-    var res = Int.MaxValue
-    while (left <= right) {
-      val mid = (left + right) >> 1
-      val mmid = (mid + right) >> 1
-      val md = dist1(points, mid, y1, y2, distX)
-      val mmd = dist1(points, mmid, y1, y2, distX)
-      if (md >= mmd) {
-        if (res > mmd) res = mmd
-        left = mid + 1
-      } else {
-        if (res > md) res = md
-        right = mmid - 1
-      }
-    }
-    res
-  }
-
-  private def dist1(points: Array[Array[Int]], x: Int, y1: Int, y2: Int, distX: Array[Int]): Int = {
-    var left = y1
-    var right = y2
-    var res = Int.MaxValue
-    points.indices.foreach(i => distX(i) = (x - points(i).head).abs)
-    while (left <= right) {
-      val mid = (left + right) / 2
-      val mmid = (mid + right) / 2
-      val md = dist(points, distX, mid)
-      val mmd = dist(points, distX, mmid)
-      if (md >= mmd) {
-        left = mid + 1
-        if (res > mmd) res = mmd
-      } else {
-        right = mmid - 1
-        if (res > md) res = md
-      }
-    }
-    res
-  }
-
-  private def dist(points: Array[Array[Int]], distX: Array[Int], y: Int): Int = {
-    var min = 0
-    points.indices.foreach(i => {
-      val t = distX(i) + (points(i)(1) - y).abs
-      if (min < t) min = t
-    })
-    min
-  }
-
-  private def count(digit: Int, N: Int, k: Int): Int = {
-    var cnt = 0
-    var i = 0
-    while (i < N) {
-      if (((1 << i) & digit) != 0) {
-        cnt += 1
-        if (cnt > k) return cnt
-      }
-      i += 1
-    }
-    cnt
+    }).reduceOption(_.min(_)).getOrElse(Int.MaxValue)
+    if (minDis == 0) 0 else (minDis - 1) / 2 + 1
   }
 }
