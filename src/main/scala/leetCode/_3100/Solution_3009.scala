a147b94ffd6d41430701bc0760d25b18061e6052
Justdwiwt <len_master@hotmail.com>
2025-01-01 16:42:39 +0800

    leetCode 3009

--- src/main/scala/leetCode/_3100/Solution_3009.scala
+++ src/main/scala/leetCode/_3100/Solution_3009.scala
@@ -1,31 +1,28 @@
 package leetCode._3100
 
-import scala.collection.mutable
-
 object Solution_3009 {
   def maxIntersectionCount(y: Array[Int]): Int = {
-    // fixme: case 848/850 error
-    val arr = mutable.ArrayBuffer[(Int, Int)]()
+    val n = y.length
+    val doubledY = y.map(_ * 2)
 
-    y.indices.dropRight(1).foreach(i => {
-      val left = y(i)
-      val right = y(i + 1)
-      if (left < right) {
-        arr += ((2 * left, 1))
-        arr += ((2 * right, -1))
-      } else {
-        arr += ((2 * right, 1))
-        arr += ((2 * left, -1))
-      }
+    val d = y.indices.drop(1).foldLeft(collection.mutable.Map.empty[Int, Int].withDefaultValue(0))((acc, i) => {
+      val l = doubledY(i).min(doubledY(i - 1))
+      val r = doubledY(i).max(doubledY(i - 1))
+
+      acc(l) += 1
+      acc(r + 1) -= 1
+      acc(doubledY(i)) -= 1
+      acc(doubledY(i) + 1) += 1
+
+      acc
     })
 
-    var cnt = 0
-    var maxCount = 0
-    arr.sorted.foreach { case (_, delta) =>
-      cnt += delta
-      maxCount = maxCount.max(cnt)
-    }
+    d(doubledY(n - 1)) += 1
+    d(doubledY(n - 1) + 1) -= 1
 
-    maxCount
+    d.toSeq.sortBy(_._1).foldLeft((0, 0)) { case ((s, res), (_, count)) =>
+      val newSum = s + count
+      (newSum, res.max(newSum))
+    }._2
   }
 }
