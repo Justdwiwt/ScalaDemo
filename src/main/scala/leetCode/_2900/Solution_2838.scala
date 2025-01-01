d2260ba383cb8107d569a8a80afb021ef1c6aa69
Justdwiwt <len_master@hotmail.com>
2025-01-01 17:21:15 +0800

    leetCode 2838

--- src/main/scala/leetCode/_2900/Solution_2838.scala
+++ src/main/scala/leetCode/_2900/Solution_2838.scala
@@ -3,9 +3,9 @@ package leetCode._2900
 import scala.collection.Searching._
 
 object Solution_2838 {
-  // fixme: case 703/706 data limit exceeded
   def maximumCoins(heroes: Array[Int], monsters: Array[Int], coins: Array[Int]): Array[Long] = {
-    val ms = (Array((0, 0)) ++ monsters.zip(coins).sorted).scanLeft((0, 0)) { case ((_, acc), (m, c)) => (m, acc + c) }.tail
-    heroes.map(h => ms(ms.search((h + 1, -1)).insertionPoint - 1)._2).map(_.toLong)
+    val sorted = monsters.zip(coins).sortBy(_._1)
+    val ms = (Array((0, BigInt(0))) ++ sorted.map { case (m, c) => (m, BigInt(c)) }).scanLeft((0, BigInt(0))) { case ((_, acc), (m, c)) => (m, acc + c) }.tail
+    heroes.map(h => ms(ms.search((h + 1, BigInt(-1))).insertionPoint - 1)._2.toLong)
   }
 }
