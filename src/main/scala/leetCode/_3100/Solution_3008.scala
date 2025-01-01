8eb8d671229a38b4661f91d8b23824b58724005d
Justdwiwt <len_master@hotmail.com>
2025-01-01 16:32:07 +0800

    leetCode 3008

--- src/main/scala/leetCode/_3100/Solution_3008.scala
+++ src/main/scala/leetCode/_3100/Solution_3008.scala
@@ -1,54 +1,49 @@
 package leetCode._3100
 
-import scala.collection.mutable.ListBuffer
-
 object Solution_3008 {
-  // fixme: case 229/237 timeout
   def beautifulIndices(s: String, a: String, b: String, k: Int): List[Int] = {
-    val bNex = getNext(b)
-    val bStart = getSelectedPos(s, b, bNex)
-    val pre = diff(bStart, s.length, k)
-    val aNex = getNext(a)
-    val aStart = ListBuffer[Int](getSelectedPos(s, a, aNex): _*)
-    aStart.indices.reverse.foreach(i => if (pre(aStart(i)) == 0) aStart.remove(i))
-    aStart.toList
-  }
+    val text = s.toCharArray
+    val posA = kmp(text, a.toCharArray)
+    val posB = kmp(text, b.toCharArray)
 
-  private def getNext(str: String): Array[Int] = {
-    val next = Array.fill(str.length)(0)
-    var j = 0
-    str.indices.drop(1).foreach(i => {
-      while (j > 0 && str(i) != str(j)) j = next(j - 1)
-      if (str(i) == str(j)) j += 1
-      next(i) = j
-    })
-    next
-  }
-
-  private def getSelectedPos(str: String, sub: String, nex: Array[Int]): List[Int] = {
-    val lb = ListBuffer.empty[Int]
-    val n = sub.length
-    var j = 0
-    str.indices.foreach(i => {
-      while (j > 0 && str(i) != sub(j)) j = nex(j - 1)
-      if (str(i) == sub(j)) j += 1
-      if (j == n) {
-        lb += (i + 1 - n)
-        j = nex(j - 1)
+    def findIndices(posA: List[Int], posB: List[Int], k: Int): List[Int] = {
+      @scala.annotation.tailrec
+      def loop(posA: List[Int], posB: List[Int], ans: List[Int]): List[Int] = (posA, posB) match {
+        case (Nil, _) => ans.reverse
+        case (ha :: _, hb :: tb) if hb < ha - k => loop(posA, tb, ans)
+        case (ha :: ta, hb :: _) if hb <= ha + k => loop(ta, posB, ha :: ans)
+        case (_, _) => loop(posA.tail, posB, ans)
       }
-    })
-    lb.toList
+
+      loop(posA, posB, Nil)
+    }
+
+    findIndices(posA, posB, k)
   }
 
-  private def diff(list: List[Int], len: Int, k: Int): Array[Int] = {
-    val res = Array.fill(len + 1)(0)
-    list.foreach(i => {
-      val left = 0.max(i - k)
-      val right = (i + k).min(len - 1)
-      res(left) += 1
-      res(right + 1) -= 1
+  private def kmp(text: Array[Char], pattern: Array[Char]): List[Int] = {
+    val n = pattern.length
+    val pi = Array.fill(n)(0)
+    var c = 0
+    pattern.indices.drop(1).foreach(i => {
+      while (c > 0 && pattern(c) != pattern(i)) c = pi(c - 1)
+      if (pattern(c) == pattern(i)) c += 1
+      pi(i) = c
     })
-    (1 to len).foreach(i => res(i) += res(i - 1))
-    res
+
+    @scala.annotation.tailrec
+    def search(text: Array[Char], pattern: Array[Char], pi: Array[Int], c: Int, i: Int, res: List[Int]): List[Int] =
+      if (i >= text.length) res.reverse
+      else {
+        val updatedC = {
+          var tempC = c
+          while (tempC > 0 && pattern(tempC) != text(i)) tempC = pi(tempC - 1)
+          if (pattern(tempC) == text(i)) tempC + 1 else tempC
+        }
+        if (updatedC == n) search(text, pattern, pi, pi(updatedC - 1), i + 1, (i - n + 1) :: res)
+        else search(text, pattern, pi, updatedC, i + 1, res)
+      }
+
+    search(text, pattern, pi, 0, 0, Nil)
   }
 }
