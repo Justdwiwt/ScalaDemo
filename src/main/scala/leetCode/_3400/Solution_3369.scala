25117f94c64ed801c08e0c5d6bb6bb0dbb6d50e2
Justdwiwt <len_master@hotmail.com>
2025-01-01 15:48:13 +0800

    leetCode 3369

--- src/main/scala/leetCode/_3400/Solution_3369.scala
+++ src/main/scala/leetCode/_3400/Solution_3369.scala
@@ -1,65 +1,86 @@
 package leetCode._3400
 
-import scala.collection.mutable
-
-// fixme: case 476/1246 wrong answer
 object Solution_3369 {
-  class StatisticsTracker() {
+  class StatisticsTracker {
+    private val queue = collection.mutable.Queue.empty[Int]
+    private val frequencies = collection.mutable.Map.empty[Int, Int].withDefaultValue(0)
+    private val frequencyNumbers = collection.mutable.TreeMap.empty[Int, collection.mutable.TreeSet[Int]]
+    private val higher = collection.mutable.PriorityQueue.empty[Int](Ordering[Int].reverse)
+    private val lower = collection.mutable.PriorityQueue.empty[Int]
+    private val removals = collection.mutable.Map.empty[Int, Int].withDefaultValue(0)
+    private var higherSize = 0
+    private var lowerSize = 0
     private var sum: Long = 0
-    private var count: Int = 0
-
-    private val sortedList = new mutable.TreeMap[Int, Int]()
-
-    private val frequencyMap = new mutable.HashMap[Int, Int]()
-
-    private val queue = new mutable.Queue[Int]()
 
     def addNumber(number: Int): Unit = {
-      sum += number
-      count += 1
       queue.enqueue(number)
-
-      frequencyMap.put(number, frequencyMap.getOrElse(number, 0) + 1)
-
-      sortedList.put(number, sortedList.getOrElse(number, 0) + 1)
+      frequencies(number) += 1
+      val frequency = frequencies(number)
+      frequencyNumbers.getOrElseUpdate(frequency, scala.collection.mutable.TreeSet[Int]()).add(number)
+      if (frequency > 1) {
+        frequencyNumbers(frequency - 1).remove(number)
+        if (frequencyNumbers(frequency - 1).isEmpty) frequencyNumbers -= (frequency - 1)
+      }
+      addNumberToPriorityQueue(number)
+      sum += number
     }
 
     def removeFirstAddedNumber(): Unit = {
-      if (queue.nonEmpty) {
-        val number = queue.dequeue()
-        sum -= number
-        count -= 1
+      val number = queue.dequeue()
+      frequencies(number) -= 1
+      val frequency = frequencies(number)
+      if (frequency == 0) frequencies -= number
+      if (frequency > 0) frequencyNumbers.getOrElseUpdate(frequency, scala.collection.mutable.TreeSet[Int]()).add(number)
+      frequencyNumbers(frequency + 1).remove(number)
+      if (frequencyNumbers(frequency + 1).isEmpty) frequencyNumbers -= (frequency + 1)
+      removeNumberFromPriorityQueue(number)
+      sum -= number
+    }
 
-        frequencyMap.put(number, frequencyMap(number) - 1)
-        if (frequencyMap(number) == 0) frequencyMap.remove(number)
+    def getMean: Int =
+      (sum / queue.size).toInt
 
-        sortedList.put(number, sortedList(number) - 1)
-        if (sortedList(number) == 0) sortedList.remove(number)
+    def getMedian: Int =
+      higher.head
+
+    def getMode: Int =
+      frequencyNumbers.last._2.head
+
+    private def addNumberToPriorityQueue(number: Int): Unit = {
+      if (higher.isEmpty || number >= higher.head) {
+        higher.enqueue(number)
+        higherSize += 1
+      } else {
+        lower.enqueue(number)
+        lowerSize += 1
       }
+      adjustPriorityQueues()
     }
 
-    def getMean(): Int = {
-      if (count == 0) 0
-      else (sum / count).toInt
+    private def removeNumberFromPriorityQueue(number: Int): Unit = {
+      removals(number) += 1
+      if (number >= higher.head) higherSize -= 1
+      else lowerSize -= 1
+      adjustPriorityQueues()
     }
 
-    def getMedian(): Int = {
-      if (count == 0) 0
-      else {
-        val midIndex = count / 2
-        var currentIndex = 0
-        var median = 0
-
-        sortedList.foreach { case (key, value) =>
-          currentIndex += value
-          if (currentIndex > midIndex) median = key
+    private def adjustPriorityQueues(): Unit = {
+      if (higherSize > lowerSize + 1) {
+        lower.enqueue(higher.dequeue())
+        higherSize -= 1
+        lowerSize += 1
+      } else if (higherSize < lowerSize) {
+        higher.enqueue(lower.dequeue())
+        higherSize += 1
+        lowerSize -= 1
+      }
+      Seq(higher, lower).foreach(pq => {
+        while (pq.nonEmpty && removals.contains(pq.head)) {
+          val number = pq.dequeue()
+          removals(number) -= 1
+          if (removals(number) == 0) removals -= number
         }
-        median
-      }
+      })
     }
-
-    def getMode(): Int =
-      if (count == 0) 0
-      else frequencyMap.maxBy(_._2)._1
   }
 }
