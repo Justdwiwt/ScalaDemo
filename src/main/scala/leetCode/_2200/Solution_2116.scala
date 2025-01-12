3020dfba0a81b6873156b8cd2bc6fbcb52746503
Justdwiwt <len_master@hotmail.com>
2025-01-12 11:31:35 +0800

    leetCode 2116

--- src/main/scala/leetCode/_2200/Solution_2116.scala
+++ src/main/scala/leetCode/_2200/Solution_2116.scala
@@ -2,16 +2,27 @@ package leetCode._2200
 
 object Solution_2116 {
   def canBeValid(s: String, locked: String): Boolean = {
-    def f(s: String, locked: String, p: Char): Boolean = {
-      val (required, budget) = s.zip(locked)./:(0, 0) { case ((required, budget), (char, lock)) =>
-        val newRequired = required + (if (lock == '0') 0 else if (char == p) 1 else -1)
-        val newBudget = budget + (if (lock == '0') 1 else 0)
-        if (newRequired + newBudget < 0) return false
-        (newRequired, newBudget)
-      }
-      required <= budget
-    }
+    val str = s.zip(locked.map(_ == '1')).view
 
-    s.length % 2 == 0 && f(s, locked, '(') && f(s.reverse, locked.reverse, ')')
+    if (str.size % 2 == 1) return false
+    val countOpen = str.count { case (c, l) => l && c == '(' }
+    val countClose = str.count { case (c, l) => l && c == ')' }
+    val start = (true, 0, 0, 0, countOpen, countClose, str.size - countOpen - countClose)
+
+    val r = str.foldLeft(start) { case ((res, openHead, closeHead, unHead, openTail, closeTail, unTail), (c, l)) =>
+      (c, l) match {
+        case (_, false) =>
+          (res && (closeHead <= openHead + unHead + 1 && openTail <= closeTail + unTail - 1),
+            openHead, closeHead, unHead + 1, openTail, closeTail, unTail - 1)
+        case ('(', _) =>
+          (res && (closeHead <= openHead + 1 + unHead && openTail - 1 <= closeTail + unTail),
+            openHead + 1, closeHead, unHead, openTail - 1, closeTail, unTail)
+        case (')', _) =>
+          (res && (closeHead + 1 <= openHead + unHead && openTail <= closeTail - 1 + unTail),
+            openHead, closeHead + 1, unHead, openTail, closeTail - 1, unTail)
+        case _ => (false, openHead, closeHead, unHead, openTail, closeTail, unTail)
+      }
+    }
+    r._1
   }
 }
