e3696bc542cbd91be07bc5e80324bdcb28da6413
Justdwiwt <len_master@hotmail.com>
2025-02-16 11:22:53 +0800

    leetCode 1718

--- src/main/scala/leetCode/_1800/Solution_1718.scala
+++ src/main/scala/leetCode/_1800/Solution_1718.scala
@@ -4,34 +4,25 @@ object Solution_1718 {
   def constructDistancedSequence(n: Int): Array[Int] = {
     val res = Array.fill(n * 2 - 1)(0)
     val visited = Array.fill(n + 1)(false)
-    backtrack(res, visited, n, 0)
-    res
-  }
 
-  def backtrack(res: Array[Int], visited: Array[Boolean], n: Int, pos: Int): Boolean = {
-    if (pos == res.length) return true
-    if (res(pos) != 0) return backtrack(res, visited, n, pos + 1)
-    (n to 1 by -1).foreach(i => {
-      if (i == 1) {
-        if (!visited(i)) {
+    def backtrack(pos: Int): Boolean =
+      if (pos == res.length) true
+      else if (res(pos) != 0) backtrack(pos + 1)
+      else (n to 1 by -1).exists(i => {
+        if (!visited(i) && (i == 1 || (pos + i < res.length && res(pos + i) == 0))) {
           res(pos) = i
+          if (i > 1) res(pos + i) = i
           visited(i) = true
-          if (backtrack(res, visited, n, pos + 1)) return true
-          res(pos) = 0
-          visited(i) = false
-        }
-      } else {
-        if (!visited(i) && pos + i < res.length && res(pos + i) == 0) {
-          res(pos) = i
-          res(pos + i) = i
-          visited(i) = true
-          if (backtrack(res, visited, n, pos + 1)) return true
-          res(pos) = 0
-          res(pos + i) = 0
-          visited(i) = false
-        }
-      }
-    })
-    false
+          if (backtrack(pos + 1)) true
+          else {
+            res(pos) = 0
+            if (i > 1) res(pos + i) = 0
+            visited(i) = false
+            false
+          }
+        } else false
+      })
+
+    if (backtrack(0)) res else Array.empty
   }
 }
