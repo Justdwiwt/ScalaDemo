aa7879d93ea320136cb9b47c28772c0f56b7f363
Justdwiwt <len_master@hotmail.com>
2025-02-16 11:28:42 +0800

    leetCode 186

--- src/main/scala/leetCode/_200/Solution_186.scala
+++ src/main/scala/leetCode/_200/Solution_186.scala
@@ -2,21 +2,21 @@ package leetCode._200
 
 object Solution_186 {
   def reverseWords(s: Array[Char]): Unit = {
-    var i = 0
-    s.indices.foreach(j => if (s(j) == ' ') {
-      reverse(s, i, j)
-      i = j + 1
-    })
-    reverse(s, i, s.length)
-    reverse(s, 0, s.length)
-  }
+    def reverse(str: Array[Char], i: Int, j: Int): Unit = {
+      (0 until (j - i) / 2).foreach(k => {
+        val g = j - 1 - k
+        val t = str(i + k)
+        str(i + k) = str(g)
+        str(g) = t
+      })
+    }
 
-  def reverse(str: Array[Char], i: Int, j: Int): Unit = {
-    (i until (i + j) / 2).foreach(k => {
-      val t = str(k)
-      val g = j - 1 - k + i
-      str(k) = str(g)
-      str(g) = t
+    val indices = s.zipWithIndex.collect { case (' ', idx) => idx }
+    (indices :+ s.length).foldLeft(0)((start, end) => {
+      reverse(s, start, end)
+      end + 1
     })
+
+    reverse(s, 0, s.length)
   }
 }
