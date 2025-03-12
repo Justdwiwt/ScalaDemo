a7df69df573c31ee41a65aaa635d9c2e373c66e6
Justdwiwt <len_master@hotmail.com>
2025-03-12 22:54:15 +0800

    leetCode 3462

---
+++ src/main/scala/leetCode/_500/Solution_3462.scala
@@ -0,0 +1,13 @@
+package leetCode._500
+
+object Solution_3462 {
+  def maxSum(grid: Array[Array[Int]], limits: Array[Int], k: Int): Long = grid
+    .map(_.sorted.reverse)
+    .zip(limits)
+    .flatMap(n => n._1.take(n._2))
+    .sorted
+    .reverse
+    .take(k)
+    .map(_.toLong)
+    .sum
+}
