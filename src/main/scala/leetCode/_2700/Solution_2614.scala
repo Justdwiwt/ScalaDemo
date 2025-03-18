e96ecc92a3cc95ec63faa58f9f58359909c4aec0
Justdwiwt <len_master@hotmail.com>
2025-03-18 22:56:02 +0800

    leetCode 2614

--- src/main/scala/leetCode/_2700/Solution_2614.scala
+++ src/main/scala/leetCode/_2700/Solution_2614.scala
@@ -2,19 +2,14 @@ package leetCode._2700
 
 object Solution_2614 {
   def diagonalPrime(nums: Array[Array[Int]]): Int = {
-    def f(n: Int): Boolean = {
-      if (n <= 2 || n % 2 == 0) return n == 2
-      (3 to math.sqrt(n).toInt by 2).foreach(i => if (n % i == 0) return false)
-      true
-    }
+    def isPrime(n: Int): Boolean =
+      n > 1 && (n == 2 || (n % 2 != 0 && (3 to math.sqrt(n).toInt by 2).forall(n % _ != 0)))
 
-    val da = nums.indices./:(0)((acc, i) => {
-      if (f(nums(i)(i))) acc.max(nums(i)(i))
-      else acc
-    })
-    nums.indices./:(da)((acc, i) => {
-      if (f(nums(nums.length - i - 1)(i))) acc.max(nums(nums.length - i - 1)(i))
-      else acc
-    })
+    nums
+      .indices
+      .flatMap(i => Seq(nums(i)(i), nums(nums.length - 1 - i)(i)))
+      .filter(isPrime)
+      .reduceOption(_.max(_))
+      .getOrElse(0)
   }
 }
