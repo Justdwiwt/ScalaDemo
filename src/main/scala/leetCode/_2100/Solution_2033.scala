3b2560a038e8b6b67598f3f3d803162c3d653d93
Justdwiwt <len_master@hotmail.com>
2025-03-26 22:49:32 +0800

    leetCode 2033

--- src/main/scala/leetCode/_2100/Solution_2033.scala
+++ src/main/scala/leetCode/_2100/Solution_2033.scala
@@ -2,11 +2,9 @@ package leetCode._2100
 
 object Solution_2033 {
   def minOperations(grid: Array[Array[Int]], x: Int): Int = {
-    val sorted = grid.flatten.sorted
-    val med = sorted(sorted.length / 2)
-    sorted./:(0)((cnt, v) => {
-      if ((v - med) % x != 0) return -1
-      cnt + (v - med).abs / x
-    })
+    val flatGrid = grid.flatten.sorted
+    val median = flatGrid(flatGrid.length / 2)
+    if (flatGrid.exists(v => (v - median) % x != 0)) -1
+    else flatGrid.map(v => (v - median).abs / x).sum
   }
 }
