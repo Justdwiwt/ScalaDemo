fa907a5db3bc1b9418b5d770d24efae465dd12a0
Justdwiwt <len_master@hotmail.com>
2025-04-29 21:11:40 +0800

    leetCode 2584

--- src/main/scala/leetCode/_2600/Solution_2584.scala
+++ src/main/scala/leetCode/_2600/Solution_2584.scala
@@ -1,15 +1,37 @@
 package leetCode._2600
 
+import scala.collection.immutable.SortedSet
+
 object Solution_2584 {
-  def findValidSplit(nums: Array[Int]): Int = {
-    var t = 0
-    nums.indices.dropRight(1).foreach(i => {
-      (t + 1 until nums.length).foreach(j => if (gcd(nums(i), nums(j)) != 1) t = j)
-      if (t <= i) return i
-    })
-    -1
+  lazy val primes: SortedSet[Int] = {
+    val ps = (3 to 1000000 by 2).filter(BigInt(_).isProbablePrime(10))
+    SortedSet(2) ++ ps
   }
 
-  @scala.annotation.tailrec
-  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  def findValidSplit(nums: Array[Int]): Int = {
+    type ASet = Set[Int]
+    val anEmptySet: ASet = Set.empty
+
+    @scala.annotation.tailrec
+    def f(n: Int, m: Int = 2, acc: ASet = anEmptySet): ASet =
+      if (primes.contains(n)) acc + n
+      else if (m * 2 > n) acc
+      else if (n % m == 0) f(n / m, m, acc + m)
+      else {
+        val next = primes.dropWhile(_ <= m).head
+        f(n, next, acc)
+      }
+
+    val fs: Vector[ASet] = nums.map(f(_)).toVector
+    val pfx: Vector[ASet] = fs.scanLeft(anEmptySet)((a, b) => a ++ b)
+    val sfx: Vector[ASet] = fs.reverse.scanLeft(anEmptySet)((a, b) => a ++ b).reverse
+
+    pfx
+      .zip(sfx)
+      .slice(1, nums.length)
+      .zipWithIndex
+      .find { case ((a, b), _) => (a & b).isEmpty }
+      .map(_._2)
+      .getOrElse(-1)
+  }
 }
