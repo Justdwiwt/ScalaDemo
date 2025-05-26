447dab3eaad6ea8e03088792a8f5ffca51ff7f3e
Justdwiwt <len_master@hotmail.com>
2025-05-26 22:08:12 +0800

    leetCode 3545

--- src/main/scala/leetCode/_3600/Solution_3545.scala
+++ src/main/scala/leetCode/_3600/Solution_3545.scala
@@ -1,5 +1,13 @@
 package leetCode._3600
 
 object Solution_3545 {
-
+  def minDeletion(s: String, k: Int): Int = s
+    .groupBy(identity)
+    .mapValues(_.length)
+    .values
+    .toList
+    .sorted
+    .dropRight(k)
+    .reduceOption(_ + _)
+    .getOrElse(0)
 }
