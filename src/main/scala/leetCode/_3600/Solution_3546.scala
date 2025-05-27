2153bacf3f6545a8cddfffa60485e9f0804263e5
Justdwiwt <len_master@hotmail.com>
2025-05-27 21:10:50 +0800

    leetCode 3546

--- src/main/scala/leetCode/_3600/Solution_3546.scala
+++ src/main/scala/leetCode/_3600/Solution_3546.scala
@@ -1,5 +1,11 @@
 package leetCode._3600
 
 object Solution_3546 {
-
+  def canPartitionGrid(grid: Array[Array[Int]]): Boolean = {
+    val sum1 = grid.map(_.map(_.toLong).sum)
+    val t1 = sum1.sum
+    val sum2 = grid.transpose.map(_.map(_.toLong).sum)
+    val t2 = sum2.sum
+    sum1.scan(0L)(_ + _).exists(_ * 2 == t1) || sum2.scan(0L)(_ + _).exists(_ * 2 == t2)
+  }
 }
