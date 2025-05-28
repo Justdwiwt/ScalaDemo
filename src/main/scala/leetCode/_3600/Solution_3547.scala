613330c166ac2a9a5fccb54792373c3d82efe324
Justdwiwt <len_master@hotmail.com>
2025-05-28 21:09:14 +0800

    leetCode 3547

--- src/main/scala/leetCode/_3600/Solution_3547.scala
+++ src/main/scala/leetCode/_3600/Solution_3547.scala
@@ -1,5 +1,9 @@
 package leetCode._3600
 
 object Solution_3547 {
-
+  def maxScore(n: Int, edges: Array[Array[Int]]): Long = {
+    val base = (2L * n * n + 5L * n - 6) * (n - 1) / 6
+    val extra = if (edges.length == n) 2L else 0L
+    base + extra
+  }
 }
