cfb2bba6dcf96f4d723bd60cb00560cd49c16179
Justdwiwt <len_master@hotmail.com>
2025-06-02 12:36:01 +0800

    leetCode 3550

--- src/main/scala/leetCode/_3600/Solution_3550.scala
+++ src/main/scala/leetCode/_3600/Solution_3550.scala
@@ -1,5 +1,8 @@
 package leetCode._3600
 
 object Solution_3550 {
-
+  def smallestIndex(nums: Array[Int]): Int = nums
+    .indices
+    .find(i => nums(i).abs.toString.map(_.asDigit).sum == i)
+    .getOrElse(-1)
 }
