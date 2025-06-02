acd1d2ff78b3482125498d4caf2453228af93111
Justdwiwt <len_master@hotmail.com>
2025-06-02 11:30:03 +0800

    leetCode 135

--- src/main/scala/leetCode/_200/Solution_135.scala
+++ src/main/scala/leetCode/_200/Solution_135.scala
@@ -1,17 +1,18 @@
 package leetCode._200
 
 object Solution_135 {
-  def candy(ratings: Seq[Int]): Int = ratings match {
-    case c +: _ =>
-      val res = ratings./:((0, (0, 0), c))(sum)
+  def candy(ratings: Array[Int]): Int =
+    if (ratings.isEmpty) 0
+    else {
+      val first = ratings.head
+      val res = ratings.foldLeft[(Int, (Int, Int), Int)]((0, (0, 0), first))(sum)
       res._1 + cnt(res._2)
-    case _ => 0
-  }
+    }
 
-  def sum(data: (Int, (Int, Int), Int), rate: Int): (Int, (Int, Int), Int) = data match {
+  private def sum(data: (Int, (Int, Int), Int), rate: Int): (Int, (Int, Int), Int) = data match {
     case (res, pattern, pre) if pre < rate => pattern match {
       case (n, 0) => (res, (n + 1, 0), rate)
-      case (_, _) => (res + cnt(pattern), (1, 0), rate)
+      case _ => (res + cnt(pattern), (1, 0), rate)
     }
     case (res, pattern, pre) if pre > rate => pattern match {
       case (n, 0) => (res, (n, 1), rate)
@@ -20,7 +21,7 @@ object Solution_135 {
     case (res, pattern, _) => (res + cnt(pattern), (0, 1), rate)
   }
 
-  def cnt(pattern: (Int, Int)): Int = pattern match {
+  private def cnt(pattern: (Int, Int)): Int = pattern match {
     case (n, 0) => (n + 2) * (n + 1) / 2 - 1
     case (0, m) => (m + 1) * m / 2
     case (n, m) if n > m => ((n + 2) * (n + 1) + m * (m + 1)) / 2 - 1
