88b314e8462179576d3a8bcf38ac975ee15d42dc
Justdwiwt <len_master@hotmail.com>
2025-07-20 16:53:53 +0800

    leetCode 3572

--- src/main/scala/leetCode/_3600/Solution_3572.scala
+++ src/main/scala/leetCode/_3600/Solution_3572.scala
@@ -1,5 +1,15 @@
 package leetCode._3600
 
 object Solution_3572 {
+  def maxSumDistinctTriplet(x: Array[Int], y: Array[Int]): Int = {
+    val bestPerX = x
+      .zip(y)
+      .groupBy(_._1)
+      .mapValues(_.map(_._2).max)
+      .values
+      .toSeq
 
+    if (bestPerX.size < 3) -1
+    else bestPerX.sorted(Ordering.Int.reverse).take(3).sum
+  }
 }
