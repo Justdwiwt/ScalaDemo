25ab32423ed97ff57978cbaf11ae036b0dbae4a7
Justdwiwt <len_master@hotmail.com>
2025-12-18 10:16:05 +0800

    leetCode 3652

--- src/main/scala/leetCode/_3700/Solution_3652.scala
+++ src/main/scala/leetCode/_3700/Solution_3652.scala
@@ -1,5 +1,13 @@
 package leetCode._3700
 
 object Solution_3652 {
+  def maxProfit(prices: Array[Int], strategy: Array[Int], k: Int): Long = {
+    val A = prices.zip(strategy).map { case (p, s) => p.toLong * s }.scan(0L)(_ + _)
+    val B = prices.map(_.toLong).scan(0L)(_ + _)
 
+    (k until A.length)
+      .map(i => A.last - A(i) + B(i) - B(i - k / 2) + A(i - k))
+      .max
+      .max(A.last)
+  }
 }
