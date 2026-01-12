6caa695e25370a34b00a67501d7497c78cd67abd
Justdwiwt <len_master@hotmail.com>
2026-01-12 10:35:42 +0800

    leetCode 1266

--- src/main/scala/leetCode/_1300/Solution_1266.scala
+++ src/main/scala/leetCode/_1300/Solution_1266.scala
@@ -2,7 +2,7 @@ package leetCode._1300
 
 object Solution_1266 {
   def minTimeToVisitAllPoints(points: Array[Array[Int]]): Int = points
-    .sliding(2, 1)
-    .map { case Array(x, y) => (x.head - y.head).abs.max((x(1) - y(1)).abs) }
+    .sliding(2)
+    .collect { case Array(x, y) => (x.head - y.head).abs.max((x(1) - y(1)).abs) }
     .sum
 }
