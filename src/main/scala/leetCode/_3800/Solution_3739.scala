8ffa776c825dbfb443056c636071f7ac308b5d42
Justdwiwt <len_master@hotmail.com>
2026-01-15 10:50:09 +0800

    leetCode 3739, 3740, 3741

--- src/main/scala/leetCode/_3800/Solution_3739.scala
+++ src/main/scala/leetCode/_3800/Solution_3739.scala
@@ -1,5 +1,39 @@
 package leetCode._3800
 
 object Solution_3739 {
+  def countMajoritySubarrays(nums: Array[Int], target: Int): Long = {
+    val prefix = nums.scanLeft(0)((s, x) => s + (if (x == target) 1 else -1))
 
+    val sorted = prefix.distinct.sorted
+    val index = sorted.zipWithIndex.toMap
+
+    val bit = Array.fill(sorted.length + 1)(0L)
+
+    def add(i: Int): Unit = {
+      var x = i + 1
+      while (x < bit.length) {
+        bit(x) += 1
+        x += x & -x
+      }
+    }
+
+    def sum(i: Int): Long = {
+      var x = i + 1
+      var res = 0L
+      while (x > 0) {
+        res += bit(x)
+        x -= x & -x
+      }
+      res
+    }
+
+    var ans = 0L
+    prefix.foreach(s => {
+      val i = index(s)
+      ans += sum(i - 1)
+      add(i)
+    })
+
+    ans
+  }
 }
--- src/main/scala/leetCode/_3800/Solution_3741.scala
+++ src/main/scala/leetCode/_3800/Solution_3741.scala
@@ -1,5 +1,11 @@
 package leetCode._3800
 
 object Solution_3741 {
-
+  def minimumDistance(nums: Array[Int]): Int = nums
+    .zipWithIndex
+    .groupBy(_._1)
+    .map { case (_, pairs) => pairs.map(_._2) }
+    .flatMap(is => is.zip(is.drop(2)).map { case (a, c) => (c - a) * 2 })
+    .reduceOption(_.min(_))
+    .getOrElse(-1)
 }
