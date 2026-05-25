684726fe0f4ce3e9608090e1b98ef3f17e366938
Justdwiwt <len_master@hotmail.com>
2026-05-25 14:35:43 +0800

    leetCode 1871

--- src/main/scala/leetCode/_1900/Solution_1871.scala
+++ src/main/scala/leetCode/_1900/Solution_1871.scala
@@ -1,16 +1,16 @@
 package leetCode._1900
 
+import scala.collection.immutable.Queue
+
 object Solution_1871 {
   def canReach(s: String, minJump: Int, maxJump: Int): Boolean = {
-    var l = minJump
-    var r = maxJump
-    s.indices.foreach(i => {
-      if (s(i) == '0' && l <= i && i <= r) {
-        if (i == s.length - 1) return true
-        if (i >= r) l = i + minJump
-        r = i + maxJump
-      }
-    })
-    false
+    @scala.annotation.tailrec
+    def f(q: Queue[Int], next: Int): Boolean =
+      if (q.isEmpty || q.head >= s.length) false
+      else if (q.head == s.length - 1) true
+      else if (s(q.head) == '1') f(q.tail, next)
+      else f(q.tail ++ Iterator.range(next.max(q.head + minJump), q.head + maxJump + 1), q.head + maxJump + 1)
+
+    s.last == '0' && f(Queue(0), 1)
   }
 }
