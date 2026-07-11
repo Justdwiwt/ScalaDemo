1e643544f562df3b1370c5884cf40a08b8e0472c
Justdwiwt <len_master@hotmail.com>
2026-07-11 20:48:34 +0800

    leetCode 3941

--- src/main/scala/leetCode/_4000/Solution_3941.scala
+++ src/main/scala/leetCode/_4000/Solution_3941.scala
@@ -1,5 +1,9 @@
 package leetCode._4000
 
 object Solution_3941 {
-
+  def passwordStrength(password: String): Int = password
+    .toSet
+    .toList
+    .map((p: Char) => if (p.isDigit) 3 else if (p.isLower) 1 else if (p.isUpper) 2 else 5)
+    .sum
 }
